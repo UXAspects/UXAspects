475359f2b02a15f8b6a200eb465380f9a350a0c0
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2017-04-05 10:43:49 +0100

    Adding partition map

--- src/ng1/wrappers/partition-map/partition-map.directive.js
+++ src/ng1/wrappers/partition-map/partition-map.directive.js
@@ -6,8 +6,15 @@ export function PartitionMapDirective() {
 			chartOptions: "=",
             chartLoading: "="
 		},
-        template: `<partition-map chart-data="chartData" chart-options="chartOptions" chart-loading="chartLoading"></partition-map>`,
-        controller: ['$scope', function($scope) {
+        template: ``,
+        controller: ['$scope', '$compile', '$timeout', '$element', function($scope, $compile, $timeout, $element) {
+            
+            // need a timeout due to the directive calling a rootscope apply in its creation
+            $timeout(() => {
+                let partitionMap = $compile('<partition-map chart-data="chartData" chart-options="chartOptions" chart-loading="chartLoading"></partition-map>')($scope);
+                $element.append(partitionMap);
+            });
+
             this.$onDestroy = function() {
                 $scope.$destroy();
             };
