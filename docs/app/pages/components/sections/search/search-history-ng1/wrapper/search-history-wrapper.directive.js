54576938f6585f7d1f2ac221cde361346c077204
Donna <donna.worby@hpe.com>
2017-04-07 11:46:21 +0100

    EL-2381
    Removed chance which ended up not being necessary.

--- docs/app/pages/components/sections/search/search-history-ng1/wrapper/search-history-wrapper.directive.js
+++ docs/app/pages/components/sections/search/search-history-ng1/wrapper/search-history-wrapper.directive.js
@@ -5,8 +5,6 @@ angular.module('app').directive('uxdSearchHistoryWrapper', () => {
         controller: ['$scope', '$templateCache', '$modal', function ($scope, $templateCache, $modal) {
             $templateCache.put('search-history-ng1/modalLayout.html', require('../snippets/modalLayout.html'));
 
-            var chance = require('chance').Chance();
-
             var vm = this;
             
             vm.scrollConfig = {
