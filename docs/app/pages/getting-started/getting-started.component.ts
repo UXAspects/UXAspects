242215efaae4d87cdccfe19e98ca9aa29a077970
Donna <donna.worby@hpe.com>
2017-04-10 13:22:41 +0100

    EL-2459

--- docs/app/pages/getting-started/getting-started.component.ts
+++ docs/app/pages/getting-started/getting-started.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { AppConfiguration } from '../../services/app-configuration/app-configuration.service';
 
 @Component({
     selector: 'uxd-getting-started',
     templateUrl: './getting-started.component.html'
 })
-export class GettingStartedPageComponent { }
+export class GettingStartedPageComponent {
+    
+    private version = this.appConfig.get('version');
+
+    constructor(private appConfig: AppConfiguration) {}
+}
