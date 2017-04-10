7a15e77b9c8736bc49dc1fddafa027d6bb5ce99e
Donna <donna.worby@hpe.com>
2017-04-10 13:34:54 +0100

    Updating for EL-2450

--- assets/lib/ux-aspects.js
+++ assets/lib/ux-aspects.js
@@ -1174,6 +1174,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
@@ -1197,7 +1200,10 @@ var UX_ASPECTS_MODULES = [
     toggleswitch_module_1.ToggleSwitchModule
 ];
 var UxAspectsModule = (function () {
-    function UxAspectsModule() {
+    function UxAspectsModule(ngZone) {
+        this.ngZone = ngZone;
+        // make ngzone global
+        window.ngZone = ngZone;
     }
     return UxAspectsModule;
 }());
@@ -1208,7 +1214,8 @@ UxAspectsModule = __decorate([
         providers: [
             color_service_1.ColorService
         ]
-    })
+    }),
+    __metadata("design:paramtypes", [core_1.NgZone])
 ], UxAspectsModule);
 exports.UxAspectsModule = UxAspectsModule;
 __export(__webpack_require__(3));
