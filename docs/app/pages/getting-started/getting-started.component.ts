5a10fb9c32aa1ec671f88032e4798cf721f1bca0
Donna <donna.worby@hpe.com>
2017-04-10 19:19:50 +0100

    EL-2459
    Added Getting Started details.
    Update version and links on landing page/footer.

--- docs/app/pages/getting-started/getting-started.component.ts
+++ docs/app/pages/getting-started/getting-started.component.ts
@@ -9,5 +9,10 @@ export class GettingStartedPageComponent {
     
     private version = this.appConfig.get('version');
 
+    private angular4Ts = require('!!prismjs-loader?lang=javascript!./snippets/angular4.txt');
+    private angular4UpgradeTs = require('!!prismjs-loader?lang=javascript!./snippets/angular4-upgrade.txt');
+    private angular1DowngradeJs = require('!!prismjs-loader?lang=javascript!./snippets/angular1-downgrade.js');
+    private stylesheetHtml = require('!!prismjs-loader?lang=html!./snippets/stylesheet.html');
+
     constructor(private appConfig: AppConfiguration) {}
 }
