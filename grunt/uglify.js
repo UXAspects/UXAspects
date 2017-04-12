744c757ffadd7b57becb52e8d8ad4325098c297d
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2017-04-12 15:35:33 +0100

    Removing uglify grunt task for the Angular 4 library as it isnt required as this will be handled by the consuming module loader. Adding ngZone to window global in our application for plugin optimization.

--- grunt/uglify.js
+++ grunt/uglify.js
@@ -9,15 +9,6 @@ module.exports = {
         },
         src: path.join(process.cwd(), 'dist', 'ng1', 'ux-aspects-ng1.js'),
         dest: path.join(process.cwd(), 'dist', 'ng1', 'ux-aspects-ng1.min.js')
-    },
-
-    library: {
-        options: {
-            preserveComments: /(?:^!|@(?:license|preserve|cc_on))/,
-            maxLineLen: 0
-        },
-        src: path.join(process.cwd(), 'dist', 'lib', 'ux-aspects.js'),
-        dest: path.join(process.cwd(), 'dist', 'lib', 'ux-aspects.min.js')
     }
 
 };
