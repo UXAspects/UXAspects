68d298fb456fbe47fca133da7f3004642d5a9df6
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2017-04-13 18:34:14 +0100

    Adding exports for components (#62)

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,17 @@ export * from './components/toggleswitch/toggleswitch.module';
 export * from './services/color/color.module';
 
 /*
+  Export Components
+*/
+export * from './components/checkbox/checkbox.component';
+export * from './components/ebox/ebox.component';
+export * from './components/flippable-card/flippable-card.component';
+export * from './components/progressbar/progressbar.component';
+export * from './components/radiobutton/radiobutton.component';
+export * from './components/spark/spark.component';
+export * from './components/toggleswitch/toggleswitch.component';
+
+/*
   Export Services
 */
 export * from './services/color/color.service';
