2c367ea560fc8c4f9cd507184f544d796e60ae2f
Donna <donna.worby@hpe.com>
2017-04-13 18:28:16 +0100

    EL-2487
    Update for Angular 4 components.

--- docs/app/pages/getting-started/snippets/angular1-downgrade.js
+++ docs/app/pages/getting-started/snippets/angular1-downgrade.js
@@ -1,2 +1,4 @@
+import { CheckboxComponent } from 'ux-aspects/components/checkbox/checkbox.component';
+
 angular.module('app')
     .directive('checkbox', upgradeAdapter.downgradeNg2Component(CheckboxComponent));
