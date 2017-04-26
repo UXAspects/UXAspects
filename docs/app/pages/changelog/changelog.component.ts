fb05a9c9ed58371882a8e98f8560fe1b64063d59
donna-marijne <donna.worby@hpe.com>
2017-04-26 09:56:13 +0100

    EL-2486 (#93)
    Release 1.1.0-beta.1.

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -15,6 +15,11 @@ export class ChangeLogPageComponent {
 
         this.logs = [
             {
+                version: '1.1.0-beta.1',
+                date: 'April 25th 2017',
+                content: require('./logs/release-v1.1.0-beta.1.md')
+            },
+            {
                 version: '1.0.0',
                 date: 'April 12th 2017',
                 content: require('./logs/release-v1.0.0.md')
