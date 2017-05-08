a664697c0162f448aa474dc09aca20ba3b91cef7
Alastair McKee <alastair.ian.mckee@hpe.com>
2017-05-08 12:59:58 +0100

    EL-2507 - Adding close of click and close on escape

--- docs/app/components/usage-link/usage-link.component.ts
+++ docs/app/components/usage-link/usage-link.component.ts
@@ -4,25 +4,54 @@ import { Input, Renderer2, Component } from '@angular/core';
 @Component({
     selector: 'uxd-usage-link',
     templateUrl: './usage-link.component.html',
-    styleUrls: ['./usage-link.component.less']
+    styleUrls: ['./usage-link.component.less'],
+    host: {
+        '(document:click)': 'onClick($event, pop)',
+        '(document:keyup.escape)': 'closePopover()'
+    }
 })
 export class UsageLinkComponent {
 
     @Input() usage: Usage;
     @Input() pop: string;
 
+    private popover: any;
     private popoverElement: any;
+    private usageElement: any;
+
 
     constructor(private renderer: Renderer2) {}
 
-    onShown(popover: any) {
-
+    onShown(event: any, popover: any) {
+        this.usageElement = event.content.elementRef.nativeElement.parentElement;
+        this.popover = popover;
         this.popoverElement = popover._popover._componentRef.location.nativeElement;
 
         this.renderer.setStyle(this.popoverElement, 'style', '300px');
         this.renderer.setStyle(this.popoverElement, 'maxWidth', '300px');
         this.renderer.setStyle(this.popoverElement, 'borderRadius', '0');
         this.renderer.setStyle(this.popoverElement, 'zIndex', '1');
+    }
+
+    closePopover () {
+        if (this.popover) {
+            this.popover.hide();
+        }
+    }
+
+    onClick(event: any){
+        if (!this.popover) {
+            return;
+        }
+        let target = event.target;
+        while (target.parentNode) {
+            if(target !== this.popoverElement && target !== this.usageElement) {
+                target = target.parentNode
+            } else {
+                return;
+            }
+        }
+        this.popover.hide();
     }
 
     // copy to clipboard button
