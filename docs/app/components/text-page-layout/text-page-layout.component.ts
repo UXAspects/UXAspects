58628f978d625294e812663fdac9ef596f841e4f
Donna <donna.worby@hpe.com>
2017-05-15 16:20:05 +0100

    EL-2524
    Removed padding from various .container elements in the site.
    Added TextPageLayoutComponent for the narrower column text pages.
    Updated fixed-position side navigation to track the width of its
    relative parent for correct responsive layout.
    Fixed blog links and corrected some colors.
    Added a permanent scrollbar and reverted previous 100vw change on the
    top menu. This ensures correct horizontal alignment on all pages.
    Fixed background color.

---
+++ docs/app/components/text-page-layout/text-page-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'uxd-text-page-layout',
+    templateUrl: './text-page-layout.component.html',
+    styleUrls: ['./text-page-layout.component.less']
+})
+export class TextPageLayoutComponent {
+    constructor() {}
+}
