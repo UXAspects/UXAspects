59a34973e3a7a54f71f262cb0ca4fbafc076132a
Donna <donna.worby@hpe.com>
2017-05-16 22:37:22 +0100

    EL-2546
    Updated the help center menu to check mutations for help-center-item
    elements being added or removed before triggering the update.
    Added a throttle to the update broadcast.

--- src/ng1/directives/helpCenter/helpCenterMenu.directive.js
+++ src/ng1/directives/helpCenter/helpCenterMenu.directive.js
@@ -1,6 +1,6 @@
-helpCenterMenu.$inject = ["$timeout", "$rootScope"];
+helpCenterMenu.$inject = ["$timeout", "$rootScope", "throttleService"];
 
-export default function helpCenterMenu($timeout, $rootScope) {
+export default function helpCenterMenu($timeout, $rootScope, throttleService) {
     return {
         restrict: "E",
         scope: {
@@ -18,16 +18,58 @@ export default function helpCenterMenu($timeout, $rootScope) {
             //populate the health center once binding intially evaluated
             $timeout(updateList);
 
-            // create an observer instance
-            var helpCenterObserver = new MutationObserver(function() {
+            var throttledBroadcast = throttleService(function() {
                 $rootScope.$broadcast('Help-Center-Update');
+            }, 500, true);
+
+            // create an observer instance
+            var helpCenterObserver = new MutationObserver(function (mutationRecords) {
+                if (isHelpCenterUpdate(mutationRecords)) {
+                    // Throttle the broadcast to max once per 500ms
+                    throttledBroadcast();
+                }
             });
 
             // pass in the target node, as well as the observer options
-            helpCenterObserver.observe(document.body, { childList: true, subtree: true });
+            helpCenterObserver.observe(document.body, {
+                childList: true,
+                subtree: true
+            });
 
             //if any of the items url/text changes we need to update the list
             $rootScope.$on('Help-Center-Update', updateList);
+
+            scope.$on('$destroy', function () {
+                helpCenterObserver.disconnect();
+            });
+
+            function isHelpCenterUpdate(mutationRecords) {
+
+                // Check the array of records, which contains the details of the DOM mutation
+                for (var mutationRecord of mutationRecords) {
+
+                    // Check if any of the added nodes have the help-center-item attribute
+                    for (var addedNode of mutationRecord.addedNodes) {
+                        if (isHelpCenterItem(addedNode)) {
+                            return true;
+                        }
+                    }
+
+                    // Check if any of the removed nodes have the help-center-item attribute
+                    for (var removedNode of mutationRecord.removedNodes) {
+                        if (isHelpCenterItem(removedNode)) {
+                            return true;
+                        }
+                    }
+                }
+
+                return false;
+            }
+
+            function isHelpCenterItem(node) {
+                // Check for nodeType 1 (Element) and the help center attribute
+                return (node.nodeType === 1 && node.hasAttribute('help-center-item'));
+            }
 
             function updateList() {
 
@@ -35,20 +77,25 @@ export default function helpCenterMenu($timeout, $rootScope) {
 
                 // get the current menu items
                 var currentItems = scope.menuItems;
-                scope.menuItems = [];
+                var newItems = [];
 
                 for (var i = 0; i < items.length; i++) {
                     var options = {
                         title: angular.element(items[i]).attr('help-center-title'),
                         url: angular.element(items[i]).attr('help-center-url')
                     };
-                    scope.menuItems.push(options);
+                    newItems.push(options);
+                }
+
+                // Sort if a function was provided
+                if (scope.sortFn) {
+                    newItems.sort(scope.sortFn);
                 }
 
                 // only update if there are changes to the list
-                if(!angular.equals(scope.menuItems, currentItems)) {
-                    scope.$evalAsync(function() {
-                        scope.menuItems.sort(scope.sortFn);
+                if (!angular.equals(newItems, currentItems)) {
+                    scope.$evalAsync(function () {
+                        scope.menuItems = newItems;
                     });
                 }
             }
---
+++ src/ng1/services/throttleService/throttle.module.js
@@ -0,0 +1,4 @@
+import throttleService from './throttle.service.js';
+
+angular.module('ux-aspects.throttleService', [])
+    .service('throttleService', throttleService);
