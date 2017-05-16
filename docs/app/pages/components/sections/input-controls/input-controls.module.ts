b08563eb53b1a8b0cd2247d1abf1b381ce7fce95
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2017-05-16 23:01:03 +0100

    EL-2436 - Slider Component (#154)
    * Adding slider component
    
    * Adding documentation for sliders
    
    * Making minor improvements
    
    * EL-2436 - Moving slider back to input controls, fixing line breaks, documenting options as interfaces

--- docs/app/pages/components/sections/input-controls/input-controls.module.ts
+++ docs/app/pages/components/sections/input-controls/input-controls.module.ts
@@ -1,5 +1,7 @@
 import { NgModule, ComponentFactoryResolver } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { DocumentationComponentsModule } from '../../../../components/components.module';
 import { ResolverService, DocumentationPage } from '../../../../services/resolver/resolver.service';
 import { DocumentationCategoryComponent } from '../../../../components/documentation-category/documentation-category.component';
@@ -16,11 +18,12 @@ import { ComponentsInputExpandNg1Component } from './input-expand-ng1/input-expa
 import { ComponentsInputMaskNg1Component } from './input-mask-ng1/input-mask-ng1.component';
 import { ComponentsRadioButtonNg1Component } from './radio-button-ng1/radio-button-ng1.component';
 import { ComponentsTagsNg1Component } from './tags-ng1/tags-ng1.component';
-import { ComponentsSlidersNg1Component } from './sliders-ng1/sliders-ng1.component';
-import { ComponentsSliderChartsNg1Component } from './slider-charts-ng1/slider-charts-ng1.component';
 import { WrappersModule } from '../../../../wrappers.module';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-import { CheckboxModule, ToggleSwitchModule } from '../../../../../../src/index';
+import { CheckboxModule, ToggleSwitchModule, ColorServiceModule, SliderModule } from '../../../../../../src/index';
+import { ComponentsSlidersComponent } from './sliders/sliders.component';
+import { ComponentsSliderChartsNg1Component } from './slider-charts-ng1/slider-charts-ng1.component';
+import { ComponentsSlidersNg1Component } from './sliders-ng1/sliders-ng1.component';
 
 const SECTIONS = [
     ComponentsCheckboxComponent,
@@ -36,7 +39,8 @@ const SECTIONS = [
     ComponentsRadioButtonNg1Component,
     ComponentsTagsNg1Component,
     ComponentsSlidersNg1Component,
-    ComponentsSliderChartsNg1Component
+    ComponentsSliderChartsNg1Component,
+    ComponentsSlidersComponent
 ];
 
 const ROUTES = [
@@ -55,6 +59,12 @@ const ROUTES = [
         TabsModule,
         CheckboxModule,
         ToggleSwitchModule,
+        CommonModule,
+        WrappersModule,
+        TabsModule,
+        SliderModule,
+        FormsModule,
+        ColorServiceModule,
         DocumentationComponentsModule,
         RouterModule.forChild(ROUTES)
     ],
