8fb5461b26ba651708b4832fd442b23a8bba8647
PearseMcMurray <pearse.mcmurray@hpe.com>
2017-05-24 13:05:05 +0100

    Committing documentation changes for 1.2.0-beta.1-gh-pages-test. Latest develop commit ID is 8c1a700ce38982f7355038633ae690fc844b4bed. (#183)

--- showcase/charts/dist/lib/components/flippable-card/flippable-card.component.d.ts
+++ showcase/charts/dist/lib/components/flippable-card/flippable-card.component.d.ts
@@ -1,14 +1,16 @@
+import { EventEmitter } from '@angular/core';
 export declare class FlippableCardComponent {
     direction: 'horizontal' | 'vertical';
     trigger: 'click' | 'hover' | 'manual';
     width: number;
     height: number;
-    private flippedState;
-    constructor();
-    setFlippedState(isFlipped: boolean): void;
-    clickTrigger(): void;
-    hoverEnter(): void;
-    hoverExit(): void;
+    flipped: boolean;
+    flippedChange: EventEmitter<boolean>;
+    setFlipped(state: boolean): void;
+    toggleFlipped(): void;
+    private clickTrigger();
+    private hoverEnter();
+    private hoverExit();
 }
 export declare class FlippableCardFrontDirective {
 }
