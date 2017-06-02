4f0eba0e4eb3c14e2ad7a43bc5700fb4bd590fd8
Alastair McKee <alastair.ian.mckee@hpe.com>
2017-06-02 14:52:09 +0100

    EL-2511 - Fixing IE bug in Expanding Text Area

--- docs/app/pages/components/sections/input-controls/expanding-text-area-ng1/wrapper/expanding-text-area-wrapper.directive.ts
+++ docs/app/pages/components/sections/input-controls/expanding-text-area-ng1/wrapper/expanding-text-area-wrapper.directive.ts
@@ -1,6 +1,25 @@
 angular.module('app').directive('uxdExpandingTextAreaWrapper', () => {
     return {
         restrict: 'E',
+        controller: 'ExpandingTextAreaDemoCtrl as vm',
         template: require('./expanding-text-area-wrapper.directive.html')
     };
 });
+
+angular.module('app').controller('ExpandingTextAreaDemoCtrl', ExpandingTextAreaDemoCtrl);
+
+ExpandingTextAreaDemoCtrl.$inject = ['$scope', '$element', '$timeout'];
+
+function ExpandingTextAreaDemoCtrl($scope: angular.IScope, $element: HTMLElement, $timeout: ng.ITimeoutService) {
+    
+    var vm = this;
+
+    vm.$onDestroy = function() {
+       $scope.$destroy();
+    };
+
+    $timeout(function () {
+        $element[0].children[0].setAttribute('placeholder', 'Enter text');
+    });
+    
+}
