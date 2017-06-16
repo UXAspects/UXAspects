181e4043bf1acf401a63ae0f35077ee3946b041d
Alastair McKee <alastair.ian.mckee@hpe.com>
2017-06-16 13:45:03 +0100

    EL-2588 - In progress

--- docs/app/pages/components/components.component.ts
+++ docs/app/pages/components/components.component.ts
@@ -8,13 +8,23 @@ import { IDocumentationPage } from '../../interfaces/IDocumentationPage';
 })
 export class ComponentsPageComponent {
 
-    private navigation: IDocumentationPage;
+    navigation: IDocumentationPage;
 
     versionRadioValue: string = 'Angular';
+
+    version: string;
 
     constructor() {
 
         // load in the navigation json for this page
         this.navigation = require('../../data/components-page.json');
+    }
+
+    radioToggled(version: string) {
+
+        if (this.version !== version) {
+            console.log(version);
+            this.version = version;
+        }
     }
 }
