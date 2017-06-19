9c82982f33362e3e35017cc5ad61c1e59bdc0a0c
donna-marijne <donna.worby@hpe.com>
2017-06-19 12:04:35 +0100

    EL-2591 (#225)
    * EL-2591
    
    Updated to clear the query when closing the search field.
    Added a search history which is displayed when the query is empty.
    Added an alt+/ keyboard shortcut.
    
    * EL-2591
    
    Added local storage of the history.
    Fixed duplicate category and section results.

--- docs/app/components/navigation-bar-search/navigation-bar-search.component.ts
+++ docs/app/components/navigation-bar-search/navigation-bar-search.component.ts
@@ -1,11 +1,15 @@
-import { Component, HostListener, ViewChildren, ElementRef, QueryList } from '@angular/core';
-import { Router } from '@angular/router';
-
+import { ICategory } from '../../interfaces/ICategory';
 import { IDocumentationPage } from '../../interfaces/IDocumentationPage';
 import { ISearchResult } from '../../interfaces/ISearch';
-import { ICategory } from '../../interfaces/ICategory';
 import { ISection } from '../../interfaces/ISection';
 import { NavigationService } from '../../services/navigation/navigation.service';
+import { Component, ElementRef, HostListener, QueryList, ViewChildren } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
+const QUERY_MIN_CHARS = 3;
+const MAX_HISTORY = 5;
+const LOCAL_STORAGE_KEY = 'uxd-search-history';
 
 @Component({
     selector: 'uxd-navigation-bar-search',
@@ -19,16 +23,17 @@ export class NavigationBarSearchComponent {
 
     @ViewChildren('searchInput') searchInput: QueryList<ElementRef>;
 
-    private searching: boolean;
-    private query: string;
-    private results: ISearchResult[];
-    private activeIdx: number;
+    protected searching: boolean;
+    protected query: BehaviorSubject<string>;
+    protected results: ISearchResult[];
+    protected activeIdx: number;
 
     private data: ISearchResult[];
+    private history: ISearchResult[] = [];
 
     constructor(private router: Router, private navigation: NavigationService) {
         this.searching = false;
-        this.query = '';
+        this.query = new BehaviorSubject<string>('');
         this.data = [];
         this.results = [];
         this.activeIdx = 0;
@@ -40,18 +45,16 @@ export class NavigationBarSearchComponent {
         this.data = this.data.concat(this.getSearchResults(componentsData));
         this.data = this.data.concat(this.getSearchResults(cssData));
         this.data = this.data.concat(this.getSearchResults(chartsData));
+
+        this.history = this.loadHistory();
+
+        this.query.debounceTime(200).subscribe(this.search.bind(this));
     }
 
     getSearchResults(page: IDocumentationPage): ISearchResult[] {
         var results: ISearchResult[] = [];
         page.categories.forEach((category: ICategory) => {
-            results.push({
-                section: page.title,
-                link: {
-                    title: category.title,
-                    link: category.link
-                }
-            });
+            let addCategoryResult = true;
             if (category.sections) {
                 this.navigation.setSectionIds(category.sections);
                 category.sections.forEach((section: ISection) => {
@@ -63,6 +66,20 @@ export class NavigationBarSearchComponent {
                             fragment: section.id
                         }
                     });
+
+                    // Prevent addition of a category entry with the same title as a child section.
+                    if (section.title === category.title) {
+                        addCategoryResult = false;
+                    }
+                });
+            }
+            if (addCategoryResult) {
+                results.push({
+                    section: page.title,
+                    link: {
+                        title: category.title,
+                        link: category.link
+                    }
                 });
             }
         });
@@ -84,16 +101,26 @@ export class NavigationBarSearchComponent {
         this.searching = !this.searching;
     }
 
+    @HostListener('window:keydown', ['$event'])
+    windowKeydown(event: KeyboardEvent) {
+        // Open the search dialog on alt+/
+        if (event.altKey && event.key === '/') {
+            this.searching = true;
+            event.preventDefault();
+        }
+    }
+
     hideSearch() {
         this.searching = false;
+        this.query.next('');
     }
 
     escapeKey() {
-        this.searching = false;
+        this.hideSearch();
     }
 
     upKey(event: KeyboardEvent) {
-        
+
         this.activeIdx--;
 
         if (this.activeIdx < 0) {
@@ -104,7 +131,7 @@ export class NavigationBarSearchComponent {
     }
 
     downKey(event: KeyboardEvent) {
-        
+
         this.activeIdx++;
 
         if (this.activeIdx >= this.results.length - 1) {
@@ -114,7 +141,7 @@ export class NavigationBarSearchComponent {
         event.preventDefault();
     }
 
-    enterKey() {       
+    enterKey() {
 
         if (this.results.length > 0) {
 
@@ -130,28 +157,34 @@ export class NavigationBarSearchComponent {
         // if search query changes reset the active index
         this.activeIdx = 0;
 
-        // store the current search query
-        this.query = value;
+        if (value === null || value === '') {
 
-        // if the search query is empty or less than 3 characters show no results
-        if (this.query === null || this.query === '' || this.query.length < 3) {
+            // If the query is empty, show last 5 selected results.
+            this.results = this.history;
+
+        } else if (value.length < QUERY_MIN_CHARS) {
+
+            // Show nothing for wide searches
             this.results = [];
-            return;
-        }
 
-        // get the latest results
-        this.results = this.data.filter((item: ISearchResult) => {
-            return item.link.title.toLowerCase().indexOf(this.query.toLowerCase()) !== -1;
-        });
+        } else {
+
+            // get the latest results
+            this.results = this.data.filter((item: ISearchResult) => {
+                return item.link.title.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+            });
+        }
     }
 
     navigate(item: ISearchResult) {
 
+        // navigate to a selected item
+        this.router.navigate([item.section.toLowerCase(), item.link.link], { fragment: item.link.fragment });
+
         // hide the search once selected
         this.hideSearch();
 
-        // navigate to a selected item
-        this.router.navigate([item.section.toLowerCase(), item.link.link], { fragment: item.link.fragment });
+        this.addToHistory(item);
     }
 
     /*
@@ -159,5 +192,37 @@ export class NavigationBarSearchComponent {
     */
     isDuplicate(item: ISearchResult) {
         return this.results.filter((result: ISearchResult) => result.link.title === item.link.title).length > 1;
+    }
+
+    /**
+     * Add an item to the bounded history list.
+     */
+    private addToHistory(item: ISearchResult) {
+
+        // Remove the item if it's already there.
+        const historyIndex = this.history.indexOf(item);
+        if (historyIndex >= 0) {
+            this.history.splice(historyIndex, 1);
+        }
+
+        // Add to the front of the list.
+        this.history.unshift(item);
+
+        // Remove items to maintain history limit
+        while (this.history.length > MAX_HISTORY) {
+            this.history.pop();
+        }
+
+        // Commit to local storage
+        this.saveHistory(this.history);
+    }
+
+    private loadHistory(): ISearchResult[] {
+        const json = localStorage.getItem(LOCAL_STORAGE_KEY);
+        return json ? JSON.parse(json) : [];
+    }
+
+    private saveHistory(history: ISearchResult[]) {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(history));
     }
 }
