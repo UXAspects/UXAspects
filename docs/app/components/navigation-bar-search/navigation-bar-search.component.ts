10184c02b78d75e0d2761fdbde2d0e4780475b81
Donna <donna.worby@hpe.com>
2017-06-19 10:19:00 +0100

    Revert "EL-2591"
    This reverts commit 2176ceb856651b6bfae737bab9b1692f20b71ab7.

--- docs/app/components/navigation-bar-search/navigation-bar-search.component.ts
+++ docs/app/components/navigation-bar-search/navigation-bar-search.component.ts
@@ -1,14 +1,11 @@
-import { ICategory } from '../../interfaces/ICategory';
+import { Component, HostListener, ViewChildren, ElementRef, QueryList } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { IDocumentationPage } from '../../interfaces/IDocumentationPage';
 import { ISearchResult } from '../../interfaces/ISearch';
+import { ICategory } from '../../interfaces/ICategory';
 import { ISection } from '../../interfaces/ISection';
 import { NavigationService } from '../../services/navigation/navigation.service';
-import { Component, ElementRef, HostListener, QueryList, ViewChildren } from '@angular/core';
-import { Router } from '@angular/router';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-
-const QUERY_MIN_CHARS = 3;
-const MAX_HISTORY = 5;
 
 @Component({
     selector: 'uxd-navigation-bar-search',
@@ -22,17 +19,16 @@ export class NavigationBarSearchComponent {
 
     @ViewChildren('searchInput') searchInput: QueryList<ElementRef>;
 
-    protected searching: boolean;
-    protected query: BehaviorSubject<string>;
-    protected results: ISearchResult[];
-    protected activeIdx: number;
+    private searching: boolean;
+    private query: string;
+    private results: ISearchResult[];
+    private activeIdx: number;
 
     private data: ISearchResult[];
-    private history: ISearchResult[] = [];
 
     constructor(private router: Router, private navigation: NavigationService) {
         this.searching = false;
-        this.query = new BehaviorSubject<string>('');
+        this.query = '';
         this.data = [];
         this.results = [];
         this.activeIdx = 0;
@@ -44,8 +40,6 @@ export class NavigationBarSearchComponent {
         this.data = this.data.concat(this.getSearchResults(componentsData));
         this.data = this.data.concat(this.getSearchResults(cssData));
         this.data = this.data.concat(this.getSearchResults(chartsData));
-
-        this.query.debounceTime(200).subscribe(this.search.bind(this));
     }
 
     getSearchResults(page: IDocumentationPage): ISearchResult[] {
@@ -90,26 +84,16 @@ export class NavigationBarSearchComponent {
         this.searching = !this.searching;
     }
 
-    @HostListener('window:keydown', ['$event'])
-    windowKeydown(event: KeyboardEvent) {
-        // Open the search dialog on alt+/
-        if (event.altKey && event.key === '/') {
-            this.searching = true;
-            event.preventDefault();
-        }
-    }
-
     hideSearch() {
         this.searching = false;
-        this.query.next('');
     }
 
     escapeKey() {
-        this.hideSearch();
+        this.searching = false;
     }
 
     upKey(event: KeyboardEvent) {
-
+        
         this.activeIdx--;
 
         if (this.activeIdx < 0) {
@@ -120,7 +104,7 @@ export class NavigationBarSearchComponent {
     }
 
     downKey(event: KeyboardEvent) {
-
+        
         this.activeIdx++;
 
         if (this.activeIdx >= this.results.length - 1) {
@@ -130,7 +114,7 @@ export class NavigationBarSearchComponent {
         event.preventDefault();
     }
 
-    enterKey() {
+    enterKey() {       
 
         if (this.results.length > 0) {
 
@@ -146,34 +130,28 @@ export class NavigationBarSearchComponent {
         // if search query changes reset the active index
         this.activeIdx = 0;
 
-        if (value === null || value === '') {
+        // store the current search query
+        this.query = value;
 
-            // If the query is empty, show last 5 selected results.
-            this.results = this.history;
-
-        } else if (value.length < QUERY_MIN_CHARS) {
-
-            // Show nothing for wide searches
+        // if the search query is empty or less than 3 characters show no results
+        if (this.query === null || this.query === '' || this.query.length < 3) {
             this.results = [];
-
-        } else {
-
-            // get the latest results
-            this.results = this.data.filter((item: ISearchResult) => {
-                return item.link.title.toLowerCase().indexOf(value.toLowerCase()) !== -1;
-            });
+            return;
         }
+
+        // get the latest results
+        this.results = this.data.filter((item: ISearchResult) => {
+            return item.link.title.toLowerCase().indexOf(this.query.toLowerCase()) !== -1;
+        });
     }
 
     navigate(item: ISearchResult) {
 
-        // navigate to a selected item
-        this.router.navigate([item.section.toLowerCase(), item.link.link], { fragment: item.link.fragment });
-
         // hide the search once selected
         this.hideSearch();
 
-        this.addToHistory(item);
+        // navigate to a selected item
+        this.router.navigate([item.section.toLowerCase(), item.link.link], { fragment: item.link.fragment });
     }
 
     /*
@@ -181,25 +159,5 @@ export class NavigationBarSearchComponent {
     */
     isDuplicate(item: ISearchResult) {
         return this.results.filter((result: ISearchResult) => result.link.title === item.link.title).length > 1;
-    }
-
-    /**
-     * Add an item to the bounded history list.
-     */
-    private addToHistory(item: ISearchResult) {
-
-        // Remove the item if it's already there.
-        const historyIndex = this.history.indexOf(item);
-        if (historyIndex >= 0) {
-            this.history.splice(historyIndex, 1);
-        }
-
-        // Add to the front of the list.
-        this.history.unshift(item);
-
-        // Remove items to maintain history limit
-        while (this.history.length > MAX_HISTORY) {
-            this.history.pop();
-        }
     }
 }
