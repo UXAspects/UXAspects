5a721db1b4b8eadfe1abaf8b7f2897e59222262e
Alastair McKee <alastair.ian.mckee@hpe.com>
2017-06-21 17:11:49 +0100

    In Progress

---
+++ docs/app/pages/components/sections/pdf-export/pdf-export.module.ts
@@ -0,0 +1,36 @@
+import { NgModule, ComponentFactoryResolver } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { DocumentationComponentsModule } from '../../../../components/components.module';
+import { ComponentsPdfExportComponent } from './pdf-export/pdf-export.component';
+import { DocumentationCategoryComponent } from '../../../../components/documentation-category/documentation-category.component';
+import { ResolverService, DocumentationPage } from '../../../../services/resolver/resolver.service';
+
+const SECTIONS = [
+    ComponentsPdfExportComponent
+];
+
+const ROUTES = [
+    {
+        path: '**',
+        component: DocumentationCategoryComponent,
+        data: {
+            category: ResolverService.resolveCategoryData(DocumentationPage.Components, 'PDF Export')
+        }
+    }
+];
+
+@NgModule({
+    imports: [
+        DocumentationComponentsModule,
+        RouterModule.forChild(ROUTES)
+    ],
+    exports: SECTIONS,
+    declarations: SECTIONS,
+    entryComponents: SECTIONS
+})
+export class ComponentsPdfExportModule {
+
+    constructor(componentFactoryResolver: ComponentFactoryResolver, resolverService: ResolverService) {
+        resolverService.registerResolver(componentFactoryResolver);
+    }
+}
---
+++ docs/app/pages/components/sections/pdf-export/pdf-export/pdf-export.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { DocumentationSectionComponent } from '../../../../../decorators/documentation-section-component';
+
+@Component({
+    selector: 'uxd-components-pdf-export',
+    templateUrl: './pdf-export.component.html'
+})
+@DocumentationSectionComponent('ComponentsPdfExportComponent')
+export class ComponentsPdfExportComponent {
+
+}
