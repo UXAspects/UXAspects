ffa5d1924d30ec90566133a45be78c6a9f293431
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2017-06-22 15:06:35 +0100

    Updating showcase pages to use relative urls

--- docs/app/pages/showcase/showcase.component.ts
+++ docs/app/pages/showcase/showcase.component.ts
@@ -11,9 +11,9 @@ export class ShowcasePageComponent {
     partitionMapImage = require('!file-loader?name=[path][name].[ext]!../../assets/img/showcase/partition-map.jpg');
     sankeyChartImage = require('!file-loader?name=[path][name].[ext]!../../assets/img/showcase/sankey-chart.jpg');
 
-    listviewUrl = process.env.ENV === 'production' ? 'https://uxaspects.github.io/UXAspects/showcase/list_view/dist/index.html' : '/showcase/list_view/dist/#/listview';
-    socialChartUrl = process.env.ENV === 'production' ? 'https://uxaspects.github.io/UXAspects/showcase/charts/dist/#/socialchart' : '/showcase/charts/dist/#/socialchart';
-    partitionMapUrl = process.env.ENV === 'production' ? 'https://uxaspects.github.io/UXAspects/showcase/charts/dist/#/partitionmap' : '/showcase/charts/dist/#/partitionmap';
-    sankeyChartUrl = process.env.ENV === 'production' ? 'https://uxaspects.github.io/UXAspects/showcase/charts/dist/#/sankeychart' : '/showcase/charts/dist/#/sankeychart';
+    listviewUrl = 'showcase/list_view/dist/#/listview';
+    socialChartUrl = 'showcase/charts/dist/#/socialchart';
+    partitionMapUrl = 'showcase/charts/dist/#/partitionmap';
+    sankeyChartUrl = 'showcase/charts/dist/#/sankeychart';
 
 }
