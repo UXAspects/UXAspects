7ddf8694e0c613cd19ec98d508bb1b549dfd6e6b
chris-brennan <chris.brennan@hpe.com>
2017-06-29 13:48:22 +0100

    EL-2606-SparkChartThemeColors (#263)
    * Added spark chart themes
    
    * Updated to reference the theme as part of the object.  Theme description updated.
    
    * Fixed typo

--- docs/app/pages/charts/sections/spark-charts/spark-charts/snippets/app.ts
+++ docs/app/pages/charts/sections/spark-charts/spark-charts/snippets/app.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ColorService } from 'ux-aspects';
+import { ColorService, ColorIdentifier } from 'ux-aspects';
 
 @Component({
     selector: 'app',
@@ -15,15 +15,13 @@ export class AppComponent {
             tooltip: 'Spark Line indicator - 2.17MB of 8.2GB occupied (35%)'
         },
         {
-            barColor: this.colorService.getColor('chart3').toHex(),
-            trackColor: this.colorService.getColor('chart3').setAlpha(0.2).toRgba(),
+            theme: 'chart3',
             value: 30,
             barHeight: 5,
             inlineLabel: '30%'
         },
         {
-            barColor: this.colorService.getColor('chart2').toHex(),
-            trackColor: this.colorService.getColor('chart2').setAlpha(0.2).toRgba(),
+            theme: 'chart2',
             value: 35,
             barHeight: 10,
             topLeftLabel: `<span class='spark-label hidden-xxxs'><span class='large'>21.7</span><span class='medium light'>&nbsp;MB&nbsp;&nbsp;Items&nbsp;&nbsp;(35%)</span></span>`,
@@ -31,23 +29,20 @@ export class AppComponent {
             tooltip: 'Spark Line indicator - 2.17MB of 8.2GB occupied (35%)'
         },
         {
-            barColor: this.colorService.getColor('chart3').toHex(),
-            trackColor: this.colorService.getColor('chart3').setAlpha(0.2).toRgba(),
+            theme: 'chart3',
             value: 30,
             barHeight: 5,
             inlineLabel: '<span class="spark-label hidden-spark"><span class="x-large">30%</span></span>',
             topLeftLabel: '<span class="spark-label hidden-xxs"><span class="small">STORAGE ON HOLD</span></span>'
         },
         {
-            barColor: this.colorService.getColor('chart4').toHex(),
-            trackColor: this.colorService.getColor('chart4').setAlpha(0.2).toRgba(),
+            theme: 'chart4',
             value: 55,
             barHeight: 10,
             topLeftLabel: "<span class='spark-label hidden-xxs'><span class='large'>8.6</span><span class='medium light'>&nbsp;GB&nbsp;Disk Space&nbsp;&nbsp;(55%)</span></span>"
         },
         {
-            barColor: this.colorService.getColor('chart5').toHex(),
-            trackColor: this.colorService.getColor('chart5').setAlpha(0.2).toRgba(),
+            theme: 'chart5',
             value: 30,
             barHeight: 10,
             topLeftLabel: '30%',
@@ -56,15 +51,13 @@ export class AppComponent {
             bottomRightLabel: '<span class="spark-label hidden-xxs"><span class="medium light">TOTAL</span></span>'
         },
         {
-            barColor: this.colorService.getColor('vibrant1').toHex(),
-            trackColor: this.colorService.getColor('vibrant1').setAlpha(0.2).toRgba(),
+            theme: 'vibrant1',
             value: 55,
             barHeight: 10,
             topLeftLabel: "<span class='spark-label hidden-xxs'><span class='large'>8.6</span><span class='medium light'>&nbsp;GB&nbsp;Disk Space&nbsp;&nbsp;(55%)</span></span>"
         },
         {
-            barColor: this.colorService.getColor('vibrant2').toHex(),
-            trackColor: this.colorService.getColor('vibrant2').setAlpha(0.2).toRgba(),
+            theme: 'vibrant2',
             value: 30,
             barHeight: 10,
             topLeftLabel: '30%',
@@ -79,9 +72,8 @@ export class AppComponent {
 }
 
 interface SparkChart {
+    theme?: ColorIdentifier;
     value: number;
-    barColor?: string;
-    trackColor?: string;
     barHeight?: number;
     topLeftLabel?: string;
     topRightLabel?: string;
