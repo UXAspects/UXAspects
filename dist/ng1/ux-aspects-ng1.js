e3621c623ea5200c9c49010b17d715ca9ebdac42
nalini-k-vardham <nalini.k.vardham@hpe.com>
2017-07-04 17:34:53 -0400

    Committing changes for package 1.3.0-package-test. Latest develop commit ID is 9939fc708b50cd600e6e7344d7fd07d9e6dfa0fb.

--- dist/ng1/ux-aspects-ng1.js
+++ dist/ng1/ux-aspects-ng1.js
@@ -1,5 +1,5 @@
 /* 
-* ux-aspects - v1.3.0-rc.1 
+* ux-aspects - v1.3.0 
 * © Copyright 2017 Hewlett Packard Enterprise Development Company LP
 */
 (function webpackUniversalModuleDefinition(root, factory) {
