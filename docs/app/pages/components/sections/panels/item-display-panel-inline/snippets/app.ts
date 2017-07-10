6fdd41b82d164e027d0db14296ae3ee32104e5b8
Alastair McKee <alastair.ian.mckee@hpe.com>
2017-07-10 15:54:03 +0100

    EL-2639 - Review changes

--- docs/app/pages/components/sections/panels/item-display-panel-inline/snippets/app.ts
+++ docs/app/pages/components/sections/panels/item-display-panel-inline/snippets/app.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
-import { ColorService, ItemDisplayPanelComponent } from 'ux-aspects';
+import { Component, ViewChild, TemplateRef } from '@angular/core';
+import { ItemDisplayPanelComponent } from 'ux-aspects';
 import 'chance';
-
 @Component({
     selector: 'app',
     templateUrl: './src/app.component.html',
@@ -13,296 +12,46 @@ import 'chance';
 })
 export class AppComponent {
 
+    @ViewChild('modalDoc') modalDoc: TemplateRef<any>;
+    @ViewChild('modalPpt') modalPpt: TemplateRef<any>;
+    @ViewChild('modalPdf') modalPdf: TemplateRef<any>;
+  
     visible: boolean = false;
-    selectedItem: Item;
+    selectedItem: DisplayPanelItem;
     previousEnabled: boolean = true;
     nextEnabled: boolean = true;
     shadow: boolean = false;
     animate: boolean = true;
-    inline: boolean = true;
-    hideCloseButton: boolean = true;
-    preventClose: boolean = true;
-    boxShadow: boolean = false;
-    
-    sparkBarColor: string;
-    sparkTrackColor: string;
 
-    // templates
-    modalPdf = `<div class="p-r-md p-l-md p-t-sm">
-  <h1>Preview PDF</h1>
-  <p>Praesent venenatis eros vel felis vehicula dictum. Phasellus augue libero, vulputate euismod purus 
-  sed, dictum porta mauris. Nunc vitae purus vel velit dapibus porttitor et sagittis mauris. Etiam non 
-  semper odio, at ultricies velit. Duis non suscipit lectus, vitae fringilla turpis.</p>
-</div>`;
+    items: DisplayPanelItem[] = [];
 
-    modalDoc = `<div class="p-r-md p-l-md p-t-sm">
-  <h1>Preview DOC</h1>
-  <p>Donec sagittis augue et pellentesque ultrices. Nulla quis orci sit amet sem ornare auctor. Ut in 
-  lobortis turpis. Vivamus ante felis, viverra sed ornare ut, ultricies eget ipsum. 
-  Vivamus commodo convallis tortor.</p>
-</div>`;
+    ngAfterContentInit() {
+        let extensions = ['.ppt', '.doc', '.pdf'];
+        let titles = ['Site Detail - UX Aspects (PPT)', 'Site Detail - UX Aspects (DOC)', 
+        'Site Detail - UX Aspects (PDF)'];
+        let content = [this.modalPpt, this.modalDoc, this.modalPdf];
+        for (let i = 1; i < 21; i++) {
+            let idx = chance.integer({min: 0, max: 2});
 
-    modalPpt = `<div class="p-r-md p-l-md p-t-sm">
-  <h1>Preview PPT</h1>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla, nunc sit amet faucibus 
-  dapibus, est purus luctus magna, ut tempus orci quam vitae diam. Proin dapibus elit et rhoncus 
-  interdum. Pellentesque ornare nibh ac nulla sodales commodo. Sed vestibulum hendrerit ultrices.</p>
-</div>`;
+            let item = {
+                id: i,
+                name: chance.name(),
+                dateString: chance.date({string: true, american: false, year: 2017}).toString(),
+                document: 'Document ' + i + extensions[idx],
+                extension: extensions[idx],
+                storage: chance.d100().toString(),
+                active: chance.bool(),
+                panel: {
+                    title: titles[idx],
+                    content: content[idx]
+                }
+            }; 
 
-    items: Item[] = [{
-        id: 1,
-        name: chance.name(),
-        dateString: '10 July 2017',
-        document: 'Document 1.ppt',
-        extension: '.ppt',
-        storage: chance.d100().toString(),
-        active: false,
-        panel: {
-            title: 'Site Detail - UX Aspects (PPT)',
-            content: this.modalPpt
+            this.items.push(item);
         }
-        }, {
-            id: 2,
-            name: chance.name(),
-            dateString: '2 July 2017',
-            document: 'Document 2.pdf',
-            extension: '.pdf',
-            storage: chance.d100().toString(),
-            active: true,
-            panel: {
-                title: 'Site Detail - UX Aspects (PDF)',
-                content: this.modalPdf
-            }
-        }, {
-            id: 3,
-            name: chance.name(),
-            dateString: '10 June 2017',
-            document: 'Document 3.doc',
-            extension: '.doc',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (DOC)',
-                content: this.modalDoc
-            }
-        }, {
-            id: 4,
-            name: chance.name(),
-            dateString: '7 July 2017',
-            document: 'Document 4.pdf',
-            extension: '.pdf',
-            storage: chance.d100().toString(),
-            active: true,
-            panel: {
-                title: 'Site Detail - UX Aspects (PDF)',
-                content: this.modalPdf
-            }
-        }, {
-            id: 5,
-            name: chance.name(),
-            dateString: '12 May 2017',
-            document: 'Document 5.doc',
-            extension: '.doc',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (DOC)',
-                content: this.modalDoc
-            }
-        }, {
-            id: 6,
-            name: chance.name(),
-            dateString: '11 July 2017',
-            document: 'Document 6.doc',
-            extension: '.ppt',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (PPT)',
-                content: this.modalPpt
-            }
-        }, {
-            id: 7,
-            name: chance.name(),
-            dateString: '6 June 2017',
-            document: 'Document 7.doc',
-            extension: '.doc',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (DOC)',
-                content: this.modalDoc
-            }
-        }, {
-            id: 8,
-            name: chance.name(),
-            dateString: '1 July 2017',
-            document: 'Document 8.ppt',
-            extension: '.ppt',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (PPT)',
-                content: this.modalPpt
-            }
-        }, {
-            id: 9,
-            name: chance.name(),
-            dateString: '12 July 2017',
-            document: 'Document 9.doc',
-            extension: '.doc',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (DOC)',
-                content: this.modalDoc
-            }
-        }, {
-            id: 10,
-            name: chance.name(),
-            dateString: '18 June 2017',
-            document: 'Document 10.pdf',
-            extension: '.pdf',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (PDF)',
-                content: this.modalPdf
-            }
-        }, {
-            id: 11,
-            name: chance.name(),
-            dateString: '9 May 2017',
-            document: 'Document 11.doc',
-            extension: '.doc',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (DOC)',
-                content: this.modalDoc
-            }
-        }, {
-            id: 12,
-            name: chance.name(),
-            dateString: '16 June 2017',
-            document: 'Document 12.pdf',
-            extension: '.pdf',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (PDF)',
-                content: this.modalPdf
-            }
-        }, {
-            id: 13,
-            name: chance.name(),
-            dateString: '11 May 2017',
-            document: 'Document 13.doc',
-            extension: '.doc',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (DOC)',
-                content: this.modalDoc
-            }
-        }, {
-            id: 14,
-            name: chance.name(),
-            dateString: '10 July 2017',
-            document: 'Document 14.ppt',
-            extension: '.ppt',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (PPT)',
-                content: this.modalPpt
-            }
-        }, {
-            id: 15,
-            name: chance.name(),
-            dateString: '4 July 2017',
-            document: 'Document 15.doc',
-            extension: '.doc',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (DOC)',
-                content: this.modalDoc
-            }
-        }, {
-            id: 16,
-            name: chance.name(),
-            dateString: '6 July 2017',
-            document: 'Document 16.pdf',
-            extension: '.pdf',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (PDF)',
-                content: this.modalPdf
-            }
-        }, {
-            id: 17,
-            name: chance.name(),
-            dateString: '23 June 2017',
-            document: 'Document 17.doc',
-            extension: '.doc',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (DOC)',
-                content: this.modalDoc
-            }
-        }, {
-            id: 18,
-            name: chance.name(),
-            dateString: '15 June 2017',
-            document: 'Document 18.ppt',
-            extension: '.ppt',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (PPT)',
-                content: this.modalPpt
-            }
-        }, {
-            id: 19,
-            name: chance.name(),
-            dateString: '10 July 2017',
-            document: 'Document 19.doc',
-            extension: '.doc',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (DOC)',
-                content: this.modalDoc
-            }
-        }, {
-            id: 20,
-            name: chance.name(),
-            dateString: '3 July 2017',
-            document: 'Document 20.pdf',
-            extension: '.pdf',
-            storage: chance.d100().toString(),
-            active: false,
-            panel: {
-                title: 'Site Detail - UX Aspects (PDF)',
-                content: this.modalPdf
-            }
-        }
-    ];
-
-    noSelectedItem = `<div *ngIf="!selectedItem" class="preview-no-record">
-    <i class="hpe-icon hpe-document"></i>
-    <h2 ng-bind="previewEmptyText" class="ng-binding">Select an item from the list</h2>
-</div>`;
-
-    constructor(colorService: ColorService) {
-        this.sparkTrackColor = colorService.getColor('accent').setAlpha(0.2).toRgba();
-        this.sparkBarColor = colorService.getColor('accent').toHex();
     }
 
-    selectItem(item: Item) {
+    selectItem(item: DisplayPanelItem) {
         this.shadow = true;
         this.selectedItem = item;
         this.updatePanel();
@@ -356,10 +105,9 @@ export class AppComponent {
             this.previousEnabled = false;
         }
     }
-
 }
 
-interface Item {
+interface DisplayPanelItem {
     id: number;
     name: string;
     dateString: string;
@@ -367,10 +115,10 @@ interface Item {
     extension: string;
     storage: string;
     active: boolean;
-    panel: Panel;
+    panel: DisplayPanel;
 }
 
-interface Panel {
+interface DisplayPanel {
     title: string;
-    content: string;
+    content: TemplateRef<any>;
 }
--- src/components/item-display-panel/item-display-panel.component.ts
+++ src/components/item-display-panel/item-display-panel.component.ts
@@ -6,31 +6,19 @@ import { Component, Directive, Input, SimpleChange, Output, EventEmitter, Render
     host: {
         '(document:click)': 'clickOff($event)',
         '(document:keyup.escape)': 'visible = false',
-        '[class.inlineHost]' : 'inline', 
-        '[class.visibleHost]' : 'visible' 
+        '[class.inline-host]' : 'inline', 
+        '[class.visible-host]' : 'visible' 
     }
 })
 export class ItemDisplayPanelComponent { 
     @Input() top: number;
-    @Input() shadow: boolean;
     @Input() title: string;
-    @Input() animate: boolean;
-    @Input() inline: boolean;
-    @Input() preventClose: boolean;
-    @Input() hideCloseButton: boolean;
-    @Input() boxShadow: boolean;
-
+    
     @Output() visibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
 
     @Input()
     get visible() {
         return this._visible;
-    }
-
-    ngOnInit() {
-        if (this.boxShadow === undefined) {
-            this.boxShadow = true;
-        }
     }
 
     set visible(visible: boolean) {
@@ -43,7 +31,91 @@ export class ItemDisplayPanelComponent {
         this.onChangeCallback(this._visible);  
     }
 
+    @Input()
+    get boxShadow() {
+        let value: any = this._boxShadow;
+        if (value === 'false') {
+            this._boxShadow = false;
+        }
+        return this._boxShadow;
+    }
+
+    set boxShadow(boxShadow: boolean) {
+        this._boxShadow = boxShadow;
+    }
+
+    @Input()
+    get closeVisible() {
+        let value: any = this._closeVisible;
+        if (value === 'false') {
+            this._closeVisible = false;
+        }
+        return this._closeVisible;
+    }
+
+    set closeVisible(closeVisible: boolean) {
+        this._closeVisible = closeVisible;
+    }
+
+    @Input()
+    get preventClose() {
+        let value: any = this._preventClose;
+        if (value === 'true') {
+            this._preventClose = true;
+        }
+        return this._preventClose;
+    }
+
+    set preventClose(preventClose: boolean) {
+        this._preventClose = preventClose;
+    }
+
+    @Input()
+    get inline() {
+        let value: any = this._inline;
+        if (value === 'true') {
+            this._inline = true;
+        }
+        return this._inline;
+    }
+
+    set inline(inline: boolean) {
+        this._inline = inline;
+    }
+
+    @Input()
+    get animate() {
+        let value: any = this._animate;
+        if (value === 'true') {
+            this._animate = true;
+        }
+        return this._animate;
+    }
+
+    set animate(animate: boolean) {
+        this._animate = animate;
+    }
+
+    @Input()
+    get shadow() {
+        let value: any = this._shadow;
+        if (value === 'true') {
+            this._shadow = true;
+        }
+        return this._shadow;
+    }
+
+    set shadow(shadow: boolean) {
+        this._shadow = shadow;
+    }
+
     private _visible: boolean = false;
+    private _boxShadow: boolean = true;
+    private _closeVisible: boolean = true;
+    private _preventClose: boolean = false;
+    private _inline: boolean = false;
+    private _animate: boolean = false;
+    private _shadow: boolean = false;
 
     // private onTouchedCallback: () => void = () => { };
     private onChangeCallback: (_: boolean) => void = () => { };
