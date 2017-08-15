caf4f0c09448b40cc3f412cc61acc25e0dec5c3a
donna-marijne <donna.worby@hpe.com>
2017-08-15 17:14:17 +0100

    Committing changes for package 1.3.3-rc.1-package-test. Latest develop commit ID is a94b652044aa5cfb296381083b2eabcb60927c22. (#326)

--- dist/lib/components/page-header/navigation/navigation-item/navigation-item.component.d.ts
+++ dist/lib/components/page-header/navigation/navigation-item/navigation-item.component.d.ts
@@ -1,12 +1,14 @@
-import { QueryList, OnInit, EventEmitter } from '@angular/core';
+import { QueryList, OnInit, EventEmitter, ElementRef } from '@angular/core';
 import { BsDropdownDirective } from 'ngx-bootstrap/dropdown';
 import { PageHeaderNavigationDropdownItemComponent } from '../navigation-dropdown-item/navigation-dropdown-item.component';
 import { PageHeaderNavigationItem, PageHeaderNavigationDropdownItem } from '../navigation.component';
 export declare class PageHeaderNavigationItemComponent implements OnInit {
+    elementRef: ElementRef;
     menu: BsDropdownDirective;
     dropdownComponents: QueryList<PageHeaderNavigationDropdownItemComponent>;
     item: PageHeaderNavigationItem;
     onSelect: EventEmitter<PageHeaderNavigationDropdownItem>;
+    constructor(elementRef: ElementRef);
     ngOnInit(): void;
     selectItem(): void;
     onItemSelect(item: PageHeaderNavigationItem | PageHeaderNavigationDropdownItem): void;
