800f4e0307c17fa013a43cea77fd2ee60b63c8bb
PearseMcMurray <pearse.mcmurray@hpe.com>
2017-08-23 15:06:17 +0100

    'src' and 'outDir' moved into tsconfig.json. 'rootDir' property is expected when 'outDir' is defined.

--- grunt/ts.js
+++ grunt/ts.js
@@ -2,8 +2,6 @@ const path = require('path');
 
 module.exports = {
     e2e: {
-        src: path.join(process.cwd(), 'e2e', 'tests', '**', '*.ts'),
-        outDir: path.join(process.cwd(), 'e2e', 'dist'),
         tsconfig: path.join(process.cwd(), 'e2e', 'tsconfig.json')
     }
 };
