f7d061627a83579652eb0aea1841b337dc3c4fb3
Brennan <chris.brennan@hpe.com>
2017-09-26 13:35:55 +0100

    Updated footer logo to control text with CSS instead of image

--- docs/app/components/page-footer/page-footer.component.ts
+++ docs/app/components/page-footer/page-footer.component.ts
@@ -30,6 +30,7 @@ export class PageFooterComponent {
         // extract specific data from the footer
         this.columns = footerData.columns;
         this.logo = footerData.logo;
+        this.title = footerData.logoTitle;
         this.copyright = footerData.copyright;
         this.feedback = footerData.feedback;
     }
