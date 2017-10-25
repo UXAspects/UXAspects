98aa7601c217f1178d48f3aad468ab061d42d2fd
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2017-10-25 12:20:16 +0100

    Release preparation (#409)

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -15,6 +15,11 @@ export class ChangeLogPageComponent {
 
         this.logs = [
             {
+                version: '1.4.1',
+                date: 'October 25th 2017',
+                content: require('./logs/release-v1.4.1.md')
+            },
+            {
                 version: '1.4.0',
                 date: 'October 11th 2017',
                 content: require('./logs/release-v1.4.0.md')
