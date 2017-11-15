da41a16c155088161930dd33d17744d99e516673
chris-brennan <chris.brennan@hpe.com>
2017-11-15 16:39:39 +0000

    Updated documentation for spark charts

--- docs/app/pages/charts/charts-sections/spark-charts/spark-charts/spark-charts.component.ts
+++ docs/app/pages/charts/charts-sections/spark-charts/spark-charts/spark-charts.component.ts
@@ -15,8 +15,9 @@ export class ChartsSparkChartsComponent extends BaseDocumentationSection impleme
     charts: SparkChart[] = [
         {
             value: 35,
-            barHeight: 10,
-            topLeftLabel: `<span class='spark-label hidden-xxxs'><span class='large'>21.7</span><span class='medium light'>&nbsp;MB&nbsp;&nbsp;Items&nbsp;&nbsp;(35%)</span></span>`,
+            trackColor: '#ECEEEF',
+            barHeight: 8,
+            topLeftLabel: `<span class='spark-label'>21.7 MB</span>`,
             tooltip: 'Spark Line indicator - 2.17MB of 8.2GB occupied (35%)'
         },
         {
@@ -28,7 +29,7 @@ export class ChartsSparkChartsComponent extends BaseDocumentationSection impleme
         {
             theme: 'chart2',
             value: 35,
-            barHeight: 10,
+            barHeight: 8,
             topLeftLabel: `<span class='spark-label hidden-xxxs'><span class='large'>21.7</span><span class='medium light'>&nbsp;MB&nbsp;&nbsp;Items&nbsp;&nbsp;(35%)</span></span>`,
             bottomLeftLabel: '<span class="spark-label hidden-xxxs"><span class="medium light">INDEX COVERAGE</span></span>',
             tooltip: 'Spark Line indicator - 2.17MB of 8.2GB occupied (35%)'
@@ -43,28 +44,28 @@ export class ChartsSparkChartsComponent extends BaseDocumentationSection impleme
         {
             theme: 'chart4',
             value: 55,
-            barHeight: 10,
-            topLeftLabel: "<span class='spark-label hidden-xxs'><span class='large'>8.6</span><span class='medium light'>&nbsp;GB&nbsp;Disk Space&nbsp;&nbsp;(55%)</span></span>"
+            barHeight: 8,
+            topLeftLabel: '<span class="spark-label hidden-xxs"><span class="large">8.6</span><span class="medium light">&nbsp;GB&nbsp;Disk Space&nbsp;&nbsp;(55%)</span></span>'
         },
         {
             theme: 'chart5',
             value: 30,
-            barHeight: 10,
-            topLeftLabel: '30%',
-            topRightLabel: '<span class="spark-label hidden-spark"><span class="medium light">75.0M</span></span>',
-            bottomLeftLabel: '<span class="spark-label hidden-xxxs"><span class="medium light">ITEMS ON HOLD</span></span>',
-            bottomRightLabel: '<span class="spark-label hidden-xxs"><span class="medium light">TOTAL</span></span>'
+            barHeight: 8,
+            topLeftLabel: '<span class="spark-label">21.7 MB</span>',
+            topRightLabel: '<span class="spark-label">75.0 MB</span>',
+            bottomLeftLabel: '<span class="spark-label">ITEMS ON HOLD</span>',
+            bottomRightLabel: '<span class="spark-label">TOTAL</span>'
         },
         {
-            theme: 'vibrant1',
             value: [70, 20, 10],
-            barHeight: 10,
+            barColor: ['#1aac60', '#fcdb1f', '#e5004c'],
+            barHeight: 6,
             topLeftLabel: "<span class='spark-label'>Multi-value</span>"
         },
         {
             theme: 'vibrant2',
             value: 30,
-            barHeight: 10,
+            barHeight: 8,
             topLeftLabel: '30%',
             bottomLeftLabel: '<span class="spark-label hidden-xxxs"><span class="medium light">ITEMS ON HOLD</span></span>',
             topRightLabel: '<span class="spark-label hidden-spark"><span class="medium light">75.0M</span></span>',
@@ -92,6 +93,8 @@ export class ChartsSparkChartsComponent extends BaseDocumentationSection impleme
 interface SparkChart {
     theme?: ColorIdentifier;
     value: number | number[];
+    barColor?: string | string[];
+    trackColor?: string;
     barHeight?: number;
     topLeftLabel?: string;
     topRightLabel?: string;
