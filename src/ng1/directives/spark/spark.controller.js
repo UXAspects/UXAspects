f5a1d3322e8eb91c4ffe596bf31dc5f247a3c88d
chris-brennan <chris.brennan@hpe.com>
2017-11-15 11:48:44 +0000

    Tidied multi-value labels

--- src/ng1/directives/spark/spark.controller.js
+++ src/ng1/directives/spark/spark.controller.js
@@ -7,28 +7,19 @@ export default function SparkCtrl() {
         'height': sc.fillheight + 'px',
         'margin-top': (sc.top !== undefined) ? sc.top : 0 + 'px'
     };
-
-    // if (!Array.isArray(sc.value)) {
-    //     sc.value = [sc.value];
-    // }
-
-    // calculate the bar widths
-    //...
         
-        debugger; // jshint ignore: line
+    // ensure 'value' is an array at this point
+    const values = Array.isArray(sc.value) ? sc.value : [sc.value];
 
-        // ensure 'value' is an array at this point
-        const values = Array.isArray(sc.value) ? sc.value : [sc.value];
+    // get the total value of all lines
+    let total = Math.max(values.reduce((previous, current) => previous + current, 0), 100);
 
-        // get the total value of all lines
-        let total = Math.max(values.reduce((previous, current) => previous + current, 0), 100);
+    // figure out the percentages for each spark line
+    this.values = values.map(val => (val / total) * 100);
 
-        // figure out the percentages for each spark line
-        this.values = values.map(val => (val / total) * 100);
+    // ensure 'barColor' is an array
+    this.barColor = Array.isArray(this.barColor) ? this.barColor : [this.barColor];
 
-        // ensure 'barColor' is an array
-        this.barColor = Array.isArray(this.barColor) ? this.barColor : [this.barColor];
-    
-        return this.values;
+    return this.values;
     
 }
