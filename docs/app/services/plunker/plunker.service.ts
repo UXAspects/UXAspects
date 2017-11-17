20825e23d8b2495722e1e80468b51e00a00a97c8
chris-brennan <chris.brennan@hpe.com>
2017-11-17 15:25:19 +0000

    Changes to spark charts

--- docs/app/services/plunker/plunker.service.ts
+++ docs/app/services/plunker/plunker.service.ts
@@ -94,11 +94,12 @@ export class PlunkerService {
         }
 
         if (!this.mainTs) {
-            this.mainTs = require('./templates/main_ts.txt')
-                .replace(MODULES_PLACEHOLDER, (modules.filter(module => module !== undefined).toString()))
-                .replace(DECLARATIONS_PLACEHOLDER, (declarations.toString()))
-                .replace(IMPORTS_PLACEHOLDER, imports.join('\n'));
+            this.mainTs = require('./templates/main_ts.txt');
         }
+
+        this.mainTs.replace(MODULES_PLACEHOLDER, (modules.filter(module => module !== undefined).toString()))
+            .replace(DECLARATIONS_PLACEHOLDER, (declarations.toString()))
+            .replace(IMPORTS_PLACEHOLDER, imports.join('\n'));
 
         let configJs = require('./templates/config_js.txt')
             .replace(MAPPINGS_PLACEHOLDER, mappings.join(',\n\t\t\t\t'));
