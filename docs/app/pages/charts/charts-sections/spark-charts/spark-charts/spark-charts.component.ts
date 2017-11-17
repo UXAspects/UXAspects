d11643beb648c723b082ff5341bbd2f7c58e0420
chris-brennan <chris.brennan@hpe.com>
2017-11-17 15:25:19 +0000

    Changes to spark charts

--- docs/app/pages/charts/charts-sections/spark-charts/spark-charts/spark-charts.component.ts
+++ docs/app/pages/charts/charts-sections/spark-charts/spark-charts/spark-charts.component.ts
@@ -16,7 +16,6 @@ export class ChartsSparkChartsComponent extends BaseDocumentationSection impleme
         {
             theme: 'chart1',
             value: 35,
-            trackColor: '#ECEEEF',
             barHeight: 8,
             topLeftLabel: `<span class='spark-label'>21.7 MB</span>`,
             tooltip: 'Spark Line indicator - 2.17MB of 8.2GB occupied (35%)'
