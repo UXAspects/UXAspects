f439b598bfe00c81f2c4ff9f8599b81b658a4675
chris-brennan <chris.brennan@hpe.com>
2017-11-22 16:09:07 +0000

    Changes after code review

--- docs/app/pages/getting-started/getting-started.component.ts
+++ docs/app/pages/getting-started/getting-started.component.ts
@@ -13,8 +13,6 @@ export class GettingStartedPageComponent {
     angular4UpgradeTs = require('!!raw-loader!./snippets/angular4-upgrade.txt');
     angular1DowngradeJs = require('!!raw-loader!./snippets/angular1-downgrade.js');
     stylesheetCss = require('!!raw-loader!./snippets/stylesheet.css');
-    headerHtml = require('!!raw-loader!./snippets/header.html');
-    webpackJs = require('!!raw-loader!./snippets/webpack.js');
     cliJson = require('!!raw-loader!./snippets/cli.txt');
     tsconfig = require('!!raw-loader!./snippets/tsconfig.txt');
 
