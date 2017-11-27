be778e709726d03f8344f4423530553b3fff50a6
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2017-11-27 11:42:30 +0000

    EL-2891 - Tree grid test fixes (#486)
    * Adding some further optimizations
    
    * Fixing tests

--- e2e/pages/polyfills.ts
+++ e2e/pages/polyfills.ts
@@ -1,7 +1,4 @@
 // Import Polyfills
-import 'core-js/es6';
+import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
-require('zone.js/dist/zone');
-
-Error['stackTraceLimit'] = Infinity;
-require('zone.js/dist/long-stack-trace-zone');
+import 'zone.js/dist/zone';
--- e2e/pages/vendor.ts
+++ e2e/pages/vendor.ts
@@ -14,24 +14,12 @@ import 'bootstrap';
 /*
   Import Angular 1 Components and their dependencies
 */
-import 'angular';
+import * as angular from 'angular';
 
 /*
   Import Angular 1 Components and their dependencies
 */
 import '../../dist/ng1/ux-aspects-ng1';
-
-/*
-    Import Angular Libraries
-*/
-import '@angular/platform-browser';
-import '@angular/core';
-import '@angular/common';
-import '@angular/http';
-import '@angular/router';
-import '@angular/upgrade';
-
-import * as angular from 'angular';
 
 // create the AngularJS module
 angular.module('app', ['ux-aspects']);
