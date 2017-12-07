911eca4c159574bf53a8b005b3983e64f124bc7a
donna-marijne <donna.worby@hpe.com>
2017-12-07 16:06:09 +0000

    Update for 1.5.1 (#500)

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -15,6 +15,11 @@ export class ChangeLogPageComponent {
 
         this.logs = [
             {
+                version: '1.5.1',
+                date: 'December 7th 2017',
+                content: require('./logs/release-v1.5.1.md')
+            },
+            {
                 version: '1.5.0',
                 date: 'November 23rd 2017',
                 content: require('./logs/release-v1.5.0.md')
