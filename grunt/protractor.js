137e3380006fa17d5065cc80505879f5e91f1173
PearseMcMurray <pearse.mcmurray@hpe.com>
2017-12-19 15:38:26 +0000

    SEPG build test.

--- grunt/protractor.js
+++ grunt/protractor.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     e2e: {
         options: {
-            configFile: path.join(process.cwd(), 'e2e', 'protractor.config.js'),
+            configFile: path.join(process.cwd(), 'e2e', 'protractor.headless.config.js'),
             keepAlive: true,
             noColor: false,
             webdriverManagerUpdate: true,            
