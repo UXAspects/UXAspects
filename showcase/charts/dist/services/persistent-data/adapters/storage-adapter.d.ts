fe3b7a735acf898c29ecdd59f964ca66da758c5b
nalini-k-vardham <nalini.k.vardham@hpe.com>
2018-01-31 19:53:15 -0500

    Committing documentation changes for 1.5.4-gh-pages-test. Latest develop commit ID is a93413cbc6f5f0f6cd0af5f681ecc55a2ede1d73.

--- showcase/charts/dist/services/persistent-data/adapters/storage-adapter.d.ts
+++
@@ -1,7 +0,0 @@
-export declare abstract class StorageAdapter {
-    abstract getItem(key: string): string;
-    abstract setItem(key: string, value: string): void;
-    abstract removeItem(key: string): void;
-    abstract clear(): void;
-    abstract getSupported(): StorageAdapter;
-}
