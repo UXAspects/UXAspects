c7b57f5bd961d58225112be919369a19721d63d6
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-02-07 13:33:43 +0000

    EL-2995 - Bundle Enhancements

--- src/ng1/directives/dropzone/dropzone.module.js
+++
@@ -1,8 +0,0 @@
-import DropZoneDirective from './dropzone.directive.js';
-
-import '../../plugins/dropzone/dropzone.js';
-
-angular.module("ux-aspects.dropZone", [])
-	.directive('dropZone', DropZoneDirective);
-
-
