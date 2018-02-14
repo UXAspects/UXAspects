85155e1bba4df14f8688f33a7bd76efc53db81b9
donna-marijne <donna.worby@hpe.com>
2018-02-14 18:52:43 +0000

    Committing documentation changes for 1.5.5-gh-pages-test. Latest develop commit ID is 11b56d929b6f6e92c9d5136e28ed6af13b4604e9. (#572)

---
+++ 1.5.5/showcase/charts/dist/components/tag-input/tag-input-event.d.ts
@@ -0,0 +1,7 @@
+export declare class TagInputEvent {
+    tag: any;
+    private _defaultPrevented;
+    constructor(tag: any);
+    preventDefault(): void;
+    defaultPrevented(): boolean;
+}
--- showcase/list_view/dist/hybrid/components/organization-chart/organization-chart.component.d.ts
+++ showcase/list_view/dist/hybrid/components/organization-chart/organization-chart.component.d.ts
@@ -1,6 +1,6 @@
 import { ElementRef, Injector, EventEmitter } from '@angular/core';
 import { UpgradeComponent } from '@angular/upgrade/static';
-export declare class OrganizationChartComponent extends UpgradeComponent {
+export declare class OrganizationChartNg1Component extends UpgradeComponent {
     data: any;
     options: any;
     dataChange: EventEmitter<any>;
