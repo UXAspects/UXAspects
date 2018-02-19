b448722d1c3ff5456a94a77c93183b3bfa4a03b8
PearseMcMurray <pearse.mcmurray@hpe.com>
2018-02-19 16:43:56 +0000

    EL-2941 Added code snippets and Plunker link. Added Protractor tests.

---
+++ e2e/tests/components/timeline/timeline.po.spec.ts
@@ -0,0 +1,40 @@
+import { browser, element, by } from 'protractor';
+
+export class TimelinePage {
+
+    getPage(): void {
+        browser.get('#/timeline');
+    }
+
+    addEvent = element(by.id('button'));
+    timeline = element(by.id('timeline'));
+    
+    getNumberOfEvents() {
+        return this.timeline.$('ul.timeline').$$('ux-timeline-event').count();
+    }
+    
+    getEvent(index: number) {
+        return this.timeline.$('ul.timeline').$$('ux-timeline-event').get(index);
+    }    
+
+    getEventBadge(index: number) {
+        return this.getEvent(index).$('li').$('.timeline-badge');
+    }
+
+    getEventPanel(index: number) {
+        return this.getEvent(index).$('li').$('.timeline-panel');
+    }
+
+    getEventBadgeTitle(index: number) {
+        return this.getEventBadge(index).getText();
+    }
+
+    getEventTimestamp(index: number) {
+        // return this.getEventPanel(index).$('div').$('span').$('span').getText();
+        return this.getEventPanel(index).$('div').$('span > span').getText();
+    }
+
+    getEventPanelText(index: number) {
+        return this.getEventPanel(index).$('p').getText();
+    }
+}
