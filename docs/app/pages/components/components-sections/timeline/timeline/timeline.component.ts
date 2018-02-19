4ffe4f29385090a215dbd004283397743fae01c1
PearseMcMurray <pearse.mcmurray@hpe.com>
2018-02-19 12:25:40 +0000

    Removed 'shortDate' value. Now using value of 'date', along with DatePipes, to generate the two displayed dates.

--- docs/app/pages/components/components-sections/timeline/timeline/timeline.component.ts
+++ docs/app/pages/components/components-sections/timeline/timeline/timeline.component.ts
@@ -17,7 +17,6 @@ export class ComponentsTimelineComponent extends BaseDocumentationSection {
 
     events: TimelineEvent[] = [{
         color: 'alternate3',
-        shortDate: 'Mon 29th',
         date: new Date(2018, 0, 29, 11, 10),
         url: '#',
         id: '1234',
@@ -25,7 +24,6 @@ export class ComponentsTimelineComponent extends BaseDocumentationSection {
         assignee: chance.name()
     }, {
         color: 'alternate2',
-        shortDate: 'Fri 26th',
         date: new Date(2018, 0, 26, 14, 0),
         url: '#',
         id: '1234',
@@ -33,7 +31,6 @@ export class ComponentsTimelineComponent extends BaseDocumentationSection {
         assignee: chance.name()
     }, {
         color: 'alternate1',
-        shortDate: 'Wed 24th',
         date: new Date(2018, 0, 24, 9, 20),
         url: '#',
         id: '1234',
@@ -41,7 +38,6 @@ export class ComponentsTimelineComponent extends BaseDocumentationSection {
         assignee: chance.name()
     }, {
         color: 'primary',
-        shortDate: 'Mon 22nd',
         date: new Date(2018, 0, 22, 13, 45),
         url: '#',
         id: '1234',
@@ -50,24 +46,9 @@ export class ComponentsTimelineComponent extends BaseDocumentationSection {
     }];
 
     addEvent(): void {
-        const now = new Date();
-        const day = now.getDate();
-        let suffix = 'th';
-        if (day === 1 || day === 21) {
-            suffix = 'st';
-        } else if (day === 2 || day === 22) {
-            suffix = 'nd';
-        } else if (day === 3 || day === 23) {
-            suffix = 'rd';
-        }
-
-        const months: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-        const month = months[now.getMonth()];
-
         this.events.unshift({
             color: 'alternate1',
-            shortDate: month + ' ' + day + suffix,
-            date: now,
+            date: new Date(),
             url: '#',
             id: '1234',
             action: 'updated',
@@ -78,7 +59,6 @@ export class ComponentsTimelineComponent extends BaseDocumentationSection {
 
 interface TimelineEvent {
     color?: string;
-    shortDate: string;
     date?: Date;
     url?: string;
     id?: string;
