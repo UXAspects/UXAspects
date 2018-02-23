d64312d17ddf04b111d6495c9e8db2c916da6965
donna-marijne <donna.worby@hpe.com>
2018-02-23 12:53:44 +0000

    Manual merge cleanup 1

---
+++ packages/library/src/hybrid/components/flot/flot.component.ts
@@ -0,0 +1,19 @@
+import { Directive, ElementRef, Injector, Input, Output, EventEmitter } from '@angular/core';
+import { UpgradeComponent } from '@angular/upgrade/static';
+
+@Directive({
+    selector: 'flot'
+})
+export class FlotNg1Component extends UpgradeComponent {
+
+    @Input() dataset: any;
+    @Input() options: any;
+    @Input() callback: any;
+    @Input() donutLabels: any;
+    @Output() onPlotClick = new EventEmitter<any>();
+    @Output() onPlotHover = new EventEmitter<any>();
+
+    constructor(elementRef: ElementRef, injector: Injector) {
+        super('uxFlotNg1', elementRef, injector);
+    }
+}
--- src/components/timeline/timeline.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'ux-timeline',
-    templateUrl: './timeline.component.html'
-})
-export class TimelineComponent {
-}
