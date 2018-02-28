c8919c31f438fb80764a9571db9efc90ec63c5b4
PearseMcMurray <pearse.mcmurray@hpe.com>
2018-02-28 12:24:03 +0000

    Creating UrlTree to determine if link is active.

--- src/components/navigation/navigation-item/navigation-item.component.ts
+++ src/components/navigation/navigation-item/navigation-item.component.ts
@@ -1,35 +1,21 @@
-import {
-    Component,
-    Input,
-    ContentChildren,
-    QueryList,
-    Renderer2,
-    ElementRef,
-    Optional,
-    SkipSelf,
-    Output,
-    AfterViewInit,
-    AfterContentInit,
-    OnDestroy
-} from '@angular/core';
-import { Router, NavigationEnd, UrlTree } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router, RouterState, UrlTree } from '@angular/router';
+import { AfterContentInit, AfterViewInit, Component, ContentChildren, ElementRef, Input, OnDestroy, Optional, QueryList, Renderer2, SkipSelf, HostBinding } from '@angular/core';
 import { filter } from 'rxjs/operators/filter';
+import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 
 @Component({
     selector: '[ux-navigation-item]',
     templateUrl: './navigation-item.component.html',
-    host: {
-        '[class.active]': 'active',
-        '[class.selected]': 'expanded',
-    }
 })
 export class NavigationItemComponent implements AfterViewInit, AfterContentInit, OnDestroy {
+
     @Input() header: string;
     @Input() icon: string;
-    @Input() expanded: boolean = false;
     @Input() link: string;
+    @Input() @HostBinding('class.selected') expanded: boolean = false;
 
+    @HostBinding('class.active')
     get active(): boolean {
         if (this.link) {
             return this._router.isActive(this.link, true);
@@ -47,29 +33,26 @@ export class NavigationItemComponent implements AfterViewInit, AfterContentInit,
 
     get children(): NavigationItemComponent[] {
         return this._children.filter(item => item !== this);
-    }    
+    }
 
     constructor(
         private _elementRef: ElementRef,
         private _renderer: Renderer2,
-        @Optional()
-        @SkipSelf()
-        private _parent: NavigationItemComponent,
-        private _router: Router
+        @Optional() @SkipSelf() private _parent: NavigationItemComponent,
+        private _router: Router,
+        private _activatedRoute: ActivatedRoute
     ) {
         this.level = _parent ? _parent.level + 1 : 1;
 
-        this._navigationEnd = _router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(event => {
-            // Expand this component if it or a descendant is active.
-            this.expanded = this.hasActiveLink(this.link);
-        });
+        this._navigationEnd = _router.events.pipe(filter(event => event instanceof NavigationEnd))
+            .subscribe(() => this.expanded = this.hasActiveLink(this.link));
     }
 
     ngAfterViewInit(): void {
         // Add classes to parent for styling
         const parentListElement = this._elementRef.nativeElement.parentElement;
         if (parentListElement) {
-            let levelClass: string = this.getLevelClass();
+            const levelClass: string = this.getLevelClass();
             if (levelClass.length > 0) {
                 this._renderer.addClass(parentListElement, 'nav');
                 this._renderer.addClass(parentListElement, levelClass);
@@ -85,14 +68,20 @@ export class NavigationItemComponent implements AfterViewInit, AfterContentInit,
         this._childrenChanges = this._children.changes.subscribe(() => this.setIndentWithoutArrow());
     }
 
-    ngOnDestroy () {
+    ngOnDestroy() {
         this._navigationEnd.unsubscribe();
         this._childrenChanges.unsubscribe();
     }
 
     private hasActiveLink(link: string | UrlTree): boolean {
-        // If this component has a link, check if it is active.
-        if (link && this._router.isActive(link, true)) {
+
+        const tree = this._router.createUrlTree([link], {
+            relativeTo: this._activatedRoute,
+            queryParams: this._activatedRoute.snapshot.queryParams,
+            fragment: this._activatedRoute.snapshot.fragment
+        });
+
+        if (link && this._router.isActive(tree, true)) {
             return true;
         }
 
@@ -120,7 +109,7 @@ export class NavigationItemComponent implements AfterViewInit, AfterContentInit,
             // If this element has children it will be indented and will have an arrow
             this.indentWithoutArrow = false;
         } else if (this._parent) {
-            // If this element has a parent, indent it if any of its siblings have children
+           // If this element has a parent, indent it if any of its siblings have children
             this.indentWithoutArrow = !this._parent.children.every((item) => item.children.length === 0);
         } else {
             // Top-level elements should be indented
