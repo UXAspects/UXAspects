91b23181f7f5a86921b0294620de93b6be6644fb
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-03-01 13:34:11 +0000

    Remove old styles and making existing styles follow stylelint rules

--- grunt/stylelint.js
+++ grunt/stylelint.js
@@ -3,5 +3,11 @@ const path = require('path');
 module.exports = {
     components: {
         src: path.join(process.cwd(), 'src', 'components', '**', '*.less')
+    },
+    stylesheets: {
+        src: [
+            path.join(process.cwd(), 'src', 'styles', '**', '*.less'),
+            '!' + path.join(process.cwd(), 'src', 'styles', 'hpe-icons.less'),
+        ]
     }
 };
