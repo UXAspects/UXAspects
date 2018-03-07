7c853fcd0c1cf806ae015c7caed46b8193f98ca2
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-03-07 11:43:36 +0000

    EL-3028 - Adding tooltip to floating action buttons (#598)
    * Adding tooltip to floating action buttons
    
    * Some performance improvements
    
    * Review changes made
    
    * Correcting documentation

--- docs/app/components/api-property/api-property.component.ts
+++ docs/app/components/api-property/api-property.component.ts
@@ -1,9 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: '[uxd-api-property]',
     templateUrl: './api-property.component.html',
-    styleUrls: ['./api-property.component.less']
+    styleUrls: ['./api-property.component.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ApiPropertyComponent {
     @Input() name: string;
