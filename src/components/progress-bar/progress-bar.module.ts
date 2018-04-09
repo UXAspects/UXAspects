d3e3bf4f8d796fac6a9622b1176d4a7f5777232b
donna-marijne <donna.worby@hpe.com>
2018-04-09 13:22:32 +0100

    Added progress bar `indeterminate` property (#641)
    Added for the MF theme, so not documented or styled in the Keppel site.

--- src/components/progress-bar/progress-bar.module.ts
+++ src/components/progress-bar/progress-bar.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { ProgressBarComponent } from './progress-bar.component';
 
 @NgModule({
+    imports: [
+        CommonModule
+    ],
     exports: [ProgressBarComponent],
     declarations: [ProgressBarComponent]
 })
