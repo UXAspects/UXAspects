21f9e68d7c3d7ef36c381af126e94924315dba64
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-04-13 16:36:22 +0100

    Adding component list example (#651)
    JIRA: https://jira.autonomy.com/browse/EL-2963
    
    - Added component list documentation example using Reactive forms.
    - Deprecated old component list section
    - Tidied up AngularJS Component List styles so we could reuse them in the Angular example

---
+++ docs/app/pages/components/components-sections/component-list/component-list/component-list.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BaseDocumentationSection } from '../../../../../components/base-documentation-section/base-documentation-section';
+import { DocumentationSectionComponent } from '../../../../../decorators/documentation-section-component';
+import { IPlunkProvider } from '../../../../../interfaces/IPlunkProvider';
+import { IPlunk } from '../../../../../interfaces/IPlunk';
+
+@Component({
+    selector: 'uxd-component-list',
+    templateUrl: './component-list.component.html'
+})
+@DocumentationSectionComponent('ComponentsComponentListComponent')
+export class ComponentsComponentListComponent extends BaseDocumentationSection implements IPlunkProvider {
+
+    plunk: IPlunk = {
+        files: {
+            'app.component.html': this.snippets.raw.appHtml,
+            'app.component.ts': this.snippets.raw.appTs,
+        }
+    };
+
+    form = new FormGroup({
+        items: new FormArray([
+            new FormControl(null, [Validators.required])
+        ])
+    });
+
+    get items(): FormArray {
+        return this.form.get('items') as FormArray;
+    }
+
+    constructor() {
+        super(require.context('./snippets/', false, /\.(html|css|js|ts)$/));
+    }
+
+    add(): void {
+        if (this.form.valid) {
+            this.items.push(new FormControl(null, [Validators.required]));
+        }
+    }
+
+    remove(index: number): void {
+        if (this.items.length > 1) {
+            this.items.removeAt(index);
+        }
+    }
+}
