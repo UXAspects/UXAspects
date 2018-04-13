9e97d9a3f58c8710701ff2deb106a14e27dfdb7f
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-04-13 16:09:27 +0100

    Adding component list example
    JIRA: https://jira.autonomy.com/browse/EL-2963
    
    - Added component list documentation example using Reactive forms.
    - Deprecated old component list section
    - Tidied up AngularJS Component List styles so we could reuse them in the Angular example

--- docs/app/pages/components/components-sections/component-list/component-list.module.ts
+++ docs/app/pages/components/components-sections/component-list/component-list.module.ts
@@ -1,15 +1,19 @@
-import { NgModule, ComponentFactoryResolver } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponentFactoryResolver, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { DocumentationComponentsModule } from '../../../../components/components.module';
-import { ResolverService, DocumentationPage } from '../../../../services/resolver/resolver.service';
-import { DocumentationCategoryComponent } from '../../../../components/documentation-category/documentation-category.component';
-
-import { ComponentsComponentListNg1Component } from './component-list-ng1/component-list-ng1.component';
-import { WrappersModule } from '../../../../wrappers/wrappers.module';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 
+import { DocumentationComponentsModule } from '../../../../components/components.module';
+import { DocumentationCategoryComponent } from '../../../../components/documentation-category/documentation-category.component';
+import { DocumentationPage, ResolverService } from '../../../../services/resolver/resolver.service';
+import { WrappersModule } from '../../../../wrappers/wrappers.module';
+import { ComponentsComponentListNg1Component } from './component-list-ng1/component-list-ng1.component';
+import { ComponentsComponentListComponent } from './component-list/component-list.component';
+
 const SECTIONS = [
-    ComponentsComponentListNg1Component
+    ComponentsComponentListNg1Component,
+    ComponentsComponentListComponent
 ];
 
 const ROUTES = [
@@ -24,8 +28,11 @@ const ROUTES = [
 
 @NgModule({
     imports: [
+        CommonModule,
         WrappersModule,
         TabsModule,
+        FormsModule, 
+        ReactiveFormsModule,
         DocumentationComponentsModule,
         RouterModule.forChild(ROUTES)
     ],
