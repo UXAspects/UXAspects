0b949b721d3d41262a4060d54b1a10ff15567569
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-04-20 17:31:18 +0100

    EL-2969 - Card Tabs - Fixing QA Issues (#662)
    * EL-2969 - Card Tabs - Fixing QA Issues
    
    * Using mixin for outline

--- docs/app/pages/components/components-sections/tabs/card-tabs-ng1/card-tabs-ng1.component.ts
+++ docs/app/pages/components/components-sections/tabs/card-tabs-ng1/card-tabs-ng1.component.ts
@@ -1,12 +1,14 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { DocumentationSectionComponent } from '../../../../../decorators/documentation-section-component';
-import { ICodePenProvider } from '../../../../../interfaces/ICodePenProvider';
-import { ICodePen } from '../../../../../interfaces/ICodePen';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { BaseDocumentationSection } from '../../../../../components/base-documentation-section/base-documentation-section';
+import { DocumentationSectionComponent } from '../../../../../decorators/documentation-section-component';
+import { ICodePen } from '../../../../../interfaces/ICodePen';
+import { ICodePenProvider } from '../../../../../interfaces/ICodePenProvider';
 
 @Component({
     selector: 'uxd-components-card-tabs-ng1',
     templateUrl: './card-tabs-ng1.component.html',
+    styleUrls: ['./card-tabs-ng1.component.less'],
+    encapsulation: ViewEncapsulation.None,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
 @DocumentationSectionComponent('ComponentsCardTabsNg1Component')
