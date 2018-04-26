a36b918d86848bcd9c89a2e1e5ebe830a218c73a
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-04-26 10:36:18 +0100

    Fixing build issue (#672)

--- src/components/card-tabs/card-tabs.module.ts
+++ src/components/card-tabs/card-tabs.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ResizeModule } from '../../directives/resize';
+import { ResizeModule } from '../../directives/resize/index';
 import { CardTabContentDirective } from './card-tab/card-tab-content.directive';
 import { CardTabComponent } from './card-tab/card-tab.component';
 import { CardTabsetComponent } from './card-tabset/card-tabset.component';
