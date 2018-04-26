15ec11be383be0cacad3915f0eac5f4a6e419382
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-04-26 12:03:29 +0100

    Committing changes for package 1.5.10-2-package-test. Latest develop commit ID is a36b918d86848bcd9c89a2e1e5ebe830a218c73a. (#673)

--- ux-aspects.d.ts
+++ ux-aspects.d.ts
@@ -2,21 +2,19 @@
  * Generated bundle index. Do not edit.
  */
 export * from './index';
-export { CardTabsService as ɵd } from './components/card-tabs/card-tabs.service';
-export { DateTimePickerService as ɵe } from './components/date-time-picker/date-time-picker.service';
-export { FloatingActionButtonsService as ɵf } from './components/floating-action-buttons/floating-action-buttons.service';
-export { MarqueeWizardStepComponent as ɵi } from './components/marquee-wizard/marquee-wizard-step.component';
-export { MarqueeWizardService as ɵh } from './components/marquee-wizard/marquee-wizard.service';
-export { MediaPlayerService as ɵm } from './components/media-player/media-player.service';
-export { PageHeaderNavigationDropdownItemComponent as ɵl } from './components/page-header/navigation/navigation-dropdown-item/navigation-dropdown-item.component';
-export { PageHeaderNavigationItemComponent as ɵk } from './components/page-header/navigation/navigation-item/navigation-item.component';
-export { PageHeaderService as ɵj } from './components/page-header/page-header.service';
-export { SidePanelService as ɵg } from './components/side-panel/side-panel.service';
-export { HoverActionService as ɵn } from './directives/hover-action/hover-action.service';
-export { ResizeModule as ɵc } from './directives/resize';
-export { SelectionItemDirective as ɵq } from './directives/selection/selection-item.directive';
-export { SelectionDirective as ɵo } from './directives/selection/selection.directive';
-export { SelectionService as ɵp } from './directives/selection/selection.service';
-export { INavigationMenuService as ɵr } from './hybrid/services/navigation-menu/navigation-menu.interface';
+export { DateTimePickerService as ɵc } from './components/date-time-picker/date-time-picker.service';
+export { FloatingActionButtonsService as ɵd } from './components/floating-action-buttons/floating-action-buttons.service';
+export { MarqueeWizardStepComponent as ɵg } from './components/marquee-wizard/marquee-wizard-step.component';
+export { MarqueeWizardService as ɵf } from './components/marquee-wizard/marquee-wizard.service';
+export { MediaPlayerService as ɵk } from './components/media-player/media-player.service';
+export { PageHeaderNavigationDropdownItemComponent as ɵj } from './components/page-header/navigation/navigation-dropdown-item/navigation-dropdown-item.component';
+export { PageHeaderNavigationItemComponent as ɵi } from './components/page-header/navigation/navigation-item/navigation-item.component';
+export { PageHeaderService as ɵh } from './components/page-header/page-header.service';
+export { SidePanelService as ɵe } from './components/side-panel/side-panel.service';
+export { HoverActionService as ɵl } from './directives/hover-action/hover-action.service';
+export { SelectionItemDirective as ɵo } from './directives/selection/selection-item.directive';
+export { SelectionDirective as ɵm } from './directives/selection/selection.directive';
+export { SelectionService as ɵn } from './directives/selection/selection.service';
+export { INavigationMenuService as ɵp } from './hybrid/services/navigation-menu/navigation-menu.interface';
 export { IPdfService as ɵa } from './hybrid/services/pdf/pdf.interface';
 export { ITimeAgoService as ɵb } from './hybrid/services/time-ago/time-ago.interface';
