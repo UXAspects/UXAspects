be7cdf81bd4f9558da0fb60a4594172a51d12573
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-05-03 16:21:49 +0100

    EL-2942 - Adding tests for hierarchy bar (#681)
    * Adding test for hierarchy bar
    
    * Removing unused dependency

---
+++ e2e/tests/components/hierarchy-bar/hierarchy-bar.po.spec.ts
@@ -0,0 +1,113 @@
+import { $, $$, ElementFinder, browser } from 'protractor';
+
+export class HierarchyBarPage {
+    
+    nodes = $$('.hierarchy-bar-node');
+    selectedLabel = $('#selected');
+    selectButton = $('#set-selected-btn');
+    
+    getPage(): void {
+        browser.get('#/hierarchy-bar');
+    }
+
+    async getSelectedNodeTitle(): Promise<string> {
+        return await this.selectedLabel.getText();
+    }
+
+    async getNodeCount(): Promise<number> {
+        return await this.nodes.count();
+    }
+
+    async getNode(index: number): Promise<ElementFinder> {
+        return await this.nodes.get(index);
+    }
+
+    async clickNode(index: number): Promise<void> {
+        const node: ElementFinder = await this.getNode(index);
+
+        await node.click();
+    }
+
+    async nodeHasChildren(index: number): Promise<boolean> {
+        const node: ElementFinder = await this.getNode(index);
+        const arrow: ElementFinder[] = await node.$$('.hierarchy-bar-node-arrow');
+
+        return arrow.length === 1;
+    }
+
+    async showNodePopover(index: number): Promise<void> {
+
+        const node: ElementFinder = await this.getNode(index);
+        const arrow: ElementFinder = await node.$('.hierarchy-bar-node-arrow');
+
+        await arrow.click();
+    }
+
+    async selectPopoverNode(index: number, childIndex: number): Promise<void> {
+        const children = await this.getNodeChildren(index);
+        const child = children[childIndex];
+
+        await child.click();
+    }
+
+    async getNodeChildren(index: number): Promise<ElementFinder[]> {
+        
+        // check if the node has any children
+        if (await this.nodeHasChildren(index) === false) {
+            return [];
+        }
+
+        // if it does have children then open the popover
+        await this.showNodePopover(index);
+
+        // return all the list items
+        return await $$('.hierarchy-bar-node-list-item');
+    }
+
+    async getNodeChildrenTitles(index: number): Promise<string[]> {
+
+        const children = await this.getNodeChildren(index);
+
+        const titles: string[] = [];
+
+        for (let child of children) {
+
+            const title: ElementFinder = await child.$('.hierarchy-bar-node-title');
+
+            titles.push(await title.getText());
+        }
+
+        return titles;
+    }
+
+    async isOverflowIndicatorVisible(): Promise<boolean> {
+        const indicator: ElementFinder[] = await $$('.hierarchy-bar-overflow-indicator');
+
+        return indicator.length !== 0;
+    }
+
+    async getOverflowNodes(): Promise<ElementFinder[]> {
+        const indicator: ElementFinder = await $('.hierarchy-bar-overflow-indicator');
+    
+        await indicator.click();
+
+        return await $$('.hierarchy-bar-node-list-item');
+    }
+
+    async getOverflowNodeTitles(): Promise<string[]> {
+
+        const children = await this.getOverflowNodes();
+
+        const titles: string[] = [];
+
+        for (let child of children) {
+
+            const title: ElementFinder = await child.$('.hierarchy-bar-node-title');
+
+            titles.push(await title.getText());
+        }
+
+        return titles;
+    }
+
+}
