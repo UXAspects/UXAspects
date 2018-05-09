4e5d3c4944a3b64a4b42beaf1993dd6803a6429d
donna-marijne <donna.worby@hpe.com>
2018-05-09 11:35:52 +0100

    EL-2965  - drag & drop cards (#686)
    * Deprecated "Draggable Panels" for Dashboard
    
    * Created Drag & Drop category
    
    * Drag & Drop cards p1
    
    * Moved dragula instance to ReorderableService to enable groups of drop containers.
    * Modified drop logic to check source and target before adding/removing data items from the array.
    * Modified other event handlers to check that the element is a child before modifying.
    
    * Finished D&D cards example
    
    * Added `ux-reorderable-container-moving` class to uxReorderable.
    * Subscription cleanup.
    * Updated Tables > Reorderable Table documentation with `reorderableGroup` details and mentioned the dynamic classes.
    
    * Plunker error fix
    
    * Added forRoot for the booststrap dependencies
    
    * Removed redundant code

--- src/directives/reorderable/index.ts
+++ src/directives/reorderable/index.ts
@@ -2,3 +2,4 @@ export * from './reorderable.module';
 export * from './reorderable.directive';
 export * from './reorderable-handle.directive';
 export * from './reorderable-model.directive';
+export * from './reorderable.service';
