d3d53286a7c4414345d0d9844aea4be338064056
donna-marijne <donna.worby@hpe.com>
2018-05-15 14:48:19 +0100

    Float label tests (#695)

---
+++ e2e/pages/app/float-label/float-label.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { FloatLabelModule, RadioButtonModule } from '../../../../dist';
+import { FloatLabelTestPageComponent } from './float-label.testpage.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forChild([{
+            path: '',
+            component: FloatLabelTestPageComponent
+        }]),
+        FloatLabelModule,
+        RadioButtonModule,
+    ],
+    declarations: [FloatLabelTestPageComponent],
+})
+export class FloatLabelTestPageModule { }
