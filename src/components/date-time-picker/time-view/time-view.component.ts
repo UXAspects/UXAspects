0ca743867988f04d41ab339ab46a26c1608d9d73
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-05-18 14:28:54 +0100

    Fixing time zone picker

--- src/components/date-time-picker/time-view/time-view.component.ts
+++ src/components/date-time-picker/time-view/time-view.component.ts
@@ -10,6 +10,17 @@ export class TimeViewComponent {
 
     constructor(public datepicker: DateTimePickerService) { }
 
+    selectTimezone(name: string): void {
+        const timezones = this.datepicker.timezones$.value;
+        
+        // find matching timezone
+        const timezone = timezones.find(_timezone => _timezone.name === name);
+
+        if (timezone) {
+            this.datepicker.setTimezone(timezone);
+        }
+    }
+
     incrementTimezone(): void {
         const timezone = this.datepicker.timezone$.value;
         const timezones = this.datepicker.timezones$.value;
