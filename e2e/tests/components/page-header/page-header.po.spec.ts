f95c5b6c466593581eed0dbe710d9b43def4db4b
donna-marijne <donna.worby@hpe.com>
2018-05-21 19:10:16 +0100

    Updated protractor tests

--- e2e/tests/components/page-header/page-header.po.spec.ts
+++ e2e/tests/components/page-header/page-header.po.spec.ts
@@ -1,11 +1,11 @@
 import { browser, element, by, ElementFinder, protractor } from 'protractor';
 
 export class PageHeaderPage {
-        
+
     getPage(): void {
         browser.get('#/page-header');
     }
-    
+
     pageHeader1 = element(by.id('pageHeader1'));
     pageHeader2 = element(by.id('pageHeader2'));
     selected = element(by.id('selected'));
@@ -15,9 +15,9 @@ export class PageHeaderPage {
     alignCenterButton = element(by.id('align-center'));
     alignRightButton = element(by.id('align-right'));
     autoselectButton = element(by.id('autoselect'));
-    
+
     condensed = false;
-    
+
     confirmClassExists(item: ElementFinder, soughtClass: string) {
         return item.getAttribute('class').then(function(classes: string) {
             var allClasses = classes.split(' ');
@@ -28,16 +28,16 @@ export class PageHeaderPage {
             }
         });
     }
-    
+
     confirmPageHeaderIsCondensed() {
         return this.confirmClassExists(this.pageHeader1, 'page-header-condensed');
     }
-    
+
     toggleTheHeader() {
         this.toggleButton.click();
         this.condensed = !this.condensed;
     }
-    
+
     // The expanded and condensed forms of most of the locators used here are the same except for the first
     // two elements. This function will return an ElementFinder for the first two elements which may then be
     // passed to the other functions, avoiding duplication of code.
@@ -56,107 +56,107 @@ export class PageHeaderPage {
         return this.pageHeader1.$('div.page-header-details').$('div.page-header-state-container').$('div.page-header-title-container').
                $('ux-breadcrumbs').$('ol.breadcrumb').$$('li').get(index).getText();
     }
-    
+
     getABreadcrumbWhenCondensed(index: number) {
         return this.pageHeader1.$('div.page-header-condensed-content').$('div.page-header-breadcrumbs').
                $('ux-breadcrumbs').$('ol.breadcrumb').$$('li').get(index).getText();
     }
-    
+
     confirmApplicationLogoIsPresent() {
         return this.getContainerElement(this.condensed).then(function(elem: ElementFinder) {
             return elem.$('div.page-header-navigation').$('ux-page-header-horizontal-navigation').
-                   $$('ux-page-header-horizontal-navigation-item').get(0).$('div.horizontal-navigation-button').
+                   $$('ux-page-header-horizontal-navigation-item').get(0).$('.horizontal-navigation-button').
                    $('span.hpe-home').isPresent();
         });
     }
-    
+
     getApplicationLogoText() {
         return this.getContainerElement(this.condensed).then(function(elem: ElementFinder) {
             return elem.$('div.page-header-navigation').$('ux-page-header-horizontal-navigation').
-                   $$('ux-page-header-horizontal-navigation-item').get(0).$('div.horizontal-navigation-button').
+                   $$('ux-page-header-horizontal-navigation-item').get(0).$('.horizontal-navigation-button').
                    $('span.navigation-item-label').getText();
         });
     }
-    
+
     confirmDropdownIsPresent() {
         return this.getContainerElement(this.condensed).then(function(elem: ElementFinder) {
             return elem.$('div.page-header-navigation').$('ux-page-header-horizontal-navigation').
-                   $$('ux-page-header-horizontal-navigation-item').get(1).$('div.horizontal-navigation-button').
+                   $$('ux-page-header-horizontal-navigation-item').get(1).$('.horizontal-navigation-button').
                    $('span.hpe-analytics').isPresent();
         });
     }
-    
+
     openDropdown() {
         this.getContainerElement(this.condensed).then(function(elem: ElementFinder) {
             elem.$('div.page-header-navigation').$('ux-page-header-horizontal-navigation').
-            $$('ux-page-header-horizontal-navigation-item').get(1).$('div.horizontal-navigation-button').click();
+            $$('ux-page-header-horizontal-navigation-item').get(1).$('.horizontal-navigation-button').click();
         });
     }
-    
+
     confirmDropdownIsOpened() {
         return this.getContainerElement(this.condensed).then(function(elem: ElementFinder) {
             return elem.$('div.page-header-navigation').$('ux-page-header-horizontal-navigation').
-                   $$('ux-page-header-horizontal-navigation-item').get(1).$('div.horizontal-navigation-button.open').isPresent();
+                   $$('ux-page-header-horizontal-navigation-item').get(1).$('.horizontal-navigation-button.open').isPresent();
         });
     }
-    
+
     getFirstDropdownMenuItem(index: number) {
         return browser.element(by.css('body')).$$('bs-dropdown-container').get(0).$('div.dropdown').$('div.dropdown-menu').
                    $$('ux-page-header-horizontal-navigation-dropdown-item').get(index).$('div').$('a.dropdown-item').$('span.dropdown-item-title').getText();
     }
-    
+
     displaySecondDropdownMenu() {
         browser.actions().mouseMove(browser.element(by.css('body')).$$('bs-dropdown-container').get(0).$('div.dropdown').$('div.dropdown-menu').
                    $$('ux-page-header-horizontal-navigation-dropdown-item').get(1)).perform();
     }
-    
+
     getSecondDropdownMenuItem(index: number) {
         return browser.element(by.css('body')).$$('bs-dropdown-container').get(1).$('div.dropdown').$('ul.dropdown-menu').
                    $$('li').get(index).$('a.dropdown-item').$('span.dropdown-item-title').getText();
     }
-    
+
     confirmNotificationIconIsPresent() {
         return this.getContainerElement(this.condensed).then(function(elem: ElementFinder) {
             return elem.$('div.page-header-icon-menus').$$('ux-page-header-icon-menu').get(0).$('div.page-header-icon-menu').
                    $('a.page-header-icon-menu-button').$('i.hpe-notification').isPresent();
         });
     }
-    
+
     openNotifications() {
         this.getContainerElement(this.condensed).then(function(elem: ElementFinder) {
             elem.$('div.page-header-icon-menus').$$('ux-page-header-icon-menu').get(0).$('div.page-header-icon-menu').
                  $('a.page-header-icon-menu-button').click();
         });
     }
-    
+
     confirmNotificationsAreDisplayed() {
         return this.getContainerElement(this.condensed).then(function(elem: ElementFinder) {
             return elem.$('div.page-header-icon-menus').$$('ux-page-header-icon-menu').get(0).
                    $('div.page-header-icon-menu').$('ul.dropdown-menu').$$('li').count();
         });
     }
-    
+
     confirmActionsIconIsPresent() {
         return this.getContainerElement(this.condensed).then(function(elem: ElementFinder) {
             return elem.$('div.page-header-icon-menus').$$('ux-page-header-icon-menu').get(1).$('div.page-header-icon-menu').
                    $('a.page-header-icon-menu-button').$('i.hpe-actions').isPresent();
         });
     }
-    
+
     openActions() {
         this.getContainerElement(this.condensed).then(function(elem: ElementFinder) {
             elem.$('div.page-header-icon-menus').$$('ux-page-header-icon-menu').get(1).$('div.page-header-icon-menu').
                  $('a.page-header-icon-menu-button').click();
         });
     }
-    
+
     confirmActionsAreDisplayed() {
         return this.getContainerElement(this.condensed).then(function(elem: ElementFinder) {
             return elem.$('div.page-header-icon-menus').$$('ux-page-header-icon-menu').get(1).
                    $('div.page-header-icon-menu').$('ul.dropdown-menu').$$('li').count();
         });
     }
-    
+
     async getSecondaryNavigation(): Promise<ElementFinder> {
         return await this.pageHeader2.$('.page-header-secondary');
     }
