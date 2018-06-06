0d572418aaab599a4b89820adf7f700eb217ff27
donna-marijne <donna.worby@hpe.com>
2018-06-06 15:50:26 +0100

    Release 1.5.14 (#716)

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -15,6 +15,11 @@ export class ChangeLogPageComponent {
 
         this.logs = [
             {
+                version: '1.5.14',
+                date: 'June 7th 2018',
+                content: require('./logs/release-v1.5.14.md')
+            },
+            {
                 version: '1.5.13',
                 date: 'May 25th 2018',
                 content: require('./logs/release-v1.5.13.md')
