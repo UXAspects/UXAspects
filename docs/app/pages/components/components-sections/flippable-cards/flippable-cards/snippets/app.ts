15677da71f3cedd087fff67086c37b810f82b589
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2018-07-10 13:44:21 +0100

    Updating code snippets

--- docs/app/pages/components/components-sections/flippable-cards/flippable-cards/snippets/app.ts
+++ docs/app/pages/components/components-sections/flippable-cards/flippable-cards/snippets/app.ts
@@ -8,48 +8,6 @@ import { ColorService } from '@ux-aspects/ux-aspects';
 })
 export class AppComponent {
 
-    donut1 = [{
-        label: 'documents',
-        color: this.colorService.getColor('grey6').toHex(),
-        value: 23456
-    }, {
-        label: 'reviewed',
-        color: this.colorService.getColor('vibrant1').toHex(),
-        value: 19876
-    }, {
-        label: 'produced',
-        color: this.colorService.getColor('vibrant2').toHex(),
-        value: 11123
-    }];
-
-    donut2 = [{
-        label: 'documents',
-        color: this.colorService.getColor('grey6').toHex(),
-        value: 15678
-    }, {
-        label: 'reviewed',
-        color: this.colorService.getColor('vibrant1').toHex(),
-        value: 10123
-    }, {
-        label: 'produced',
-        color: this.colorService.getColor('vibrant2').toHex(),
-        value: 3123
-    }];
-
-    donut3 = [{
-        label: 'documents',
-        color: this.colorService.getColor('grey6').toHex(),
-        value: 256987
-    }, {
-        label: 'reviewed',
-        color: this.colorService.getColor('vibrant1').toHex(),
-        value: 143567
-    }, {
-        label: 'produced',
-        color: this.colorService.getColor('vibrant2').toHex(),
-        value: 45678
-    }];
-
     options = {
         size: 70,
         donutWidth: 3,
@@ -63,6 +21,86 @@ export class AppComponent {
         }
     };
 
-    constructor(public colorService: ColorService) { }
+    cards: Card[] = [
+        {
+            title: 'NYC vs Volkswagen',
+            flipped: false,
+            trigger: 'manual',
+            direction: 'vertical',
+            stats: {
+                documents: 23456,
+                reviewed: 19876,
+                produced: 11123,
+                size: 1.2
+            },
+            chart: this.getChartData(23456, 19876, 11123)
+        },
+        {
+            title: 'The Dorling Case',
+            flipped: false,
+            trigger: 'hover',
+            direction: 'horizontal',
+            stats: {
+                documents: 15678,
+                reviewed: 10123,
+                produced: 3123,
+                size: 0.8
+            },
+            chart: this.getChartData(15678, 10123, 3123)
+        },
+        {
+            title: 'The Salisbury Case',
+            flipped: false,
+            trigger: 'click',
+            direction: 'horizontal',
+            stats: {
+                documents: 256987,
+                reviewed: 143567,
+                produced: 45678,
+                size: 10.7
+            },
+            chart: this.getChartData(256987, 143567, 45678)
+        }
+    ];
 
+    constructor(public colorService: ColorService) {}
+
+    getChartData(documents: number, reviewed: number, produced: number): ChartData[] {
+        return [
+            {
+                label: 'documents',
+                color: this.colorService.getColor('grey6').toHex(),
+                value: documents
+            }, {
+                label: 'reviewed',
+                color: this.colorService.getColor('vibrant1').toHex(),
+                value: reviewed
+            }, {
+                label: 'produced',
+                color: this.colorService.getColor('vibrant2').toHex(),
+                value: produced
+            }
+        ];
+    }
+
+}
+
+export interface Card {
+    title: string;
+    flipped: boolean;
+    trigger: string;
+    direction: string;
+    stats: {
+        documents: number;
+        reviewed: number;
+        produced: number;
+        size: number;
+    };
+    chart: ChartData[];
+}
+
+export interface ChartData {
+    label: string;
+    color: string;
+    value: number;
 }
