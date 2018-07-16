29c75bb0b96c272c4491949df5ba64de50deccbc
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2018-07-16 09:39:07 +0100

    Fixing unit test

--- src/ng1/directives/overflowTooltip/singleLineOverflowTooltip.spec.js
+++ src/ng1/directives/overflowTooltip/singleLineOverflowTooltip.spec.js
@@ -1,14 +1,13 @@
 describe('single line overflow tooltip', function () {
-  var $compile, $rootScope, $timeout;
+  var $compile, $rootScope;
 
   beforeEach(module("ux-aspects.overflowTooltip"));
 
   beforeEach(function () {
 
-    inject(function (_$compile_, _$rootScope_, _$timeout_) {
+    inject(function (_$compile_, _$rootScope_) {
       $compile = _$compile_;
       $rootScope = _$rootScope_;
-      $timeout = _$timeout_;
     });
   });
 
@@ -17,18 +16,21 @@ describe('single line overflow tooltip', function () {
     var html = '<p style="width:10px; height:10px; display:inline-block;" single-line-overflow-tooltip>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>';
     var element = $compile(html)($scope);
     $scope.$digest();
-    $timeout.flush();
     return {
       element: element,
       scope: $scope
     };
   }
 
-  it('should remove tooltip on destroy', function () {
+  it('should remove tooltip on destroy', function (done) {
     var directive = create();
-    expect(directive.element.data()["bs.tooltip"]).toBeTruthy();
-    directive.scope.$destroy();
-    expect(directive.element.data()["bs.tooltip"]).not.toBeTruthy();
+
+    setTimeout(() => {
+      expect(directive.element.data()["bs.tooltip"]).toBeTruthy();
+      directive.scope.$destroy();
+      expect(directive.element.data()["bs.tooltip"]).not.toBeTruthy();
+      done();
+    }, 1);
   });
 
 });
