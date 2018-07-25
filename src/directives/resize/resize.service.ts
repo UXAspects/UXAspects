6643d6b7eaa8a50852135b69e107f2f98f5557f6
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-07-25 10:37:14 +0100

    EL-3191 - Resize Service (#768)
    Updated the resize service to use the build in browser ResizeObserver. Also added polyfill for when the ResizeObserver is not supported (it no longer polls to check for size changes).

--- src/directives/resize/resize.service.ts
+++ src/directives/resize/resize.service.ts
@@ -1,91 +1,44 @@
-import { Injectable, NgZone, OnDestroy, Renderer2, RendererFactory2 } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { fromEvent } from 'rxjs/observable/fromEvent';
-import { Subscription } from 'rxjs/Subscription';
-
+import { Injectable, NgZone, OnDestroy } from '@angular/core';
+import ResizeObserver from 'resize-observer-polyfill';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
 
 @Injectable()
 export class ResizeService implements OnDestroy {
 
-    private _renderer: Renderer2;
-    private _subscription = new Subscription();
+    private _observer = new ResizeObserver(this.elementDidResize.bind(this));
+    private _targets = new WeakMap<HTMLElement, ReplaySubject<ResizeDimensions>>();
 
-    constructor(rendererFactory: RendererFactory2, private _ngZone: NgZone) {
-        this._renderer = rendererFactory.createRenderer(null, null);
-    }
+    constructor(private _zone: NgZone) {}
 
     ngOnDestroy(): void {
-        this._subscription.unsubscribe();
+        this._observer.disconnect();
     }
 
-    addResizeListener(nativeElement: HTMLElement): BehaviorSubject<ResizeDimensions> {
+    addResizeListener(target: HTMLElement): ReplaySubject<ResizeDimensions> {
+        this._zone.runOutsideAngular(() => this._observer.observe(target));
 
-        // create a behavior subject subject
-        const subject = new BehaviorSubject<ResizeDimensions>({ width: nativeElement.offsetWidth, height: nativeElement.offsetHeight });
-
-        // determine the style of the element
-        const displayMode = window.getComputedStyle(nativeElement).getPropertyValue('display');
-
-        // create the iframe element
-        const iframe: HTMLIFrameElement = this._renderer.createElement('iframe');
-
-        // style the iframe to be invisible but fill containing element
-        this._renderer.setStyle(iframe, 'position', 'absolute');
-        this._renderer.setStyle(iframe, 'width', '100%');
-        this._renderer.setStyle(iframe, 'height', '100%');
-        this._renderer.setStyle(iframe, 'top', '0');
-        this._renderer.setStyle(iframe, 'right', '0');
-        this._renderer.setStyle(iframe, 'bottom', '0');
-        this._renderer.setStyle(iframe, 'left', '0');
-        this._renderer.setStyle(iframe, 'z-index', '-1');
-        this._renderer.setStyle(iframe, 'opacity', '0');
-        this._renderer.setStyle(iframe, 'border', 'none');
-        this._renderer.setStyle(iframe, 'margin', '0');
-        this._renderer.setStyle(iframe, 'pointer-events', 'none');
-        this._renderer.setStyle(iframe, 'overflow', 'hidden');
-
-        // ensure the iframe ignores any tabbing
-        this._renderer.setAttribute(iframe, 'tabindex', '-1');
-        this._renderer.setAttribute(iframe, 'aria-hidden', 'true');
-
-        // statically positioned elements need changed to relative for this method to work
-        if (displayMode !== 'relative' && displayMode !== 'absolute' && displayMode !== 'fixed') {
-            this._renderer.setStyle(nativeElement, 'position', 'relative');
+        if (this._targets.has(target)) {
+            return this._targets.get(target);
+        } else {
+            const emitter = new ReplaySubject<ResizeDimensions>();
+            this._targets.set(target, emitter);
+            return emitter;
         }
+    }
 
-        // add the iframe to the container element
-        this._renderer.appendChild(nativeElement, iframe);
+    removeResizeListener(target: HTMLElement): void {
+        this._observer.unobserve(target);
+    }
 
-        this.waitUntilReady(iframe, () => {
-            const iframeDoc = iframe.contentDocument || iframe.contentWindow.document as Document;
-
-            const attachListener = () => {
-
-                // watch for any future resizes - run inside ngzone as an iframe event listener is not patched
-                this._subscription.add(fromEvent(iframe.contentWindow, 'resize').subscribe(() =>
-                    this._ngZone.run(() => subject.next({ width: nativeElement.offsetWidth, height: nativeElement.offsetHeight }))
-                ));
-            };
-
-            if (iframeDoc.readyState === 'complete') {
-                attachListener();
-            } else {
-
-                // wait for iframe to load
-                iframe.addEventListener('load', () => attachListener());
+    private elementDidResize(entries: ResizeObserverEntry[]): void {
+        this._zone.run(() => {
+            for (const entry of entries) {
+                if (this._targets.has(entry.target as HTMLElement)) {
+                    const emitter = this._targets.get(entry.target as HTMLElement);
+                    emitter.next({ width: (entry.target as HTMLElement).offsetWidth, height: (entry.target as HTMLElement).offsetHeight });
+                }
             }
         });
-
-
-        return subject;
-    }
-
-    private waitUntilReady(iframe: HTMLIFrameElement, callback: () => void) {
-        if (iframe.contentDocument || iframe.contentWindow) {
-            callback.call(this);
-        } else {
-            setTimeout(() => this.waitUntilReady(iframe, callback));
-        }
     }
 }
 
