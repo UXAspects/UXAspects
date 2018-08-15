36873adf94f191315df617bebe7c6a7bd591d418
donna-marijne <donna.worby@hpe.com>
2018-08-15 10:20:34 +0100

    Revert "EL-3063 - WCAG Progress Bar (#792)" (#794)
    This reverts commit f2dd940197d2f669380f3410e2a10cbec790f9bc.

--- src/components/progress-bar/progress-bar.component.ts
+++ src/components/progress-bar/progress-bar.component.ts
@@ -1,23 +1,14 @@
-import { ChangeDetectionStrategy, Component, HostBinding, Input } from '@angular/core';
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
     selector: 'ux-progress-bar',
     templateUrl: './progress-bar.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    host: {
-        role: 'progressbar'
-    }
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProgressBarComponent {
     @Input() value: number = 0;
-    @Input() @HostBinding('attr.aria-valuemin') min: number = 0;
-    @Input() @HostBinding('attr.aria-valuemax') max: number = 100;
+    @Input() max: number = 100;
     @Input() indeterminate: boolean = false;
     @Input() trackColor: string;
     @Input() barColor: string;
-
-    /** When indeteminate we should omit the valuenow label */
-    @HostBinding('attr.aria-valuenow') get valueNow() {
-        return this.indeterminate ? null : this.value;
-    }
 }
