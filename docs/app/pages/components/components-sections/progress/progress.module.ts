de5e031f859a7d9294cc8947cfc083cf1aea287d
donna-marijne <donna.worby@hpe.com>
2018-08-22 09:41:16 +0100

    WCAG Progress (#804)
    * Corrected the text slightly.
    * Switched example code tabset to accessible version.

--- docs/app/pages/components/components-sections/progress/progress.module.ts
+++ docs/app/pages/components/components-sections/progress/progress.module.ts
@@ -1,7 +1,6 @@
 import { ComponentFactoryResolver, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { TabsModule } from 'ngx-bootstrap/tabs';
-import { ProgressBarModule } from '../../../../../../src/index';
+import { ProgressBarModule, TabsetModule } from '../../../../../../src/index';
 import { DocumentationComponentsModule } from '../../../../components/components.module';
 import { DocumentationCategoryComponent } from '../../../../components/documentation-category/documentation-category.component';
 import { DocumentationPage, ResolverService } from '../../../../services/resolver/resolver.service';
@@ -27,11 +26,11 @@ const ROUTES = [
 
 @NgModule({
     imports: [
-        WrappersModule,
-        TabsModule,
-        ProgressBarModule,
         DocumentationComponentsModule,
-        RouterModule.forChild(ROUTES)
+        ProgressBarModule,
+        RouterModule.forChild(ROUTES),
+        TabsetModule,
+        WrappersModule,
     ],
     exports: SECTIONS,
     declarations: SECTIONS,
