08671f56474ef26f2bd17b5f7708451f29ddb778
donna-marijne <donna.worby@hpe.com>
2018-08-24 20:22:25 +0100

    Color picker corrections
    Fixed build errors
    Updated documentation
    Fixed showTooltips option
    Update aria-label
    Fixed inputSubmit issue

--- src/components/color-picker/color-picker.module.ts
+++ src/components/color-picker/color-picker.module.ts
@@ -1,8 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AccessibilityModule } from '../../directives/accessibility/accessibility.module';
-import { NumberPickerModule } from '../number-picker/number-picker.module';
+import { AccessibilityModule } from '../../directives/accessibility';
+import { NumberPickerModule } from '../number-picker';
 import { TooltipModule } from '../tooltip';
 import { ColorPickerComponent } from './color-picker.component';
 
