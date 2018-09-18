a424420dc11ee69d55a857992dfb271a71dd600f
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2018-09-18 12:09:14 +0100

    Refactoring and adding outside click disabling functionality

--- src/ng1/directives/displayPanels/displayPanelItem/displayPanelItem.directive.js
+++ src/ng1/directives/displayPanels/displayPanelItem/displayPanelItem.directive.js
@@ -1,6 +1,6 @@
-displayPanelItem.$inject = ["$displayPanel", "keyboardService", "$timeout"];
+DisplayPanelItemDirective.$inject = ['$displayPanel', 'keyboardService', '$timeout'];
 
-export default function displayPanelItem($displayPanel, keyboardService, $timeout) {
+export function DisplayPanelItemDirective($displayPanel, keyboardService, $timeout) {
   return {
     restrict: 'A',
     scope: true,
@@ -8,47 +8,39 @@ export default function displayPanelItem($displayPanel, keyboardService, $timeou
       scope.displayPanelItem = scope.$eval(attrs.displayPanelItem);
       scope.shadow = scope.$eval(attrs.shadow);
 
-      var showDisplayPanel = function(){
-        var previous = (element.is(':first-child')) ? true : false;
-        var next = (element.is(':last-child')) ? true : false;
+      const showDisplayPanel = function () {
+        const isFirst = (element.is(':first-child')) ? true : false;
+        const isLast = (element.is(':last-child')) ? true : false;
 
-        $displayPanel.open(element, scope.displayPanelItem, scope.shadow, previous, next);          
+        $displayPanel.open(element, scope.displayPanelItem, scope.shadow, isFirst, isLast);
       };
 
-      var hideDisplayPanel = function(){
-        $displayPanel.close(scope.displayPanelItem);          
-      };
+      const hideDisplayPanel = () => $displayPanel.close(scope.displayPanelItem);
 
       //update the panel if it visible
-      var updatePanel = function() {
+      const updatePanel = function () {
         if ($displayPanel.panelOpen() && !$displayPanel.panelHidden()) {
-          $timeout( function() {
-            showDisplayPanel();
-          });
+          $timeout(() => showDisplayPanel());
         }
       };
 
+      // this will be the item we are focusing on
+      let goToItem = element;
+
       //listen for click
-      element[0].addEventListener("click", function() {
-        $timeout(function() {
-          showDisplayPanel();  
-        });          
-      });
+      element[0].addEventListener('click', () => $timeout(() => showDisplayPanel()));
 
       //listen for focus
-      element[0].addEventListener("focus", updatePanel);
+      element[0].addEventListener('focus', updatePanel);
 
       // down key press - go to next item
-      keyboardService.keydown(element, 40, function(evt) {
+      keyboardService.keydown(element, 40, function (evt) {
         evt.preventDefault();
         goToNext();
       });
 
-      // this will be the item we are focusing on
-      var goToItem = element;
-      
       // up keypress - go to previous item
-      keyboardService.keydown(element, 38, function(evt) {
+      keyboardService.keydown(element, 38, function (evt) {
         evt.preventDefault();
         goToPrevious();
       });
@@ -60,7 +52,7 @@ export default function displayPanelItem($displayPanel, keyboardService, $timeou
       }
 
       // down key press - go to next item
-      keyboardService.keydown(element, 40, function(evt) {
+      keyboardService.keydown(element, 40, function (evt) {
         evt.preventDefault();
         goToNext();
       });
@@ -73,22 +65,22 @@ export default function displayPanelItem($displayPanel, keyboardService, $timeou
 
       // when clicking Previous button go to previous item
       scope.$on('$displayPanelPrevious', function () {
-        if($displayPanel.getCurrentPanel() === element){
+        if ($displayPanel.getCurrentPanel() === element) {
           goToPrevious();
         }
       });
 
       // when clicking Next button go to next item
       scope.$on('$displayPanelNext', function () {
-        if($displayPanel.getCurrentPanel() === element){
+        if ($displayPanel.getCurrentPanel() === element) {
           goToNext();
         }
       });
 
       // pg up key press - go to 10th previous item or 1st item if not possible
-      keyboardService.keydown(element, 33, function(evt) {
+      keyboardService.keydown(element, 33, function (evt) {
         evt.preventDefault();
-        var itemToJumpTo = element.prevAll('[display-panel-item]');
+        const itemToJumpTo = element.prevAll('[display-panel-item]');
         if (itemToJumpTo.length) {
           goToItem = element.prevAll('[display-panel-item]:eq(10)').length ? element.prevAll('[display-panel-item]:eq(10)') : itemToJumpTo[itemToJumpTo.length - 1];
           goToItem.focus();
@@ -96,9 +88,9 @@ export default function displayPanelItem($displayPanel, keyboardService, $timeou
       });
 
       // pg down key press - go to 10th next item or last item if not possible
-      keyboardService.keydown(element, 34, function(evt) {
+      keyboardService.keydown(element, 34, function (evt) {
         evt.preventDefault();
-        var itemToJumpTo = element.nextAll('[display-panel-item]');
+        const itemToJumpTo = element.nextAll('[display-panel-item]');
         if (itemToJumpTo.length) {
           goToItem = element.nextAll('[display-panel-item]:eq(10)').length ? element.nextAll('[display-panel-item]:eq(10)') : itemToJumpTo[itemToJumpTo.length - 1];
           goToItem.focus();
@@ -106,40 +98,34 @@ export default function displayPanelItem($displayPanel, keyboardService, $timeou
       });
 
       // home key press - go to the first item
-      keyboardService.keydown(element, 36, function(evt) {
+      keyboardService.keydown(element, 36, function (evt) {
         evt.preventDefault();
         goToItem = element.prevAll('[display-panel-item]:last');
         goToItem.focus();
       });
 
       // end key press - go to the last item
-      keyboardService.keydown(element, 35, function(evt) {
+      keyboardService.keydown(element, 35, function (evt) {
         evt.preventDefault();
         goToItem = element.nextAll('[display-panel-item]:last');
         goToItem.focus();
       });
 
       // esc key press - if display panel is open, close it. Otherwise blur focus on item
-      keyboardService.keydown(element, 27, function(evt) {
+      keyboardService.keydown(element, 27, function (evt) {
         evt.preventDefault();
         if ($displayPanel.panelOpen() && !$displayPanel.panelHidden()) {
           hideDisplayPanel();
         } else {
           element.blur();
-        }  
+        }
       });
 
-      // enter key press - show the display panel 
-      keyboardService.keydown(element, 13, function() {
-        $timeout( function() {
-          showDisplayPanel();
-        });
-      });
+      // enter key press - show the display panel
+      keyboardService.keydown(element, 13, () => $timeout(() => showDisplayPanel()));
 
-      // spacebar press - show the display panel 
-      keyboardService.keydown(element, 32, function(evt) {
-        evt.preventDefault();
-      });
+      // spacebar press - show the display panel
+      keyboardService.keydown(element, 32, evt => evt.preventDefault());
 
     }
   };
--- src/ng1/directives/displayPanels/displayPanels.module.js
+++ src/ng1/directives/displayPanels/displayPanels.module.js
@@ -1,9 +1,9 @@
-import DisplayPanelService from './displayPanel.service.js';
-import DisplayPanelDirective from './displayPanel/displayPanel.directive.js';
-import DisplayPanelItemDirective from './displayPanelItem/displayPanelItem.directive.js';
-import '../../services/keyboardService/keyboardService.module.js';
-  
+import '../../services/keyboardService/keyboardService.module';
+import { DisplayPanelService } from './displayPanel.service';
+import { DisplayPanelDirective } from './displayPanel/displayPanel.directive';
+import { DisplayPanelItemDirective } from './displayPanelItem/displayPanelItem.directive';
+
 angular.module("ux-aspects.displayPanels", ['ux-aspects.keyboardService'])
-	.factory('$displayPanel', DisplayPanelService)
+	.service('$displayPanel', DisplayPanelService)
 	.directive('displayPanel', DisplayPanelDirective)
 	.directive('displayPanelItem', DisplayPanelItemDirective);
