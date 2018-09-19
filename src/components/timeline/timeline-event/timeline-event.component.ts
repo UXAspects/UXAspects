27d895f51aee1af3521b6d8b09524d904ffe2da5
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-09-19 11:09:48 +0100

    EL-3071 - WCAG-Timeline (#830)

--- src/components/timeline/timeline-event/timeline-event.component.ts
+++ src/components/timeline/timeline-event/timeline-event.component.ts
@@ -1,11 +1,14 @@
 import { Component, Input } from '@angular/core';
 
+let uniqueId: number = 0;
+
 @Component({
     selector: 'ux-timeline-event',
     templateUrl: './timeline-event.component.html'
 })
 export class TimelineEventComponent {
 
+    @Input() id: string = `ux-timeline-event-${uniqueId++}`;
     @Input() badgeColor: string;
     @Input() badgeTitle: string;
 }
