54bc1c41508795acb2208e1b653e330930c71b40
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-10-03 14:18:55 +0100

    EL-3075 - Utilities Documentation (#843)
    * Initial Commit
    
    * EL-3075 - Document Utilities
    
    * Making review changes

---
+++ docs/app/pages/components/components-sections/utilities/focus/focus.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { BaseDocumentationSection } from '../../../../../components/base-documentation-section/base-documentation-section';
+import { DocumentationSectionComponent } from '../../../../../decorators/documentation-section-component';
+import { IPlunk } from '../../../../../interfaces/IPlunk';
+import { IPlunkProvider } from '../../../../../interfaces/IPlunkProvider';
+
+@Component({
+    selector: 'uxd-components-focus',
+    templateUrl: './focus.component.html',
+    styleUrls: ['./focus.component.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+@DocumentationSectionComponent('ComponentsFocusComponent')
+export class ComponentsFocusComponent extends BaseDocumentationSection implements IPlunkProvider {
+
+    focus: boolean = false;
+
+    plunk: IPlunk = {
+        files: {
+            'app.component.html': this.snippets.raw.appHtml,
+            'app.component.ts': this.snippets.raw.appTs,
+            'app.component.css': this.snippets.raw.appCss,
+        },
+        modules: [
+            {
+                imports: ['AccessibilityModule'],
+                library: '@ux-aspects/ux-aspects'
+            }
+        ]
+    };
+
+    constructor() {
+        super(require.context('./snippets/', false, /\.(html|css|js|ts)$/));
+    }
+}
--- docs/app/pages/components/components-sections/utilities/utilities.module.ts
+++ docs/app/pages/components/components-sections/utilities/utilities.module.ts
@@ -1,6 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { ComponentFactoryResolver, NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FocusIfModule, TabsetModule } from '../../../../../../src/index';
+import { AccessibilityModule, FocusIfModule, TabsetModule } from '../../../../../../src/index';
 import { DocumentationComponentsModule } from '../../../../components/components.module';
 import { DocumentationCategoryComponent } from '../../../../components/documentation-category/documentation-category.component';
 import { DocumentationPage, ResolverService } from '../../../../services/resolver/resolver.service';
@@ -10,12 +11,14 @@ import { ComponentsColorServiceComponent } from './color-service/color-service.c
 import { ComponentsExpandingContentNg1Component } from './expanding-content-ng1/expanding-content-ng1.component';
 import { ComponentsFocusIfComponent } from './focus-if/focus-if.component';
 import { ComponentsFocusOnShowNg1Component } from './focus-on-show-ng1/focus-on-show-component';
+import { ComponentsFocusComponent } from './focus/focus.component';
 import { ComponentsForceFocusNg1Component } from './force-focus-ng1/force-focus-ng1.component';
 import { ComponentsListItemFilterNg1Component } from './list-item-filter-ng1/list-item-filter-ng1.component';
 import { ComponentsPdfServiceNg1Component } from './pdf-service-ng1/pdf-service-ng1.component';
 import { ComponentsPersistentDataServiceComponent } from './persistent-data-service/persistent-data-service.component';
 import { ComponentsSafeIntervalNg1Component } from './safe-interval-ng1/safe-interval-ng1.component';
 import { ComponentsSafeTimeoutNg1Component } from './safe-timeout-ng1/safe-timeout-ng1.component';
+import { ComponentsTabbableListComponent } from './tabbable-list/tabbable-list.component';
 import { ComponentsTimeAgoServiceNg1Component } from './time-ago-service-ng1/time-ago-service-ng1.component';
 import { ComponentsWindowCommunicationServiceNg1Component } from './window-communication-service-ng1/window-communication-service-ng1.component';
 
@@ -34,6 +37,8 @@ const SECTIONS = [
     ComponentsListItemFilterNg1Component,
     ComponentsWindowCommunicationServiceNg1Component,
     ComponentsPersistentDataServiceComponent,
+    ComponentsTabbableListComponent,
+    ComponentsFocusComponent
 ];
 
 const ROUTES = [
@@ -50,7 +55,9 @@ const ROUTES = [
     imports: [
         WrappersModule,
         TabsetModule,
+        CommonModule,
         FocusIfModule,
+        AccessibilityModule,
         DocumentationComponentsModule,
         RouterModule.forChild(ROUTES)
     ],
