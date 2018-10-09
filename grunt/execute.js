3a6a1d3297899e00955d2ef836f52307f20edabc
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2018-10-09 12:40:21 +0100

    Adding initial versioning script

--- grunt/execute.js
+++ grunt/execute.js
@@ -1,19 +1,20 @@
-const path = require('path');
+const { join } = require('path');
+const { cwd } = require('process');
 
 module.exports = {
     iconset: {
-        src: [ path.join(process.cwd(), 'scripts', 'iconset.js') ]
+        src: [ join(cwd(), 'scripts', 'iconset.js') ]
     },
     licenses: {
-        src: [ path.join(process.cwd(), 'scripts', 'licenses.js') ]
+        src: [ join(cwd(), 'scripts', 'licenses.js') ]
     },
     less: {
-        src: [ path.join(process.cwd(), 'scripts', 'inline-less.js') ]
+        src: [ join(cwd(), 'scripts', 'inline-less.js') ]
     },
     shim: {
-        src: [ path.join(process.cwd(), 'scripts', 'shim.js') ]
+        src: [ join(cwd(), 'scripts', 'shim.js') ]
     },
     protractor: {
-        src: [ path.join(process.cwd(), 'scripts', 'protractor.js') ]
+        src: [ join(cwd(), 'scripts', 'protractor.js') ]
     }
 };
