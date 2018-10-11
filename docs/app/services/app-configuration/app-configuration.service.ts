00810fbb4ba1a3874cedf5295fa2bb66c67c9248
donna-marijne <donna.worby@hpe.com>
2018-10-11 14:39:25 +0100

    Release 1.6.5
    To accommodate the Stack B hosting of the site, as well as future CI builds, I have removed the explicit `assetsUrl` config setting and instead derived the URL from the active location.
    
    This also means that plunker can load in Firefox in development mode by accessing the documentation at http://127.0.0.1:8080.

--- docs/app/services/app-configuration/app-configuration.service.ts
+++ docs/app/services/app-configuration/app-configuration.service.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 const jsonTemplate = require('json-templater/object');
@@ -5,46 +6,67 @@ const jsonTemplate = require('json-templater/object');
 @Injectable()
 export class AppConfiguration {
 
-    private _data = {};
-
-    private _templateVars = {
-        'VERSION': environment.version
-    };
-
     public documentationPages = ['components-page', 'css-page', 'charts-page'];
 
-    constructor() {
+    get version(): string {
+        return this._config['version'];
+    }
 
+    get assetsUrl(): string {
+        if (!this._config['assetsUrl']) {
+            // If not configured, derive from the application's base URL.
+            this._config['assetsUrl'] = Location.joinWithSlash(this.getBaseUrl(), 'assets');
+        }
+        return this._config['assetsUrl'];
+    }
+
+    get codePen(): string {
+        return this._config['codePen'];
+    }
+
+    get plunker(): string {
+        return this._config['plunker'];
+    }
+
+    private _data = {};
+    private _config: {[key: string]: any};
+
+    private _templateVars = {
+        VERSION: environment.version
+    };
+
+    constructor(private _location: Location) {
         this.setConfigurationTemplateData('config', require('../../data/config.json'));
         this.setConfigurationTemplateData('config.dev', require('../../data/config.dev.json'));
         this.setConfigurationTemplateData('footer-navigation', require('../../data/footer-navigation.json'));
         this.setConfigurationTemplateData('landing-page', require('../../data/landing-page.json'));
-        this.setConfigurationData('team-page',  require('../../data/team-page.json'));
-        this.setConfigurationData('top-navigation',  require('../../data/top-navigation.json'));
-        this.setConfigurationData('components-page',  require('../../data/components-page.json'));
-        this.setConfigurationData('css-page',  require('../../data/css-page.json'));
-        this.setConfigurationData('charts-page',  require('../../data/charts-page.json'));
+        this.setConfigurationData('team-page', require('../../data/team-page.json'));
+        this.setConfigurationData('top-navigation', require('../../data/top-navigation.json'));
+        this.setConfigurationData('components-page', require('../../data/components-page.json'));
+        this.setConfigurationData('css-page', require('../../data/css-page.json'));
+        this.setConfigurationData('charts-page', require('../../data/charts-page.json'));
+
+        this._config = environment.production ? this._data['config'] : this._data['config.dev'];
     }
 
     get(key: string): any {
-        return key.split('.').reduce((prev, curr) => {
-            return prev ? prev[curr] : undefined;
-        }, this.getConfig());
+        return this._config[key];
     }
 
-    getConfigurationData(key: string) {
+    getConfigurationData(key: string): any {
         return this._data[key];
     }
 
-    setConfigurationData(key: string, data: any) {
+    setConfigurationData(key: string, data: any): void {
         this._data[key] = data;
     }
 
-    setConfigurationTemplateData(key: string, data: any) {
+    setConfigurationTemplateData(key: string, data: any): void {
         this._data[key] = jsonTemplate(data, this._templateVars);
     }
 
-    private getConfig() {
-        return environment.production ? this._data['config'] : this._data['config.dev'];
+    private getBaseUrl(): string {
+        const path = this._location.prepareExternalUrl(this._location.path());
+        return window.location.href.substr(0, window.location.href.indexOf(path));
     }
 }
