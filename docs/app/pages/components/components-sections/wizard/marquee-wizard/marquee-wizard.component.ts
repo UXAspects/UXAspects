8605570e04f8d7243280f9517dbed6d4c95f51dd
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-10-12 14:36:20 +0100

    Finishing wizard wcag

--- docs/app/pages/components/components-sections/wizard/marquee-wizard/marquee-wizard.component.ts
+++ docs/app/pages/components/components-sections/wizard/marquee-wizard/marquee-wizard.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { Validators, FormControl } from '@angular/forms';
-import { DocumentationSectionComponent } from '../../../../../decorators/documentation-section-component';
+import { FormControl, Validators } from '@angular/forms';
 import { BaseDocumentationSection } from '../../../../../components/base-documentation-section/base-documentation-section';
-import { IPlunkProvider } from '../../../../../interfaces/IPlunkProvider';
+import { DocumentationSectionComponent } from '../../../../../decorators/documentation-section-component';
 import { IPlunk } from '../../../../../interfaces/IPlunk';
+import { IPlunkProvider } from '../../../../../interfaces/IPlunkProvider';
 
 @Component({
     selector: 'uxd-components-marquee-wizard',
@@ -18,7 +18,7 @@ export class ComponentsMarqueeWizardComponent extends BaseDocumentationSection i
     validate: boolean = false;
     modalOpen: boolean = false;
     requiredText = new FormControl('', Validators.required);
-    
+
     plunk: IPlunk = {
         files: {
             'app.component.html': this.snippets.raw.appHtml,
@@ -34,6 +34,10 @@ export class ComponentsMarqueeWizardComponent extends BaseDocumentationSection i
             {
                 imports: ['MarqueeWizardModule', 'CheckboxModule', 'FocusIfModule'],
                 library: '@ux-aspects/ux-aspects'
+            },
+            {
+                imports: ['A11yModule'],
+                library: '@angular/cdk/a11y'
             }
         ]
     };
