08b531b4529bf836898bba1949ac5abb3a455985
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-10-12 16:54:17 +0100

    EL-3054 - Hierarchy Bar WCAG

--- src/components/hierarchy-bar/hierarchy-bar.module.ts
+++ src/components/hierarchy-bar/hierarchy-bar.module.ts
@@ -1,16 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { AccessibilityModule } from '../../directives/accessibility/index';
 import { FocusIfModule } from '../../directives/focus-if/index';
 import { ResizeModule } from '../../directives/resize/index';
-import { HierarchyBarComponent } from './hierarchy-bar.component';
 import { PopoverModule } from '../popover/index';
+import { HierarchyBarComponent } from './hierarchy-bar.component';
 
 @NgModule({
     imports: [
         CommonModule,
         ResizeModule,
         FocusIfModule,
-        PopoverModule
+        PopoverModule,
+        AccessibilityModule
     ],
     exports: [HierarchyBarComponent],
     declarations: [HierarchyBarComponent],
