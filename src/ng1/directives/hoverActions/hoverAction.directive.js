f3a415927195334633a7122aee252824eeb06321
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-10-18 15:47:25 +0100

    EL-3255 - Hover action disabled

--- src/ng1/directives/hoverActions/hoverAction.directive.js
+++ src/ng1/directives/hoverActions/hoverAction.directive.js
@@ -1,42 +1,71 @@
+import { ENTER, LEFT_ARROW, RIGHT_ARROW } from '@angular/cdk/keycodes';
+
 export default function hoverAction() {
   return {
     restrict: 'E',
     replace: true,
     template: require('./hoverAction.html'),
     scope: {
-      icon: "@",
-      name: "@",
-      click: "&"
+      icon: '@',
+      name: '@',
+      click: '&',
+      disabled: '=?'
     },
     require: '^hoverActions',
-    link: function (scope, element, attrs, hoverActionCtrl) {
+    /**
+     * @param {ng.IScope} scope
+     * @param {JQuery} element
+     * @param {ng.IAttributes} attrs
+     * @param {*} hoverActionCtrl
+     */
+    link: function (scope, element, _attrs, hoverActionCtrl) {
 
-      /*
-        Properties
-      */
-      var container = element.parent();
+      const container = element.parent();
 
-      /*
-        Bind to events
-      */
-      element.focus(onFocus);
-      element.blur(onBlur);
-      element.keydown(onKeyDown);
-      element.click(onClick);
+      // setup the component
+      onInit();
 
-      /*
-        Event handlers
-      */
+      // on component destroy cleanup
+      scope.$on('$destroy', onDestroy);
 
-      function onClick(evt) {
-        //evaluate the function
-        scope.click();
+      // apply a class if the item is disabled
+      scope.$watch('disabled', isDisabled => {
+        // add or remove the class accordingly
+        isDisabled ? element.addClass('hover-action-disabled') : element.removeClass('hover-action-disabled');
+
+        // set the tab index accordingly
+        element.attr('tabindex', isDisabled ? '-1' : '0');
+      });
+
+      function onInit() {
+        // attach event handlers
+        element.on('focus', onFocus);
+        element.on('blur', onBlur);
+        element.on('keydown', onKeyDown);
+        element.on('click', onClick);
+      }
+
+      function onDestroy() {
+        element.off('focus', onFocus);
+        element.off('blur', onBlur);
+        element.off('keydown', onKeyDown);
+        element.off('click', onClick);
+      }
+
+
+      function onClick(event) {
+
+        // if we are disabled force focus
+        if (!scope.disabled) {
+          //evaluate the function
+          scope.click();
+        }
 
         //no longer force focus
         container.removeClass('action-hovered');
 
-        evt.stopPropagation();
-        evt.preventDefault();
+        event.stopPropagation();
+        event.preventDefault();
 
       }
 
@@ -48,40 +77,62 @@ export default function hoverAction() {
         container.removeClass('action-hovered');
       }
 
-      function onKeyDown(evt) {
-        //if left arrow key is pressed
-        if (evt.keyCode === 37) {
-          //try and select the previous action if there is one
-          var previousBtn = element.prev('.hover-action').focus();
+      function onKeyDown(event) {
 
-          //if there are no more previous elements, highlight the hover element again
-          if (previousBtn.length === 0) {
-            hoverActionCtrl.hoverElement.focus();
-            container.removeClass('action-hovered');
+        switch (event.which) {
+
+          case LEFT_ARROW: {
+
+            // get the previous sibling if there is one and focus it
+            const sibling = getSibling(false).focus();
+
+            //if there are no more previous elements, highlight the hover element again
+            if (sibling.length === 0) {
+              hoverActionCtrl.hoverElement.focus();
+              container.removeClass('action-hovered');
+            }
+
+            event.stopPropagation();
+            event.preventDefault();
+            break;
           }
 
-          evt.stopPropagation();
-          evt.preventDefault();
+          case RIGHT_ARROW: {
+            // get the next sibling if there is one and focus it
+            getSibling(true).focus();
 
+            event.stopPropagation();
+            event.preventDefault();
+            break;
+          }
+
+          case ENTER: {
+            //evaluate the function - unlike click retain focus
+            if (!scope.disabled) {
+              scope.click();
+            }
+
+            event.stopPropagation();
+            event.preventDefault();
+            break;
+          }
+        }
+      }
+
+      /**
+       * @param {boolean} isNext
+       * @param {JQuery} source
+       */
+      function getSibling(isNext, source = element) {
+        // get the sibling
+        const sibling = isNext ? source.next('.hover-action') : source.prev('.hover-action');
+
+        // check if the sibling is disabled
+        if (sibling.attr('disabled')) {
+          return getSibling(isNext, sibling);
         }
 
-        //if right arrow key is pressed
-        if (evt.keyCode === 39) {
-          //try and select the next action if there is one
-          element.next('.hover-action').focus();
-          evt.stopPropagation();
-          evt.preventDefault();
-
-        }
-
-        //if return key was pressed
-        if (evt.keyCode === 13) {
-          //evaluate the function - unlike click retain focus
-          scope.click();
-
-          evt.stopPropagation();
-          evt.preventDefault();
-        }
+        return sibling;
       }
     }
   };
