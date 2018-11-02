9c291b5f7da9aa7619c6e849afe00de0fb7017dd
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2018-11-02 17:36:02 +0000

    Fixing up unit tests

--- src/ng1/directives/selectTable/selectTable.spec.js
+++ src/ng1/directives/selectTable/selectTable.spec.js
@@ -1,63 +1,46 @@
 describe('Select table', function () {
     var $compile, $rootScope, $scope, $timeout;
     var vm = {};
-    var element, objectElement;
+    var element;
+
+    var authors = [
+        "Dale Holmes",
+        "Emily Cain",
+        "Marsha Glover",
+        "Vanessa Barrett",
+        "Sarah Lyons",
+        "Johanna Cobb",
+        "Greg Watson",
+        "Frankie Young",
+        "Phil Garcia",
+        "Levi Smith",
+        "Aaron Morgan",
+        "Lucy Gill"
+    ];
+
+    var users = [{
+            id: 1,
+            name: "Dale"
+        }, {
+            id: 2,
+            name: "Emily"
+        }, {
+            id: 3,
+            name: "Sarah"
+        },
+        {
+            id: 4,
+            name: "Phil"
+        }, {
+            id: 5,
+            name: "Frankie"
+        }, {
+            id: 6,
+            name: "Vanessa"
+        }
+    ];
 
     beforeEach(module("ux-aspects.selectTable"));
-
-    function getVM() {
-        var vm = {};
-        vm.clear = "clear";
-        vm.reselect = "reselect";
-        vm.selectedVal = "";
-        vm.tableId = "example-table";
-        vm.searchText = "";
-        vm.heading = "Select an author";
-        vm.selectKey = "name";
-        vm.authors = [
-            "Dale Holmes",
-            "Emily Cain",
-            "Marsha Glover",
-            "Vanessa Barrett",
-            "Sarah Lyons",
-            "Johanna Cobb",
-            "Greg Watson",
-            "Frankie Young",
-            "Phil Garcia",
-            "Levi Smith",
-            "Aaron Morgan",
-            "Lucy Gill"
-
-        ];
-        vm.users = [{
-                id: 1,
-                name: "Dale"
-            }, {
-                id: 2,
-                name: "Emily"
-            }, {
-                id: 3,
-                name: "Sarah"
-            },
-            {
-                id: 4,
-                name: "Phil"
-
-            }, {
-                id: 5,
-                name: "Frankie"
-
-            }, {
-                id: 6,
-                name: "Vanessa"
-
-            }, {
-
-            }
-        ];
-
-        return vm;
-    }
 
     beforeEach(inject(function (_$compile_, _$rootScope_, _$timeout_) {
         $compile = _$compile_;
@@ -68,28 +51,36 @@ describe('Select table', function () {
 
     describe("Select table control directive", function () {
 
-        beforeEach(function () {
-            vm = {};
-            vm = getVM();
+        function createSelectTable(values, mode, selectKey) {
+            vm = {
+                tableId: 'example-table',
+                heading: 'Select an author',
+                selectedVal: '',
+                searchText: '',
+                values,
+                selectKey,
+                mode
+            };
 
             $scope.scopeValues = vm;
 
-            var html = '<select-table values="scopeValues.authors" selected="scopeValues.selectedVal" id="scopeValues.tableId" search-text="scopeValues.searchText" table-height="300"></select-table>';
+            var html = '<select-table values="scopeValues.values" selected="scopeValues.selectedVal" id="scopeValues.tableId" search-text="scopeValues.searchText" select-key="scopeValues.selectKey" select-hidden-items="scopeValues.mode" table-height="300"></select-table>';
             element = $compile(html)($scope);
-            var objectHtml = '<select-table values="scopeValues.users" selected="scopeValues.selectedVal" id="scopeValues.tableId" select-key="scopeValues.selectKey" search-text="scopeValues.searchText" table-height="300"></select-table>';
-            objectElement = $compile(objectHtml)($scope);
             $timeout.flush();
             $scope.$digest();
-        });
+
+            return vm;
+        }
 
         it('should create the select table with options provided', function () {
+            createSelectTable(authors);
             expect(element.find('tr')[0].innerText.trim()).toBe("Dale Holmes");
             expect(element.find('tr')[3].innerText.trim()).toBe("Vanessa Barrett");
 
         });
 
         it('should update the selected value to the selected attribute', function () {
-
+            var vm = createSelectTable(authors);
             var e = document.createEvent('MouseEvents');
             e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
             element.find('tr')[4].dispatchEvent(e);
@@ -97,6 +88,9 @@ describe('Select table', function () {
         });
 
         it('should filter the options according to the search text', function () {
+
+            var vm = createSelectTable(authors);
+
             $scope.$apply(function () {
                 vm.searchText = "na";
             });
@@ -108,33 +102,23 @@ describe('Select table', function () {
         });
 
         it('should select items hidden by the text filter by default', function () {
-            expect(element.children().eq(0).scope().vm.selectFilteredItems).toBeTruthy();
+            createSelectTable(authors);
+            expect(element.children().eq(0).scope().vm._keepFilteredItemsSelected).toBeTruthy();
         });
 
         it('should deselect items hidden by the text filter by configuration', function () {
-            var html = '<select-table values="scopeValues.authors" selected="scopeValues.selectedVal" id="scopeValues.tableId" search-text="scopeValues.searchText" table-height="300" select-hidden-items="scopeValues.clear"></select-table>';
-            var element = $compile(html)($scope);
-            $timeout.flush();
-            $scope.$digest();
-
-            expect(element.children().eq(0).scope().vm.selectFilteredItems).toBeFalsy();
+            createSelectTable(authors, 'clear');
+            expect(element.children().eq(0).scope().vm._keepFilteredItemsSelected).toBeFalsy();
         });
 
         it('should reselect items hidden by the text filter by configuration', function () {
-            var html = '<select-table values="scopeValues.authors" selected="scopeValues.selectedVal" id="scopeValues.tableId" search-text="scopeValues.searchText" table-height="300" select-hidden-items="scopeValues.reselect"></select-table>';
-            var element = $compile(html)($scope);
-            $timeout.flush();
-            $scope.$digest();
-
-            expect(element.children().eq(0).scope().vm.selectFilteredItems).toBeFalsy();
-            expect(element.children().eq(0).scope().vm.reselectFilteredItems).toBeTruthy();
+            createSelectTable(authors, 'reselect');
+            expect(element.children().eq(0).scope().vm._keepFilteredItemsSelected).toBeFalsy();
+            expect(element.children().eq(0).scope().vm._reselectFilteredItems).toBeTruthy();
         });
 
         it('should, during filtering, deselect the selected value if no longer visible, when configured to do so', function () {
-            var html = '<select-table values="scopeValues.authors" selected="scopeValues.selectedVal" id="scopeValues.tableId" search-text="scopeValues.searchText" table-height="300" select-hidden-items="scopeValues.clear"></select-table>';
-            var element = $compile(html)($scope);
-            $timeout.flush();
-            $scope.$digest();
+            var vm = createSelectTable(authors, 'clear');
 
             var e = document.createEvent('MouseEvents');
             e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
@@ -150,14 +134,13 @@ describe('Select table', function () {
         });
 
         it('should reselect a filtered item if configured to do so', function () {
-            var html = '<select-table values="scopeValues.authors" selected="scopeValues.selectedVal" id="scopeValues.tableId" search-text="scopeValues.searchText" table-height="300" select-hidden-items="scopeValues.reselect"></select-table>';
-            var element = $compile(html)($scope);
-            $timeout.flush();
-            $scope.$digest();
+
+            var vm = createSelectTable(authors, 'reselect');
 
             var e = document.createEvent('MouseEvents');
             e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
             element.find('tr')[3].dispatchEvent(e);
+
             expect(vm.selectedVal).toBeTruthy();
 
             $scope.$apply(function () {
@@ -166,6 +149,7 @@ describe('Select table', function () {
 
             expect(vm.selectedVal).toBeFalsy();
 
+            debugger;
             $scope.$apply(function () {
                 vm.searchText = "";
             });
@@ -175,6 +159,8 @@ describe('Select table', function () {
         });
 
         it('should, during filtering, retain the selected value if no longer visible, when configured to do so', function () {
+            var vm = createSelectTable(authors);
+
             var e = document.createEvent('MouseEvents');
             e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
             element.find('tr')[3].dispatchEvent(e);
@@ -189,40 +175,47 @@ describe('Select table', function () {
         });
 
         it('should deselect the value when selected value is clicked again', function () {
+            var vm = createSelectTable(authors);
+
             //Select a value by clicking a row
             var e = document.createEvent('MouseEvents');
             e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
             element.find('tr')[3].dispatchEvent(e);
+
             expect(vm.selectedVal).toBe("Vanessa Barrett");
             //Click on the same row and check if it is deselected
             element.find('tr')[3].dispatchEvent(e);
-            expect(vm.selectedVal).toBe("");
+            expect(vm.selectedVal).toBeFalsy();
 
         });
 
         //Below are the unit tests for values passed as array of objects
 
         it('should create the select table with the values from select key provided', function () {
-            expect(objectElement.find('tr')[0].innerText.trim()).toBe("Dale");
-            expect(objectElement.find('tr')[3].innerText.trim()).toBe("Phil");
+            createSelectTable(users, '', 'name');
+
+            expect(element.find('tr')[0].innerText.trim()).toBe("Dale");
+            expect(element.find('tr')[3].innerText.trim()).toBe("Phil");
 
         });
 
         it('should update the selected value to the selected attribute when options are passed as array of objects', function () {
-
+            var vm = createSelectTable(users, '', 'name');
             var e = document.createEvent('MouseEvents');
             e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-            objectElement.find('tr')[4].dispatchEvent(e);
+            element.find('tr')[4].dispatchEvent(e);
             expect(vm.selectedVal.id).toBe(5);
             expect(vm.selectedVal.name).toBe("Frankie");
 
         });
 
         it('should filter the options according to the search text when options are passed as array of objects', function () {
+            var vm = createSelectTable(users, '', 'name');
+
             $scope.$apply(function () {
                 vm.searchText = "ar";
             });
-            var filteredRows = objectElement.find('tr');
+            var filteredRows = element.find('tr');
             angular.forEach(filteredRows, function (row) {
                 expect(row.innerText.trim().toLowerCase()).toContain('ar');
             });
@@ -230,14 +223,16 @@ describe('Select table', function () {
         });
 
         it('should deselect the value when selected value is clicked again when options are passed as array of objects', function () {
+            var vm = createSelectTable(users, '', 'name');
+
             //Select a value by clicking a row
             var e = document.createEvent('MouseEvents');
             e.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-            objectElement.find('tr')[3].dispatchEvent(e);
+            element.find('tr')[3].dispatchEvent(e);
             expect(vm.selectedVal.name).toBe("Phil");
             //Click on the same row and check if it is deselected
-            objectElement.find('tr')[3].dispatchEvent(e);
-            expect(vm.selectedVal).toBe("");
+            element.find('tr')[3].dispatchEvent(e);
+            expect(vm.selectedVal).toBeFalsy();
 
         });
     });
