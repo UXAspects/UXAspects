f4ed60164329f58d2b8ec851b0c05f9f15abcbee
donna-marijne <donna.worby@hpe.com>
2018-11-28 18:10:52 +0000

    Revert "Changed prepack and postpack to node scripts"
    This reverts commit d3e3f93de2f439f84f105dbb621f2a48b7aab03f.

--- scripts/postpack-docs.js
+++
@@ -1,9 +0,0 @@
-const { copyFileSync, unlinkSync } = require('fs');
-const { cwd } = require('process');
-const { join } = require('path');
-
-const packageFile = join(cwd(), 'src', 'package.json');
-const tempFile = join(cwd(), 'src_package.json');
-
-copyFileSync(tempFile, packageFile);
-unlinkSync(tempFile);
