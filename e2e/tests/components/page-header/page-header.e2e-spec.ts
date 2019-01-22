571e5295c8823cdbd37f950ebce89efe5a57aced
Ashley Hunter <ashley-glenn.hunter@hpe.com>
2019-01-22 15:27:27 +0000

    EL-3372 - Fixing Protractor Tests (#958)
    * Starting e2e fixes
    
    * Fixing final test cases
    
    * Enabling tests in CI build
    
    * Reverting Commit

--- e2e/tests/components/page-header/page-header.e2e-spec.ts
+++ e2e/tests/components/page-header/page-header.e2e-spec.ts
@@ -3,253 +3,265 @@ import { PageHeaderPage } from './page-header.po.spec';
 
 describe('Page Header Tests', () => {
 
-  let page: PageHeaderPage;
+    let page: PageHeaderPage;
 
-  beforeEach(() => {
-    page = new PageHeaderPage();
-    page.getPage();
+    beforeEach(() => {
+        page = new PageHeaderPage();
+        page.getPage();
 
-    page.condensed = false;
-  });
+        page.condensed = false;
+    });
 
-  it('should have correct initial states', () => {
+    it('should have correct initial states', async () => {
 
-    // Initial values.
-    expect(page.confirmPageHeaderIsCondensed()).toBeFalsy();
-    expect<any>(page.getABreadcrumb(0)).toEqual('Archive');
-    expect(page.confirmApplicationLogoIsPresent()).toBeTruthy();
-    expect<any>(page.getApplicationLogoText()).toEqual('Home');
-    expect(page.confirmDropdownIsPresent()).toBeTruthy();
-    expect(page.confirmDropdownIsOpened()).toBeFalsy();
-    expect(page.confirmNotificationIconIsPresent()).toBeTruthy();
-    expect(page.confirmActionsIconIsPresent()).toBeTruthy();
+        // Initial values.
+        expect(await page.confirmPageHeaderIsCondensed()).toBeFalsy();
+        expect<any>(await page.getABreadcrumb(0)).toEqual('Archive');
+        expect(await page.confirmApplicationLogoIsPresent()).toBeTruthy();
+        expect<any>(await page.getApplicationLogoText()).toEqual('Home');
+        expect(await page.confirmDropdownIsPresent()).toBeTruthy();
+        expect(await page.confirmDropdownIsOpened()).toBeFalsy();
+        expect(await page.confirmNotificationIconIsPresent()).toBeTruthy();
+        expect(await page.confirmActionsIconIsPresent()).toBeTruthy();
 
-  });
+    });
 
-  it('should display breadcrumbs when condensed', () => {
+    it('should display breadcrumbs when condensed', () => {
 
-    page.toggleTheHeader();
-    expect<any>(page.getABreadcrumbWhenCondensed(0)).toEqual('Archive');
-    expect<any>(page.getABreadcrumbWhenCondensed(1)).toEqual('My Page');
+        page.toggleTheHeader();
+        expect<any>(page.getABreadcrumb(0)).toEqual('Archive');
+        expect<any>(page.getABreadcrumb(1)).toEqual('My Page');
 
-  });
+    });
 
-  it('should display an application logo and text when condensed', () => {
+    it('should display an application logo and text when condensed', () => {
 
-    page.toggleTheHeader();
-    expect(page.confirmApplicationLogoIsPresent()).toBeTruthy();
-    expect<any>(page.getApplicationLogoText()).toEqual('Home');
+        page.toggleTheHeader();
+        expect(page.confirmApplicationLogoIsPresent()).toBeTruthy();
+        expect<any>(page.getApplicationLogoText()).toEqual('Home');
 
-  });
+    });
 
-  it('should display the dropdown menu', () => {
+    it('should display the dropdown menu', () => {
 
-    expect(page.confirmDropdownIsPresent()).toBeTruthy();
-    page.openDropdown();
-    expect(page.confirmDropdownIsOpened()).toBeTruthy();
+        expect(page.confirmDropdownIsPresent()).toBeTruthy();
+        page.openDropdown();
+        expect(page.confirmDropdownIsOpened()).toBeTruthy();
 
-  });
+    });
 
-  it('should display the dropdown menu when condensed', () => {
+    it('should display the dropdown menu when condensed', () => {
 
-    page.toggleTheHeader();
-    expect(page.confirmDropdownIsPresent()).toBeTruthy();
-    page.openDropdown();
-    expect(page.confirmDropdownIsOpened()).toBeTruthy();
+        page.toggleTheHeader();
+        expect(page.confirmDropdownIsPresent()).toBeTruthy();
+        page.openDropdown();
+        expect(page.confirmDropdownIsOpened()).toBeTruthy();
 
-  });
+    });
 
-  it('should display the dropdown menu items', () => {
+    it('should display the dropdown menu items', () => {
 
-    expect(page.confirmDropdownIsPresent()).toBeTruthy();
-    page.openDropdown();
-    expect(page.confirmDropdownIsOpened()).toBeTruthy();
-    expect<any>(page.getFirstDropdownMenuItem(1)).toEqual('Pie Charts');
-    page.displaySecondDropdownMenu();
-    expect<any>(page.getSecondDropdownMenuItem(2)).toEqual('Monthly View');
+        expect(page.confirmDropdownIsPresent()).toBeTruthy();
+        page.openDropdown();
+        expect(page.confirmDropdownIsOpened()).toBeTruthy();
+        expect<any>(page.getFirstDropdownMenuItem(1)).toEqual('Pie Charts');
+        page.displaySecondDropdownMenu();
+        expect<any>(page.getSecondDropdownMenuItem(2)).toEqual('Monthly View');
 
-  });
+    });
 
-  it('should display the dropdown menu items when condensed', () => {
+    it('should display the dropdown menu items when condensed', () => {
 
-    page.toggleTheHeader();
-    expect(page.confirmDropdownIsPresent()).toBeTruthy();
-    page.openDropdown();
-    expect(page.confirmDropdownIsOpened()).toBeTruthy();
-    expect<any>(page.getFirstDropdownMenuItem(1)).toEqual('Pie Charts');
-    page.displaySecondDropdownMenu();
-    expect<any>(page.getSecondDropdownMenuItem(2)).toEqual('Monthly View');
+        page.toggleTheHeader();
+        expect(page.confirmDropdownIsPresent()).toBeTruthy();
+        page.openDropdown();
+        expect(page.confirmDropdownIsOpened()).toBeTruthy();
+        expect<any>(page.getFirstDropdownMenuItem(1)).toEqual('Pie Charts');
+        page.displaySecondDropdownMenu();
+        expect<any>(page.getSecondDropdownMenuItem(2)).toEqual('Monthly View');
 
-  });
+    });
 
-  it('should display the notifications menu', () => {
+    it('should display the notifications menu', () => {
 
-    expect(page.confirmNotificationIconIsPresent()).toBeTruthy();
-    page.openNotifications();
-    expect<any>(page.confirmNotificationsAreDisplayed()).toEqual(3);
+        expect(page.confirmNotificationIconIsPresent()).toBeTruthy();
+        page.openNotifications();
+        expect<any>(page.confirmNotificationsAreDisplayed()).toEqual(3);
 
-  });
+    });
 
-  it('should display the notifications menu when condensed', () => {
+    it('should display the notifications menu when condensed', () => {
 
-    page.toggleTheHeader();
-    expect(page.confirmNotificationIconIsPresent()).toBeTruthy();
-    page.openNotifications();
-    expect<any>(page.confirmNotificationsAreDisplayed()).toEqual(3);
+        page.toggleTheHeader();
+        expect(page.confirmNotificationIconIsPresent()).toBeTruthy();
+        page.openNotifications();
+        expect<any>(page.confirmNotificationsAreDisplayed()).toEqual(3);
 
-  });
+    });
 
-  it('should display the actions menu', () => {
+    it('should display the actions menu', () => {
 
-    expect(page.confirmActionsIconIsPresent()).toBeTruthy();
-    page.openActions();
-    expect<any>(page.confirmActionsAreDisplayed()).toEqual(4);
+        expect(page.confirmActionsIconIsPresent()).toBeTruthy();
+        page.openActions();
+        expect<any>(page.confirmActionsAreDisplayed()).toEqual(4);
 
-  });
+    });
 
-  it('should display the actions menu when condensed', () => {
+    it('should display the actions menu when condensed', () => {
 
-    page.toggleTheHeader();
-    expect(page.confirmActionsIconIsPresent()).toBeTruthy();
-    page.openActions();
-    expect<any>(page.confirmActionsAreDisplayed()).toEqual(4);
+        page.toggleTheHeader();
+        expect(page.confirmActionsIconIsPresent()).toBeTruthy();
+        page.openActions();
+        expect<any>(page.confirmActionsAreDisplayed()).toEqual(4);
 
-  });
+    });
 
-  it('should not show secondary navigation when nothing is selected', async () => {
-    const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
-    expect<any>(secondaryNavigation.isPresent()).toBe(false);
-  });
+    it('should not show secondary navigation when nothing is selected', async () => {
+        const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
+        expect<any>(secondaryNavigation.isPresent()).toBe(false);
+    });
 
-  it('should not show secondary navigation items when there is no children on the selected item', async () => {
+    it('should not show secondary navigation items when there is no children on the selected item', async () => {
 
-    // click the first menu item
-    await page.pageHeader2.$$('.horizontal-navigation-button').get(0).click();
+        // enable autoselect
+        await page.autoselectButton.click();
 
-    // get the required elements
-    const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
-    const tabset = await secondaryNavigation.$('.nav-tabs');
+        // click the first menu item
+        await page.pageHeader2.$$('.horizontal-navigation-button').get(0).click();
 
-    expect<any>(secondaryNavigation.isPresent()).toBe(true);
-    expect<any>(tabset.isPresent()).toBe(false);
-  });
+        // get the required elements
+        const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
+        const tabset = await secondaryNavigation.$('.nav-tabs');
 
-  it('should show secondary navigation items when there are children on the selected item', async () => {
+        expect<any>(await secondaryNavigation.isPresent()).toBe(false);
+        expect<any>(await tabset.isPresent()).toBe(false);
+    });
 
-    // click the second menu item
-    await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
+    it('should show secondary navigation items when there are children on the selected item', async () => {
 
-    // get the required elements
-    const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
-    const tabset = await secondaryNavigation.$('.nav-tabs');
-    const tabs: ElementArrayFinder = await tabset.$$('li');
+        // enable autoselect
+        await page.autoselectButton.click();
 
-    expect<any>(secondaryNavigation.isPresent()).toBe(true);
-    expect<any>(tabset.isPresent()).toBe(true);
+        // click the second menu item
+        await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
 
-    // check the number of tabs are correct
-    expect<any>(tabs.length).toBe(3);
-  });
+        // get the required elements
+        const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
+        const tabset = await secondaryNavigation.$('.nav-tabs');
+        const tabs: ElementArrayFinder = await tabset.$$('li');
 
-  it('should align the tabset accordingly', async () => {
+        expect<any>(await secondaryNavigation.isPresent()).toBe(true);
+        expect<any>(await tabset.isPresent()).toBe(true);
 
-    // click the second menu item
-    await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
+        // check the number of tabs are correct
+        expect<any>(tabs.length).toBe(3);
+    });
 
-    // get the required elements
-    const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
+    it('should align the tabset accordingly', async () => {
 
-    // should contain the 'center' class initially
-    expect(await page.getClasses(secondaryNavigation)).toContain('center');
+        // enable autoselect
+        await page.autoselectButton.click();
 
-    // click the left align button
-    await page.alignLeftButton.click();
+        // click the second menu item
+        await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
 
-    // should now contain the 'left' class
-    expect(await page.getClasses(secondaryNavigation)).toContain('left');
+        // get the required elements
+        const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
 
-    // click the right align button
-    await page.alignRightButton.click();
+        // should contain the 'center' class initially
+        expect(await page.getClasses(secondaryNavigation)).toContain('center');
 
-    // should now contain the 'right' class
-    expect(await page.getClasses(secondaryNavigation)).toContain('right');
+        // click the left align button
+        await page.alignLeftButton.click();
 
-    // click the center align button
-    await page.alignCenterButton.click();
+        // should now contain the 'left' class
+        expect(await page.getClasses(secondaryNavigation)).toContain('left');
 
-    // should now contain the 'center' class
-    expect(await page.getClasses(secondaryNavigation)).toContain('center');
-  });
+        // click the right align button
+        await page.alignRightButton.click();
 
-  it('should call the select functions when secondary items are clicked', async () => {
+        // should now contain the 'right' class
+        expect(await page.getClasses(secondaryNavigation)).toContain('right');
 
-    // click the second menu item
-    await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
+        // click the center align button
+        await page.alignCenterButton.click();
 
-    // get the required elements
-    const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
-    const tabset = await secondaryNavigation.$('.nav-tabs');
-    const tabs: ElementArrayFinder = await tabset.$$('li');
+        // should now contain the 'center' class
+        expect(await page.getClasses(secondaryNavigation)).toContain('center');
+    });
 
-    // click the first item
-    await tabs[0].click();
+    it('should call the select functions when secondary items are clicked', async () => {
 
-    // check the selected text
-    expect(await page.selected.getText()).toBe('Daily View');
+        // enable autoselect
+        await page.autoselectButton.click();
 
-    // click the second item
-    await tabs[1].click();
+        // click the second menu item
+        await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
 
-    // check the selected text
-    expect(await page.selected.getText()).toBe('Weekly View');
+        // get the required elements
+        const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
+        const tabset = await secondaryNavigation.$('.nav-tabs');
+        const tabs: ElementArrayFinder = await tabset.$$('li');
 
-    // click the third item
-    await tabs[2].click();
+        // click the first item
+        await tabs[0].click();
 
-    // check the selected text
-    expect(await page.selected.getText()).toBe('Monthly View');
-  });
+        // check the selected text
+        expect(await page.selected.getText()).toBe('Daily View');
 
-  it('should automatically select the first item when autoselect is enabled', async () => {
+        // click the second item
+        await tabs[1].click();
 
-    // enable autoselect
-    await page.autoselectButton.click();
+        // check the selected text
+        expect(await page.selected.getText()).toBe('Weekly View');
 
-    // click the second menu item
-    await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
+        // click the third item
+        await tabs[2].click();
 
-    // get the required elements
-    const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
-    const tabset = await secondaryNavigation.$('.nav-tabs');
-    const tabs: ElementArrayFinder = await tabset.$$('li');
+        // check the selected text
+        expect(await page.selected.getText()).toBe('Monthly View');
+    });
 
-    // check the selected text
-    expect(await page.selected.getText()).toBe('Daily View');
-  });
+    it('should automatically select the first item when autoselect is enabled', async () => {
 
-  it('should reselect the first item when the parent it clicked with autoselect enabled', async () => {
+        // enable autoselect
+        await page.autoselectButton.click();
 
-    // enable autoselect
-    await page.autoselectButton.click();
+        // click the second menu item
+        await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
 
-    // click the second menu item
-    await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
+        // get the required elements
+        const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
+        const tabset = await secondaryNavigation.$('.nav-tabs');
+        const tabs: ElementArrayFinder = await tabset.$$('li');
 
-    // get the required elements
-    const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
-    const tabset = await secondaryNavigation.$('.nav-tabs');
-    const tabs: ElementArrayFinder = await tabset.$$('li');
+        // check the selected text
+        expect(await page.selected.getText()).toBe('Daily View');
+    });
 
-    // click the second item
-    await tabs[1].click();
+    it('should reselect the first item when the parent it clicked with autoselect enabled', async () => {
 
-    // check the selected text
-    expect(await page.selected.getText()).toBe('Weekly View');
+        // enable autoselect
+        await page.autoselectButton.click();
 
-    // click the second menu item again
-    await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
+        // click the second menu item
+        await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
 
-    // check the first child is selected
-    expect(await page.selected.getText()).toBe('Daily View');
-  });
+        // get the required elements
+        const secondaryNavigation: ElementFinder = await page.getSecondaryNavigation();
+        const tabset = await secondaryNavigation.$('.nav-tabs');
+        const tabs: ElementArrayFinder = await tabset.$$('li');
+
+        // click the second item
+        await tabs[1].click();
+
+        // check the selected text
+        expect(await page.selected.getText()).toBe('Weekly View');
+
+        // click the second menu item again
+        await page.pageHeader2.$$('.horizontal-navigation-button').get(1).click();
+
+        // check the first child is selected
+        expect(await page.selected.getText()).toBe('Daily View');
+    });
 });
