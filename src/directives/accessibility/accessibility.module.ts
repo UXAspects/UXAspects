2840e0f4040c562ebc2715d826c36e387973b3f9
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2019-02-18 13:11:58 +0000

    EL-3406 - Focus Origin Directive

--- src/directives/accessibility/accessibility.module.ts
+++ src/directives/accessibility/accessibility.module.ts
@@ -1,7 +1,9 @@
 import { A11yModule } from '@angular/cdk/a11y';
-import { ModuleWithProviders, NgModule } from '@angular/core';
+import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { DefaultFocusIndicatorDirective } from './focus-indicator/default-focus-indicator.directive';
 import { FocusIndicatorOptionsDirective } from './focus-indicator/focus-indicator-options/focus-indicator-options.directive';
+import { FocusIndicatorOriginDirective } from './focus-indicator/focus-indicator-origin/focus-indicator-origin.directive';
+import { FocusIndicatorOriginService } from './focus-indicator/focus-indicator-origin/focus-indicator-origin.service';
 import { FocusIndicatorDirective } from './focus-indicator/focus-indicator.directive';
 import { FocusIndicatorService } from './focus-indicator/focus-indicator.service';
 import { FocusWithinDirective } from './focus-within/focus-within.directive';
@@ -12,6 +14,25 @@ import { SplitterAccessibilityDirective } from './splitter/splitter-accessibilit
 import { TabbableListItemDirective } from './tabbable-list/tabbable-list-item.directive';
 import { TabbableListDirective } from './tabbable-list/tabbable-list.directive';
 
+
+/**
+ * We want this service to be a singleton service (even across lazy modules).
+ * This allows us to ensure that it is a singleton without having to have
+ * the consumer call the `forRoot()` method on the module.
+ *
+ * This can be removed once Angular 5 support is dropped as it can be changed
+ * to be `providedIn: 'root'` instead.
+ */
+export function FOCUS_INDICATOR_ORIGIN_SERVICE_PROVIDER_FACTORY(parentFocusIndicatorOriginService: FocusIndicatorOriginService) {
+    return parentFocusIndicatorOriginService || new FocusIndicatorOriginService();
+}
+
+export const FOCUS_INDICATOR_ORIGIN_SERVICE_PROVIDER = {
+    provide: FocusIndicatorOriginService,
+    deps: [[new Optional(), new SkipSelf(), FocusIndicatorOriginService]],
+    useFactory: FOCUS_INDICATOR_ORIGIN_SERVICE_PROVIDER_FACTORY
+};
+
 @NgModule({
     declarations: [
         DefaultFocusIndicatorDirective,
@@ -21,6 +42,7 @@ import { TabbableListDirective } from './tabbable-list/tabbable-list.directive';
         SplitterAccessibilityDirective,
         TabbableListDirective,
         TabbableListItemDirective,
+        FocusIndicatorOriginDirective
     ],
     imports: [
         A11yModule
@@ -33,10 +55,12 @@ import { TabbableListDirective } from './tabbable-list/tabbable-list.directive';
         SplitterAccessibilityDirective,
         TabbableListDirective,
         TabbableListItemDirective,
+        FocusIndicatorOriginDirective
     ],
     providers: [
         AccessibilityOptionsService,
-        FocusIndicatorService
+        FocusIndicatorService,
+        FOCUS_INDICATOR_ORIGIN_SERVICE_PROVIDER
     ]
 })
 export class AccessibilityModule {
@@ -45,7 +69,8 @@ export class AccessibilityModule {
         return {
             ngModule: AccessibilityModule,
             providers: [
-                { provide: ACCESSIBILITY_OPTIONS_TOKEN, useValue: options }
+                { provide: ACCESSIBILITY_OPTIONS_TOKEN, useValue: options },
+                FOCUS_INDICATOR_ORIGIN_SERVICE_PROVIDER
             ]
         };
     }
