88997cc6a20ccc25c4ae20783faf9818aee8fbc6
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2019-02-20 16:41:20 +0000

    EL-3241 - Dashboard Test Hardening (#990)

--- e2e/pages/app/dashboard/dashboard.testpage.component.ts
+++ e2e/pages/app/dashboard/dashboard.testpage.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ColorService, DashboardOptions } from '@ux-aspects/ux-aspects';
+import { DashboardOptions } from '@ux-aspects/ux-aspects';
 import 'chance';
 
 @Component({
@@ -9,62 +9,6 @@ import 'chance';
 })
 export class DashboardTestPageComponent {
 
-    // configure the directive data
-    lineChartData: Chart.ChartDataSets[] = [{
-        data: [],
-        borderWidth: 2,
-        fill: false
-    },
-    {
-        data: [],
-        borderWidth: 2,
-        fill: false
-    }];
-
-    lineChartOptions: Chart.ChartOptions = {
-        maintainAspectRatio: false,
-        responsive: true,
-        elements: {
-            line: {
-                tension: 0
-            },
-            point: {
-                radius: 0
-            }
-        },
-        scales: {
-            xAxes: [{
-                gridLines: {
-                    color: 'transparent'
-                },
-                ticks: {
-                    min: 0,
-                    max: 49,
-                    maxRotation: 0
-                } as Chart.LinearTickOptions
-            }],
-            yAxes: [{
-                gridLines: {
-                    color: '#ddd'
-                },
-                ticks: {
-                    beginAtZero: true,
-                    stepSize: 100
-                } as Chart.LinearTickOptions,
-            }]
-        },
-    };
-
-    lineChartLabels: string[] = [];
-    lineChartLegend: boolean = false;
-    lineChartColors = [
-        {
-            borderColor: this.colorService.getColor('vibrant1').toHex(),
-        },
-        {
-            borderColor: this.colorService.getColor('vibrant2').toHex(),
-        }];
-
     options: DashboardOptions = {
         columns: 4,
         padding: 10,
@@ -72,37 +16,5 @@ export class DashboardTestPageComponent {
         emptyRow: false,
         minWidth: 187
     };
-
-    constructor(public colorService: ColorService) {
-
-        // generate the chart data
-        for (let idx = 0; idx < 50; idx++) {
-
-            let label = '';
-
-            if (idx === 0) {
-                label = 'Jan 1, 2017';
-            }
-
-            if (idx === 49) {
-                label = 'Mar 30, 2017';
-            }
-
-            this.lineChartLabels.push(label);
-
-            let dataset1 = this.lineChartData[0].data as Chart.ChartPoint[];
-            let dataset2 = this.lineChartData[1].data as Chart.ChartPoint[];
-
-            dataset1.push({
-                x: idx,
-                y: chance.integer({ min: 280, max: 460 })
-            });
-
-            dataset2.push({
-                x: idx,
-                y: chance.integer({ min: 50, max: 250 })
-            });
-        }
-    }
 
 }
--- e2e/tests/components/dashboard/dashboard.e2e-spec.ts
+++ e2e/tests/components/dashboard/dashboard.e2e-spec.ts
@@ -9,171 +9,115 @@ describe('Dashboard Tests', () => {
     let widget3: ElementFinder;
     let widget4: ElementFinder;
 
-    beforeEach(() => {
+    beforeEach(async () => {
         page = new DashboardPage();
         page.getPage();
 
-        widget1 = page.getWidget(0);
-        widget2 = page.getWidget(1);
-        widget3 = page.getWidget(2);
-        widget4 = page.getWidget(3);
-        expect(widget1).not.toBeNull();
-        expect(widget2).not.toBeNull();
-        expect(widget3).not.toBeNull();
-        expect(widget4).not.toBeNull();
+        // set the browser window to a specific size to ensure consistency
+        await browser.driver.manage().window().setSize(1320, 800);
+
+        widget1 = await page.getWidget(0);
+        widget2 = await page.getWidget(1);
+        widget3 = await page.getWidget(2);
+        widget4 = await page.getWidget(3);
     });
 
-    it('should have correct initial states', () => {
-
-        expect<any>(page.getNumberOfWidgets()).toEqual(4);
-
-        // Widget 1 should be at the top of the page. The tops of the others should be below its base.
-        expect<any>(page.getWidgetLocationValue(widget1, 'top')).toEqual(0);
-        page.getWidgetLocationValue(widget1, 'top').then(function (top: number) {
-            page.getWidgetLocationValue(widget1, 'height').then(function (height: number) {
-                expect(page.getWidgetLocationValue(widget2, 'top')).toBeGreaterThan(top + height - 1);
-                expect(page.getWidgetLocationValue(widget3, 'top')).toBeGreaterThan(top + height - 1);
-                expect(page.getWidgetLocationValue(widget4, 'top')).toBeGreaterThan(top + height - 1);
-            });
-        });
-
-        // Widget 2 should be at the left of the page. Widget 3 should be to its right.
-        expect<any>(page.getWidgetLocationValue(widget1, 'left')).toEqual(0);
-        page.getWidgetLocationValue(widget2, 'left').then(function (left: number) {
-            page.getWidgetLocationValue(widget2, 'width').then(function (width: number) {
-                expect(page.getWidgetLocationValue(widget3, 'left')).toBeGreaterThan(left + width - 1);
-            });
-        });
-
-        // Widget 4 should be to the right of widget 3.
-        page.getWidgetLocationValue(widget3, 'left').then(function (left: number) {
-            page.getWidgetLocationValue(widget3, 'width').then(function (width: number) {
-                expect(page.getWidgetLocationValue(widget4, 'left')).toBeGreaterThan(left + width - 1);
-            });
-        });
+    // restore the window to its original size after all these tests have run
+    afterAll(async () => {
+        await browser.driver.manage().window().setSize(800, 600);
     });
 
-    it('should react correctly when a widget is moved down', () => {
+    it('should have correct initial states', async () => {
+        expect(await page.getWidgetLocationValue(widget1, 'top')).toBe(0);
+        expect(await page.getWidgetLocationValue(widget1, 'left')).toBe(0);
 
-        // Drag widget 1 to the location occupied by widget 2
-        browser.actions().dragAndDrop(widget1, widget2).perform();
-        expect<any>(page.getNumberOfWidgets()).toEqual(4);
+        expect(await page.getWidgetLocationValue(widget2, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget2, 'left')).toBe(0);
 
-        // Widgets 1, 2 and 3 should now be at the top of the page. Widget 1 should be below them.
-        expect<any>(page.getWidgetLocationValue(widget2, 'top')).toEqual(0);
-        expect<any>(page.getWidgetLocationValue(widget3, 'top')).toEqual(0);
-        expect<any>(page.getWidgetLocationValue(widget4, 'top')).toEqual(0);
-        page.getWidgetLocationValue(widget2, 'top').then(function (top: number) {
-            page.getWidgetLocationValue(widget2, 'height').then(function (height: number) {
-                expect(page.getWidgetLocationValue(widget1, 'top')).toBeGreaterThan(top + height - 1);
-            });
-        });
+        expect(await page.getWidgetLocationValue(widget3, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget3, 'left')).toBe(554);
 
-        // Widget 2 should still be at the left of the page. Widget 3 should be to its right.
-        expect<any>(page.getWidgetLocationValue(widget2, 'left')).toEqual(0);
-        page.getWidgetLocationValue(widget2, 'left').then(function (left: number) {
-            page.getWidgetLocationValue(widget2, 'width').then(function (width: number) {
-                expect(page.getWidgetLocationValue(widget3, 'left')).toBeGreaterThan(left + width - 1);
-            });
-        });
-        // Widget 4 should still be to the right of widget 3.
-        page.getWidgetLocationValue(widget3, 'left').then(function (left: number) {
-            page.getWidgetLocationValue(widget3, 'width').then(function (width: number) {
-                expect(page.getWidgetLocationValue(widget4, 'left')).toBeGreaterThan(left + width - 1);
-            });
-        });
+        expect(await page.getWidgetLocationValue(widget4, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget4, 'left')).toBe(831);
     });
 
-    /** TODO: This should be uncommented once EL-3381 is resolved */
-    // it('should react correctly when a widget is moved up', () => {
+    it('should react correctly when a widget is moved down', async () => {
 
-    //     // Drag widget 3 to the location occupied by widget 1.
-    //     browser.actions().dragAndDrop(widget3, widget1).perform();
-    //     expect<any>(page.getNumberOfWidgets()).toEqual(4);
+        // drag the top widget down
+        await browser.actions().dragAndDrop(widget1, { x: 0, y: 250 }).perform();
 
-    //     // Widgets 3 should now be at the top of the page. Widget 1 should be below it.
-    //     expect<any>(page.getWidgetLocationValue(widget3, 'top')).toEqual(0);
-    //     page.getWidgetLocationValue(widget3, 'top').then(function (top: number) {
-    //         page.getWidgetLocationValue(widget3, 'height').then(function (height: number) {
-    //             expect(page.getWidgetLocationValue(widget1, 'top')).toBeGreaterThan(top + height - 1);
-    //         });
-    //     });
-    //     // Widgets 2 and 4 should now be below widget 1.
-    //     page.getWidgetLocationValue(widget1, 'top').then(function (top: number) {
-    //         page.getWidgetLocationValue(widget1, 'height').then(function (height: number) {
-    //             expect(page.getWidgetLocationValue(widget2, 'top')).toBeGreaterThan(top + height - 1);
-    //             expect(page.getWidgetLocationValue(widget4, 'top')).toBeGreaterThan(top + height - 1);
-    //         });
-    //     });
+        expect(await page.getWidgetLocationValue(widget1, 'top')).toBe(220);
+        expect(await page.getWidgetLocationValue(widget1, 'left')).toBe(0);
 
-    //     // Widget 4 should still be to the right of widget 2.
-    //     page.getWidgetLocationValue(widget2, 'left').then(function (left: number) {
-    //         page.getWidgetLocationValue(widget2, 'width').then(function (width: number) {
-    //             expect(page.getWidgetLocationValue(widget4, 'left')).toBeGreaterThan(left + width - 1);
-    //         });
-    //     });
-    // });
+        expect(await page.getWidgetLocationValue(widget2, 'top')).toBe(0);
+        expect(await page.getWidgetLocationValue(widget2, 'left')).toBe(0);
 
-    it('should react correctly when a widget is moved left', () => {
+        expect(await page.getWidgetLocationValue(widget3, 'top')).toBe(0);
+        expect(await page.getWidgetLocationValue(widget3, 'left')).toBe(554);
 
-        // Drag widget 4 to the location occupied by widget 3.
-        browser.actions().dragAndDrop(widget4, widget3).perform();
-        expect<any>(page.getNumberOfWidgets()).toEqual(4);
+        expect(await page.getWidgetLocationValue(widget4, 'top')).toBe(0);
+        expect(await page.getWidgetLocationValue(widget4, 'left')).toBe(831);
 
-        // Widget 1 should still be at the top of the page. The tops of the others should still be below its base.
-        expect<any>(page.getWidgetLocationValue(widget1, 'top')).toEqual(0);
-        page.getWidgetLocationValue(widget1, 'top').then(function (top: number) {
-            page.getWidgetLocationValue(widget1, 'height').then(function (height: number) {
-                expect(page.getWidgetLocationValue(widget2, 'top')).toBeGreaterThan(top + height - 1);
-                expect(page.getWidgetLocationValue(widget3, 'top')).toBeGreaterThan(top + height - 1);
-                expect(page.getWidgetLocationValue(widget4, 'top')).toBeGreaterThan(top + height - 1);
-            });
-        });
-
-        // Widget 4 should now be to the right of widget 2.
-        expect<any>(page.getWidgetLocationValue(widget2, 'left')).toEqual(0);
-        page.getWidgetLocationValue(widget2, 'left').then(function (left: number) {
-            page.getWidgetLocationValue(widget2, 'width').then(function (width: number) {
-                expect(page.getWidgetLocationValue(widget4, 'left')).toBeGreaterThan(left + width - 1);
-            });
-        });
-        // Widget 3 should now be to the right of widget 4.
-        page.getWidgetLocationValue(widget4, 'left').then(function (left: number) {
-            page.getWidgetLocationValue(widget4, 'width').then(function (width: number) {
-                expect(page.getWidgetLocationValue(widget3, 'left')).toBeGreaterThan(left + width - 1);
-            });
-        });
     });
 
-    it('should react correctly when a widget is moved right', () => {
+    it('should react correctly when a widget is moved up', async () => {
 
-        // Drag widget 2 to the location occupied by widget 4.
-        browser.actions().dragAndDrop(widget2, widget4).perform();
-        expect<any>(page.getNumberOfWidgets()).toEqual(4);
+        // drag the top widget down
+        await browser.actions().dragAndDrop(widget1, { x: 0, y: 250 }).perform();
 
-        // Widget 1 should still be at the top of the page. The tops of the others should still be below its base.
-        expect<any>(page.getWidgetLocationValue(widget1, 'top')).toEqual(0);
-        page.getWidgetLocationValue(widget1, 'top').then(function (top: number) {
-            page.getWidgetLocationValue(widget1, 'height').then(function (height: number) {
-                expect(page.getWidgetLocationValue(widget2, 'top')).toBeGreaterThan(top + height - 1);
-                expect(page.getWidgetLocationValue(widget3, 'top')).toBeGreaterThan(top + height - 1);
-                expect(page.getWidgetLocationValue(widget4, 'top')).toBeGreaterThan(top + height - 1);
-            });
-        });
+        // drag the top widget back up
+        await browser.actions().dragAndDrop(widget1, { x: 0, y: -250 }).perform();
 
-        // Widget 4 should still be to the right of widget 3.
-        expect<any>(page.getWidgetLocationValue(widget3, 'left')).toEqual(0);
-        page.getWidgetLocationValue(widget3, 'left').then(function (left: number) {
-            page.getWidgetLocationValue(widget3, 'width').then(function (width: number) {
-                expect(page.getWidgetLocationValue(widget4, 'left')).toBeGreaterThan(left + width - 1);
-            });
-        });
-        // Widget 2 should now be to the right of widget 4.
-        page.getWidgetLocationValue(widget4, 'left').then(function (left: number) {
-            page.getWidgetLocationValue(widget4, 'width').then(function (width: number) {
-                expect(page.getWidgetLocationValue(widget2, 'left')).toBeGreaterThan(left + width - 1);
-            });
-        });
+        expect(await page.getWidgetLocationValue(widget1, 'top')).toBe(0);
+        expect(await page.getWidgetLocationValue(widget1, 'left')).toBe(0);
+
+        expect(await page.getWidgetLocationValue(widget2, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget2, 'left')).toBe(0);
+
+        expect(await page.getWidgetLocationValue(widget3, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget3, 'left')).toBe(554);
+
+        expect(await page.getWidgetLocationValue(widget4, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget4, 'left')).toBe(831);
+
+    });
+
+    it('should react correctly when a widget is moved right', async () => {
+
+        // drag the widget right
+        await browser.actions().dragAndDrop(widget2, { x: 250, y: 0 }).perform();
+
+        expect(await page.getWidgetLocationValue(widget1, 'top')).toBe(0);
+        expect(await page.getWidgetLocationValue(widget1, 'left')).toBe(0);
+
+        expect(await page.getWidgetLocationValue(widget2, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget2, 'left')).toBe(277);
+
+        expect(await page.getWidgetLocationValue(widget3, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget3, 'left')).toBe(0);
+
+        expect(await page.getWidgetLocationValue(widget4, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget4, 'left')).toBe(831);
+
+    });
+
+    it('should react correctly when a widget is moved left', async () => {
+
+        // drag the widget left
+        await browser.actions().dragAndDrop(widget4, { x: -250, y: 0 }).perform();
+
+        expect(await page.getWidgetLocationValue(widget1, 'top')).toBe(0);
+        expect(await page.getWidgetLocationValue(widget1, 'left')).toBe(0);
+
+        expect(await page.getWidgetLocationValue(widget2, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget2, 'left')).toBe(0);
+
+        expect(await page.getWidgetLocationValue(widget3, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget3, 'left')).toBe(831);
+
+        expect(await page.getWidgetLocationValue(widget4, 'top')).toBe(440);
+        expect(await page.getWidgetLocationValue(widget4, 'left')).toBe(554);
+
     });
 
     it('should allow grab mode to be activated', async () => {
