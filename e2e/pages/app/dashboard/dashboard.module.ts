782a788710725b632121ed753ae2c05212d1f24d
donna-marijne <donna.worby@hpe.com>
2019-03-01 17:18:02 +0000

    Protractor tests for dashboard grab handle tab order

--- e2e/pages/app/dashboard/dashboard.module.ts
+++ e2e/pages/app/dashboard/dashboard.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { DashboardModule } from '@ux-aspects/ux-aspects';
+import { CheckboxModule, DashboardModule } from '@ux-aspects/ux-aspects';
 import { DashboardTestPageComponent } from './dashboard.testpage.component';
+import { CommonModule } from '@angular/common';
 
 
 
 @NgModule({
     imports: [
+        CheckboxModule,
+        CommonModule,
         DashboardModule,
         RouterModule.forChild([
             {
