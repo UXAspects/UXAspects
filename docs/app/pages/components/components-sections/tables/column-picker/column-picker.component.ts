d518e72ca3fdd290ba30611c76407b6a2bc6fd70
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2019-03-06 14:55:13 +0000

    Migrating to column picker component

--- docs/app/pages/components/components-sections/tables/column-picker/column-picker.component.ts
+++ docs/app/pages/components/components-sections/tables/column-picker/column-picker.component.ts
@@ -48,15 +48,6 @@ export class ComponentsColumnPickerComponent extends BaseDocumentationSection im
         'Work Completed'
     ];
 
-    /** Store the list of deselected columns that can be moved */
-    deselectedSelection: ReadonlyArray<string> = [];
-
-    /** Store the list of selected columns that can be moved */
-    selectedSelection: ReadonlyArray<string> = [];
-
-    /** Cache selection during reordering */
-    private _selection: ReadonlyArray<string> = [];
-
     plunk: IPlunk = {
         files: {
             'app.component.html': this.snippets.raw.appHtml,
@@ -73,50 +64,5 @@ export class ComponentsColumnPickerComponent extends BaseDocumentationSection im
 
     constructor() {
         super(require.context('./snippets/', false, /\.(html|css|js|ts)$/));
-    }
-
-    /** Select the currently selected columns */
-    selectColumns(columns: ReadonlyArray<string> = this.deselectedSelection): void {
-
-        // add each item to the selected columns list
-        columns.forEach(column => this.selected = [...this.selected, column]);
-
-        // remove each item from the deselected columns list
-        this.deselected = this.deselected.filter(column => columns.indexOf(column) === -1);
-
-        // clear the current selection
-        this.deselectedSelection = [];
-    }
-
-    /** Deselect the currently selected columns */
-    deselectColumns(columns: ReadonlyArray<string> = this.selectedSelection): void {
-        // add each item to the deselected columns list
-        columns.forEach(column => this.deselected = [...this.deselected, column]);
-
-        // remove each item from the selected columns list
-        this.selected = this.selected.filter(column => columns.indexOf(column) === -1);
-
-        // clear the current selection
-        this.selectedSelection = [];
-    }
-
-    /** Select all deselected columns */
-    selectAllColumns(): void {
-        this.selectColumns(this.deselected);
-    }
-
-    /** Deselect all selected columns */
-    deselectAllColumns(): void {
-        this.deselectColumns(this.selected);
-    }
-
-    /** Ensure we don't select while dragging */
-    storeSelection(): void {
-        this._selection = [...this.selectedSelection];
-    }
-
-    /** Restore the selection once dragging ends */
-    restoreSelection(): void {
-        this.selectedSelection = this._selection;
     }
 }
