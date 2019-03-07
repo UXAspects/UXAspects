dbe78ed73b4465ad130c327fa84788ce5b37c568
donna-marijne <donna.worby@hpe.com>
2019-03-07 17:08:55 +0000

    Release 1.7.7
    * Added release notes

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -16,6 +16,11 @@ export class ChangeLogPageComponent {
 
         this.logs = [
             {
+                version: '1.7.7',
+                date: 'March 7th 2019',
+                content: require('./logs/release-v1.7.7.md')
+            },
+            {
                 version: '1.7.6',
                 date: 'February 21st 2019',
                 content: require('./logs/release-v1.7.6.md')
