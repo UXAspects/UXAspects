55c1489042b8be1c2a575a1fda340dcc2e7c7bc8
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2019-04-01 12:00:24 +0100

    EL-3548 - Select Enhancements QA (#1041)
    * EL-3548 - Select Enhancements QA
    
    * Fixing firefox issue

--- src/components/select/select.module.ts
+++ src/components/select/select.module.ts
@@ -1,10 +1,11 @@
+import { PlatformModule } from '@angular/cdk/platform';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { InfiniteScrollModule } from '../../directives/infinite-scroll/index';
 import { TagInputModule } from '../tag-input/index';
 import { TypeaheadModule } from '../typeahead/index';
 import { SelectComponent } from './select.component';
-import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
@@ -12,7 +13,8 @@ import { FormsModule } from '@angular/forms';
         FormsModule,
         InfiniteScrollModule,
         TagInputModule,
-        TypeaheadModule
+        TypeaheadModule,
+        PlatformModule
     ],
     exports: [SelectComponent],
     declarations: [SelectComponent]
