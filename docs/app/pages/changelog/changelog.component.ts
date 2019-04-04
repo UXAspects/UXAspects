b6afc3232458c2ee3fdd48536778fea937aca35d
donna-marijne <donna.worby@microfocus.com>
2019-04-04 11:29:16 +0100

    Release 1.7.10
    * Added release notes.

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -16,6 +16,11 @@ export class ChangeLogPageComponent {
 
         this.logs = [
             {
+                version: '1.7.10',
+                date: 'April 4th 2019',
+                content: require('./logs/release-v1.7.10.md')
+            },
+            {
                 version: '1.7.9',
                 date: 'March 28th 2019',
                 content: require('./logs/release-v1.7.9.md')
