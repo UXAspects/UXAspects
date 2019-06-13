034ddd06400dba63b5b132b39103d8f4cf161ee2
donna-marijne <donna.worby@microfocus.com>
2019-06-13 10:28:30 +0100

    Added modification notice

--- src/ng1/plugins/jasny-bootstrap/jasny-bootstrap.js
+++ src/ng1/plugins/jasny-bootstrap/jasny-bootstrap.js
@@ -3,6 +3,7 @@ LICENSE-START
  * Jasny Bootstrap v3.1.1 (http://jasny.github.io/bootstrap)
  * Copyright 2012-2014 Arnold Daniels
  * Licensed under Apache-2.0 (https://github.com/jasny/bootstrap/blob/master/LICENSE)
+ * NOTE: Modified for UX Aspects
 LICENSE-END
  */
 
@@ -88,17 +89,17 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
     this.options  = $.extend({}, OffCanvas.DEFAULTS, options)
     this.state    = null
     this.placement = null
-    
+
     if (this.options.recalc) {
       this.calcClone()
       $(window).on('resize', $.proxy(this.recalc, this))
     }
-    
+
     if (this.options.autohide)
       $(document).on('click', $.proxy(this.autohide, this))
 
     if (this.options.toggle) this.toggle()
-    
+
     if (this.options.disablescrolling) {
         this.options.disableScrolling = this.options.disablescrolling
         delete this.options.disablescrolling
@@ -121,38 +122,38 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
       case 'bottom': return this.$element.outerHeight()
     }
   }
-  
+
   OffCanvas.prototype.calcPlacement = function () {
     if (this.options.placement !== 'auto') {
         this.placement = this.options.placement
         return
     }
-    
+
     if (!this.$element.hasClass('in')) {
       this.$element.css('visiblity', 'hidden !important').addClass('in')
-    } 
-    
+    }
+
     var horizontal = $(window).width() / this.$element.width()
     var vertical = $(window).height() / this.$element.height()
-        
+
     var element = this.$element
     function ab(a, b) {
       if (element.css(b) === 'auto') return a
       if (element.css(a) === 'auto') return b
-      
+
       var size_a = parseInt(element.css(a), 10)
       var size_b = parseInt(element.css(b), 10)
-  
+
       return size_a > size_b ? b : a
     }
-    
+
     this.placement = horizontal >= vertical ? ab('left', 'right') : ab('top', 'bottom')
-      
+
     if (this.$element.css('visibility') === 'hidden !important') {
       this.$element.removeClass('in').css('visiblity', '')
     }
   }
-  
+
   OffCanvas.prototype.opposite = function (placement) {
     switch (placement) {
       case 'top':    return 'bottom'
@@ -161,18 +162,18 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
       case 'right':  return 'left'
     }
   }
-  
+
   OffCanvas.prototype.getCanvasElements = function() {
     // Return a set containing the canvas plus all fixed elements
     var canvas = this.options.canvas ? $(this.options.canvas) : this.$element
-    
+
     var fixed_elements = canvas.find('*').filter(function() {
       return $(this).css('position') === 'fixed'
     }).not(this.options.exclude)
-    
+
     return canvas.add(fixed_elements)
   }
-  
+
   OffCanvas.prototype.slide = function (elements, offset, callback) {
     // Use jQuery animation if CSS transitions aren't supported
     if (!$.support.transition) {
@@ -183,15 +184,15 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
 
     var placement = this.placement
     var opposite = this.opposite(placement)
-    
+
     elements.each(function() {
       if ($(this).css(placement) !== 'auto')
         $(this).css(placement, (parseInt($(this).css(placement), 10) || 0) + offset)
-      
+
       if ($(this).css(opposite) !== 'auto')
         $(this).css(opposite, (parseInt($(this).css(opposite), 10) || 0) - offset)
     })
-    
+
     this.$element
       .one($.support.transition.end, callback)
       .emulateTransitionEnd(350)
@@ -204,12 +205,12 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
     if ($('body').data('offcanvas-style') === undefined) {
       $('body').data('offcanvas-style', $('body').attr('style') || '')
     }
-      
+
     $('body').css('overflow', 'hidden')
 
     if ($('body').width() > bodyWidth) {
       var padding = parseInt($('body').css(prop), 10) + $('body').width() - bodyWidth
-      
+
       setTimeout(function() {
         $('body').css(prop, padding)
       }, 1)
@@ -218,14 +219,14 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
 
   OffCanvas.prototype.show = function () {
     if (this.state) return
-    
+
     var startEvent = $.Event('show.bs.offcanvas')
     this.$element.trigger(startEvent)
     if (startEvent.isDefaultPrevented()) return
 
     this.state = 'slide-in'
     this.calcPlacement();
-    
+
     var elements = this.getCanvasElements()
     var placement = this.placement
     var opposite = this.opposite(placement)
@@ -245,12 +246,12 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
         $(this).css(placement, 0)
       }
     })
-    
+
     if (this.options.disableScrolling) this.disableScrolling()
-    
+
     var complete = function () {
       if (this.state != 'slide-in') return
-      
+
       this.state = 'slid'
 
       elements.removeClass('canvas-sliding').addClass('canvas-slid')
@@ -278,12 +279,12 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
 
     var complete = function () {
       if (this.state != 'slide-out') return
-      
+
       this.state = null
       this.placement = null
-      
+
       this.$element.removeClass('in')
-      
+
       elements.removeClass('canvas-sliding')
       elements.add(this.$element).add('body').each(function() {
         $(this).attr('style', $(this).data('offcanvas-style')).removeData('offcanvas-style')
@@ -293,7 +294,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
     }
 
     elements.removeClass('canvas-slid').addClass('canvas-sliding')
-    
+
     setTimeout($.proxy(function() {
       this.slide(elements, offset, $.proxy(complete, this))
     }, this), 1)
@@ -313,19 +314,19 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
 
   OffCanvas.prototype.recalc = function () {
     if (this.$calcClone.css('display') === 'none' || (this.state !== 'slid' && this.state !== 'slide-in')) return
-    
+
     this.state = null
     this.placement = null
     var elements = this.getCanvasElements()
-    
+
     this.$element.removeClass('in')
-    
+
     elements.removeClass('canvas-slid')
     elements.add(this.$element).add('body').each(function() {
       $(this).attr('style', $(this).data('offcanvas-style')).removeData('offcanvas-style')
     })
   }
-  
+
   OffCanvas.prototype.autohide = function (e) {
     if ($(e.target).closest(this.$element).length === 0) this.hide()
   }
@@ -402,7 +403,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
   var Rowlink = function (element, options) {
     this.$element = $(element)
     this.options = $.extend({}, Rowlink.DEFAULTS, options)
-    
+
     this.$element.on('click.bs.rowlink', 'td:not(.rowlink-skip)', $.proxy(this.click, this))
   }
 
@@ -413,19 +414,19 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
   Rowlink.prototype.click = function(e) {
     var target = $(e.currentTarget).closest('tr').find(this.options.target)[0]
     if ($(e.target)[0] === target) return
-    
+
     e.preventDefault();
-    
+
     if (target.click) {
       target.click()
     } else if (document.createEvent) {
-      var evt = document.createEvent("MouseEvents"); 
-      evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null); 
+      var evt = document.createEvent("MouseEvents");
+      evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
       target.dispatchEvent(evt);
     }
   }
 
-  
+
   // ROWLINK PLUGIN DEFINITION
   // ===========================
 
@@ -456,13 +457,13 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
 
   $(document).on('click.bs.rowlink.data-api', '[data-link="row"]', function (e) {
     if ($(e.target).closest('.rowlink-skip').length !== 0) return
-    
+
     var $this = $(this)
     if ($this.data('bs.rowlink')) return
     $this.rowlink($this.data())
     $(e.target).trigger('click.bs.rowlink')
   })
-  
+
 }(window.jQuery);
 
 
@@ -494,7 +495,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
 
   var Fileinput = function (element, options) {
     this.$element = $(element)
-    
+
     this.$input = this.$element.find(':file')
     if (this.$input.length === 0) return
 
@@ -510,27 +511,27 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
     if (this.$preview.css('display') !== 'inline' && height !== '0px' && height !== 'none') {
       this.$preview.css('line-height', height)
     }
-        
+
     this.original = {
       exists: this.$element.hasClass('fileinput-exists'),
       preview: this.$preview.html(),
       hiddenVal: this.$hidden.val()
     }
-    
+
     this.listen()
   }
-  
+
   Fileinput.prototype.listen = function() {
     this.$input.on('change.bs.fileinput', $.proxy(this.change, this))
     $(this.$input[0].form).on('reset.bs.fileinput', $.proxy(this.reset, this))
-    
+
     this.$element.find('[data-trigger="fileinput"]').on('click.bs.fileinput', $.proxy(this.trigger, this))
     this.$element.find('[data-dismiss="fileinput"]').on('click.bs.fileinput', $.proxy(this.clear, this))
   },
 
   Fileinput.prototype.change = function(e) {
     var files = e.target.files === undefined ? (e.target && e.target.value ? [{ name: e.target.value.replace(/^.+\\/, '')}] : []) : e.target.files
-    
+
     e.stopPropagation()
 
     if (files.length === 0) {
@@ -553,12 +554,12 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
         var $img = $('<img>')
         $img[0].src = re.target.result
         files[0].result = re.target.result
-        
+
         element.find('.fileinput-filename').text(file.name)
-        
+
         // if parent has max-height, using `(max-)height: 100%` on child doesn't take padding and border into account
         if (preview.css('max-height') != 'none') $img.css('max-height', parseInt(preview.css('max-height'), 10) - parseInt(preview.css('padding-top'), 10) - parseInt(preview.css('padding-bottom'), 10)  - parseInt(preview.css('border-top'), 10) - parseInt(preview.css('border-bottom'), 10))
-        
+
         preview.html($img)
         element.addClass('fileinput-exists').removeClass('fileinput-new')
 
@@ -569,22 +570,22 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
     } else {
       this.$element.find('.fileinput-filename').text(file.name)
       this.$preview.text(file.name)
-      
+
       this.$element.addClass('fileinput-exists').removeClass('fileinput-new')
-      
+
       this.$element.trigger('change.bs.fileinput')
     }
   },
 
   Fileinput.prototype.clear = function(e) {
     if (e) e.preventDefault()
-    
+
     this.$hidden.val('')
     this.$hidden.attr('name', this.name)
     this.$input.attr('name', '')
 
     //ie8+ doesn't support changing the value of input with type=file so clone instead
-    if (isIE) { 
+    if (isIE) {
       var inputClone = this.$input.clone(true);
       this.$input.after(inputClone);
       this.$input.remove();
@@ -596,7 +597,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
     this.$preview.html('')
     this.$element.find('.fileinput-filename').text('')
     this.$element.addClass('fileinput-new').removeClass('fileinput-exists')
-    
+
     if (e !== undefined) {
       this.$input.trigger('change')
       this.$element.trigger('clear.bs.fileinput')
@@ -612,7 +613,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
 
     if (this.original.exists) this.$element.addClass('fileinput-exists').removeClass('fileinput-new')
      else this.$element.addClass('fileinput-new').removeClass('fileinput-exists')
-    
+
     this.$element.trigger('reset.bs.fileinput')
   },
 
@@ -621,12 +622,12 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
     e.preventDefault()
   }
 
-  
+
   // FILEUPLOAD PLUGIN DEFINITION
   // ===========================
 
   var old = $.fn.fileinput
-  
+
   $.fn.fileinput = function (options) {
     return this.each(function () {
       var $this = $(this),
@@ -655,7 +656,7 @@ if (typeof jQuery === 'undefined') { throw new Error('Jasny Bootstrap\'s JavaScr
     var $this = $(this)
     if ($this.data('bs.fileinput')) return
     $this.fileinput($this.data())
-      
+
     var $target = $(e.target).closest('[data-dismiss="fileinput"],[data-trigger="fileinput"]');
     if ($target.length > 0) {
       e.preventDefault()
