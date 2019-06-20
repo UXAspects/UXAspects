7359b470cad033193e65a0dd78c3b54f13e3a919
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2019-06-20 15:03:13 +0100

    UX Icon documentation

--- docs/app/pages/css/css-sections/icons/icons.module.ts
+++ docs/app/pages/css/css-sections/icons/icons.module.ts
@@ -1,18 +1,19 @@
-import { NgModule, ComponentFactoryResolver } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { DocumentationComponentsModule } from '../../../../components/components.module';
-import { ResolverService, DocumentationPage } from '../../../../services/resolver/resolver.service';
-import { DocumentationCategoryComponent } from '../../../../components/documentation-category/documentation-category.component';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
-import { CssIconColorsComponent } from './icon-colors/icon-colors.component';
-import { CssIconButtonsComponent } from './icon-buttons/icon-buttons.component';
-import { CssRotateFlipIconsComponent } from './rotate-flip-icons/rotate-flip-icons.component';
-import { CssFixedWidthComponent } from './fixed-width/fixed-width.component';
-import { CssIconSizeComponent } from './icon-size/icon-size.component';
+import { ComponentFactoryResolver, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { SelectModule } from '@ux-aspects/ux-aspects';
+import { DocumentationComponentsModule } from '../../../../components/components.module';
+import { DocumentationCategoryComponent } from '../../../../components/documentation-category/documentation-category.component';
+import { DocumentationPage, ResolverService } from '../../../../services/resolver/resolver.service';
 import { CssBasicUsageComponent } from './basic-usage/basic-usage.component';
+import { CssFixedWidthComponent } from './fixed-width/fixed-width.component';
+import { CssIconButtonsComponent } from './icon-buttons/icon-buttons.component';
+import { CssIconColorsComponent } from './icon-colors/icon-colors.component';
+import { CssIconSizeComponent } from './icon-size/icon-size.component';
+import { CssRotateFlipIconsComponent } from './rotate-flip-icons/rotate-flip-icons.component';
 import { CssUxIconsComponent } from './ux-icons/ux-icons.component';
+
 
 const SECTIONS = [
     CssUxIconsComponent,
@@ -39,6 +40,7 @@ const ROUTES = [
         DocumentationComponentsModule,
         CommonModule,
         FormsModule,
+        SelectModule,
         RouterModule.forChild(ROUTES)
     ],
     exports: SECTIONS,
