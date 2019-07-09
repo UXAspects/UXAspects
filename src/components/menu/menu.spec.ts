04e17941415afc4c464dced7e5147a39bf99fcc9
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2019-07-09 14:48:11 +0100

    Update menu.spec.ts

--- src/components/menu/menu.spec.ts
+++ src/components/menu/menu.spec.ts
@@ -6,8 +6,8 @@ import { MenuTriggerDirective } from './menu-trigger/menu-trigger.directive';
 import { MenuModule } from './menu.module';
 
 @Component({
-  selector: 'app-menu-test',
-  template: `
+    selector: 'app-menu-test',
+    template: `
   <ux-menu #menu>
     <button uxMenuItem>Item One</button>
     <ux-menu-divider></ux-menu-divider>
@@ -26,276 +26,276 @@ import { MenuModule } from './menu.module';
 })
 export class MenuTestComponent {
 
-  @ViewChild('menuTrigger', { static: true }) trigger: MenuTriggerDirective;
-  @ViewChild('subMenuTrigger', { static: true }) subMenuTrigger: MenuTriggerDirective;
+    @ViewChild('menuTrigger', { static: true } as any) trigger: MenuTriggerDirective;
+    @ViewChild('subMenuTrigger', { static: true } as any) subMenuTrigger: MenuTriggerDirective;
 
 }
 
 describe('MenuComponent', () => {
-  let component: MenuTestComponent;
-  let fixture: ComponentFixture<MenuTestComponent>;
-  let nativeElement: HTMLElement;
-  let triggerElement: HTMLButtonElement;
-  let overlayContainer: OverlayContainer;
-  let overlayContainerElement: HTMLElement;
+    let component: MenuTestComponent;
+    let fixture: ComponentFixture<MenuTestComponent>;
+    let nativeElement: HTMLElement;
+    let triggerElement: HTMLButtonElement;
+    let overlayContainer: OverlayContainer;
+    let overlayContainerElement: HTMLElement;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [MenuModule, NoopAnimationsModule],
-      declarations: [MenuTestComponent],
-    })
-      .compileComponents();
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [MenuModule, NoopAnimationsModule],
+            declarations: [MenuTestComponent],
+        })
+            .compileComponents();
 
-    // access the overlay container
-    inject([OverlayContainer], (oc: OverlayContainer) => {
-      overlayContainer = oc;
-      overlayContainerElement = oc.getContainerElement();
-    })();
-  }));
+        // access the overlay container
+        inject([OverlayContainer], (oc: OverlayContainer) => {
+            overlayContainer = oc;
+            overlayContainerElement = oc.getContainerElement();
+        })();
+    }));
 
-  afterEach(() => {
-    overlayContainer.ngOnDestroy();
-  });
+    afterEach(() => {
+        overlayContainer.ngOnDestroy();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MenuTestComponent);
-    component = fixture.componentInstance;
-    nativeElement = fixture.nativeElement;
-    triggerElement = nativeElement.querySelector('#trigger');
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MenuTestComponent);
+        component = fixture.componentInstance;
+        nativeElement = fixture.nativeElement;
+        triggerElement = nativeElement.querySelector('#trigger');
 
-    fixture.detectChanges();
-  });
+        fixture.detectChanges();
+    });
 
-  it('should create not intially show the menus', () => {
-    // attempt to find the .ux-menu elements
-    expect(document.querySelectorAll('.ux-menu').length).toBe(0);
-  });
+    it('should create not intially show the menus', () => {
+        // attempt to find the .ux-menu elements
+        expect(document.querySelectorAll('.ux-menu').length).toBe(0);
+    });
 
-  it('should show the menu on trigger click', () => {
-    // perform a click on the trigger element
-    triggerElement.click();
+    it('should show the menu on trigger click', () => {
+        // perform a click on the trigger element
+        triggerElement.click();
 
-    // run change detection
-    fixture.detectChanges();
+        // run change detection
+        fixture.detectChanges();
 
-    // only the root level menu item should be open
-    expect(document.querySelectorAll('.ux-menu').length).toBe(1);
-  });
+        // only the root level menu item should be open
+        expect(document.querySelectorAll('.ux-menu').length).toBe(1);
+    });
 
-  it('should hide when the trigger is clicked again', async () => {
-    // perform a click on the trigger element
-    triggerElement.click();
+    it('should hide when the trigger is clicked again', async () => {
+        // perform a click on the trigger element
+        triggerElement.click();
 
-    // run change detection
-    fixture.detectChanges();
+        // run change detection
+        fixture.detectChanges();
 
-    // only the root level menu item should be open
-    expect(document.querySelectorAll('.ux-menu').length).toBe(1);
+        // only the root level menu item should be open
+        expect(document.querySelectorAll('.ux-menu').length).toBe(1);
 
-    // perform a click on the overlay
-    triggerElement.click();
+        // perform a click on the overlay
+        triggerElement.click();
 
-    // run change detection
-    fixture.detectChanges();
+        // run change detection
+        fixture.detectChanges();
 
-    // allow noop animation to complete
-    await Promise.resolve();
+        // allow noop animation to complete
+        await Promise.resolve();
 
-    // no menus should be visible
-    expect(document.querySelectorAll('.ux-menu').length).toBe(0);
-  });
+        // no menus should be visible
+        expect(document.querySelectorAll('.ux-menu').length).toBe(0);
+    });
 
-  it('should hide when the outside the menu is clicked', async () => {
-    // perform a click on the trigger element
-    triggerElement.click();
+    it('should hide when the outside the menu is clicked', async () => {
+        // perform a click on the trigger element
+        triggerElement.click();
 
-    // run change detection
-    fixture.detectChanges();
+        // run change detection
+        fixture.detectChanges();
 
-    // only the root level menu item should be open
-    expect(document.querySelectorAll('.ux-menu').length).toBe(1);
+        // only the root level menu item should be open
+        expect(document.querySelectorAll('.ux-menu').length).toBe(1);
 
-    // get the backdrop element
-    const backdrop = overlayContainerElement.querySelector('.cdk-overlay-backdrop') as HTMLDivElement;
-    expect(backdrop).toBeTruthy();
+        // get the backdrop element
+        const backdrop = overlayContainerElement.querySelector('.cdk-overlay-backdrop') as HTMLDivElement;
+        expect(backdrop).toBeTruthy();
 
-    // perform a click on the overlay
-    backdrop.click();
+        // perform a click on the overlay
+        backdrop.click();
 
-    // run change detection
-    fixture.detectChanges();
+        // run change detection
+        fixture.detectChanges();
 
-    // allow noop animation to complete
-    await Promise.resolve();
+        // allow noop animation to complete
+        await Promise.resolve();
 
-    // no menus should be visible
-    expect(document.querySelectorAll('.ux-menu').length).toBe(0);
-  });
+        // no menus should be visible
+        expect(document.querySelectorAll('.ux-menu').length).toBe(0);
+    });
 
-  it('should show the menu when the trigger is programmatically called', () => {
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    expect(document.querySelectorAll('.ux-menu').length).toBe(1);
-  });
+    it('should show the menu when the trigger is programmatically called', () => {
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        expect(document.querySelectorAll('.ux-menu').length).toBe(1);
+    });
 
-  it('should close the menu when the trigger is programmatically called', async () => {
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    await Promise.resolve();
-    expect(document.querySelectorAll('.ux-menu').length).toBe(1);
-    component.trigger.closeMenu();
-    fixture.detectChanges();
-    await Promise.resolve();
-    expect(document.querySelectorAll('.ux-menu').length).toBe(0);
-  });
+    it('should close the menu when the trigger is programmatically called', async () => {
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        await Promise.resolve();
+        expect(document.querySelectorAll('.ux-menu').length).toBe(1);
+        component.trigger.closeMenu();
+        fixture.detectChanges();
+        await Promise.resolve();
+        expect(document.querySelectorAll('.ux-menu').length).toBe(0);
+    });
 
-  it('should toggle the menu when the trigger is programmatically called', async () => {
-    component.trigger.toggleMenu();
-    fixture.detectChanges();
-    await Promise.resolve();
-    expect(document.querySelectorAll('.ux-menu').length).toBe(1);
-    component.trigger.toggleMenu();
-    fixture.detectChanges();
-    await Promise.resolve();
-    expect(document.querySelectorAll('.ux-menu').length).toBe(0);
-  });
+    it('should toggle the menu when the trigger is programmatically called', async () => {
+        component.trigger.toggleMenu();
+        fixture.detectChanges();
+        await Promise.resolve();
+        expect(document.querySelectorAll('.ux-menu').length).toBe(1);
+        component.trigger.toggleMenu();
+        fixture.detectChanges();
+        await Promise.resolve();
+        expect(document.querySelectorAll('.ux-menu').length).toBe(0);
+    });
 
-  it('should have the correct number of menu items', () => {
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    // find the menu items
-    expect(document.querySelectorAll('button[uxmenuitem]').length).toBe(3);
-  });
+    it('should have the correct number of menu items', () => {
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        // find the menu items
+        expect(document.querySelectorAll('button[uxmenuitem]').length).toBe(3);
+    });
 
-  it('should display the menu divider', () => {
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    // find the menu items
-    expect(document.querySelectorAll('ux-menu-divider').length).toBe(1);
-  });
+    it('should display the menu divider', () => {
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        // find the menu items
+        expect(document.querySelectorAll('ux-menu-divider').length).toBe(1);
+    });
 
-  it('should update the aria-expanded state on the trigger', () => {
-    expect(triggerElement.getAttribute('aria-haspopup')).toBe('true');
-    expect(triggerElement.getAttribute('aria-expanded')).toBe('false');
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    expect(triggerElement.getAttribute('aria-expanded')).toBe('true');
-  });
+    it('should update the aria-expanded state on the trigger', () => {
+        expect(triggerElement.getAttribute('aria-haspopup')).toBe('true');
+        expect(triggerElement.getAttribute('aria-expanded')).toBe('false');
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        expect(triggerElement.getAttribute('aria-expanded')).toBe('true');
+    });
 
-  it('should make only the first menu item tabbable', async () => {
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    await Promise.resolve();
+    it('should make only the first menu item tabbable', async () => {
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        await Promise.resolve();
 
-    const items = document.querySelectorAll('button[uxmenuitem]');
+        const items = document.querySelectorAll('button[uxmenuitem]');
 
-    expect(items.item(0).getAttribute('tabindex')).toBe('0');
-    expect(items.item(1).getAttribute('tabindex')).toBe('-1');
-    expect(items.item(2).getAttribute('tabindex')).toBe('-1');
-  });
+        expect(items.item(0).getAttribute('tabindex')).toBe('0');
+        expect(items.item(1).getAttribute('tabindex')).toBe('-1');
+        expect(items.item(2).getAttribute('tabindex')).toBe('-1');
+    });
 
-  it('should focus the first item when the menu is shown', async () => {
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    await Promise.resolve();
+    it('should focus the first item when the menu is shown', async () => {
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        await Promise.resolve();
 
-    const items = document.querySelectorAll('button[uxmenuitem]');
+        const items = document.querySelectorAll('button[uxmenuitem]');
 
-    expect(document.activeElement).toBe(items.item(0));
-  });
+        expect(document.activeElement).toBe(items.item(0));
+    });
 
-  it('should focus the menu trigger when the menu is closed', async () => {
+    it('should focus the menu trigger when the menu is closed', async () => {
 
-    // open menu
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    await Promise.resolve();
+        // open menu
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        await Promise.resolve();
 
-    // close menu by clicking on an item
-    const items = document.querySelectorAll<HTMLButtonElement>('button[uxmenuitem]');
-    items.item(0).click();
-    fixture.detectChanges();
-    await Promise.resolve();
+        // close menu by clicking on an item
+        const items = document.querySelectorAll<HTMLButtonElement>('button[uxmenuitem]');
+        items.item(0).click();
+        fixture.detectChanges();
+        await Promise.resolve();
 
-    // the trigger element should be focused
-    expect(document.activeElement).toBe(triggerElement);
-  });
+        // the trigger element should be focused
+        expect(document.activeElement).toBe(triggerElement);
+    });
 
-  it('should show the submenu when the menu item is hovered', async () => {
-    // open menu
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    await Promise.resolve();
+    it('should show the submenu when the menu item is hovered', async () => {
+        // open menu
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        await Promise.resolve();
 
-    // dispatch a mouse enter event
-    component.subMenuTrigger._onMouseEnter();
+        // dispatch a mouse enter event
+        component.subMenuTrigger._onMouseEnter();
 
-    fixture.detectChanges();
-    await Promise.resolve();
+        fixture.detectChanges();
+        await Promise.resolve();
 
-    expect(document.querySelectorAll('.ux-menu').length).toBe(2);
-  });
+        expect(document.querySelectorAll('.ux-menu').length).toBe(2);
+    });
 
-  it('should close all menus when backdrop is clicked', async () => {
-    // open menu
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    await Promise.resolve();
+    it('should close all menus when backdrop is clicked', async () => {
+        // open menu
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        await Promise.resolve();
 
-    // dispatch a mouse enter event
-    component.subMenuTrigger._onMouseEnter();
+        // dispatch a mouse enter event
+        component.subMenuTrigger._onMouseEnter();
 
-    fixture.detectChanges();
-    await Promise.resolve();
+        fixture.detectChanges();
+        await Promise.resolve();
 
-    // get the backdrop element
-    const backdrop = overlayContainerElement.querySelector('.cdk-overlay-backdrop') as HTMLDivElement;
-    expect(backdrop).toBeTruthy();
+        // get the backdrop element
+        const backdrop = overlayContainerElement.querySelector('.cdk-overlay-backdrop') as HTMLDivElement;
+        expect(backdrop).toBeTruthy();
 
-    // perform a click on the overlay
-    backdrop.click();
+        // perform a click on the overlay
+        backdrop.click();
 
-    // run change detection
-    fixture.detectChanges();
+        // run change detection
+        fixture.detectChanges();
 
-    // allow noop animation to complete
-    await Promise.resolve();
+        // allow noop animation to complete
+        await Promise.resolve();
 
-    // no menus should be visible
-    expect(document.querySelectorAll('.ux-menu').length).toBe(0);
-  });
+        // no menus should be visible
+        expect(document.querySelectorAll('.ux-menu').length).toBe(0);
+    });
 
-  it('should close the menu when an item is clicked', async () => {
-    // open menu
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    await Promise.resolve();
+    it('should close the menu when an item is clicked', async () => {
+        // open menu
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        await Promise.resolve();
 
-    // close menu by clicking on an item
-    const items = document.querySelectorAll<HTMLButtonElement>('button[uxmenuitem]');
-    items.item(0).click();
-    fixture.detectChanges();
-    await Promise.resolve();
-    expect(document.querySelectorAll('.ux-menu').length).toBe(0);
-  });
+        // close menu by clicking on an item
+        const items = document.querySelectorAll<HTMLButtonElement>('button[uxmenuitem]');
+        items.item(0).click();
+        fixture.detectChanges();
+        await Promise.resolve();
+        expect(document.querySelectorAll('.ux-menu').length).toBe(0);
+    });
 
-  it('should close both menus when a menu item is clicked', async () => {
-    // open menu
-    component.trigger.openMenu();
-    fixture.detectChanges();
-    await Promise.resolve();
+    it('should close both menus when a menu item is clicked', async () => {
+        // open menu
+        component.trigger.openMenu();
+        fixture.detectChanges();
+        await Promise.resolve();
 
-    // dispatch a mouse enter event
-    component.subMenuTrigger._onMouseEnter();
+        // dispatch a mouse enter event
+        component.subMenuTrigger._onMouseEnter();
 
-    fixture.detectChanges();
-    await Promise.resolve();
+        fixture.detectChanges();
+        await Promise.resolve();
 
-    // close menu by clicking on an item
-    const items = document.querySelectorAll<HTMLButtonElement>('button[uxmenuitem]');
-    items.item(0).click();
-    fixture.detectChanges();
-    await Promise.resolve();
-    expect(document.querySelectorAll('.ux-menu').length).toBe(0);
-  });
+        // close menu by clicking on an item
+        const items = document.querySelectorAll<HTMLButtonElement>('button[uxmenuitem]');
+        items.item(0).click();
+        fixture.detectChanges();
+        await Promise.resolve();
+        expect(document.querySelectorAll('.ux-menu').length).toBe(0);
+    });
 });
