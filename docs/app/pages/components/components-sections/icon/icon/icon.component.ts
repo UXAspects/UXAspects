bc588cb3fa9cb5438c09d4b566a1a76b6114e590
TaraTodd <tara.todd@microfocus.com>
2019-08-06 11:12:59 +0100

    ChangeDetectionStrategy.OnPush - preformance issues

--- docs/app/pages/components/components-sections/icon/icon/icon.component.ts
+++ docs/app/pages/components/components-sections/icon/icon/icon.component.ts
@@ -1,14 +1,18 @@
-import { Component, Inject } from '@angular/core';
-import { BaseDocumentationSection } from '../../../../../components/base-documentation-section/base-documentation-section';
-import { DocumentationSectionComponent } from '../../../../../decorators/documentation-section-component';
-import { IPlayground } from '../../../../../interfaces/IPlayground';
-import { IPlaygroundProvider } from '../../../../../interfaces/IPlaygroundProvider';
-import { DOCUMENTATION_TOKEN, DocumentationType } from '../../../../../services/playground/tokens/documentation.token';
+import {ChangeDetectionStrategy, Component, Inject} from '@angular/core';
+import {BaseDocumentationSection} from '../../../../../components/base-documentation-section/base-documentation-section';
+import {DocumentationSectionComponent} from '../../../../../decorators/documentation-section-component';
+import {IPlayground} from '../../../../../interfaces/IPlayground';
+import {IPlaygroundProvider} from '../../../../../interfaces/IPlaygroundProvider';
+import {
+    DOCUMENTATION_TOKEN,
+    DocumentationType
+} from '../../../../../services/playground/tokens/documentation.token';
 
 @Component({
     selector: 'uxd-components-icon',
     templateUrl: './icon.component.html',
-    styleUrls: ['./icon.component.less']
+    styleUrls: ['./icon.component.less'],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 
 @DocumentationSectionComponent('ComponentsIconComponent')
--- docs/app/pages/css/css-sections/icons/icons.module.ts
+++ docs/app/pages/css/css-sections/icons/icons.module.ts
@@ -26,6 +26,7 @@ const SECTIONS = [
     CssRotateFlipIconsComponent,
     CssIconButtonsComponent,
     CssIconColorsComponent,
+    IconPreviewComponent,
     IconSnippetComponent
 ];
 
