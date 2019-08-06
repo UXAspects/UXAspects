67ae52d297f72f823574db33a341f72b8aa1ca0c
donna-marijne <donna.worby@microfocus.com>
2019-08-06 10:39:55 +0100

    Number Picker

--- src/components/number-picker/number-picker.module.ts
+++ src/components/number-picker/number-picker.module.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-
+import { IconModule } from '../icon/index';
 import { NumberPickerComponent } from './number-picker.component';
 
 @NgModule({
     imports: [
         CommonModule,
+        IconModule,
         FormsModule
     ],
     exports: [NumberPickerComponent],
