c670e8634c49fa2a1c359ace066ad7a5c59c2c49
TaraTodd <tara.todd@microfocus.com>
2019-08-13 10:57:25 +0100

    Fixing plunker and removing redundant code

--- docs/app/pages/components/components-sections/input-controls/typeahead/snippets/app.ts
+++ docs/app/pages/components/components-sections/input-controls/typeahead/snippets/app.ts
@@ -1,47 +1,38 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { TypeaheadKeyService, TypeaheadOptionEvent } from '@ux-aspects/ux-aspects';
 import { BehaviorSubject } from 'rxjs';
 
 @Component({
-    selector: 'uxd-components-typeahead',
-    templateUrl: 'typeahead.component.html',
-    styleUrls: ['./typeahead.component.less'],
+    selector: 'app',
+    templateUrl: 'app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
 
     dropdownOpened: boolean = false;
-
     selectOnEnter: boolean = false;
-
-    private _input$ = new BehaviorSubject<string>('');
-
-    private  _value$ = new BehaviorSubject<any>(null);
-
     dropDirection: 'up' | 'down' = 'down';
-
     selectFirst: boolean = false;
 
+    private _input$ = new BehaviorSubject<string>('');
+    private  _value$ = new BehaviorSubject<string>(null);
+
+    /** Load the options and filter the them */
     loadOptions(pageNum: number, pageSize: number, filter: string) {
 
-        const allTags = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega'];
-
-        return allTags.filter(tag => tag.toLowerCase().indexOf(filter.toLowerCase()) !== -1).slice(pageNum * pageSize, (pageNum + 1) * pageSize);
+        const displayValues = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega'];
+        return displayValues.filter(tag => tag.toLowerCase().indexOf(filter.toLowerCase()) !== -1).slice(pageNum * pageSize, (pageNum + 1) * pageSize);
     }
 
-    @ViewChild('singleInput', { static: false }) singleInput: ElementRef;
-
-
-    /** The selected option */
-    @Input()
-    set value(value: any) {
+    /** The selected option in input field */
+    set value(value: string) {
         this._value$.next(value);
     }
     get value() {
         return this._value$.value;
     }
 
-    /** selected option and closing dropdown **/
-
+    /** selected typeahead option and closing dropdown **/
     singleOptionSelected(event: TypeaheadOptionEvent): void {
         if (event.option) {
             this.value = event.option;
@@ -49,14 +40,12 @@ export class AppComponent {
         }
     }
 
-
-    /** Opening the dropdown with filter options. */
+    /** Opening the typeahead dropdown with filter options. */
     dropdown() {
         this.dropdownOpened = true;
     }
 
-    /** The text in the input area. This is used to filter the options dropdown. */
-    @Input()
+    /** The text in the input area. This is used as the filter value. */
     set input(value: string) {
         this._input$.next(value);
     }
