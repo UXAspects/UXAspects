1a61caf83bceee0b22cbfe1bd8b804e2ad2fb5e4
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2019-08-14 14:38:21 +0100

    Starting the icon improvements

--- src/components/checkbox/checkbox.module.ts
+++ src/components/checkbox/checkbox.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccessibilityModule } from '../../directives/accessibility/index';
+import { IconModule } from '../icon/index';
 import { CheckboxComponent } from './checkbox.component';
-
 
 @NgModule({
     imports: [
         AccessibilityModule,
-        FormsModule
+        FormsModule,
+        IconModule
     ],
     exports: [
         CheckboxComponent
--- src/components/color-picker/color-picker.module.ts
+++ src/components/color-picker/color-picker.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccessibilityModule } from '../../directives/accessibility/index';
+import { IconModule } from '../icon/index';
 import { NumberPickerModule } from '../number-picker/index';
 import { TooltipModule } from '../tooltip/index';
 import { ColorPickerComponent } from './color-picker.component';
@@ -13,6 +14,7 @@ import { ColorPickerComponent } from './color-picker.component';
         FormsModule,
         NumberPickerModule,
         TooltipModule,
+        IconModule
     ],
     exports: [
         ColorPickerComponent
