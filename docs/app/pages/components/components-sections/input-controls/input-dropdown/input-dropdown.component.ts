c4ca66af7beafe3e7c650e9308b347bbf4222b1f
Tobias Schreiner <Tobias.Schreiner@microfocus.com>
2019-08-16 11:39:00 +0200

    Input dropdown component (#1161)
    * Select custom component: initial commit
    
    * Select custom component: fix plunkr
    
    * Select custom component: add keyboard navigation
    
    * Select custom component: remove bootstrap dependency; adapt to select style
    
    * Select custom component: remove bootstrap dependency; adapt to select style (fix trailing whitespace)
    
    * Select custom component: remove bootstrap dependency; adapt to select style (fix unit tests)
    
    * Rebasing on top of latest changes
    
    * Select custom component: fix build / test errors, some smaller enhancements
    Disable page header tests
    
    * Select custom component: fix build / test errors, some smaller enhancements
    Disable page header tests
    
    * Select custom component: use a highlight pipe in the demo component
    
    * Select custom component: remove debouncing
    
    * Select custom component: remove width attribute
    
    * Select custom component: use filtered list in demo
    
    * Select custom component: close menu after selection
    
    * Select custom component: close menu after selection (bug fix)
    
    * Select custom component: use tabbable list for demo component
    
    * Select custom component: use form-control class
    
    * Select custom component: fix build error
    
    * Select custom component: refactor in preparation of Micro Focus styling
    
    * Select custom component: rename to Input Dropdown
    
    * Input dropdown component: change embedded templates
    
    * Input dropdown component: remove busy indicator
    
    * Input dropdown component: incorporate documentation feedback
    
    * Input dropdown component: correct typo from renaming
    
    * Input dropdown component: incorporate feedback
    
    * Input dropdown component: incorporate feedback, part two
    
    * Input dropdown component: incorporate feedback, fix
    
    * Input dropdown component: incorporate feedback, third part
    
    * Input dropdown component: incorporate feedback, part four
    
    * Input dropdown component: incorporate feedback, part five
    
    * Input dropdown component: incorporate feedback, part six
    
    * Input dropdown component: incorporate feedback, part seven

---
+++ docs/app/pages/components/components-sections/input-controls/input-dropdown/input-dropdown.component.ts
@@ -0,0 +1,82 @@
+import { Component, Pipe, PipeTransform } from '@angular/core';
+import { BaseDocumentationSection } from '../../../../../components/base-documentation-section/base-documentation-section';
+import { DocumentationSectionComponent } from '../../../../../decorators/documentation-section-component';
+import { IPlayground } from '../../../../../interfaces/IPlayground';
+import { IPlaygroundProvider } from '../../../../../interfaces/IPlaygroundProvider';
+
+
+@Pipe({name: 'highlightSearch'})
+export class HighlightSearch implements PipeTransform {
+    transform(text: string, filter: string): string {
+        const highlightIndex = text.toLowerCase().indexOf(filter.toLowerCase());
+        return (highlightIndex < 0) ?
+               text :
+               text.substr(0, highlightIndex) +
+                 '<b>' + text.substr(highlightIndex, filter.length) + '</b>' +
+                 text.substr(highlightIndex + filter.length);
+    }
+}
+
+
+@Component({
+    selector: 'uxd-components-input-dropdown',
+    templateUrl: './input-dropdown.component.html',
+    styleUrls: ['./input-dropdown.component.less']
+})
+@DocumentationSectionComponent('ComponentsInputDropdownComponent')
+export class ComponentsInputDropdownComponent extends BaseDocumentationSection implements IPlaygroundProvider {
+
+    selected: RadioOption;
+    optionList: ReadonlyArray<RadioOption> = [
+        { name: 'One' }, { name: 'Two' }, { name: 'Three' }, { name: 'Four' }
+    ];
+    filteredOptionList: ReadonlyArray<RadioOption> = this.optionList;
+    filter: string = '';
+    allowNull: boolean = false;
+    maxHeight: string = '400px';
+    placeholder: string = 'Type to search...';
+
+    playground: IPlayground = {
+        files: {
+            'app.component.html': this.snippets.raw.appHtml,
+            'app.component.ts': this.snippets.raw.appTs,
+            'app.component.css': this.snippets.raw.appCss
+        },
+        modules: [
+            {
+                imports: ['HighlightSearch'],
+                library: './app.component',
+                declaration: true
+            }, 
+            {
+                imports: ['AccordionModule', 'CheckboxModule', 'NumberPickerModule', 'RadioButtonModule', 'InputDropdownModule'],
+                library: '@ux-aspects/ux-aspects'
+            }
+        ]
+    };
+
+    constructor() {
+        super(require.context('./snippets/', false, /\.(html|css|js|ts)$/));
+    }
+
+    private index(text: string): number {
+        return text.toLowerCase().indexOf(this.filter.toLowerCase());
+    }
+
+    selectOption(event: KeyboardEvent, option: RadioOption): void {
+        this.selected = option;
+        event.preventDefault();
+    }
+
+    setFilter(filter: string): void {
+        this.filter = filter;
+        this.filteredOptionList =
+          this.filter && (this.filter.length > 0) ?
+          this.optionList.filter(option => (this.index(option.name) > -1)) :
+          this.optionList;
+    }
+}
+
+export interface RadioOption {
+    name: string;
+}
---
+++ docs/app/pages/components/components-sections/input-controls/input-dropdown/snippets/app.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit, Pipe, PipeTransform } from '@angular/core';
+
+
+@Pipe({name: 'highlightSearch'})
+export class HighlightSearch implements PipeTransform {
+    transform(text: string, filter: string): string {
+        const highlightIndex = text.toLowerCase().indexOf(filter.toLowerCase());
+        return (highlightIndex < 0) ?
+               text :
+               text.substr(0, highlightIndex) +
+                 '<b>' + text.substr(highlightIndex, filter.length) + '</b>' +
+                 text.substr(highlightIndex + filter.length);
+    }
+}
+
+
+@Component({
+    selector: 'app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+    selected: RadioOption;
+    optionList: ReadonlyArray<RadioOption> = [
+        { name: 'One' }, { name: 'Two' }, { name: 'Three' }, { name: 'Four' }
+    ];
+    filteredOptionList: ReadonlyArray<RadioOption> = this.optionList;
+    filter: string = '';
+    allowNull: boolean = false;
+    maxHeight: string = '400px';
+    placeholder: string = 'Type to search...';
+
+    private index(text: string): number {
+        return text.toLowerCase().indexOf(this.filter.toLowerCase());
+    }
+
+    selectOption(event: KeyboardEvent, option: RadioOption): void {
+        this.selected = option;
+        event.preventDefault();
+    }
+
+    setFilter(filter: string): void {
+        this.filter = filter;
+        this.filteredOptionList =
+          this.filter && (this.filter.length > 0) ?
+          this.optionList.filter(option => (this.index(option.name) > -1)) :
+          this.optionList;
+    }
+}
+
+export interface RadioOption {
+    name: string;
+}
