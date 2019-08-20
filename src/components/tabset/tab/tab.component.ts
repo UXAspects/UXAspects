2080c28c5f210ec767587960991f4818d0bfc86d
TaraTodd <tara.todd@microfocus.com>
2019-08-20 14:09:11 +0100

    uncommited file

--- src/components/tabset/tab/tab.component.ts
+++ src/components/tabset/tab/tab.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChild, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChild, EventEmitter, Input, OnDestroy, OnInit, Output, TemplateRef } from '@angular/core';
 import { TabsetService } from '../tabset.service';
 import { TabHeadingDirective } from './tab-heading.directive';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 let uniqueTabId = 0;
 
@@ -9,7 +11,7 @@ let uniqueTabId = 0;
     templateUrl: './tab.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class TabComponent {
+export class TabComponent implements OnInit, OnDestroy {
 
     /** Define the tab unique id */
     @Input() id: string = `ux-tab-${++uniqueTabId}`;
@@ -26,19 +28,51 @@ export class TabComponent {
     /** provide a custom class for the tab */
     @Input() customClass: string;
 
-    /** Emit when this tab is selected */
+    /** @deprecated Emit when this tab is selected - use activated output instead */
+        // tslint:disable-next-line
     @Output() select = new EventEmitter<void>();
 
-    /** Emit when this tab is deselected */
+    /** Emit when this tab is selected */
+    @Output() activated = new EventEmitter<void>();
+
+    /** @deprecated Emit when this tab is deselected - - use deactivated output instead */
     @Output() deselect = new EventEmitter<void>();
+
+    /** Emit when this tab is deselected */
+    @Output() deactivated = new EventEmitter<void>();
 
     /** Store a custom header templateRef */
     @ContentChild(TabHeadingDirective, { read: TemplateRef, static: false }) headingRef: TemplateRef<void>;
+
+    /** Unsubscribe from all subscriptions when component is destroyed */
+    private _onDestroy = new Subject<void>();
 
     constructor(
         private readonly _tabset: TabsetService,
         private readonly _changeDetector: ChangeDetectorRef
     ) { }
+
+    ngOnInit(): void {
+        // warn anyone using the select output that it is deprecated
+        if (this.select.observers.length > 0) {
+            console.warn('Tab Component - (select) output is deprecated use activated instead');
+        }
+
+        // warn anyone using the deselect output that it is deprecated
+        if (this.deselect.observers.length > 0) {
+            console.warn('Tab Component - (deselect) output is deprecated use deactivated instead');
+        }
+
+        // for backwards compatibility emit deprecated select event
+        this.activated.pipe(takeUntil(this._onDestroy)).subscribe(() => this.select.emit());
+
+        // for backwards compatibility emit deprecated deselect event
+        this.deactivated.pipe(takeUntil(this._onDestroy)).subscribe(() => this.deselect.emit());
+    }
+
+    ngOnDestroy(): void {
+        this._onDestroy.next();
+    }
 
     selectTab(): void {
         // if this tab is currently active do nothing
@@ -50,7 +84,7 @@ export class TabComponent {
             this.active = true;
         }
 
-        this.select.emit();
+        this.activated.emit();
 
         this._changeDetector.detectChanges();
     }
@@ -66,7 +100,7 @@ export class TabComponent {
             this.active = false;
         }
 
-        this.deselect.emit();
+        this.deactivated.emit();
 
         this._changeDetector.detectChanges();
     }
