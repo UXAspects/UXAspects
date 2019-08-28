0682e65d005aedd5b6630cf9b63c7cab7d8fbe09
TaraTodd <tara.todd@microfocus.com>
2019-08-28 13:49:16 +0100

    update karma file

--- karma.conf.js
+++ karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function (config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['ChromeHeadlessNoSandbox'],
         customLaunchers: {
             ChromeHeadlessNoSandbox: {
                 base: 'ChromeHeadless',
