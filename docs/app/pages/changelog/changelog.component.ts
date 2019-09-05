5be14975f34c385d485aef0809a4ccb857f462bd
donna-marijne <donnamarijne+gh@gmail.com>
2019-09-05 10:20:29 +0100

    Release 1.8.4 (#1189)

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -16,6 +16,11 @@ export class ChangeLogPageComponent {
 
         this.logs = [
             {
+                version: '1.8.4',
+                date: 'September 5th 2019',
+                content: require('./logs/release-v1.8.4.md')
+            },
+            {
                 version: '1.8.3',
                 date: 'August 22nd 2019',
                 content: require('./logs/release-v1.8.3.md')
