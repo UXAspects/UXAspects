ff790c18afe51ba2e950ff0d640f479e306e6024
TaraTodd <tara.todd@microfocus.com>
2019-09-25 08:51:55 +0100

    update

--- src/ng1/directives/infiniteScroll/infiniteScroll.directive.js
+++ src/ng1/directives/infiniteScroll/infiniteScroll.directive.js
@@ -55,7 +55,7 @@ export default function infiniteScroll($compile, $templateRequest) {
 
                 const indicator = angular.element(`
                     <div class="infinite-scroll-loading" ng-show="$ctrl.loading">
-                        <div class="spinner spinner-bounce-middle spinner-accent"></div>
+                        <div class="spinner spinner-accent spinner-bounce-middle"></div>
                         <span class="spinner-text">Loading...</span>
                     </div>
                 `);
