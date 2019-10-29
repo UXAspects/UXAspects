e9e2cb5c0cc4e92e4cdd49483e4fe70df70a31d0
TaraTodd <tara.todd@microfocus.com>
2019-10-29 16:13:48 +0000

    screenshots update with fullscreen

--- e2e/tests/components/common/image-compare.ts
+++ e2e/tests/components/common/image-compare.ts
@@ -10,3 +10,13 @@ import { browser } from 'protractor';
 export async function imageCompare(name: string): Promise<number> {
     return platform() === 'linux' ? await browser.imageComparison.checkScreen(name) : 0;
 }
+
+/**
+ * Compare the full page screenshots to detect visual changes.
+ *
+ * We can only compare on a Linux environment, otherwise font rendering
+ * is different and we will always have variations causing tests to fail.
+ */
+export async function imageCompareFullPageScreen(name: string): Promise<number> {
+    return platform() === 'linux' ? await browser.imageComparison.checkFullPageScreen(name) : 0;
+}
