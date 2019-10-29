5c70874a4e427404a476342de6828ba618d81c64
Dermot.Hughes.76004559 <Dermot.Hughes@microfocus.com>
2019-10-29 13:19:55 +0000

    Added scroll to top for screenshot tests

--- e2e/tests/components/select/standard/select.e2e-spec.ts
+++ e2e/tests/components/select/standard/select.e2e-spec.ts
@@ -1,4 +1,4 @@
-import { Key } from 'protractor';
+import { browser, Key } from 'protractor';
 import { imageCompare } from '../../common/image-compare';
 import { numberOfCountries, SelectPage } from './select.po.spec';
 
@@ -538,7 +538,7 @@ describe('Select Tests', () => {
         expect(await imageCompare('select-tag-overflow')).toEqual(0);
     });
 
-    it('should allow a custom icon', async () => {
+    fit('should allow a custom icon', async () => {
         const customIcon = page.getCustomIcon();
 
         await page.toggleCustomIcon();
@@ -547,6 +547,7 @@ describe('Select Tests', () => {
         expect(await customIcon.isPresent()).toBeTruthy();
         expect(await imageCompare('select-custom-icon-single')).toEqual(0);
         await page.toggleClearButton();
+        await browser.executeScript('window.scroll(0, 0)');
         expect(await imageCompare('select-custom-icon-single-clear-btn')).toEqual(0);
         await page.toggleClearButton();
         await page.clickOnCheckbox(page.checkboxMulti);
@@ -556,6 +557,7 @@ describe('Select Tests', () => {
         expect(await customIcon.isPresent()).toBeTruthy();
         expect(await imageCompare('select-custom-icon-multiple')).toEqual(0);
         await page.toggleClearButton();
+        await browser.executeScript('window.scroll(0, 0)');
         expect(await imageCompare('select-custom-icon-multiple-clear-btn')).toEqual(0);
     });
 
