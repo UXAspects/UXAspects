3d9ebb1a334b9466ae9e52c2681a90b7c848d92b
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2019-10-30 16:40:36 +0000

    Removing E2E reporting

--- e2e/protractor.config.js
+++ e2e/protractor.config.js
@@ -1,10 +1,6 @@
-const fs = require('fs');
 const { env } = require('process');
-const path = require('canonical-path');
 const _ = require('lodash');
 const { join } = require('path');
-const JasmineReporters = require('jasmine-reporters');
-const Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
 
 const isJenkinsBuild = !!env.RE_BUILD_TYPE;
 
@@ -70,56 +66,6 @@ exports.config = {
     });
 
     jasmine.getEnv().addReporter(new Reporter(browser.params));
-
-    // Allow changing bootstrap mode to NG1 for upgrade tests
-    global.setProtractorToNg1Mode = function () {
-      browser.useAllAngular2AppRoots = false;
-      browser.rootEl = 'body';
-    };
-
-    // returning the promise makes protractor wait for the reporter config before executing tests
-    return browser.getProcessedConfig().then(function (config) {
-
-      browser.driver.getCapabilities().then(function (caps) {
-        browser.browserName = caps.get('browserName');
-      });
-
-      var browserName = config.capabilities.browserName;
-
-      // Add reporter which will output results in XML format
-      var junitReporter = new JasmineReporters.JUnitXmlReporter({
-        consolidateAll: true,
-
-        // Save XML results files in this folder
-        savePath: './e2e/xml',
-
-        //filePrefix: 'reportXMLoutput',
-        // this will produce distinct xml files for each capability
-        filePrefix: browserName + '-xmloutput',
-        modifySuiteName: function (generatedSuiteName) {
-          // this will produce distinct suite names for each capability,
-          // e.g. 'firefox.login tests' and 'chrome.login tests'
-          return browserName + '.' + generatedSuiteName;
-        }
-      });
-
-      jasmine.getEnv().addReporter(junitReporter);
-
-      // Add reporter which will output results in HTML format
-      jasmine.getEnv().addReporter(
-        new Jasmine2HtmlReporter({
-          takeScreenshots: false,
-          // Save HTML results files in this folder
-          savePath: './e2e/html',
-          // Iclude browser name and date in the name of the HTML results file
-          fileNamePrefix: browserName,
-          fileNameDateSuffix: true,
-          // Set to false to display only failures in the HTML results file
-          showPassed: true,
-          cleanDestination: false
-        })
-      );
-    });
   },
 
   jasmineNodeOpts: {
@@ -132,17 +78,11 @@ exports.config = {
 // Custom reporter
 function Reporter(options) {
 
-  var _defaultOutputFile = path.resolve(process.cwd(), './e2e/_test-output', 'protractor-results.txt');
-  options.outputFile = options.outputFile || _defaultOutputFile;
-
-  try {
-      initOutputFile(options.outputFile);
-  } catch(err) {}
 
   options.appDir = options.appDir || './';
-  var _root = { appDir: options.appDir, suites: [] };
+  const _root = { appDir: options.appDir, suites: [] };
   log('AppDir: ' + options.appDir, +1);
-  var _currentSuite;
+  let _currentSuite;
 
   this.suiteStarted = function (suite) {
     _currentSuite = { description: suite.description, status: null, specs: [] };
@@ -151,11 +91,11 @@ function Reporter(options) {
   };
 
   this.suiteDone = function (suite) {
-    var statuses = _currentSuite.specs.map(function (spec) {
+    let statuses = _currentSuite.specs.map(function (spec) {
       return spec.status;
     });
     statuses = _.uniq(statuses);
-    var status = statuses.indexOf('failed') >= 0 ? 'failed' : statuses.join(', ');
+    const status = statuses.indexOf('failed') >= 0 ? 'failed' : statuses.join(', ');
     _currentSuite.status = status;
     log('Suite ' + _currentSuite.status + ': ' + suite.description, -1);
   };
@@ -178,74 +118,15 @@ function Reporter(options) {
     spec.failedExpectations.forEach(failure => log(failure.message, 4));
   };
 
-  this.jasmineDone = function () {
-    const outputFile = options.outputFile;
-    //// Alternate approach - just stringify the _root - not as pretty
-    //// but might be more useful for automation.
-    // var output = JSON.stringify(_root, null, 2);
-    var output = formatOutput(_root);
-    fs.appendFileSync(outputFile, output);
-  };
-
-  function ensureDirectoryExistence(filePath) {
-    var dirname = path.dirname(filePath);
-    if (directoryExists(dirname)) {
-      return true;
-    }
-    ensureDirectoryExistence(dirname);
-    fs.mkdirSync(dirname);
-  }
-
-  function directoryExists(path) {
-    try {
-      return fs.statSync(path).isDirectory();
-    }
-    catch (err) {
-      return false;
-    }
-  }
-
-  function initOutputFile(outputFile) {
-    ensureDirectoryExistence(outputFile);
-    var header = "Protractor results for: " + (new Date()).toLocaleString() + "\n\n";
-    fs.writeFileSync(outputFile, header);
-  }
-
-  // for output file output
-  function formatOutput(output) {
-    var indent = '  ';
-    var pad = '  ';
-    var results = [];
-    results.push('AppDir:' + output.appDir);
-    output.suites.forEach(function (suite) {
-      results.push(pad + 'Suite: ' + suite.description + ' -- ' + suite.status);
-      pad += indent;
-      suite.specs.forEach(function (spec) {
-        results.push(pad + spec.status + ' - ' + spec.description);
-        if (spec.failedExpectations) {
-          pad += indent;
-          spec.failedExpectations.forEach(function (fe) {
-            results.push(pad + 'message: ' + fe.message);
-          });
-          pad = pad.substr(2);
-        }
-      });
-      pad = pad.substr(2);
-      results.push('');
-    });
-    results.push('');
-    return results.join('\n');
-  }
-
   // for console output
   var _pad;
   function log(str, indent) {
     _pad = _pad || '';
-    if (indent == -1) {
+    if (indent === -1) {
       _pad = _pad.substr(2);
     }
     console.log(_pad + str);
-    if (indent == 1) {
+    if (indent === 1) {
       _pad = _pad + '  ';
     }
   }
