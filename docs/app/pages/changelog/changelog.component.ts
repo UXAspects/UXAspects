d6b0590654e07abbe4792b76be3e044632de4497
donna-marijne <donna.worby@microfocus.com>
2019-11-20 15:11:48 +0000

    Release 1.8.9

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -16,6 +16,11 @@ export class ChangeLogPageComponent {
 
         this.logs = [
             {
+                version: '1.8.9',
+                date: 'November 21st 2019',
+                content: require('./logs/release-v1.8.9.md')
+            },
+            {
                 version: '1.8.8',
                 date: 'October 31st 2019',
                 content: require('./logs/release-v1.8.8.md')
