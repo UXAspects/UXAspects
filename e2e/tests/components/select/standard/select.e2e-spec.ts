6b9afaf3fb9248cbcd951f2282038405a53384ee
Tobias Schreiner <Tobias.Schreiner@microfocus.com>
2019-11-21 12:26:22 +0100

    Typeahead recent options: end-to-end tests, open drop-down test screen shot (multi-select), second try

--- e2e/tests/components/select/standard/select.e2e-spec.ts
+++ e2e/tests/components/select/standard/select.e2e-spec.ts
@@ -553,7 +553,7 @@ describe('Select Tests', () => {
         await page.clickOnDropdown(multi);
 
         // Initial state: recent option list is not shown
-        expect(await imageCompare('select-open' + multi ? '-multi' : '-single')).toEqual(0);
+        expect(await imageCompare('select-open-' + (multi ? 'multi' : 'single'))).toEqual(0);
 
         await page.clickOnCountry(multi, 1);
         await checkRecentOptions(multi, ['United Kingdom']);
