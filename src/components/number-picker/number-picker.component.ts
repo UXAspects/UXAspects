ae1b29e4364b97398733bb80de85a9edbbe06bc0
andrew-Jolly <60877228+andrew-Jolly@users.noreply.github.com>
2020-02-13 09:16:04 +0000

    validation check OnChanges rather than writeValue

--- src/components/number-picker/number-picker.component.ts
+++ src/components/number-picker/number-picker.component.ts
@@ -1,5 +1,5 @@
 import { coerceBooleanProperty, coerceNumberProperty } from '@angular/cdk/coercion';
-import { ChangeDetectorRef, Component, EventEmitter, forwardRef, Input, OnDestroy, Optional, Output } from '@angular/core';
+import { ChangeDetectorRef, Component, EventEmitter, forwardRef, Input, OnDestroy, Optional, Output, OnChanges } from '@angular/core';
 import { ControlValueAccessor, FormGroupDirective, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 let uniqueId = 0;
@@ -18,7 +18,7 @@ export const NUMBER_PICKER_VALUE_ACCESSOR: any = {
         '[class.ux-number-picker-invalid]': '!_valid && !disabled && !_formGroup'
     }
 })
-export class NumberPickerComponent implements ControlValueAccessor, OnDestroy {
+export class NumberPickerComponent implements ControlValueAccessor, OnDestroy, OnChanges {
 
     private _min: number = -Infinity;
     private _max: number = Infinity;
@@ -113,6 +113,10 @@ export class NumberPickerComponent implements ControlValueAccessor, OnDestroy {
 
     ) { }
 
+    ngOnChanges(): void {
+        this._valid = this.isValid();
+    }
+
     ngOnDestroy(): void {
         this._isDestroyed = true;
     }
@@ -163,7 +167,6 @@ export class NumberPickerComponent implements ControlValueAccessor, OnDestroy {
     writeValue(value: number): void {
         if (value !== undefined) {
             this._value = value;
-            this._valid = this.isValid();
             // if the component is not destroyed then run change detection
             // workaround for Angular bug (https://portal.digitalsafe.net/browse/EL-3694)
             if (!this._isDestroyed) {
