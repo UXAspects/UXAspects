37186204ac8f264817257831190bf3cdd3bec681
andrew-Jolly <60877228+andrew-Jolly@users.noreply.github.com>
2020-02-18 09:59:22 +0000

    option changes tests

--- src/components/dashboard/dashboard.spec.ts
+++ src/components/dashboard/dashboard.spec.ts
@@ -6,11 +6,7 @@ import { DashboardModule } from './dashboard.module';
 
 @Component({
     selector: 'app-ux-dashboard',
-    template: `
-        <ux-dashboard
-            [options]="options">
-        </ux-dashboard>
-    `
+    template: `<ux-dashboard [options]="options"></ux-dashboard>`
 })
 export class DashboardComponent {
 
@@ -21,13 +17,11 @@ export class DashboardComponent {
         emptyRow: false,
         minWidth: 187
     };
-
 }
 
 fdescribe('Dashboard', () => {
     let component: DashboardComponent;
     let fixture: ComponentFixture<DashboardComponent>;
-    let nativeElement: HTMLElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -40,7 +34,6 @@ fdescribe('Dashboard', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(DashboardComponent);
         component = fixture.componentInstance;
-        nativeElement = fixture.nativeElement;
         fixture.detectChanges();
     });
 
@@ -48,4 +41,53 @@ fdescribe('Dashboard', () => {
         expect(component).toBeTruthy();
     });
 
+    it('should change columns option if columns option adjusted', async() => {
+        expect(component.options.columns).toBe(3);
+
+        component.options.columns = 6;
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        expect(component.options.columns).toBe(6);
+    });
+
+    it('should change padding option if padding option adjusted', async() => {
+        expect(component.options.padding).toBe(10);
+
+        component.options.padding = 20;
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        expect(component.options.padding).toBe(20);
+    });
+
+    it('should change rowHeight option if rowHeight option adjusted', async() => {
+        expect(component.options.rowHeight).toBe(300);
+
+        component.options.rowHeight = 600;
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        expect(component.options.rowHeight).toBe(600);
+    });
+
+    it('should change minWidth option if minWidth option adjusted', async() => {
+        expect(component.options.minWidth).toBe(187);
+
+        component.options.minWidth = 100;
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        expect(component.options.minWidth).toBe(100);
+    });
+
+    it('should change emptyRow option if emptyRow option adjusted', async() => {
+        expect(component.options.emptyRow).toBe(false);
+
+        component.options.emptyRow = true;
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        expect(component.options.emptyRow).toBe(true);
+    });
 })
