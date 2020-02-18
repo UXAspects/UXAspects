c66a8c67a7c706f6c03e0d37683578c31a587538
andrew-Jolly <60877228+andrew-Jolly@users.noreply.github.com>
2020-02-18 14:37:43 +0000

    single test flag removed

--- src/components/dashboard/dashboard.spec.ts
+++ src/components/dashboard/dashboard.spec.ts
@@ -19,7 +19,7 @@ export class DashboardComponent {
     };
 }
 
-fdescribe('Dashboard', () => {
+describe('Dashboard', () => {
     let component: DashboardComponent;
     let fixture: ComponentFixture<DashboardComponent>;
 
