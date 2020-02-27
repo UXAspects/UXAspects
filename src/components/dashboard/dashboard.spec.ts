00ed12ea85c5a00a64c75c3637278496f496da85
donna-marijne <donna.worby@microfocus.com>
2020-02-27 12:00:31 +0000

    Apply rowHeight by default to make widget positions predictable

--- src/components/dashboard/dashboard.spec.ts
+++ src/components/dashboard/dashboard.spec.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { DashboardOptions } from './dashboard.component';
 import { DashboardModule } from './dashboard.module';
@@ -13,48 +13,39 @@ import { DashboardModule } from './dashboard.module';
                         id="widget-A"
                         name="Hosts"
                         [colSpan]="3">
-
                     </ux-dashboard-widget>
 
                     <ux-dashboard-widget
                         id="widget-B"
                         name="Runs"
                         [colSpan]="2">
-
-
                     </ux-dashboard-widget>
 
                     <ux-dashboard-widget
                         id="widget-C"
                         name="Runs"
                         [colSpan]="2">
-
                     </ux-dashboard-widget>
 
                 </ux-dashboard>
     `
 })
 export class DashboardComponent {
-
-    // defaultOptions: DashboardOptions = { columns: 5, padding: 5, minWidth: 100, minHeight: 100, emptyRow: true };
-    options: DashboardOptions;
-
+    options: DashboardOptions = { columns: 5, padding: 5, minWidth: 100, rowHeight: 100, emptyRow: true };
 }
 
-describe('Dashboard', () => {
+fdescribe('Dashboard', () => {
     let component: DashboardComponent;
     let fixture: ComponentFixture<DashboardComponent>;
-    let nativeElement;
-    let widgetA;
-    let widgetC;
+    let nativeElement: HTMLElement;
+    let widgetA: HTMLElement;
+    let widgetB: HTMLElement;
+    let widgetC: HTMLElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             imports: [DashboardModule],
             declarations: [DashboardComponent]
-        })
-        .overrideComponent(DashboardComponent, {
-            set: { changeDetection: ChangeDetectionStrategy.Default }
         })
             .compileComponents();
     }));
@@ -64,6 +55,7 @@ describe('Dashboard', () => {
         component = fixture.componentInstance;
         nativeElement = fixture.nativeElement;
         widgetA = nativeElement.querySelector('#widget-A');
+        widgetB = nativeElement.querySelector('#widget-B');
         widgetC = nativeElement.querySelector('#widget-C');
         fixture.detectChanges();
     });
@@ -73,14 +65,7 @@ describe('Dashboard', () => {
     });
 
     it('should stack widgets if columns reduced allowing only one widget per row', async() => {
-        // default columns option needs to be applied for test to be setup correctly
-        const defaultOptions = { ...component.options, columns: 5 };
-
-        component.options = defaultOptions;
-        fixture.detectChanges();
-        await fixture.whenStable();
-
-        expect(widgetC.style.top).toBe('154px');
+        expect(widgetB.style.top).toBe('0px');
 
         const adjustedOptions = { ...component.options, columns: 3 };
 
@@ -88,7 +73,7 @@ describe('Dashboard', () => {
         fixture.detectChanges();
         await fixture.whenStable();
 
-        expect(widgetC.style.top).toBe('256.667px');
+        expect(widgetB.style.top).toBe('100px');
     });
 
     it('should increase widget padding if padding option increased', async() => {
@@ -113,7 +98,7 @@ describe('Dashboard', () => {
         expect(widgetA.style.height).toBe('400px');
     });
 
-    it('should increase dashboard rows from two to three when minWidth Increased', async() => {
+    it('should switch to stacked mode when minWidth increased', async() => {
         // setup columns option so row one has widgetA and Row two has widgetB/C
         const columnsSetup = { ...component.options, columns: 4 };
 
@@ -121,7 +106,7 @@ describe('Dashboard', () => {
         fixture.detectChanges();
         await fixture.whenStable();
 
-        expect(widgetC.style.left).toBe('384px');
+        expect(getPixels(widgetC.style.left)).toBeGreaterThan(0);
 
         const increasedMinWidth = { ...component.options, minWidth: 3000 };
 
@@ -133,3 +118,7 @@ describe('Dashboard', () => {
     });
 
 });
+
+function getPixels(cssValue: string): number {
+    return parseInt(cssValue.replace('px', ''));
+}
