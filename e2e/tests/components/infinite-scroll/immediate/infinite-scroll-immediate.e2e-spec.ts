fe9b8773c2878101bece050de05f6adb78f409f1
cmallon367 <cmallon111@gmail.com>
2020-03-31 12:45:18 +0100

    Removing unused code

--- e2e/tests/components/infinite-scroll/immediate/infinite-scroll-immediate.e2e-spec.ts
+++ e2e/tests/components/infinite-scroll/immediate/infinite-scroll-immediate.e2e-spec.ts
@@ -1,7 +1,5 @@
 import { imageCompare } from '../../common/image-compare';
 import { InfiniteScrollImmediatePage } from './infinite-scroll-immediate.po.spec';
-import { tick, fakeAsync } from '@angular/core/testing';
-import { Key } from 'selenium-webdriver';
 
 describe('Infinite Scroll (Immediate) Tests', () => {
 
@@ -18,16 +16,6 @@ describe('Infinite Scroll (Immediate) Tests', () => {
 
         // loadMore not visible
         expect(await page.confirmLoadMoreIsVisible()).toBeFalsy();
-
-        // loadOnScroll checked
-        expect(await page.confirmLoadOnScrollIsChecked()).toBeTruthy();
-
-        // arrows enabled
-        expect(await page.confirmPageSizeButtonIsDisabled('up')).toBeFalsy();
-        expect(await page.confirmPageSizeButtonIsDisabled('down')).toBeFalsy();
-
-        // valid value in number picker
-        expect(await page.confirmValueIsInvalid()).toBeFalsy();
 
         expect(await imageCompare('infinite-scroll-initial')).toEqual(0);
 
