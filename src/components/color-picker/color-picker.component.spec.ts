46cbe23da10cf2265f32a176ada4ac1f31f0592e
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2020-04-06 17:19:36 +0100

    Making some fixes

--- src/components/color-picker/color-picker.component.spec.ts
+++ src/components/color-picker/color-picker.component.spec.ts
@@ -1,46 +1,21 @@
-import { Component, ElementRef, ViewChild, ChangeDetectionStrategy } from '@angular/core';
+import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ColorService, ColorServiceModule } from '../../services/color';
-import { IconModule } from '../icon';
+import { ColorService, ColorServiceModule, colorSets } from '../../services/color/index';
 import { ColorPickerColor } from './color-picker-color';
 import { ColorPickerModule } from './color-picker.module';
 
 
 @Component({
-    selector: 'color-picker-test-app',
+    selector: 'app-color-picker-test',
     template: `
-            <ux-color-picker
-                [colors]='colors'
-                [(selected)]='selected'
-                (selectedChange)='colorPickerSelectedChange(); toggleButton.focus()'
-                [columns]='columns'
-                [buttonStyle]='buttonStyle'
-                [buttonSize]='buttonSize'
-                [showInput]='showInput'
-                (inputSubmit)='isPickerOpen = false'></ux-color-picker>
-
-            <div *ngIf='showInput' class='btn-container demo-color-picker-close'>
-                <button type='button' class='btn button-primary'>
-                    Accept
-                </button>
-            </div>
-// `,
-        changeDetection: ChangeDetectionStrategy.OnPush
+        <ux-color-picker
+            [colors]="colors"
+            [(selected)]="selected"
+            [columns]="4"
+            [showInput]="true">
+         </ux-color-picker>`
 })
 export class ColorPickerTestComponent {
-    colors: ColorPickerColor[][];
-    selected: ColorPickerColor;
-    columns = 4;
-    buttonStyle = 'circle';
-    buttonSize = 'md';
-    showInput = true;
-
-    @ViewChild('toggleButton', { static: false }) toggleButton: ElementRef;
-    @ViewChild('dropdownMenu', { static: false }) dropdownMenu: ElementRef;
 
     private _colorNames = [
         ['Primary', 'Accent', 'Secondary', 'Alternate1', 'Alternate2', 'Alternate3', 'Vibrant1',
@@ -48,11 +23,10 @@ export class ColorPickerTestComponent {
         ['Grey1', 'Grey2', 'Grey3', 'Grey4', 'Grey5', 'Grey6', 'Grey7', 'Grey8']
     ];
 
-    constructor(colorService: ColorService) {
-        this.colors = this._colorNames.map(row =>
-            row.map(colorName => new ColorPickerColor(colorName, colorService.resolve(colorName))));
-        this.selected = this.colors[0][0];
-    }
+    colors = this._colorNames.map(row => row.map(colorName => new ColorPickerColor(colorName, this._colorService.resolve(colorName))));
+    selected: ColorPickerColor = this.colors[0][0];
+
+    constructor(private _colorService: ColorService) {}
 }
 
 fdescribe('Color Picker Component', () => {
@@ -64,14 +38,7 @@ fdescribe('Color Picker Component', () => {
         TestBed.configureTestingModule({
             imports: [
                 ColorPickerModule,
-                IconModule,
-                BrowserModule,
-                FormsModule,
-                ReactiveFormsModule,
-                BrowserAnimationsModule,
-                ColorPickerModule,
-                ColorServiceModule,
-                BsDropdownModule.forRoot(),
+                ColorServiceModule.forRoot(colorSets.keppel),
             ],
             declarations: [ColorPickerTestComponent]
         }).compileComponents();
@@ -90,26 +57,19 @@ fdescribe('Color Picker Component', () => {
     });
 
     it('should not invalidate input when color InputMode changes', async () => {
-        const inputModeSwapButton = nativeElement.querySelector<HTMLButtonElement>(
-            '.ux-color-picker-input-toggle'
-        );
-        inputModeSwapButton.click();
-        debugger;
-        // await button click to register
-        await fixture.whenStable();
-        fixture.detectChanges();
-        // await validation to update to true
-        await fixture.whenStable();
-        fixture.detectChanges();
-        await fixture.whenStable();
-        fixture.detectChanges();
-        debugger;
-        const colorInputBox = nativeElement.querySelector(
-            '.ux-color-picker .ux-color-picker-input-panel .ux-color-picker-input input'
-        );
-        // Check input is valid following changing input mode
-        expect(colorInputBox.classList.contains('ng-valid')).toBeTruthy();
+        // get the button that changes the color format
+        const colorFormatButton = nativeElement.querySelector<HTMLButtonElement>('.ux-color-picker-input-toggle');
+        const colorInput = nativeElement.querySelector('.ux-color-picker .ux-color-picker-input-panel .ux-color-picker-input input');
 
+        // change the color format
+        colorFormatButton.click();
+
+        // allow the UI to update
+        fixture.detectChanges();
+        await fixture.whenStable();
+
+        // check that the color input field is not invalid
+        expect(colorInput.classList.contains('ng-invalid')).toBeFalsy();
     });
 
 });
--- src/components/color-picker/color-picker.component.ts
+++ src/components/color-picker/color-picker.component.ts
@@ -1,8 +1,9 @@
-import { Component, EventEmitter, HostBinding, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, HostBinding, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
 import { BehaviorSubject, combineLatest, Subject } from 'rxjs';
 import { pairwise, takeUntil } from 'rxjs/operators';
 import { ColorPickerColor } from './color-picker-color';
 import { ColorPickerButtonSize, ColorPickerButtonStyle, ColorPickerInputColors, ColorPickerInputMode } from './color-picker.type';
+import { NgModel } from '@angular/forms';
 
 // Values corresponding to stylesheet
 const BUTTON_MARGIN = 8;
@@ -113,7 +114,10 @@ export class ColorPickerComponent implements OnInit, OnDestroy {
         'rgba': /^(?:rgb\(\d{1,3},\s*\d{1,3},\s*\d{1,3}\))|(?:rgba\(\d{1,3},\s*\d{1,3},\s*\d{1,3},\s*\d(\.\d+)?\))$/
     };
 
-    private _onDestroy = new Subject();
+    /** Access the ngModel instance of the input field */
+    @ViewChild('inputField', { static: false }) inputFormControl: NgModel;
+
+    private readonly _onDestroy = new Subject();
 
     ngOnInit(): void {
 
@@ -125,7 +129,7 @@ export class ColorPickerComponent implements OnInit, OnDestroy {
         });
 
         // Set the width based on column count and button size
-        combineLatest(this.columns$, this.buttonSize$)
+        combineLatest([this.columns$, this.buttonSize$])
             .pipe(takeUntil(this._onDestroy))
             .subscribe(([columns, buttonSize]) => {
                 if (columns > 0) {
@@ -149,6 +153,27 @@ export class ColorPickerComponent implements OnInit, OnDestroy {
     }
 
     toggleColorEntryType(): void {
+
+        // update the input mode
         this.inputMode = (this.inputMode === 'hex') ? 'rgba' : 'hex';
+
+        // get the current color value if there is one
+        const color = this.selected$.value;
+
+        // if there is no selected color property then skip
+        if (color) {
+
+            // get the new color value
+            const newColor = this.inputMode === 'hex' ? color.hex : color.rgba;
+
+            // update the selected color value based on the input mode
+            this.updateColorValue(newColor, this.inputMode);
+
+            // forcibly update the validation status of ngModel to prevent any
+            // incorrect error states in the underlying form control
+            // (running change detection will not suffice)
+            this.inputFormControl.control.setValue(newColor);
+        }
+
     }
 }
