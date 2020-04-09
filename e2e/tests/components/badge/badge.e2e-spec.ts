ca937e19f44289d663b0f74d5a7e8de556f79f94
Darren.McCready.76005806 <Darren.McCready@microfocus.com>
2020-04-09 17:53:10 +0100

    Add in screenshot tests

--- e2e/tests/components/badge/badge.e2e-spec.ts
+++ e2e/tests/components/badge/badge.e2e-spec.ts
@@ -1,4 +1,5 @@
 import { anchorData, buttonData } from '../../../pages/app/badge/badge.testpage.data';
+import { imageCompare } from '../common/image-compare';
 import { BadgePage } from './badge.po.spec';
 
 describe('Badge Tests', () => {
@@ -61,6 +62,8 @@ describe('Badge Tests', () => {
 
         // check not hidden
         expect(await page.buttonBadge.getCssValue('display')).toBe('block');
+
+        expect(await imageCompare('badge-initial')).toEqual(0);
     });
 
     it('should update the anchor content based on max value being set', async () => {
@@ -69,6 +72,8 @@ describe('Badge Tests', () => {
 
         // check content is limited as number
         expect(await page.anchorBadge.getText()).toBe('999+');
+
+        expect(await imageCompare('badge-anchor-max-value')).toEqual(0);
     });
 
     it('should update the anchor content when changed', async () => {
@@ -77,6 +82,8 @@ describe('Badge Tests', () => {
 
         // check content has the new value
         expect(await page.anchorBadge.getText()).toBe('1894');
+
+        expect(await imageCompare('badge-anchor-change-content')).toEqual(0);
     });
 
     it('should set the anchor badge color when changed', async () => {
@@ -85,6 +92,8 @@ describe('Badge Tests', () => {
 
         // check background color is set
         expect(await page.anchorBadge.getCssValue('background-color')).toBe('rgba(153, 153, 153, 1)');
+
+        expect(await imageCompare('badge-anchor-change-color')).toEqual(0);
     });
 
     it('should set the anchor badge size when changed', async () => {
@@ -93,6 +102,8 @@ describe('Badge Tests', () => {
 
         // check class for new size exists
         expect(await page.anchor.getAttribute('class')).toContain('ux-badge-large');
+
+        expect(await imageCompare('badge-anchor-size-large')).toEqual(0);
     });
 
     it('should update the button content based on max value being set', async () => {
@@ -101,6 +112,8 @@ describe('Badge Tests', () => {
 
         // check content is limited as string with ellipsis
         expect(await page.buttonBadge.getText()).toBe('Some really long…');
+
+        expect(await imageCompare('badge-button-max-value')).toEqual(0);
     });
 
     it('should set the button badge color when changed', async () => {
@@ -109,6 +122,8 @@ describe('Badge Tests', () => {
 
         // check background color is correct
         expect(await page.buttonBadge.getCssValue('background-color')).toBe('rgba(123, 99, 163, 1)');
+
+        expect(await imageCompare('badge-button-change-button')).toEqual(0);
     });
 
     it('should toggle button badge visibility', async () => {
@@ -118,11 +133,15 @@ describe('Badge Tests', () => {
         // check badge is not visible
         expect(await page.buttonBadge.getCssValue('display')).toBe('none');
 
+        expect(await imageCompare('badge-button-hide')).toEqual(0);
+
         // click again to unhide
         await page.toggleHideButtonBadge();
 
         // check badge is visible
         expect(await page.buttonBadge.getCssValue('display')).toBe('block');
+
+        expect(await imageCompare('badge-button-show')).toEqual(0);
     });
 
     it('should set the badge position on the button', async () => {
@@ -135,6 +154,8 @@ describe('Badge Tests', () => {
         expect(buttonClasses).not.toContain(`ux-badge-${buttonData.verticalPosition}`);
         expect(buttonClasses).toContain('ux-badge-above');
         expect(buttonClasses).toContain('ux-badge-after');
+
+        expect(await imageCompare('badge-button-reposition')).toEqual(0);
     });
 
     it('should remove the badge overlap on the button', async () => {
@@ -143,5 +164,7 @@ describe('Badge Tests', () => {
 
         // check correct position styles are present
         expect(await page.button.getAttribute('class')).not.toContain('ux-badge-overlap');
+
+        expect(await imageCompare('badge-button-remove-overlap')).toEqual(0);
     });
 });
