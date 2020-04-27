eb54c80c3cd4de9c61e9fbe1920e674c161c763b
darrenmcc-microfocus <62879374+darrenmcc-microfocus@users.noreply.github.com>
2020-04-27 17:51:40 +0100

    Remove duplication in lifecycle hooks

--- src/directives/badge/badge.directive.ts
+++ src/directives/badge/badge.directive.ts
@@ -118,78 +118,42 @@ export class BadgeDirective implements AfterViewInit, OnChanges, OnDestroy {
     ) { }
 
     ngAfterViewInit(): void {
-        const badgeElement: HTMLSpanElement = this._renderer.createElement('span');
-        this._renderer.addClass(badgeElement, this._className);
-        this._renderer.addClass(badgeElement, `ux-badge-${this.badgeSize}`);
-        this._renderer.setStyle(badgeElement, 'display', 'none');
-        this._renderer.setProperty(badgeElement, 'innerHTML', this._badgeDisplayContent);
-        this._renderer.setStyle(badgeElement, 'color', this.determineContentTextColor().toHex());
-
-        if (this._badgeColor) {
-            this._renderer.setStyle(badgeElement, 'background', this._badgeColor.toHex());
-        }
-
-        if (this._badgeBorderColor) {
-            this._renderer.setStyle(badgeElement, 'border-color', this._badgeBorderColor.toHex());
-        }
-
-        this._badgeElement = badgeElement;
+        this._badgeElement = this._renderer.createElement('span');
+        this._renderer.addClass(this._badgeElement, this._className);
+        this._renderer.setStyle(this._badgeElement, 'display', 'none');
+        this.setBadgeColor();
+        this.setBadgeBorderColor();
+        this.setBadgeSize(null);
+        this.setContent(this._badgeContent, this.badgeMaxValue);
         this._renderer.appendChild(this._element.nativeElement, this._badgeElement);
         this._renderer.removeStyle(this._badgeElement, 'display');
     }
 
     ngOnChanges(changes: SimpleChanges): void {
-        let contentChanged: boolean = false;
-        // get display friendly version of text based on max length and type of val;
-        if (changes.badgeContent || changes.badgeMaxValue) {
-            let finalText: string =
-                (changes.badgeContent && changes.badgeContent.currentValue) || this.badgeContent || null;
-            const maxValue: number =
-                (changes.badgeMaxValue && changes.badgeMaxValue.currentValue) || this.badgeMaxValue || null;
 
-            if (finalText && maxValue && maxValue > 0) {
-                if (this._isNumber && parseInt(finalText) > maxValue) {
-                    finalText = `${maxValue}+`;
-                } else if (finalText.length > maxValue) {
-                    finalText = `${finalText.substr(0, maxValue)}&hellip;`;
-                }
-            }
-
-            this._badgeDisplayContent = finalText;
-            contentChanged = true;
-        }
 
         // if the badge is visible set changed values
         if (this._badgeElement) {
-            // set the badge content
-            if (contentChanged) {
-                this._renderer.setProperty(this._badgeElement, 'innerHTML', this._badgeDisplayContent);
+            // set badge content and get display friendly version of text based on max length and type of val
+            if (changes.badgeContent || changes.badgeMaxValue) {
+                let finalText: string = (changes.badgeContent && changes.badgeContent.currentValue) || this.badgeContent || null;
+                const maxValue: number = (changes.badgeMaxValue && changes.badgeMaxValue.currentValue) || this.badgeMaxValue || null;
+                this.setContent(finalText, maxValue);
             }
 
             // set the badge color
             if (changes.badgeColor && changes.badgeColor.currentValue !== changes.badgeColor.previousValue) {
-                if (this._badgeColor) {
-                    this._renderer.setStyle(this._badgeElement, 'background', this._badgeColor.toHex());
-                } else {
-                    this._renderer.removeStyle(this._badgeElement, 'background');
-                }
-
-                this._renderer.setStyle(this._badgeElement, 'color', this.determineContentTextColor().toHex());
+                this.setBadgeColor();
             }
 
             // set the badge border color
             if (changes.badgeBorderColor && changes.badgeBorderColor.currentValue !== changes.badgeBorderColor.previousValue) {
-                if (this._badgeBorderColor) {
-                    this._renderer.setStyle(this._badgeElement, 'border-color', this._badgeBorderColor.toHex());
-                } else {
-                    this._renderer.removeStyle(this._badgeElement, 'border-color');
-                }
+                this.setBadgeBorderColor();
             }
 
             // set badge size
             if (changes.badgeSize && changes.badgeSize.currentValue !== changes.badgeSize.previousValue) {
-                this._renderer.removeClass(this._badgeElement, `ux-badge-${changes.badgeSize.previousValue}`);
-                this._renderer.addClass(this._badgeElement, `ux-badge-${this.badgeSize}`);
+                this.setBadgeSize(changes.badgeSize.previousValue);
             }
         }
     }
@@ -198,6 +162,45 @@ export class BadgeDirective implements AfterViewInit, OnChanges, OnDestroy {
         if (this._renderer.destroyNode) {
             this._renderer.destroyNode(this._badgeElement);
         }
+    }
+
+    private setContent(finalText: string, maxValue: number): void {
+        if (finalText && maxValue && maxValue > 0) {
+            if (this._isNumber && parseInt(finalText) > maxValue) {
+                finalText = `${maxValue}+`;
+            } else if (finalText.length > maxValue) {
+                finalText = `${finalText.substr(0, maxValue)}…`;
+            }
+        }
+
+        this._badgeDisplayContent = finalText;
+        this._badgeElement.textContent = this._badgeDisplayContent;
+    }
+
+    private setBadgeColor(): void {
+        if (this._badgeColor) {
+            this._renderer.setStyle(this._badgeElement, 'background', this._badgeColor.toHex());
+        } else {
+            this._renderer.removeStyle(this._badgeElement, 'background');
+        }
+
+        this._renderer.setStyle(this._badgeElement, 'color', this.determineContentTextColor().toHex());
+    }
+
+    private setBadgeBorderColor(): void {
+        if (this._badgeBorderColor) {
+            this._renderer.setStyle(this._badgeElement, 'border-color', this._badgeBorderColor.toHex());
+        } else {
+            this._renderer.removeStyle(this._badgeElement, 'border-color');
+        }
+    }
+
+    private setBadgeSize(previousValue: string): void {
+        if (previousValue) {
+            this._renderer.removeClass(this._badgeElement, `ux-badge-${previousValue}`);
+        }
+
+        this._renderer.addClass(this._badgeElement, `ux-badge-${this.badgeSize}`);
     }
 
     private determineContentTextColor(): ThemeColor {
