4ad5269e6af4315c755e333c3ab5a43b584b4e47
donna-marijne <donnamarijne+gh@gmail.com>
2020-05-21 10:40:14 +0100

    EL-3664 - Fix "bad request" Plunker error (#1337)
    * Fix "bad request" Plunker error
    
    Provide empty app.css content if none is provided.
    
    * Replace "angularjs" playground framework with "css"
    
    The legacy "angularjs" framework causes an error because ux-aspects-ng1.js is no longer provided. Since all AngularJS content has now been removed, the legacy playgrounds should be HTML+CSS only. The `CssPlaygroundStrategy` therefore replaces the `AngularJSPlaygroundStrategy` for legacy content, and only includes CSS dependencies.
    
    * Default to angular
    
    Co-authored-by: Ashley Hunter <ashley-glenn.hunter@microfocus.com>

---
+++ docs/app/services/playground/strategies/css-strategy.ts
@@ -0,0 +1,49 @@
+import { IPlayground } from '../../../interfaces/IPlayground';
+import { DocumentationType } from '../tokens/documentation.token';
+import { SystemJSHelper } from '../utilities/system-helper';
+import { PlaygroundStrategy } from './playground-strategy';
+
+export class CssPlaygroundStrategy extends PlaygroundStrategy {
+
+    getGlobalExternalScripts(_assetsUrl: string): string[] {
+        return [];
+    }
+
+    getBodyExternalScripts(): string[] {
+        return [];
+    }
+
+    getGlobalExternalStyles(assetsUrl: string): string[] {
+        const stylesheets = [
+            SystemJSHelper.getPackageUrl({ name: 'bootstrap', path: 'bootstrap@3.3.7/dist/css/bootstrap.min.css' }),
+        ];
+
+        if (this.documentationType === DocumentationType.Keppel) {
+            stylesheets.push(`${assetsUrl}/css/ux-aspects.css`);
+        } else {
+            stylesheets.push(`${assetsUrl}/styles/ux-aspects.css`);
+            stylesheets.push(`${assetsUrl}/styles/quantum-ux-aspects.css`);
+        }
+
+        stylesheets.push(`app.css`);
+
+        return stylesheets;
+    }
+
+    getGlobalStyles(): string[] {
+        return [`body { padding: 15px; background-color: #fff; }`];
+    }
+
+    getBody(playground: IPlayground): string {
+        return [
+            playground.files['app.html']
+        ].join('\n');
+    }
+
+    /** Get the files to include in the playground */
+    getFiles(playground: IPlayground): { [filename: string]: string } {
+        return {
+            'app.css': playground.files['app.css'] || '',
+        };
+    }
+}
