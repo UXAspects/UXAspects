26c50c606dc1428ba758f88466d8ea124f2b3a06
donna-marijne <donna.worby@microfocus.com>
2020-06-15 17:46:19 +0100

    Close dropdown before capturing screenshot
    The dropdown scroll position appears to be unstable, but is not part of this test case. Click to close the dropdown before capturing the screenshot.

--- e2e/tests/components/select/standard/select.e2e-spec.ts
+++ e2e/tests/components/select/standard/select.e2e-spec.ts
@@ -534,6 +534,7 @@ describe('Select Tests', () => {
         await page.clickOnCheckbox(page.checkboxMulti);
         await page.clickOnDropdown(true);
         await page.clickOnCountry(true, 250);
+        await page.clickOnDropdown(true);
         expect(await page.getSelectedLocationText()).toBe('[ "Daenerys of the House Targaryen, the First of Her Name, The Unburnt, Queen of the Andals, the Rhoynar and the First Men, Queen of Meereen, Khaleesi of the Great Grass Sea, Protector of the Realm, Lady Regent of the Seven Kingdoms, Breaker of Chains and Mother of Dragons" ]');
         expect(await imageCompare('select-tag-overflow')).toEqual(0);
     });
