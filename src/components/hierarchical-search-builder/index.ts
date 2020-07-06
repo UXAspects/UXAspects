478f5211b67f7293d95ea87b409af093f89483f1
Erik Littwin <erik.littwin@microfocus.com>
2020-07-06 08:48:28 +0200

    update index.ts with interface definitions

--- src/components/hierarchical-search-builder/index.ts
+++ src/components/hierarchical-search-builder/index.ts
@@ -1,3 +1,7 @@
 export * from './hierarchical-search-builder.module';
+export * from './interfaces/FieldDefinition';
+export * from './interfaces/OperatorDefinition';
+export * from './interfaces/OperatorDefinitionList';
+export * from './interfaces/LogicalOperatorDefinition';
 export * from './hierarchical-search-builder.component';
 export * from './input-components/text-input/text-input.component';
