162ebf4b97876b65627fadaff60e6ccd049d198b
Erik Littwin <erik.littwin@microfocus.com>
2020-07-06 15:49:30 +0200

    display groups and conditions

--- src/components/hierarchical-search-builder/hierarchical-search-builder-group/hierarchical-search-builder-group.component.ts
+++ src/components/hierarchical-search-builder/hierarchical-search-builder-group/hierarchical-search-builder-group.component.ts
@@ -1,11 +1,12 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { QueryGroup } from '../interfaces/HierarchicalSearchBuilderQuery';
 
 @Component({
     selector: 'ux-hierarchical-search-builder-group',
     templateUrl: './hierarchical-search-builder-group.component.html',
-    styleUrls: ['./hierarchical-search-builder-group.component.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HierarchicalSearchBuilderGroupComponent {
-
+    @Input() subquery: QueryGroup;
+    @Input() logicalOperatorName: string;
 }
