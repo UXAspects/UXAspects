88170d485e8ff189ca9edd09f5306c0c67cd1553
Erik Littwin <erik.littwin@microfocus.com>
2020-07-06 11:22:22 +0200

    add query to example output

--- src/components/hierarchical-search-builder/index.ts
+++ src/components/hierarchical-search-builder/index.ts
@@ -3,5 +3,6 @@ export * from './interfaces/FieldDefinition';
 export * from './interfaces/OperatorDefinition';
 export * from './interfaces/OperatorDefinitionList';
 export * from './interfaces/LogicalOperatorDefinition';
+export * from './interfaces/HierarchicalSearchBuilderQuery';
 export * from './hierarchical-search-builder.component';
 export * from './input-components/text-input/text-input.component';
