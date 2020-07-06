2d857ed5987184fb201db86f491e401831b82423
Erik Littwin <erik.littwin@microfocus.com>
2020-07-06 11:31:49 +0200

    setup for group and condition components

--- src/components/hierarchical-search-builder/hierarchical-search-builder.module.ts
+++ src/components/hierarchical-search-builder/hierarchical-search-builder.module.ts
@@ -1,10 +1,17 @@
 import { A11yModule } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { TextInputComponent } from "./input-components/text-input/text-input.component";
-import { HierarchicalSearchBuilderComponent } from "./hierarchical-search-builder.component";
+import { TextInputComponent } from './input-components/text-input/text-input.component';
+import { HierarchicalSearchBuilderComponent } from './hierarchical-search-builder.component';
+import { HierarchicalSearchBuilderGroupComponent } from './hierarchical-search-builder-group/hierarchical-search-builder-group.component';
+import { HierarchicalSearchBuilderConditionComponent } from './hierarchical-search-builder-condition/hierarchical-search-builder-condition.component';
 
-const DECLARATIONS = [TextInputComponent, HierarchicalSearchBuilderComponent];
+const DECLARATIONS = [
+    TextInputComponent,
+    HierarchicalSearchBuilderComponent,
+    HierarchicalSearchBuilderGroupComponent,
+    HierarchicalSearchBuilderConditionComponent,
+];
 
 @NgModule({
     imports: [
@@ -14,4 +21,5 @@ const DECLARATIONS = [TextInputComponent, HierarchicalSearchBuilderComponent];
     exports: DECLARATIONS,
     declarations: DECLARATIONS,
 })
-export class HierarchicalSearchBuilderModule { }
+export class HierarchicalSearchBuilderModule {
+}
