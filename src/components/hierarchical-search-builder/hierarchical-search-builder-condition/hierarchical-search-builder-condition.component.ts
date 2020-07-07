799dae41d92fc1edb8afa2bf9e3d01739ef8dabb
Erik Littwin <erik.littwin@microfocus.com>
2020-07-07 14:20:59 +0200

    add component factory resolver to condition

--- src/components/hierarchical-search-builder/hierarchical-search-builder-condition/hierarchical-search-builder-condition.component.ts
+++ src/components/hierarchical-search-builder/hierarchical-search-builder-condition/hierarchical-search-builder-condition.component.ts
@@ -1,4 +1,13 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import {
+    AfterViewInit,
+    ChangeDetectionStrategy,
+    Component, ComponentFactoryResolver,
+    ComponentRef,
+    Input,
+    OnInit,
+    ViewChild,
+    ViewContainerRef
+} from '@angular/core';
 import { HierarchicalSearchBuilderService } from '../hierarchical-search-builder.service';
 import { FieldDefinition } from '../interfaces/FieldDefinition';
 import { OperatorDefinition } from '../interfaces/OperatorDefinition';
@@ -8,16 +17,36 @@ import { OperatorDefinition } from '../interfaces/OperatorDefinition';
     templateUrl: './hierarchical-search-builder-condition.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HierarchicalSearchBuilderConditionComponent implements OnInit {
-    @Input() field: string;
-    @Input() operator: string;
+export class HierarchicalSearchBuilderConditionComponent implements OnInit, AfterViewInit {
+    @ViewChild('inputContainer', { read: ViewContainerRef }) inputContainer: ViewContainerRef;
+    private _inputComponentRef: ComponentRef<any>;
+
+    @Input() fieldName: string;
+    @Input() operatorName: string;
     @Input() value: any;
 
     fields: FieldDefinition[] = this._hsbService.getFields();
-    operators: OperatorDefinition[] = this._hsbService.getOperatorsByFieldType('text');
+    operators: OperatorDefinition[];
 
-    constructor(private _hsbService: HierarchicalSearchBuilderService) {}
+    _field: FieldDefinition;
+    _operator: OperatorDefinition;
+
+    constructor(private _hsbService: HierarchicalSearchBuilderService, private _cfr: ComponentFactoryResolver) {}
 
     ngOnInit(): void {
+        this._field = this.fields.find((field) => field.name === this.fieldName);
+        this.operators = this._hsbService.getOperatorsByFieldType(this._field.fieldType);
+        this._operator = this.operators.find((operator) => operator.name === this.operatorName);
+    }
+
+    ngAfterViewInit(): void {
+        this.inputContainer.clear();
+        const resolver = this._cfr.resolveComponentFactory(this._operator.component);
+        this._inputComponentRef = this.inputContainer.createComponent(resolver);
+        this._inputComponentRef.instance.value = this.value;
+        this._inputComponentRef.instance.data = {};
+        this._inputComponentRef.instance.valueChange.subscribe((value: any) => {
+            console.log(value);
+        });
     }
 }
