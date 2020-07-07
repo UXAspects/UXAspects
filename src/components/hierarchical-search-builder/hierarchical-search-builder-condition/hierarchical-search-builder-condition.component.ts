f9a1a1c044cff61fb7670fb3e4031641f715b621
Erik Littwin <erik.littwin@microfocus.com>
2020-07-07 08:32:15 +0200

    add fields and operators to service

--- src/components/hierarchical-search-builder/hierarchical-search-builder-condition/hierarchical-search-builder-condition.component.ts
+++ src/components/hierarchical-search-builder/hierarchical-search-builder-condition/hierarchical-search-builder-condition.component.ts
@@ -1,12 +1,23 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { HierarchicalSearchBuilderService } from '../hierarchical-search-builder.service';
+import { FieldDefinition } from '../interfaces/FieldDefinition';
+import { OperatorDefinition } from '../interfaces/OperatorDefinition';
 
 @Component({
     selector: 'ux-hierarchical-search-builder-condition',
     templateUrl: './hierarchical-search-builder-condition.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HierarchicalSearchBuilderConditionComponent {
+export class HierarchicalSearchBuilderConditionComponent implements OnInit {
     @Input() field: string;
     @Input() operator: string;
     @Input() value: any;
+
+    fields: FieldDefinition[] = this._hsbService.getFields();
+    operators: OperatorDefinition[] = this._hsbService.getOperatorsByFieldType('text');
+
+    constructor(private _hsbService: HierarchicalSearchBuilderService) {}
+
+    ngOnInit(): void {
+    }
 }
--- src/components/hierarchical-search-builder/hierarchical-search-builder.service.ts
+++ src/components/hierarchical-search-builder/hierarchical-search-builder.service.ts
@@ -1,16 +1,22 @@
 import { Injectable } from '@angular/core';
 import { LogicalOperatorDefinition } from './interfaces/LogicalOperatorDefinition';
 import { HierarchicalSearchBuilderModule } from './hierarchical-search-builder.module';
+import { FieldDefinition } from './interfaces/FieldDefinition';
+import { OperatorDefinitionList } from './interfaces/OperatorDefinitionList';
+import { OperatorDefinition } from './interfaces/OperatorDefinition';
 
 @Injectable({
     providedIn: HierarchicalSearchBuilderModule,
 })
 export class HierarchicalSearchBuilderService {
     private _logicalOperators: LogicalOperatorDefinition[];
+    private _fields: FieldDefinition[];
+    private _operators: OperatorDefinitionList;
 
     constructor() {
     }
 
+    // Logical operators
     setLogicalOperators(logicalOperators: LogicalOperatorDefinition[]): void {
         this._logicalOperators = [...logicalOperators];
     }
@@ -21,5 +27,23 @@ export class HierarchicalSearchBuilderService {
 
     getLogicalOperatorByName(name: string): LogicalOperatorDefinition {
         return this._logicalOperators.find((operator) => operator.name === name);
+    }
+
+    // Fields
+    setFields(fields: FieldDefinition[]): void {
+        this._fields = [...fields];
+    }
+
+    getFields(): FieldDefinition[] {
+        return this._fields;
+    }
+
+    // Operators
+    setOperators(operators: OperatorDefinitionList): void {
+        this._operators = { ...operators };
+    }
+
+    getOperatorsByFieldType(fieldType: string): OperatorDefinition[] {
+        return this._operators?.[fieldType] ?? [];
     }
 }
