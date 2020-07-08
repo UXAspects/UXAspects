70bb8a7f3d375c44387b9b5167f603239f38491e
Phillip Kiele <phillip.kiele@microfocus.com>
2020-07-08 16:29:29 +0200

    Fix predefined widgets types

--- docs/app/pages/components/components-sections/dashboard-extensions/dashboard-extensions.module.ts
+++ docs/app/pages/components/components-sections/dashboard-extensions/dashboard-extensions.module.ts
@@ -1,5 +1,5 @@
-import { ComponentFactoryResolver, NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {ComponentFactoryResolver, NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 import {
     ColorServiceModule,
     DashboardModule,
@@ -10,14 +10,14 @@ import {
     TabsetModule
 } from '@ux-aspects/ux-aspects';
 import 'chart.js';
-import { ChartsModule } from 'ng2-charts';
-import { DocumentationComponentsModule } from '../../../../components/components.module';
-import { DocumentationCategoryComponent } from '../../../../components/documentation-category/documentation-category.component';
-import { DocumentationPage, ResolverService } from '../../../../services/resolver/resolver.service';
-import { ComponentsDashboardActionsWidgetComponent } from './dashboard-actions-widget/dashboard-actions-widget.component';
-import { ComponentsDashboardTextWidgetComponent } from "./dashboard-text-widget/dashboard-text-widget.component";
-import {ComponentsDashboardEnumWidgetComponent} from "./dashboard-enum-widget/dashboard-enum-widget.component";
-import {ComponentsDashboardDataTableWidgetComponent} from "./dashboard-data-table-widget/dashboard-data-table-widget.component";
+import {ChartsModule} from 'ng2-charts';
+import {DocumentationComponentsModule} from '../../../../components/components.module';
+import {DocumentationCategoryComponent} from '../../../../components/documentation-category/documentation-category.component';
+import {DocumentationPage, ResolverService} from '../../../../services/resolver/resolver.service';
+import {ComponentsDashboardActionsWidgetComponent} from './dashboard-actions-widget/dashboard-actions-widget.component';
+import {ComponentsDashboardTextWidgetComponent} from './dashboard-text-widget/dashboard-text-widget.component';
+import {ComponentsDashboardEnumWidgetComponent} from './dashboard-enum-widget/dashboard-enum-widget.component';
+import {ComponentsDashboardDataTableWidgetComponent} from './dashboard-data-table-widget/dashboard-data-table-widget.component';
 
 const SECTIONS = [
     ComponentsDashboardActionsWidgetComponent,
