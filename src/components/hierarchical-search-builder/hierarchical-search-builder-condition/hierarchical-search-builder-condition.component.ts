d49d304d39f758ab3d239512ffcbb32c94d456dd
Erik Littwin <erik.littwin@microfocus.com>
2020-07-08 15:41:46 +0200

    fixed issue with value

--- src/components/hierarchical-search-builder/hierarchical-search-builder-condition/hierarchical-search-builder-condition.component.ts
+++ src/components/hierarchical-search-builder/hierarchical-search-builder-condition/hierarchical-search-builder-condition.component.ts
@@ -30,13 +30,16 @@ export class HierarchicalSearchBuilderConditionComponent implements OnInit, Afte
 
     _field: FieldDefinition;
     _operator: OperatorDefinition;
+    _value: any;
 
-    constructor(private _hsbService: HierarchicalSearchBuilderService, private _cfr: ComponentFactoryResolver) {}
+    constructor(private _hsbService: HierarchicalSearchBuilderService, private _cfr: ComponentFactoryResolver) {
+    }
 
     ngOnInit(): void {
         this._field = this.fields.find((field) => field.name === this.fieldName);
         this.operators = this._hsbService.getOperatorsByFieldType(this._field.fieldType);
         this._operator = this.operators.find((operator) => operator.name === this.operatorName);
+        this._value = this.value;
     }
 
     ngAfterViewInit(): void {
@@ -44,20 +47,23 @@ export class HierarchicalSearchBuilderConditionComponent implements OnInit, Afte
     }
 
     createInputComponent(): void {
-        this.inputContainer.clear();
-        const resolver = this._cfr.resolveComponentFactory(this._operator.component);
-        this._inputComponentRef = this.inputContainer.createComponent(resolver);
-        this._inputComponentRef.instance.value = this.value;
-        this._inputComponentRef.instance.data = this._field?.data ?? {};
-        this._inputComponentRef.instance.valueChange.subscribe((value: any) => {
-            console.log(value);
-        });
+        if (this._operator?.component) {
+            this.inputContainer.clear();
+            const resolver = this._cfr.resolveComponentFactory(this._operator.component);
+            this._inputComponentRef = this.inputContainer.createComponent(resolver);
+            this._inputComponentRef.instance.value = this._value;
+            this._inputComponentRef.instance.data = this._field?.data ?? {};
+            this._inputComponentRef.instance.valueChange.subscribe((value: any) => {
+                this._value = value;
+            });
+        }
     }
 
     handleFieldSelected(): void {
         // get operators for new field type
         this.operators = this._hsbService.getOperatorsByFieldType(this._field.fieldType);
         this._operator = null;
+        this._value = null;
 
         this.inputContainer.clear();
     }
