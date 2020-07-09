d0dd91ac4197438b5afb76796494d99e91832a44
donna-marijne <donna.worby@microfocus.com>
2020-07-09 10:25:26 +0100

    Release 2.0.6
    * Add release notes.

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -16,6 +16,11 @@ export class ChangeLogPageComponent {
 
         this.logs = [
             {
+                version: '2.0.6',
+                date: 'July 9th 2020',
+                content: require('./logs/release-v2.0.6.md')
+            },
+            {
                 version: '2.0.5',
                 date: 'June 26th 2020',
                 content: require('./logs/release-v2.0.5.md')
