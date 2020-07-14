512a1e9446b77f7f9e0a89e3fab8b9f6a0c4ac5e
Erik Littwin <erik.littwin@microfocus.com>
2020-07-14 10:29:23 +0200

    first select input tests

--- src/components/hierarchical-search-builder/input-components/select-input/select-input.component.spec.ts
+++ src/components/hierarchical-search-builder/input-components/select-input/select-input.component.spec.ts
@@ -1,10 +1,13 @@
 import { SelectInputComponent } from './select-input.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { SelectModule } from '../../../select';
+import { TextInputComponent } from '../..';
 
 describe('SelectInputComponent', () => {
     let component: SelectInputComponent;
     let fixture: ComponentFixture<SelectInputComponent>;
+    let selectInput: HTMLInputElement;
+    let optionsContainer: HTMLElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -21,7 +24,22 @@ describe('SelectInputComponent', () => {
         fixture.detectChanges();
     });
 
+    beforeEach(async(() => {
+        fixture = TestBed.createComponent(SelectInputComponent);
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            component = fixture.componentInstance;
+            component.value = [];
+            component.data = { options: [] };
+        });
+    }));
+
     it('should be created', () => {
         expect(component).toBeDefined();
+    });
+
+    it('should be empty be default', () => {
+        selectInput = fixture.debugElement.nativeElement.querySelector('input.ux-tag-input');
+        expect(selectInput.value).toEqual('');
     });
 });
