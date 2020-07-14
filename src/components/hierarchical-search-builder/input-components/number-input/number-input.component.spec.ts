8c3e8a2a1e0cf4fd6a437ba1547544340d08d4b9
Erik Littwin <erik.littwin@microfocus.com>
2020-07-14 09:47:49 +0200

    unit test for number input

--- src/components/hierarchical-search-builder/input-components/number-input/number-input.component.spec.ts
+++ src/components/hierarchical-search-builder/input-components/number-input/number-input.component.spec.ts
@@ -5,6 +5,7 @@ import { NumberPickerModule } from '../../../number-picker';
 describe('NumberInputComponent', () => {
     let component: NumberInputComponent;
     let fixture: ComponentFixture<NumberInputComponent>;
+    let numberInput: HTMLInputElement;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -13,13 +14,29 @@ describe('NumberInputComponent', () => {
         }).compileComponents();
     }));
 
-    beforeEach(() => {
+    beforeEach(async(() => {
         fixture = TestBed.createComponent(NumberInputComponent);
-        component = fixture.componentInstance;
+
         fixture.detectChanges();
-    });
+        fixture.whenStable().then(() => {
+            component = fixture.componentInstance;
+            numberInput = fixture.debugElement.nativeElement.querySelector('input');
+        });
+    }));
 
     it('should be created', () => {
         expect(component).toBeDefined();
     });
+
+    it('should display 0 by default', () => {
+        expect(numberInput.valueAsNumber).toEqual(0);
+    });
+
+    it('should display input value', async(() => {
+        component.value = 5;
+        fixture.detectChanges();
+        fixture.whenStable().then(() => {
+            expect(numberInput.valueAsNumber).toEqual(5);
+        });
+    }));
 });
