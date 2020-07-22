622f8e5642bbd940059d7215605ef9a3b7b5ffc7
Donna Marijne <donnamarijne+gh@gmail.com>
2020-07-22 15:18:53 +0100

    EL-4001 - Tree Grid expandedChange fix (#1380)
    * Fix expandedChange to only emit on user changes
    
    * Fix an ExpressionChanged error
    
    * Add a test for EL-4001
    
    In EL-4001, the UI locks up because the expanded property is endlessly flipping. This causes `whenStable` to time out in this test.
    
    * Remove fdescribe

---
+++ src/directives/tree-grid/tree-grid.spec.ts
@@ -0,0 +1,86 @@
+import { Component, QueryList, ViewChildren } from '@angular/core';
+import { async, ComponentFixture, TestBed, fakeAsync, tick, flush } from '@angular/core/testing';
+import { TreeGridItem, TreeGridModule, TreeGridRowDirective } from './index';
+
+interface TreeGridTestItem extends TreeGridItem {
+    title: string;
+    children?: TreeGridTestItem[];
+}
+
+@Component({
+    selector: 'tree-grid-test',
+    template: `
+        <table [uxTreeGrid]="items" [(rows)]="rows">
+            <tr *ngFor="let row of rows"
+                [uxTreeGridRow]="row"
+                [canExpand]="true"
+                [(expanded)]="row.expanded">
+                <td>{{ row.title }}</td>
+            </tr>
+        </table>
+    `
+})
+export class TreeGridTestComponent {
+    items: TreeGridTestItem[] = [];
+    rows: TreeGridTestItem[];
+}
+
+describe('Tree Grid', () => {
+    let component: TreeGridTestComponent;
+    let fixture: ComponentFixture<TreeGridTestComponent>;
+    let nativeElement: HTMLElement;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [TreeGridModule],
+            declarations: [TreeGridTestComponent]
+        }).compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TreeGridTestComponent);
+        component = fixture.componentInstance;
+        nativeElement = fixture.nativeElement;
+    });
+
+    describe('with pre-expanded rows', () => {
+
+        beforeEach(async () => {
+            component.items = [
+                {
+                    title: 'Root',
+                    expanded: true,
+                    children: [
+                        {
+                            title: 'Node 1',
+                            children: [
+                                {
+                                    title: 'Leaf 1'
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ];
+            fixture.detectChanges();
+            await fixture.whenStable();
+            fixture.detectChanges();
+            await fixture.whenStable();
+        });
+
+        it('should initially render children of pre-expanded rows', () => {
+            const titles = getRowTitles();
+            expect(titles.length).toBe(2);
+            expect(titles[0]).toBe('Root');
+            expect(titles[1]).toBe('Node 1');
+        });
+    });
+
+    function getRowElements(): HTMLTableRowElement[] {
+        return Array.from(nativeElement.querySelectorAll('tr'));
+    }
+
+    function getRowTitles(): string[] {
+        return getRowElements().map(row => row.innerText);
+    }
+});
