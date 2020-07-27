e905caf13daed80f9ae99c4efc809d13070f8227
Phillip Kiele <phillip.kiele@microfocus.com>
2020-07-27 21:07:08 +0200

    Switch enum, text and data widget to OnPush

--- src/components/dashboard-widgets/dashboard-table-widget/dashboard-table-widget.component.ts
+++ src/components/dashboard-widgets/dashboard-table-widget/dashboard-table-widget.component.ts
@@ -1,11 +1,21 @@
-import { Component, Input } from '@angular/core';
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    HostListener,
+    Input,
+    OnDestroy,
+    ViewChild
+} from '@angular/core';
 import { DataWidgetConfig } from '../interfaces/data-widget';
+import { DashboardWidgetComponent } from '../../dashboard';
 
 @Component({
     selector: 'ux-dashboard-table-widget',
     templateUrl: './dashboard-table-widget.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DashboardTableWidgetComponent implements DataWidgetConfig {
+export class DashboardTableWidgetComponent implements DataWidgetConfig, OnDestroy {
     @Input() id: string = '';
     @Input() name: string = '';
     @Input() heading: string = '';
@@ -13,11 +23,24 @@ export class DashboardTableWidgetComponent implements DataWidgetConfig {
     @Input() colSpan: number = 1;
     @Input() rowSpan: number = 1;
 
+    @ViewChild('widget') widget: DashboardWidgetComponent;
     @Input() header: ReadonlyArray<any> = [];
     @Input() data: ReadonlyArray<ReadonlyArray<any>> = [];
     @Input() editable: boolean = false;
 
-    constructor() {
+    private _isDragged: boolean = false;
 
+    constructor(private changeDetectorRef: ChangeDetectorRef) {}
+
+    ngOnDestroy() {
+        this._isDragged = false;
+        this.widget.dashboardService.isDragging$.unsubscribe();
+    }
+
+    @HostListener('window:mousemove', [])
+    onMouseEvent() {
+        if (this._isDragged) {
+            this.changeDetectorRef.markForCheck();
+        }
     }
 }
