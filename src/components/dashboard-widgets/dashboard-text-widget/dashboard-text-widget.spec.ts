d9123d738bac26f1ed35fa697690fd0135ddbe4e
Phillip Kiele <phillip.kiele@microfocus.com>
2020-08-08 22:44:04 +0200

    Implement unit tests

---
+++ src/components/dashboard-widgets/dashboard-text-widget/dashboard-text-widget.spec.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DashboardModule } from '../../dashboard';
+import { DashboardPredefinedWidgetsModule } from '../dashboard-predefined-widgets.module';
+
+@Component({
+    selector: 'app-ux-text-widget',
+    template: `<ux-dashboard>
+                    <ux-dashboard-text-widget class="widget"
+                                              id="widget-text"
+                                              [name]="'Text Widget'"
+                                              [heading]="'Text Widget'"
+                                              [fixedMode]="false"
+                                              [text]="loremIpsum"
+                                              [editable]="true">
+                    </ux-dashboard-text-widget>
+               </ux-dashboard>
+    `
+})
+export class DashboardTextWidgetTestComponent {
+    loremIpsum: string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eget elit libero. Praesent placerat iaculis urna, ac iaculis ipsum consectetur quis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc nec faucibus magna, vel condimentum elit. Mauris auctor purus ut risus mattis, non pharetra sem ornare. Etiam interdum elementum elit, ut vulputate eros vestibulum nec. Fusce sed odio finibus justo mattis aliquam. Curabitur pulvinar, elit sit amet mollis feugiat, augue justo consectetur augue, sed elementum metus orci ac risus. Mauris elementum, tellus malesuada porttitor convallis, ligula ligula pulvinar diam, vitae ornare sapien velit at nulla.';
+}
+
+describe('Text Widget', () => {
+    let component: DashboardTextWidgetTestComponent;
+    let fixture: ComponentFixture<DashboardTextWidgetTestComponent>;
+    let nativeElement: HTMLElement;
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [DashboardModule, DashboardPredefinedWidgetsModule],
+            declarations: [DashboardTextWidgetTestComponent]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DashboardTextWidgetTestComponent);
+        component = fixture.componentInstance;
+        nativeElement = fixture.nativeElement;
+        fixture.detectChanges();
+    });
+
+    it('should initialise correctly', () => {
+        expect(component).toBeTruthy();
+    });
+});
