9cface13e94fdf15bb81a025320aeb0cb8eb6ce5
Erik Littwin <erik.littwin@microfocus.com>
2020-08-14 14:00:31 +0200

    testing: group

--- src/components/logical-expression-builder/leb-group/leb-group.component.spec.ts
+++ src/components/logical-expression-builder/leb-group/leb-group.component.spec.ts
@@ -1,20 +1,53 @@
-/*
 import { LebGroupComponent } from './leb-group.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { Expression, LogicalExpressionBuilderModule, LogicalOperatorDefinition } from '..';
+import { LogicalExpressionBuilderService } from '../services/logical-expression-builder.service';
 
-// TODO: remove xdescribe
-xdescribe('LebGroupComponent', () => {
-    let component: LebGroupComponent;
-    let fixture: ComponentFixture<LebGroupComponent>;
+@Component({
+    selector: 'ux-group',
+    template: `
+        <ux-leb-group
+            [path]="path"
+            [indent]="indent"
+            [logicalOperatorName]="logicalOperatorName"
+            [subExpression]="subExpression">
+        </ux-leb-group>
+    `
+})
+export class GroupTestComponent {
+    path = [0];
+    indent = 0;
+    logicalOperatorName = 'and';
+    subExpression: Expression = {type: 'group', logicalOperator: 'and', children: []};
+}
+
+describe('LebGroupComponent', () => {
+    let component: GroupTestComponent;
+    let fixture: ComponentFixture<GroupTestComponent>;
+
+    const lebService: Partial<LogicalExpressionBuilderService> = {
+        getLogicalOperatorByName(_name: string): LogicalOperatorDefinition {
+            return { name: 'and', label: 'and' };
+        },
+        getLogicalOperators(): LogicalOperatorDefinition[] {
+            return [{ name: 'and', label: 'and' }];
+        },
+        getLocalizedStrings(): any {
+            return {};
+        }
+    };
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            declarations: [LebGroupComponent]
+            declarations: [GroupTestComponent],
+            imports: [LogicalExpressionBuilderModule],
+            providers: [{ provide: LogicalExpressionBuilderService, useValue: lebService }]
         }).compileComponents();
     }));
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(LebGroupComponent);
+        fixture = TestBed.createComponent(GroupTestComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
@@ -23,4 +56,3 @@ xdescribe('LebGroupComponent', () => {
         expect(component).toBeDefined();
     });
 });
-*/
