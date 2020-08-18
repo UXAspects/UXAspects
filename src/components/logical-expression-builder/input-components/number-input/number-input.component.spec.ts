aebcc9dc93423f0168545b3d236d9afdb049b00d
Erik Littwin <erik.littwin@microfocus.com>
2020-08-18 09:59:03 +0200

    test: Number

--- src/components/logical-expression-builder/input-components/number-input/number-input.component.spec.ts
+++ src/components/logical-expression-builder/input-components/number-input/number-input.component.spec.ts
@@ -1,59 +1,60 @@
 import { NumberInputComponent } from './number-input.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NumberPickerModule } from '../../../number-picker';
+import { Component } from '@angular/core';
+import { LogicalExpressionBuilderModule } from '../../logical-expression-builder.module';
+
+@Component({
+    selector: 'ux-number-input-test',
+    template: `
+        <ux-number-input
+            [data]="data"
+            (valid)="onValidChange($event)"
+            [value]="value"
+            (valueChange)="onValueChange($event)">
+        </ux-number-input>
+    `
+})
+class NumberInputTestComponent {
+    value: number = 4;
+    data: any = {};
+
+    onValueChange(_: number): void {
+    }
+
+    onValidChange(_: boolean): void {
+    }
+}
 
 describe('NumberInputComponent', () => {
-    let component: NumberInputComponent;
-    let fixture: ComponentFixture<NumberInputComponent>;
-    let numberInput: HTMLInputElement;
+    let component: NumberInputTestComponent;
+    let fixture: ComponentFixture<NumberInputTestComponent>;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [NumberPickerModule],
-            declarations: [NumberInputComponent]
-        }).compileComponents();
+            declarations: [NumberInputTestComponent],
+            imports: [LogicalExpressionBuilderModule]
+        });
     }));
 
     beforeEach(async(() => {
-        fixture = TestBed.createComponent(NumberInputComponent);
-
+        fixture = TestBed.createComponent(NumberInputTestComponent);
+        component = fixture.componentInstance;
         fixture.detectChanges();
-        fixture.whenStable().then(() => {
-            component = fixture.componentInstance;
-            numberInput = fixture.debugElement.nativeElement.querySelector('input');
-        });
     }));
 
     it('should be created', () => {
         expect(component).toBeDefined();
     });
 
-    it('should display 0 by default', async(() => {
-        component.value = null;
-        fixture.detectChanges();
-        fixture.whenStable().then(() => {
-            expect(numberInput.valueAsNumber).toEqual(0);
-            component.valueChange.subscribe((value: number) => expect(value).toEqual(0));
-        });
-    }));
+    it('should fire valid event when value received', async(() => {
+        spyOn(component, 'onValidChange');
 
-    it('should display input value', async(() => {
         component.value = 5;
-        fixture.detectChanges();
-        fixture.whenStable().then(() => {
-            expect(numberInput.valueAsNumber).toEqual(5);
-            component.valueChange.subscribe((value: number) => expect(value).toEqual(5));
-        });
-    }));
-
-    it('should apply data', async(() => {
-        component.value = null;
-        component.data = { min: -5, max: 5 };
 
         fixture.detectChanges();
         fixture.whenStable().then(() => {
-            expect(numberInput.min).toEqual('-5');
-            expect(numberInput.max).toEqual('5');
+            expect(component.onValidChange).toHaveBeenCalled();
         });
     }));
 });
+
