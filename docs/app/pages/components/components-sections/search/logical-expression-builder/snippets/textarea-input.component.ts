dfe2bf4c8ec92437d71bdb0e3fd14b3a171884a8
Erik Littwin <erik.littwin@microfocus.com>
2020-08-21 08:02:33 +0200

    docs: Input Component example

--- docs/app/pages/components/components-sections/search/logical-expression-builder/snippets/textarea-input.component.ts
+++ docs/app/pages/components/components-sections/search/logical-expression-builder/snippets/textarea-input.component.ts
@@ -6,21 +6,24 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class TextareaInputComponent {
     @Output() valueChange: EventEmitter<string> = new EventEmitter<string>();
+    @Output() valid: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+    @Input()
+    set data(data: { maxHeight: number, validateFunction: (value: string) => boolean }) {
+        this._maxHeight = data.maxHeight;
+        this._validate = data.validateFunction;
+    }
 
     @Input()
     set value(value: string) {
         this._value = value ?? '';
-        this.valueChange.emit(this.value);
+        this.valueChange.emit(this._value);
+        this.valid.emit(this._validate(this._value));
     }
 
     get value() { return this._value; }
 
-    _value: string;
-
-    @Input()
-    set data(data: { maxHeight: number }) {
-        this.maxHeight = data.maxHeight;
-    }
-
-    maxHeight: number = 75;
+    public _value: string;
+    public _maxHeight: number = 75;
+    private _validate: (value: string) => boolean = () => true;
 }
