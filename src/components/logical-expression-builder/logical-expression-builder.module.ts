e7309955ac4cb2f37f89699e4958ba3ed7bfd1e4
Erik Littwin <erik.littwin@microfocus.com>
2020-08-24 15:15:08 +0200

    made imports more explicit

--- src/components/logical-expression-builder/logical-expression-builder.module.ts
+++ src/components/logical-expression-builder/logical-expression-builder.module.ts
@@ -5,23 +5,22 @@ import { LogicalExpressionBuilderComponent } from './logical-expression-builder.
 import { LebGroupComponent } from './leb-group/leb-group.component';
 import { LebConditionComponent } from './leb-condition/leb-condition.component';
 import { LogicalExpressionBuilderService } from './services/logical-expression-builder.service';
-import { SelectModule } from '../select';
-import { NumberPickerModule } from '../number-picker';
+import { SelectModule } from '../select/select.module';
+import { NumberPickerModule } from '../number-picker/number-picker.module';
 import { NumberInputComponent } from './input-components/number-input/number-input.component';
 import { SelectInputComponent } from './input-components/select-input/select-input.component';
 import { DateInputComponent } from './input-components/date-input/date-input.component';
-import { DateTimePickerModule } from '../date-time-picker';
-import { PopoverModule } from '../popover';
-import { AccessibilityModule } from '../../directives/accessibility';
+import { DateTimePickerModule } from '../date-time-picker/date-time-picker.module';
+import { PopoverModule } from '../popover/popover.module';
+import { AccessibilityModule } from '../../directives/accessibility/accessibility.module';
 import { L10nPipe } from './pipes/l10n.pipe';
 import { DateRangeInputComponent } from './input-components/date-range-input/date-range-input.component';
-import { DateRangePickerModule } from '../date-range-picker';
+import { DateRangePickerModule } from '../date-range-picker/date-range-picker.module';
 import { FormsModule } from '@angular/forms';
-import { HoverActionModule } from '../../directives/hover-action';
-import { MenuModule } from '../menu';
-import { IconModule } from '../icon';
+import { HoverActionModule } from '../../directives/hover-action/hover-action.module';
+import { MenuModule } from '../menu/menu.module';
+import { IconModule } from '../icon/icon.module';
 import { DisplayValuePipe } from './pipes/display-value.pipe';
-import { FocusIfModule } from '../../directives/focus-if';
 import { ValidationService } from './services/validation.service';
 import { A11yModule } from '@angular/cdk/a11y';
 import { FocusHandlerService } from './services/focus-handler.service';
@@ -66,7 +65,6 @@ const DECLARATIONS = [
     HoverActionModule,
     MenuModule,
     IconModule,
-    FocusIfModule,
     A11yModule
   ],
     exports: [...EXPORTS],
