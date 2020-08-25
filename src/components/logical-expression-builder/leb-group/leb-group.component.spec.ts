43de697b330418a28be07860f122b24a16dd549b
Erik Littwin <erik.littwin@microfocus.com>
2020-08-25 14:16:34 +0200

    fix unit test for group with service stubs

--- src/components/logical-expression-builder/leb-group/leb-group.component.spec.ts
+++ src/components/logical-expression-builder/leb-group/leb-group.component.spec.ts
@@ -1,8 +1,15 @@
 import { LebGroupComponent } from './leb-group.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { Component } from '@angular/core';
-import { Expression, LogicalExpressionBuilderModule, LogicalOperatorDefinition } from '..';
+import {
+    Expression,
+    FocusHandlerService,
+    LogicalExpressionBuilderModule,
+    LogicalOperatorDefinition,
+    ValidationService
+} from '..';
 import { LogicalExpressionBuilderService } from '../services/logical-expression-builder.service';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Component({
     selector: 'ux-group',
@@ -36,11 +43,42 @@ describe('LebGroupComponent', () => {
         }
     };
 
+    const validationService: Partial<ValidationService> = {
+        setValidationState(_: number[], __: boolean) {
+        }
+    };
+
+    const focusHandlerService: Partial<FocusHandlerService> = {
+        getRowInEditMode(): Observable<number[]> {
+            return new BehaviorSubject<number[]>([0]).asObservable();
+        },
+        getEditBlocked(): Observable<boolean> {
+            return new BehaviorSubject<boolean>(false).asObservable();
+        },
+        onTabindexChange$: new Subject<void>(),
+        isItemActive(_: any): boolean {
+            return false;
+        },
+        register(_: any) {
+        }
+    };
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [GroupTestComponent],
             imports: [LogicalExpressionBuilderModule],
-            providers: [{ provide: LogicalExpressionBuilderService, useValue: lebService }]
+            providers: [
+                {
+                    provide: LogicalExpressionBuilderService,
+                    useValue: lebService
+                }, {
+                    provide: ValidationService,
+                    useValue: validationService
+                }, {
+                    provide: FocusHandlerService,
+                    useValue: focusHandlerService
+                }
+            ]
         }).compileComponents();
     }));
 
