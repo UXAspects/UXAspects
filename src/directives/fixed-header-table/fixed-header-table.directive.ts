25a1ef9adbc521b4725532c8455eb1a84fe63813
Sasi <sasikumar.c@microfocus.com>
2020-08-26 17:19:51 +0530

    EL-4028-FixedHeader
    Added new class to achieve fixed header box shadow.

--- src/directives/fixed-header-table/fixed-header-table.directive.ts
+++ src/directives/fixed-header-table/fixed-header-table.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, Renderer2, HostBinding } from '@angular/core';
 import { takeUntil } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { ResizeService } from '../resize/index';
@@ -19,6 +19,10 @@ export class FixedHeaderTableDirective<T> implements OnInit, OnDestroy {
 
     /** Emit when the table tries to load more data */
     @Output() tablePaging: EventEmitter<number> = new EventEmitter<number>();
+
+
+    /** Apply a class whenever the table has scrolled */
+    @HostBinding('class.ux-fixed-header-table-scrolled') _hasScrolled: boolean = false;
 
     /** Store the table head element */
     private _tableHead: HTMLTableSectionElement;
@@ -105,6 +109,9 @@ export class FixedHeaderTableDirective<T> implements OnInit, OnDestroy {
         if (delta < 1) {
             this.tablePaging.emit();
         }
+
+       // update the class based on the scroll position
+        scrollTop > 0 ? this._hasScrolled = true : this._hasScrolled = false;
     }
 
 }
