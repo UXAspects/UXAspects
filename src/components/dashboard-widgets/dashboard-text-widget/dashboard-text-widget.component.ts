bdd0e337e18d7015db72196a4e037464e9691de4
Phillip Kiele <phillip.kiele@microfocus.com>
2020-08-31 11:07:45 +0200

    Add l10n strings

--- src/components/dashboard-widgets/dashboard-text-widget/dashboard-text-widget.component.ts
+++ src/components/dashboard-widgets/dashboard-text-widget/dashboard-text-widget.component.ts
@@ -1,15 +1,7 @@
-import {
-    ChangeDetectionStrategy,
-    Component,
-    ElementRef,
-    EventEmitter,
-    Input,
-    Output,
-    ViewChild
-} from '@angular/core';
-import { PredefinedWidgetConfig } from '../interfaces/predefined-widget.interface';
-import { TextWidgetConfig } from '../interfaces/text-widget.interface';
-import { SidePanelComponent } from '../../side-panel';
+import {ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, Output, ViewChild} from '@angular/core';
+import {PredefinedWidgetConfig} from '../interfaces/predefined-widget.interface';
+import {TextWidgetConfig, TextWidgetLocalizedStrings} from '../interfaces/text-widget.interface';
+import {SidePanelComponent} from '../../side-panel';
 
 @Component({
     selector: 'ux-dashboard-text-widget',
@@ -23,6 +15,12 @@ export class DashboardTextWidgetComponent implements PredefinedWidgetConfig, Tex
     @Input() fixedMode: boolean = false;
     @Input() colSpan: number = 1;
     @Input() rowSpan: number = 1;
+    @Input() localizedStrings: TextWidgetLocalizedStrings = {
+        save: 'Save',
+        cancel: 'Cancel',
+        edit: 'Edit',
+        showFullText: 'Show full text'
+    };
 
     @Input() text: string = '';
     @Input() editable: boolean = false;
@@ -36,7 +34,9 @@ export class DashboardTextWidgetComponent implements PredefinedWidgetConfig, Tex
         this.textArea.nativeElement.value = this.text;
         this.sidePanel.openPanel();
         if (this.editable) {
-            setTimeout(() => { this.textArea.nativeElement.focus(); }, 0);
+            setTimeout(() => {
+                this.textArea.nativeElement.focus();
+            }, 0);
         }
     }
 
