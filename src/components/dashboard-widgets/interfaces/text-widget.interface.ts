2236a705c9865f781296449b4c3ede18e4458422
Phillip Kiele <phillip.kiele@microfocus.com>
2020-08-31 11:07:45 +0200

    Add l10n strings

--- src/components/dashboard-widgets/interfaces/text-widget.interface.ts
+++ src/components/dashboard-widgets/interfaces/text-widget.interface.ts
@@ -3,5 +3,13 @@ import { EventEmitter } from '@angular/core';
 export interface TextWidgetConfig {
     text: string;
     editable?: boolean;
+    localizedStrings: TextWidgetLocalizedStrings;
     textChange?: EventEmitter<string>;
+}
+
+export interface TextWidgetLocalizedStrings {
+    edit: string;
+    showFullText: string;
+    save: string;
+    cancel: string;
 }
