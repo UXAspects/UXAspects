ef4cc18e3723ca332dccf9e47c17121a42818327
Erik Littwin <erik.littwin@microfocus.com>
2020-09-04 08:51:08 +0200

    review: documentation feedback

--- docs/app/pages/components/components-sections/search/logical-expression-builder/snippets/fields-definition.ts
+++ docs/app/pages/components/components-sections/search/logical-expression-builder/snippets/fields-definition.ts
@@ -9,7 +9,7 @@ fields: FieldDefinition[] = [
             options: [
                 { name: 'male', label: 'Male' },
                 { name: 'female', label: 'Female' },
-                { name: 'diverse', label: 'Diverse' }
+                { name: 'diverse', label: 'Non-binary' }
             ],
             validateFunction: (value: string[]): boolean => value.length > 0
         }
