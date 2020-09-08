0be1da97f15378243c2377a4a09307dd7529e0a6
Dean Haire <deanbenjamin.haire@microfocus.com>
2020-09-08 12:21:24 +0100

    code review changes

--- docs/app/services/playground/strategies/playground-strategy.ts
+++ docs/app/services/playground/strategies/playground-strategy.ts
@@ -1,7 +1,6 @@
 import { IPlayground } from '../../../interfaces/IPlayground';
+import { ThemeStrategy } from './themes/theme-strategy';
 import { DocumentationType } from '../tokens/documentation.token';
-import { SystemJSHelper } from '../utilities/system-helper';
-import { SiteThemeService } from '../../site-theme/site-theme.service';
 
 /**
  * This is the base implementation of a strategy to create code playground
@@ -10,7 +9,7 @@ import { SiteThemeService } from '../../site-theme/site-theme.service';
 export abstract class PlaygroundStrategy {
 
     constructor(protected documentationType: DocumentationType,
-                private _siteThemeService: SiteThemeService) { }
+                private _themeStrategy: ThemeStrategy) { }
 
     /** Get all the external scripts that should be loaded in the `head` of the HTML */
     getGlobalExternalScripts(assetsUrl: string): string[] {
@@ -34,28 +33,7 @@ export abstract class PlaygroundStrategy {
 
     /** Get all the external styles that should be loaded in the `head` of the HTML */
     getGlobalExternalStyles(assetsUrl: string): string[] {
-        const stylesheets = [
-            SystemJSHelper.getPackageUrl({ name: 'bootstrap', path: 'bootstrap@3.3.7/dist/css/bootstrap.min.css' }),
-        ];
-
-        if (this.documentationType === DocumentationType.Keppel) {
-            stylesheets.push(`${assetsUrl}/css/ux-aspects.css`);
-        } else {
-            stylesheets.push(`${assetsUrl}/styles/ux-aspects.css`);
-
-            const theme = this._siteThemeService.theme$.value;
-
-            if (theme === 'MicroFocus2020') {
-                stylesheets.push(`https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap`);
-                stylesheets.push(`${assetsUrl}/styles/quantum-ux-aspects-micro-focus-2020.css`);
-            } else if (theme === 'WhiteLabel') {
-                stylesheets.push(`${assetsUrl}/styles/quantum-ux-aspects-white-label.css`);
-            } else {
-                stylesheets.push(`${assetsUrl}/styles/quantum-ux-aspects.css`);
-            }
-        }
-
-        return stylesheets;
+        return this._themeStrategy.getStylesheets(assetsUrl);
     }
 
     /** Allow adding custom style tags to the `head` element */
