b6e386993feb7e15e57284881b5df5412e3df622
donna-marijne <donna.worby@microfocus.com>
2020-10-14 15:17:18 +0100

    Revert link button changes

--- docs/app/pages/css/css-sections/buttons/link-buttons/link-buttons.component.ts
+++ docs/app/pages/css/css-sections/buttons/link-buttons/link-buttons.component.ts
@@ -1,23 +1,23 @@
 import { Component } from '@angular/core';
 import { BaseDocumentationSection } from '../../../../../components/base-documentation-section/base-documentation-section';
 import { DocumentationSectionComponent } from '../../../../../decorators/documentation-section-component';
+import { IPlayground } from '../../../../../interfaces/IPlayground';
 import { IPlaygroundProvider } from '../../../../../interfaces/IPlaygroundProvider';
 import { playgroundAdapter } from '../../../../../services/playground/adapters/legacy-playground-adapter';
 
 @Component({
     selector: 'uxd-css-link-buttons',
-    templateUrl: './link-buttons.component.html',
+    templateUrl: './link-buttons.component.html'
 })
 @DocumentationSectionComponent('CssLinkButtonsComponent')
 export class CssLinkButtonsComponent extends BaseDocumentationSection implements IPlaygroundProvider {
 
-    playground = () => {
-        return playgroundAdapter({
-            html: this.snippets.raw.sampleHtml,
-        });
-    }
+    playground: IPlayground = playgroundAdapter({
+        html: this.snippets.raw.sampleHtml
+    });
 
     constructor() {
         super(require.context('./snippets/', false, /(html|css|js|ts)$/));
     }
+
 }
