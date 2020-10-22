e3286c2980f61e60f763af50af403b2f47607e96
Naveen-KumarP <naveen.kumarp@microfocus.com>
2020-10-22 17:08:28 +0530

    Update media-player.testpage.component.ts

--- e2e/pages/app/media-player/media-player.testpage.component.ts
+++ e2e/pages/app/media-player/media-player.testpage.component.ts
@@ -10,6 +10,6 @@ export class MediaPlayerTestPageComponent {
     mode: string = 'standard';
 
     videoSource: string =  'data:video/mp4;base64,aHR0cHM6Ly9wYWdlcy5naXRodWIuaG91c3Rvbi5zb2Z0d2FyZWdycC5uZXQvY2FmL3V4LWFzcGVjdHMtbWljcm8tZm9jdXMvYXNzZXRzL21lZGlhL2NhdGNoaW5nd2F2ZS5tcDQ=';
-    subtitles: string =  'data:video/mp4;base64,/+aHR0cHM6Ly9wYWdlcy5naXRodWIuaG91c3Rvbi5zb2Z0d2FyZWdycC5uZXQvY2FmL3V4LWFzcGVjdHMtbWljcm8tZm9jdXMvMDA4NDQyNDc3MzAxM2ZhNTdjNTI4MzJiZjE0NWUxZTQudnR0';
+    subtitles: string =  'data:text/vtt;base64,aHR0cHM6Ly9wYWdlcy5naXRodWIuaG91c3Rvbi5zb2Z0d2FyZWdycC5uZXQvY2FmL3V4LWFzcGVjdHMtbWljcm8tZm9jdXMvMDA4NDQyNDc3MzAxM2ZhNTdjNTI4MzJiZjE0NWUxZTQudnR0';
 
 }
