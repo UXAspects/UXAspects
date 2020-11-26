df5aea2aa247259fd0fde594003ca2329141fbbb
Dean Haire <deanbenjamin.haire@microfocus.com>
2020-11-26 16:35:08 +0000

    Remove Angular 8 Support

--- src/directives/drag/drag.module.ts
+++ src/directives/drag/drag.module.ts
@@ -1,29 +1,9 @@
-import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { DragDirective } from './drag.directive';
 import { DropDirective } from './drop.directive';
-import { DragService } from './drag.service';
-
-/**
- * Note: This is a workaround for the Angular 8 providedIn: 'root'
- * issue.
- *
- * This provider allows us to have only a single instance
- * of the service throughout out entire application
- * regardless of how many times this module is imported.
- */
-export function DRAG_SERVICE_PROVIDER_FACTORY(parentService: DragService) {
-    return parentService || new DragService();
-}
-
-export const DRAG_SERVICE_PROVIDER = {
-    provide: DragService,
-    deps: [[new Optional(), new SkipSelf(), DragService]],
-    useFactory: DRAG_SERVICE_PROVIDER_FACTORY
-};
 
 @NgModule({
     exports: [DragDirective, DropDirective],
-    declarations: [DragDirective, DropDirective],
-    providers: [DRAG_SERVICE_PROVIDER]
+    declarations: [DragDirective, DropDirective]
 })
 export class DragModule {}
--- src/directives/help-center/help-center.service.ts
+++ src/directives/help-center/help-center.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
-@Injectable()
+@Injectable({
+    providedIn: 'root'
+})
 export class HelpCenterService {
 
     items: BehaviorSubject<HelpCenterItem[]> = new BehaviorSubject<HelpCenterItem[]>([]);
