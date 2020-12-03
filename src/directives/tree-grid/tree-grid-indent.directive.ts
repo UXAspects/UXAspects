60505a6ec632f764bcee0b76f052b1a2c0baf5e5
Steve Berube <steve.berube@microfocus.com>
2020-12-03 11:43:47 -0500

    fixed tree grid indentation

--- src/directives/tree-grid/tree-grid-indent.directive.ts
+++ src/directives/tree-grid/tree-grid-indent.directive.ts
@@ -1,5 +1,6 @@
-import { Directive, HostBinding, Input } from '@angular/core';
-import { TreeGridRowDirective } from './tree-grid-row.directive';
+import {Directive, HostBinding, Input} from '@angular/core';
+import {TreeGridRowDirective} from './tree-grid-row.directive';
+import {Directionality} from "@angular/cdk/bidi";
 
 @Directive({
     selector: '[uxTreeGridIndent]',
@@ -9,11 +10,28 @@ export class TreeGridIndentDirective {
     /** The amount each level should be indented by */
     @Input() uxTreeGridIndent: number;
 
-    /** The padding value applied to each level */
-    @HostBinding('style.padding-left.px')
-    get indentation(): number {
-        return this._row && this._row.item ? 7 + (this._row.item.state.level * (this.uxTreeGridIndent || 25)) : 7;
+    constructor(private _row: TreeGridRowDirective,
+                private _directionality: Directionality) {
     }
 
-    constructor(private _row: TreeGridRowDirective) {}
+    /** The padding value applied to each level */
+    @HostBinding('style.padding-left.px')
+    get leftIndentation(): number {
+        if (this._directionality.value === 'ltr') {
+            return this.getIndentation();
+        }
+        return 0;
+    }
+
+    @HostBinding('style.padding-right.px')
+    get rightIndentation(): number {
+        if (this._directionality.value === 'rtl') {
+            return this.getIndentation();
+        }
+        return 0;
+    }
+
+    private getIndentation() {
+        return this._row && this._row.item ? 7 + (this._row.item.state.level * (this.uxTreeGridIndent || 25)) : 7;
+    }
 }
