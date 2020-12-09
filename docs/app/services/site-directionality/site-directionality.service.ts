f2c5c24ee3626aabc89fae94d5051d9103311091
Steve Berube <steve.berube@microfocus.com>
2020-12-09 11:28:23 -0500

    update

--- docs/app/services/site-directionality/site-directionality.service.ts
+++ docs/app/services/site-directionality/site-directionality.service.ts
@@ -1,6 +1,7 @@
-import {Injectable} from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 import {Direction} from "@angular/cdk/bidi";
+import {DOCUMENT} from "@angular/common";
 
 @Injectable({
     providedIn: 'root'
@@ -9,11 +10,16 @@ export class SiteDirectionalityService {
 
     direction$ = new BehaviorSubject<Direction>('ltr');
 
-    constructor() {
+    constructor(@Inject(DOCUMENT) private _document: Document) {
+        this.setDirection('ltr');
     }
 
     setDirection(direction: Direction): void {
         this.direction$.next(direction || 'ltr');
+        
+        // the html element direction is also updated to ensure CSS styles on body and html are reflected properly.
+        const html = this._document.querySelector('link') as HTMLElement;
+        html.setAttribute('dir', direction);
     }
 }
 
