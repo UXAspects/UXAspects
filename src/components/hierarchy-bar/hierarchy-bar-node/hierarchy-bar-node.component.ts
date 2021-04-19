e1e56f58ad9521b41ade96c2e8479fa32c1f0be3
Christina Mallon <56357626+cmallon367@users.noreply.github.com>
2021-04-19 16:56:31 +0100

    EL-4208- Updated overflow appearance (#1512)
    * Updated overflow appearance in standard mode & updated overflow screenshot
    
    * code review
    
    * Screenshot update
    
    * Code review
    
    * Changes
    
    * Pushing some changes
    
    * Code review
    
    * Code Review
    
    * code review
    
    * Code review
    
    * code review
    
    * Layout fix
    
    * Pushing some changes
    
    * Code Review
    
    * screenshot update
    
    * Removed unused code
    
    Co-authored-by: Ashley Hunter <ashley-glenn.hunter@microfocus.com>

--- src/components/hierarchy-bar/hierarchy-bar-node/hierarchy-bar-node.component.ts
+++ src/components/hierarchy-bar/hierarchy-bar-node/hierarchy-bar-node.component.ts
@@ -1,14 +1,16 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
 import { HierarchyBarService } from '../hierarchy-bar.service';
 import { HierarchyBarNode } from '../interfaces/hierarchy-bar-node.interface';
 
 @Component({
     selector: 'ux-hierarchy-bar-node',
     templateUrl: './hierarchy-bar-node.component.html',
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    host: {
+        '[style.visibility]': 'visible ? "visible" : "hidden"',
+    },
 })
 export class HierarchyBarNodeComponent {
-
     /** Specify the node data */
     @Input() node: HierarchyBarNode;
 
@@ -21,9 +23,22 @@ export class HierarchyBarNodeComponent {
     /** Determine read only state */
     @Input() readonly: boolean;
 
+    /** Optionally define the horizontal offset */
+    @Input() offset: number = 0;
+
     /** Emit when the node is selected */
     @Output() selected = new EventEmitter<HierarchyBarNode>();
 
-    constructor(public readonly hierarchyBar: HierarchyBarService) { }
+    /** Determine if this node should be hidden due to overflow */
+    visible: boolean = true;
 
+    /** Get the width of the element */
+    get width(): number {
+        return this._elementRef.nativeElement.offsetWidth;
+    }
+
+    constructor(
+        public readonly hierarchyBar: HierarchyBarService,
+        private readonly _elementRef: ElementRef<HTMLElement>
+    ) {}
 }
