e8f36dda6a7d1f9a9c2e64775224305ebe8aacf4
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2021-04-21 17:28:07 +0100

    Adding readonlyInput to e2e select test

--- e2e/pages/app/select/standard/select.testpage.component.ts
+++ e2e/pages/app/select/standard/select.testpage.component.ts
@@ -24,6 +24,7 @@ export class SelectTestPageComponent implements OnInit {
     customIcon = false;
     filterDebounceTime = 200;
     autoCloseDropdown = true;
+    readonlyInput = false;
 
     pageSize = 20;
 
