6256e5389fed2a8239b82cdae5a785d476aef068
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2021-04-30 11:59:35 +0100

    removing space

--- src/components/dashboard/dashboard.component.ts
+++ src/components/dashboard/dashboard.component.ts
@@ -96,7 +96,6 @@ export class DashboardComponent implements AfterViewInit, AfterContentInit, OnDe
 
     /** Shift widgets up where possible to fill any available space to optimize the dashboard layout */
     refreshLayout(): void {
-
         const didChangeLayout = this.dashboardService.shiftWidgetsUp();
 
         // emit information about the layout
