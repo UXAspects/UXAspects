458e7e15d773c71e9709396862342085578f700a
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2021-05-07 14:39:27 +0100

    tslint fixes

--- src/services/color/color.service.spec.ts
+++ src/services/color/color.service.spec.ts
@@ -13,12 +13,12 @@ describe('Color Service - Micro Focus Color Set', () => {
 
   it('should return the correct rgb values for aliases', () => {
     const service: ColorService = TestBed.inject(ColorService);
-    service.getColorSet()
+    service.getColorSet();
 
     expect(service.getColor('critical').toRgb()).toBe('rgb(229, 0, 76)');
     expect(service.getColor('danger').toRgb()).toBe('rgb(244, 139, 52)');
     expect(service.getColor('warning').toRgb()).toBe('rgb(252, 219, 31)');
     expect(service.getColor('ok').toRgb()).toBe('rgb(26, 172, 96)');
     expect(service.getColor('info').toRgb()).toBe('rgb(0, 171, 243)');
-  })
+  });
 });
