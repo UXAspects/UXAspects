863809fe6e029fcd72ea691d66779b668ee524c6
cmallon367 <cmallon111@gmail.com>
2021-05-11 10:52:38 +0100

    Code Review

--- e2e/pages/app/select/custom-heading/select-custom-heading.testpage.component.ts
+++ e2e/pages/app/select/custom-heading/select-custom-heading.testpage.component.ts
@@ -1,41 +1,16 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
     selector: 'app-select-custom-heading',
     templateUrl: './select-custom-heading.testpage.component.html',
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SelectCustomHeadingTestPageComponent implements OnInit {
+export class SelectCustomHeadingTestPageComponent {
 
     // ux-select configuration properties
-    options: string[] | Function;
+    options: string[] = [ 'United States', 'United Kingdom', 'Afghanistan', 'Aland Islands', 'Albania', 'Algeria', 'American Samoa'];
     dropdownOpen: boolean;
     placeholder = 'Select a country';
     recentOptions: ReadonlyArray<string> = null;
-
-    // Customize settings
-    dataSet = new BehaviorSubject<string>('strings');
-
-    dataSets: { strings?: any[], objects?: any[] } = {};
-
-    constructor() {
-
-        // "strings" data set
-        this.dataSets.strings = ['United States', 'United Kingdom', 'Afghanistan', 'Aland Islands', 'Albania', 'Algeria', 'American Samoa'];
-
-        // "objects" data set
-        this.dataSets.objects = this.dataSets.strings.map((option, i) => {
-            return { id: i, name: option };
-        });
-    }
-
-    ngOnInit() {
-        this.options = this.selectedDataSet();
-    }
-
-    selectedDataSet(): any[] {
-        return this.dataSets[this.dataSet.getValue()];
-    }
 
 }
--- e2e/tests/components/select/custom-heading/select-custom-heading.po.spec.ts
+++ e2e/tests/components/select/custom-heading/select-custom-heading.po.spec.ts
@@ -1,34 +1,26 @@
-import { browser, by, element, ElementFinder } from 'protractor';
+import { browser, by, element, ElementFinder, promise } from 'protractor';
 
 export class SelectCustomHeadingPage {
 
     dropdown = element(by.id('dropdown'));
-    optionsHeading = element(by.id('Nodes'));
-    recentHeadingOptions = element(by.id('recentNodes'));
+    optionsHeader = element(by.id('options-header'));
+    recentHeadingOptions = element(by.id('recent-options-header'));
     checkboxRecentOptions = element(by.id('checkbox1'));
 
     async getPage(): Promise<void> {
         await browser.get('#/select/custom-heading');
     }
 
-    getCustomHeadingText() {
-        return this.optionsHeading.getText();
-    }
-
-    getRecentOptionHeadingText() {
-        return this.recentHeadingOptions.getText();
-    }
-
-    getCountry(_allowMultiple: boolean, index: number) {
+    getCountry(_allowMultiple: boolean, index: number): ElementFinder {
         return this.dropdown.$$('.ux-typeahead-all-options li').get(index);
     }
 
-    getRecentCountry(_allowMultiple: boolean, index: number) {
+    getRecentCountry(_allowMultiple: boolean, index: number): ElementFinder {
         return this.dropdown.$$('.ux-typeahead-recent-options li').get(index);
     }
 
     // get item
-    getDropdown(allowMultiple: boolean) {
+    getDropdown(allowMultiple: boolean): ElementFinder {
         if (allowMultiple) {
             return this.dropdown.$('input.ux-tag-input');
         } else {
@@ -36,28 +28,28 @@ export class SelectCustomHeadingPage {
         }
     }
 
-    getRecentCountryText(allowMultiple: boolean, index: number) {
+    getRecentCountryText(allowMultiple: boolean, index: number): promise.Promise<string> {
         return this.getRecentCountry(allowMultiple, index).$('span.ux-typeahead-option').getText();
     }
 
-    getNumberOfRecentCountries(_allowMultiple: boolean) {
+    getNumberOfRecentCountries(_allowMultiple: boolean): promise.Promise<number> {
         return this.dropdown.$$('.ux-typeahead-recent-options li').count();
     }
 
     // click
-    clickOnDropdown(allowMultiple: boolean) {
+    clickOnDropdown(allowMultiple: boolean): promise.Promise<void> {
         return this.getDropdown(allowMultiple).click();
     }
 
-    async clickOnCheckbox(checkbox: ElementFinder) {
+    async clickOnCheckbox(checkbox: ElementFinder): Promise<void> {
         await checkbox.$('.ux-checkbox').click();
     }
 
-    clickOnCountry(allowMultiple: boolean, index: number) {
+    clickOnCountry(allowMultiple: boolean, index: number): promise.Promise<void> {
         return this.getCountry(allowMultiple, index).click();
     }
 
-    async checkRecentOptions(multi: boolean, expectedOptions: string[]) {
+    async checkRecentOptions(multi: boolean, expectedOptions: string[]): Promise<void> {
         if (!multi) {
             await this.clickOnDropdown(multi);
         }
