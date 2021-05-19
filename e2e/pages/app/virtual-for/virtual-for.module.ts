48150c372c92e156ce9e8354ef93bc6da86a8044
Donna Marijne <donna.worby@microfocus.com>
2021-05-19 17:55:39 +0100

    add test coverage

--- e2e/pages/app/virtual-for/virtual-for.module.ts
+++ e2e/pages/app/virtual-for/virtual-for.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AccessibilityModule, FixedHeaderTableModule, VirtualScrollModule } from '@ux-aspects/ux-aspects';
+import {
+    AccessibilityModule,
+    FixedHeaderTableModule,
+    RadioButtonModule,
+    SelectionModule,
+    VirtualScrollModule,
+} from '@ux-aspects/ux-aspects';
+import {
+    FormatTestItemPipe,
+    VirtualForSelectionTestPageComponent,
+} from './selection/virtual-for-selection.testpage.component';
 import { VirtualForTestPageComponent } from './virtual-for.testpage.component';
+
+const ROUTES = [
+    {
+        path: '',
+        component: VirtualForTestPageComponent,
+    },
+    {
+        path: 'selection',
+        component: VirtualForSelectionTestPageComponent,
+    },
+];
 
 @NgModule({
     imports: [
         AccessibilityModule,
-        VirtualScrollModule,
         FixedHeaderTableModule,
-        RouterModule.forChild([
-            {
-                path: '',
-                component: VirtualForTestPageComponent
-            }
-        ])
+        RadioButtonModule,
+        RouterModule.forChild(ROUTES),
+        SelectionModule,
+        VirtualScrollModule,
     ],
-    declarations: [VirtualForTestPageComponent]
+    declarations: [FormatTestItemPipe, VirtualForTestPageComponent, VirtualForSelectionTestPageComponent],
 })
-export class VirtualForTestPageModule { }
+export class VirtualForTestPageModule {}
