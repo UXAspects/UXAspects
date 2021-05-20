509fcda0744703c0ebfe794abe2a28e3e979375c
Donna Marijne <donnamarijne+gh@gmail.com>
2021-05-20 09:52:14 +0100

    EL-4219 - Node 12 build image (#1539)
    * node 12 build image
    
    * Update official-build.props
    
    * update buildenv to 2.0.0
    
    * fix iconset script
    
    * add generate:iconset task to run `grunt iconset` in a Node 10 container
    
    * update documentation
    
    * use known good version of standalone-chrome image
    
    * remove remote resource from hierarchy bar test page

--- scripts/docker.js
+++ scripts/docker.js
@@ -1,20 +1,21 @@
 const { execSync } = require('child_process');
-const { exit } = require('process');
+const { argv, exit } = require('process');
 const { join } = require('path');
 const { mkdirpSync } = require('fs-extra');
 
-// access environment
+const args = argv.slice(2);
+const image = args[0] || 'uxaspects/buildenv:latest';
+const command = args[1] ? `/bin/bash -c "${args[1]}"` : '/bin/bash';
 const http_proxy = process.env.HTTP_PROXY;
 const https_proxy = process.env.HTTPS_PROXY;
 const cwd = process.cwd();
-
 const nodeModulesDocker = join(cwd, '.node_modules__docker');
 
 // Ensure the docker node_modules directory exists
 mkdirpSync(nodeModulesDocker);
 
 // produce the docker command string
-const dockerCommand = `docker run --rm -it --memory=4g -e "http_proxy=${http_proxy}" -e "https_proxy=${https_proxy}" -v "${cwd}:/wd" -v "${nodeModulesDocker}:/wd/node_modules" -w /wd --entrypoint /bin/bash uxaspects/buildenv:1.4.0`;
+const dockerCommand = `docker run --rm -it --memory=4g -e "http_proxy=${http_proxy}" -e "https_proxy=${https_proxy}" -v "${cwd}:/wd" -v "${nodeModulesDocker}:/wd/node_modules" -w /wd ${image} ${command}`;
 
 // run the command string with the inherited terminal
 try {
