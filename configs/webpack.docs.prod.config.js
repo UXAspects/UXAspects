a177cb404c075516034e18deb6be68852b048d21
Donna Marijne <donna.worby@microfocus.com>
2021-05-21 16:34:42 +0100

    fix e2e tests

--- configs/webpack.docs.prod.config.js
+++ configs/webpack.docs.prod.config.js
@@ -6,24 +6,25 @@ const { cwd } = require('process');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const rxAlias = require('rxjs/_esm5/path-mapping');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { AngularWebpackPlugin  } = require('@ngtools/webpack');
+const { AngularWebpackPlugin } = require('@ngtools/webpack');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-const { IndexHtmlWebpackPlugin } = require('@angular-devkit/build-angular/src/webpack/plugins/index-html-webpack-plugin');
+const {
+    IndexHtmlWebpackPlugin,
+} = require('@angular-devkit/build-angular/src/webpack/plugins/index-html-webpack-plugin');
 const { BuildOptimizerWebpackPlugin } = require('@angular-devkit/build-optimizer');
 
 const CssLoader = {
     loader: 'css-loader',
     options: {
-        esModule: false
-    }
+        esModule: false,
+    },
 };
 
 // Node has a limit to the number of files that can be open - prevent the error
 gracefulFs.gracefulify(fs);
 
 module.exports = {
-
     mode: 'production',
     devtool: false,
     stats: 'minimal',
@@ -36,21 +37,20 @@ module.exports = {
     resolveLoader: {
         alias: {
             'code-snippet-loader': join(cwd(), 'configs', 'loaders', 'code-snippet-loader.js'),
-            'markdown-highlighter-loader': join(cwd(), 'configs', 'loaders', 'markdown-highlighter-loader.js')
-        }
+            'markdown-highlighter-loader': join(cwd(), 'configs', 'loaders', 'markdown-highlighter-loader.js'),
+        },
     },
 
     entry: {
         main: './docs/main.ts',
         polyfills: './docs/polyfills.ts',
-        styles: './docs/styles.less'
+        styles: './docs/styles.less',
     },
-
 
     output: {
         path: join(cwd(), 'dist', 'docs'),
         filename: '[name].js',
-        chunkFilename: 'modules/[id].chunk.js'
+        chunkFilename: 'modules/[id].chunk.js',
     },
 
     module: {
@@ -58,16 +58,16 @@ module.exports = {
             {
                 test: /\.html$/,
                 use: 'html-loader',
-                exclude: /(directives|templates|snippets)/
+                exclude: /(directives|templates|snippets)/,
             },
             {
                 test: /\.css$/,
                 exclude: /snippets/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
                 test: /\.md$/,
-                use: ['html-loader', 'markdown-highlighter-loader']
+                use: ['html-loader', 'markdown-highlighter-loader'],
             },
             {
                 test: /\.ts$/,
@@ -76,34 +76,28 @@ module.exports = {
                     {
                         loader: '@angular-devkit/build-optimizer/webpack-loader',
                         options: {
-                            sourceMap: false
-                        }
+                            sourceMap: false,
+                        },
                     },
-                    '@ngtools/webpack'
-                ]
+                    '@ngtools/webpack',
+                ],
             },
             {
                 test: /\.js$/,
                 loader: '@angular-devkit/build-optimizer/webpack-loader',
                 options: {
-                    sourceMap: false
-                }
+                    sourceMap: false,
+                },
             },
             {
                 test: /\.less$/,
-                include: [
-                    join(cwd(), 'docs', 'app')
-                ],
-                use: ['to-string-loader', CssLoader, 'less-loader']
+                include: [join(cwd(), 'docs', 'app')],
+                use: ['to-string-loader', CssLoader, 'less-loader'],
             },
             {
                 test: /\.less$/,
-                exclude: [
-                    join(cwd(), 'docs', 'app'),
-                    join(cwd(), 'src', 'components'),
-                    join(cwd(), 'src', 'services')
-                ],
-                use: [MiniCssExtractPlugin.loader, CssLoader, 'less-loader']
+                exclude: [join(cwd(), 'docs', 'app'), join(cwd(), 'src', 'components'), join(cwd(), 'src', 'services')],
+                use: [MiniCssExtractPlugin.loader, CssLoader, 'less-loader'],
             },
             {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico|otf|mp4|mp3)$/,
@@ -111,10 +105,11 @@ module.exports = {
                     {
                         loader: 'file-loader',
                         options: {
-                            esModule: false
-                        }
-                    }
-                ]
+                            esModule: false,
+                            name: 'assets/[name].[ext]',
+                        },
+                    },
+                ],
             },
 
             /*
@@ -123,18 +118,18 @@ module.exports = {
             {
                 test: /\.(html|js|css|ts)$/,
                 use: 'code-snippet-loader',
-                include: /snippets/
+                include: /snippets/,
             },
 
             {
                 test: /\.txt$/,
                 use: 'raw-loader',
-                include: /templates/
+                include: /templates/,
             },
             // Ignore warnings about System.import in Angular
             {
                 test: /[\/\\]@angular[\/\\].+\.js$/,
-                parser: { system: true }
+                parser: { system: true },
             },
 
             // Downlevel Angular Packages
@@ -147,25 +142,23 @@ module.exports = {
                             [
                                 '@babel/preset-env',
                                 {
-                                    targets: ['chrome 84', 'ie 11',],
+                                    targets: ['chrome 84', 'ie 11'],
                                     modules: false,
                                     exclude: ['transform-typeof-symbol'], // 'transform-typeof-symbol' generates slower code
                                 },
-                            ]
+                            ],
                         ],
-                        plugins: [
-                            ['@babel/plugin-transform-spread', { loose: true }]
-                        ],
+                        plugins: [['@babel/plugin-transform-spread', { loose: true }]],
                         inputSourceMap: false,
                         babelrc: false,
                         configFile: false,
                         minified: false, // handled by TerserPlugin
                         compact: false,
-                        cacheDirectory: true
-                    }
-                }
-            }
-        ]
+                        cacheDirectory: true,
+                    },
+                },
+            },
+        ],
     },
 
     optimization: {
@@ -176,31 +169,31 @@ module.exports = {
                 default: {
                     chunks: 'async',
                     minChunks: 2,
-                    priority: 10
+                    priority: 10,
                 },
                 common: {
                     name: 'common',
                     chunks: 'async',
                     minChunks: 2,
                     enforce: true,
-                    priority: 5
+                    priority: 5,
                 },
                 defaultVendors: false,
-                vendor: false
-            }
+                vendor: false,
+            },
         },
         minimizer: [
             new CssMinimizerPlugin({
                 minimizerOptions: {
-                  preset: [
-                    'default',
-                    {
-                      svgo: false,
-                      calc: false,
-                      cssDeclarationSorter: false
-                    }
-                  ]
-                }
+                    preset: [
+                        'default',
+                        {
+                            svgo: false,
+                            calc: false,
+                            cssDeclarationSorter: false,
+                        },
+                    ],
+                },
             }),
             new TerserPlugin({
                 sourceMap: false,
@@ -214,7 +207,7 @@ module.exports = {
                         ascii_only: true,
                         comments: false,
                         webkit: true,
-                        beautify: false
+                        beautify: false,
                     },
                     compress: {
                         ecma: 5,
@@ -223,74 +216,70 @@ module.exports = {
                         global_defs: {
                             ngDevMode: false,
                             ngI18nClosureMode: false,
-                            ngJitMode: false
-                        }
+                            ngJitMode: false,
+                        },
                     },
-                    mangle: true
-                }
-            })
-        ]
+                    mangle: true,
+                },
+            }),
+        ],
     },
 
     plugins: [
         new IndexHtmlWebpackPlugin({
             indexPath: './docs/index.html',
             outputPath: 'index.html',
-            entrypoints: [
-                'polyfills',
-                'styles',
-                'main'
-            ],
+            entrypoints: ['polyfills', 'styles', 'main'],
             noModuleEntrypoints: [],
             moduleEntrypoints: [],
-            sri: false
+            sri: false,
         }),
 
         new MiniCssExtractPlugin({
             filename: '[name].css',
-            chunkFilename: '[id].css'
+            chunkFilename: '[id].css',
         }),
 
         new CopyWebpackPlugin({
             patterns: [
                 {
                     from: join(cwd(), 'docs', 'favicon.ico'),
-                    to: join(cwd(), 'dist', 'docs', 'favicon.ico')
+                    to: join(cwd(), 'dist', 'docs', 'favicon.ico'),
                 },
                 {
                     from: join(cwd(), 'dist', 'library', 'bundles', 'ux-aspects-ux-aspects.umd.js'),
-                    to: join(cwd(), 'dist', 'docs', 'assets', 'lib', 'index.js')
+                    to: join(cwd(), 'dist', 'docs', 'assets', 'lib', 'index.js'),
                 },
                 {
                     from: join(cwd(), 'docs', 'app', 'assets'),
-                    to: join(cwd(), 'dist', 'docs', 'assets')
+                    to: join(cwd(), 'dist', 'docs', 'assets'),
                 },
                 {
                     from: join(cwd(), 'dist', 'library', 'styles'),
-                    to: join(cwd(), 'dist', 'docs', 'assets', 'css')
+                    to: join(cwd(), 'dist', 'docs', 'assets', 'css'),
                 },
                 {
                     from: join(cwd(), 'src', 'fonts'),
-                    to: join(cwd(), 'dist', 'docs', 'assets', 'fonts')
+                    to: join(cwd(), 'dist', 'docs', 'assets', 'fonts'),
                 },
                 {
                     from: join(cwd(), 'src', 'img'),
-                    to: join(cwd(), 'dist', 'docs', 'assets', 'img')
+                    to: join(cwd(), 'dist', 'docs', 'assets', 'img'),
                 },
-            ]
+            ],
         }),
 
         new AngularWebpackPlugin({
-            tsconfig: join(cwd(), 'tsconfig-prod.json')
+            tsconfig: join(cwd(), 'tsconfig-prod.json'),
         }),
 
         new BuildOptimizerWebpackPlugin(),
 
         new DefinePlugin({
             VERSION: JSON.stringify(require('../src/package.json').version),
-            PRODUCTION: true
+            PRODUCTION: true,
         }),
     ],
 
-    node: false
+    node: false,
 };
