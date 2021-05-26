72099ae3103b8ed9b33969204b3a0cf6c9e7ef52
Donna Marijne <donna.worby@microfocus.com>
2021-05-26 09:43:45 +0100

    Update webpack.lib-assets.dev.config.js

--- configs/webpack.lib-assets.dev.config.js
+++ configs/webpack.lib-assets.dev.config.js
@@ -3,6 +3,7 @@ const { cwd } = require('process');
 const { ContextReplacementPlugin } = require('webpack');
 
 module.exports = {
+
     mode: 'production',
 
     entry: join(cwd(), 'src', 'index.ts'),
@@ -10,43 +11,44 @@ module.exports = {
     output: {
         path: join(cwd(), 'dist', 'docs', 'assets', 'lib'),
         filename: 'index.js',
-        libraryTarget: 'umd',
+        libraryTarget: 'umd'
     },
 
     resolve: {
-        extensions: ['.ts', '.js'],
+        extensions: ['.ts', '.js']
     },
 
-    externals: [/^@angular\//, /^rxjs\//, /^angular-split/],
+    externals: [
+        /^@angular\//,
+        /^rxjs\//,
+        /^angular-split/
+    ],
 
     module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: 'raw-loader',
-            },
-            {
-                test: /\.ts$/,
-                use: [
-                    {
-                        loader: 'awesome-typescript-loader',
-                        options: {
-                            configFileName: join(cwd(), 'src', 'tsconfig-build.json'),
-                        },
-                    },
-                    {
-                        loader: 'angular2-template-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.less$/,
-                use: ['raw-loader', 'less-loader'],
-            },
-        ],
+        rules: [{
+            test: /\.html$/,
+            use: 'raw-loader'
+        }, {
+            test: /\.ts$/,
+            use: [{
+                loader: 'awesome-typescript-loader',
+                options: {
+                    configFileName: join(cwd(), 'src', 'tsconfig-build.json')
+                },
+            }, {
+                loader: 'angular2-template-loader'
+            }]
+        }, {
+            test: /\.less$/,
+            use: ['raw-loader', 'less-loader']
+        }
+        ]
     },
 
     plugins: [
-        new ContextReplacementPlugin(/(.+)?angular(\\|\/)core(.+)?/, join(cwd(), 'docs')),
-    ],
+        new ContextReplacementPlugin(
+            /(.+)?angular(\\|\/)core(.+)?/,
+            join(cwd(), 'docs')
+        )
+    ]
 };
