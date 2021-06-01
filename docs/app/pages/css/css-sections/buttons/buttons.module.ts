d63ea5282bde1e86840850429eefc91a010ae8f8
Donna Marijne <donna.worby@microfocus.com>
2021-06-01 12:05:59 +0100

    replaced hpe-icon and removed redundant doc sections

--- docs/app/pages/css/css-sections/buttons/buttons.module.ts
+++ docs/app/pages/css/css-sections/buttons/buttons.module.ts
@@ -4,24 +4,17 @@ import { RouterModule } from '@angular/router';
 import { DocumentationComponentsModule } from '../../../../components/components.module';
 import { DocumentationCategoryComponent } from '../../../../components/documentation-category/documentation-category.component';
 import { DocumentationPage, ResolverService } from '../../../../services/resolver/resolver.service';
-import { CssButtonDropdownsComponent } from './button-dropdowns/button-dropdowns.component';
-import { CssCircularIconButtonsComponent } from './circular-icon-buttons/circular-icon-buttons.component';
 import { CssColoredButtonsComponent } from './colored-buttons/colored-buttons.component';
 import { CssHyperlinksComponent } from './hyperlinks/hyperlinks.component';
 import { IconButtonsDocumentationComponent } from './icon-buttons/icon-buttons.component';
 import { CssLinkButtonsComponent } from './link-buttons/link-buttons.component';
 import { CssButtonsSizeVariationsComponent } from './size-variations/size-variations.component';
-import { CssSplitButtonDropdownsComponent } from './split-button-dropdowns/split-button-dropdowns.component';
-
 
 const SECTIONS = [
-    CssButtonDropdownsComponent,
-    CssCircularIconButtonsComponent,
     CssColoredButtonsComponent,
     CssHyperlinksComponent,
     CssLinkButtonsComponent,
     CssButtonsSizeVariationsComponent,
-    CssSplitButtonDropdownsComponent,
     IconButtonsDocumentationComponent
 ];
 
--- e2e/pages/app/reorderable-table/reorderable-table.module.ts
+++ e2e/pages/app/reorderable-table/reorderable-table.module.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { AccessibilityModule, ReorderableModule, SparkModule } from '@ux-aspects/ux-aspects';
+import { AccessibilityModule, IconModule, ReorderableModule, SparkModule } from '@ux-aspects/ux-aspects';
 import { MapPipe, ReorderableTableTestPageComponent } from './reorderable-table.testpage.component';
 
 @NgModule({
     imports: [
         CommonModule,
+        IconModule,
         ReorderableModule,
         SparkModule,
         AccessibilityModule,
