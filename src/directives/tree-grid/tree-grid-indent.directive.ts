5086d5c2e8f372ca6a7b94db8ac978d1f34b5c2e
Donna Marijne <donna.worby@microfocus.com>
2021-06-07 17:39:47 +0100

    fix strict compilation error "Type 'string' is not assignable to type 'number'."

--- src/directives/tree-grid/tree-grid-indent.directive.ts
+++ src/directives/tree-grid/tree-grid-indent.directive.ts
@@ -1,3 +1,4 @@
+import { coerceNumberProperty } from '@angular/cdk/coercion';
 import { Directive, HostBinding, Input } from '@angular/core';
 import { TreeGridRowDirective } from './tree-grid-row.directive';
 
@@ -7,13 +8,22 @@ import { TreeGridRowDirective } from './tree-grid-row.directive';
 export class TreeGridIndentDirective {
 
     /** The amount each level should be indented by */
-    @Input() uxTreeGridIndent: number;
+    @Input() 
+    set uxTreeGridIndent(value: number | string | undefined) {
+        this._indent = coerceNumberProperty(value, 25);
+    }
+
+    get uxTreeGridIndent(): number | string | undefined {
+        return this._indent;
+    }
 
     /** The padding value applied to each level */
     @HostBinding('style.padding-left.px')
     get indentation(): number {
-        return this._row && this._row.item ? 7 + (this._row.item.state.level * (this.uxTreeGridIndent || 25)) : 7;
+        return this._row && this._row.item ? 7 + (this._row.item.state.level * this._indent) : 7;
     }
+
+    private _indent: number;
 
     constructor(private _row: TreeGridRowDirective) {}
 }
