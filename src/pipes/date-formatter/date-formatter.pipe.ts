5d5652a2a78a7d058c3c5968ea642f554ccbdf0d
Jonathan Todd <jonathan.todd@microfocus.com>
2021-06-08 15:11:09 +0100

    EL-4241: Date Range Picker: "Missing locale data" error (#1545)
    * test
    
    * formatting code
    
    * underscoring private property

--- src/pipes/date-formatter/date-formatter.pipe.ts
+++ src/pipes/date-formatter/date-formatter.pipe.ts
@@ -1,11 +1,13 @@
-import { Pipe, PipeTransform } from '@angular/core';
 import { formatDate } from '@angular/common';
+import { Inject, LOCALE_ID, Pipe, PipeTransform } from '@angular/core';
 import { DateFormatter } from './date-formatter.type';
 
 @Pipe({
     name: 'formatDate'
 })
 export class DateFormatterPipe implements PipeTransform {
+
+    constructor(@Inject(LOCALE_ID) private _locale: string) { }
 
     transform(value: Date, formatter: string | DateFormatter): string {
 
@@ -14,7 +16,7 @@ export class DateFormatterPipe implements PipeTransform {
             return '';
         }
 
-        return typeof formatter === 'function' ? formatter(value) : formatDate(value, formatter, navigator.language);
+        return typeof formatter === 'function' ? formatter(value) : formatDate(value, formatter, this._locale);
     }
 
 }
