f5341f34138e49d17bd1126dccac199623a6eced
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2021-07-02 18:28:22 +0100

    updated test page layout

--- e2e/pages/app/dashboard/dynamic-layout/dynamic-layout.testpage.component.ts
+++ e2e/pages/app/dashboard/dynamic-layout/dynamic-layout.testpage.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { DashboardOptions } from '@ux-aspects/ux-aspects';
+import { DashboardLayoutData, DashboardOptions } from '@ux-aspects/ux-aspects';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
     selector: 'dashboard-layout-app',
@@ -22,6 +23,22 @@ export class DynamicLayoutTestPageComponent {
         minWidth: 90
     };
 
+    initialLayout: DashboardLayoutData[] = [
+        { id: 'widget-1', col: 0, row: 0, colSpan: 2, rowSpan: 1 },
+        { id: 'widget-2', col: 2, row: 0, colSpan: 1, rowSpan: 1 },
+        { id: 'widget-3', col: 3, row: 0, colSpan: 1, rowSpan: 1 },
+        { id: 'widget-4', col: 0, row: 1, colSpan: 4, rowSpan: 2, minColSpan: 4, minRowSpan: 2 },
+    ];
+
+    alternativeLayout: DashboardLayoutData[] = [
+        { id: 'widget-1', col: 0, row: 0, colSpan: 3, rowSpan: 2 },
+        { id: 'widget-2', col: 3, row: 0, colSpan: 1, rowSpan: 1 },
+        { id: 'widget-3', col: 3, row: 1, colSpan: 1, rowSpan: 1 },
+        { id: 'widget-4', col: 0, row: 2, colSpan: 4, rowSpan: 2 },
+    ];
+
+    layout$: BehaviorSubject<DashboardLayoutData[]> = new BehaviorSubject<DashboardLayoutData[]>(this.initialLayout);
+
     changeWidget1(row: number, column: number): void {
         this.widget1RowSpan += row;
         this.widget1ColSpan += column;
@@ -32,5 +49,12 @@ export class DynamicLayoutTestPageComponent {
         this.widget4ColSpan += column;
     }
 
+    changeLayout(): void {
+        this.widget1ColSpan = 3;
+        this.widget1RowSpan = 2;
+        this.widget4ColSpan = 4;
+        this.widget4RowSpan = 2;
 
+        this.layout$.next(this.alternativeLayout);
+    }
 }
