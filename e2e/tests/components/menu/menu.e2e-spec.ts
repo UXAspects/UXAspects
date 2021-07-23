97339166bab975f1add5ecde68f94220839fd4c5
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2021-07-23 13:57:08 +0100

    test coverage

--- e2e/tests/components/menu/menu.e2e-spec.ts
+++ e2e/tests/components/menu/menu.e2e-spec.ts
@@ -1,3 +1,4 @@
+import { browser } from 'protractor';
 import { imageCompare } from '../common/image-compare';
 import { MenuPage } from './menu.po.spec';
 
@@ -10,15 +11,15 @@ describe('Menu', () => {
         await page.getPage();
     });
 
-    it('should initialize with placement to the left', async () => {
-        await page.openMenuBtn.click();
-        expect(await imageCompare('menu-dynamic-placement-left')).toEqual(0);
-    });
-
-    it('should change menu placement to right after initialization', async () => {
-        await page.placementRightBtn.click();
+    it('should initialize with placement to the right', async () => {
         await page.openMenuBtn.click();
         expect(await imageCompare('menu-dynamic-placement-right')).toEqual(0);
+    });
+
+    it('should change menu placement to left after initialization', async () => {
+        await page.placementLeftBtn.click();
+        await page.openMenuBtn.click();
+        expect(await imageCompare('menu-dynamic-placement-left')).toEqual(0);
     });
 
     it('should change menu placement to bottom after initialization', async () => {
@@ -30,16 +31,48 @@ describe('Menu', () => {
     it('should change menu placement to top after initialization', async () => {
         await page.placementTopBtn.click();
         await page.openMenuBtn.click();
+
+        await browser.sleep(500);
         expect(await imageCompare('menu-dynamic-placement-top')).toEqual(0);
     });
 
-    it('should change menu placement to left after initialization', async () => {
+    it('should change menu placement to right after initialization', async () => {
         // first change placement to right
-        await page.placementRightBtn.click();
-
         await page.placementLeftBtn.click();
+
+        await page.placementRightBtn.click();
         await page.openMenuBtn.click();
-        expect(await imageCompare('menu-dynamic-placement-left')).toEqual(0);
+        expect(await imageCompare('menu-dynamic-placement-right')).toEqual(0);
+    });
+
+    describe('Menu - sub menu', () => {
+
+        it('should open the submenu when the menu is open', async () => {
+            await page.openMenuBtn.click();
+
+            await browser.actions().mouseMove(page.menuItem1).perform();
+            expect(await imageCompare('menu-sub-menu-item1-open')).toEqual(0);
+        });
+
+        it('should open the submenu 2 when the submenu 1 is open with the same uxMenuTriggerFor', async () => {
+            await page.openMenuBtn.click();
+
+            await browser.actions().mouseMove(page.menuItem1).perform();
+            await browser.actions().mouseMove(page.menuItem2).perform();
+
+            await browser.sleep(500);
+            expect(await imageCompare('menu-sub-menu-item2-open')).toEqual(0);
+        });
+
+        it('should open the submenu 3 when the submenu 2 is open with a different uxMenuTriggerFor', async () => {
+            await page.openMenuBtn.click();
+
+            await browser.actions().mouseMove(page.menuItem2).perform();
+            await browser.actions().mouseMove(page.menuItem3).perform();
+
+            await browser.sleep(500);
+            expect(await imageCompare('menu-sub-menu-item3-open')).toEqual(0);
+        });
     });
 
 });
--- e2e/tests/components/menu/menu.po.spec.ts
+++ e2e/tests/components/menu/menu.po.spec.ts
@@ -8,6 +8,11 @@ export class MenuPage {
     placementBottomBtn = $('#placement-bottom');
     openMenuBtn = $('#open-menu');
 
+    menuItem1 = $('#menu-item-1');
+    menuItem2 = $('#menu-item-2');
+    menuItem3 = $('#menu-item-3');
+    menuItem4 = $('#menu-item-4');
+
     async getPage(): Promise<void> {
         await browser.get('#/menu');
     }
