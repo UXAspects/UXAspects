c2f0131010c3ca53a732c57bc7c17c021934207b
Jonathan Todd <jonathan.todd@microfocus.com>
2021-10-22 17:23:31 +0100

    EL-4274 - Menu: alignment requires placement property (#1591)
    * fix
    
    * fix for issue
    
    * lint fix

--- src/components/menu/menu/menu.component.ts
+++ src/components/menu/menu/menu.component.ts
@@ -89,6 +89,7 @@ export class MenuComponent implements AfterContentInit, OnDestroy, OnChanges {
 
     /** Emit placement change */
     readonly _placement$ = new BehaviorSubject<AnchorPlacement>('bottom');
+    readonly _alignment$ = new BehaviorSubject<AnchorAlignment>('start');
 
     /** Access all child menu items for accessibility purposes */
     private readonly _items$ = new BehaviorSubject<(MenuItemComponent | MenuTabbableItemDirective)[]>([]);
@@ -150,6 +151,10 @@ export class MenuComponent implements AfterContentInit, OnDestroy, OnChanges {
 
         if (changes.placement && changes.placement.currentValue !== changes.placement.previousValue) {
             this._placement$.next(changes.placement.currentValue);
+        }
+
+        if (changes.alignment && changes.alignment.currentValue !== changes.alignment.previousValue) {
+            this._alignment$.next(changes.alignment.currentValue);
         }
     }
 
