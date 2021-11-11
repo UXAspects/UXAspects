396e4953fef3c0cec9d9abcb838476f59a2bff60
Donna Marijne <donna.worby@microfocus.com>
2021-11-11 12:07:19 +0000

    function binding

--- docs/app/pages/components/components-sections/dashboard/dashboard/snippets/aria.ts
+++ docs/app/pages/components/components-sections/dashboard/dashboard/snippets/aria.ts
@@ -1,10 +1,32 @@
-export class DashboardComponent {
+class Component {
+    getChangeSuccessAnnouncement(widget: DashboardWidgetComponent, differences: DashboardLayoutDiff[]): string {
+        // map the differences to strings
+        const differenceStrings = differences.map((diff) => {
+            const changes: string[] = [];
 
-  grabStartAnnouncement(widget): string {
-    return `${ widget.id } has been grabbed`;
-  }
+            // Handle movement strings
+            if (diff.isMovedHorizontally && diff.isMovedVertically) {
+                changes.push(`moved to row ${diff.currentRow}, column ${diff.currentColumn}`);
+            } else if (diff.isMovedDown) {
+                changes.push(`moved down to row ${diff.currentRow}, column ${diff.currentColumn}`);
+            } else if (diff.isMovedUp) {
+                changes.push(`moved up to row ${diff.currentRow}, column ${diff.currentColumn}`);
+            } else if (diff.isMovedLeft) {
+                changes.push(`moved left to row ${diff.currentRow}, column ${diff.currentColumn}`);
+            } else if (diff.isMovedRight) {
+                changes.push(`moved right to row ${diff.currentRow}, column ${diff.currentColumn}`);
+            }
 
-  grabConfirmAnnouncement(widget): string {
-    return `${ widget.id } has been resized to ${ widget.height }`;
-  }
+            // handle resize strings
+            if (diff.isResized) {
+                changes.push(`resized to ${diff.currentColumnSpan} columns wide and ${diff.currentRowSpan} rows high`);
+            }
+
+            return `${diff.widget.name} panel is ${changes.join(' and ')}.`;
+        });
+
+        return `${differenceStrings.join(
+            ' '
+        )} Use the cursor keys to continue moving and resizing, enter to commit, or escape to cancel.`;
+    }
 }
