7aaa72ac22546a0a056f439aeb34320a2d33e5f7
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2021-11-15 16:12:42 +0000

    removing downlevel angular packages

--- configs/webpack.docs.dev.config.js
+++ configs/webpack.docs.dev.config.js
@@ -126,33 +126,6 @@ module.exports = {
                 test: /[\/\\]@angular[\/\\].+\.js$/,
                 parser: { system: true },
             },
-
-            // Downlevel Angular Packages
-            {
-                test: /[\/\\]@angular[\/\\].+\.js$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [
-                            [
-                                '@babel/preset-env',
-                                {
-                                    targets: ['chrome 84', 'ie 11'],
-                                    modules: false,
-                                    exclude: ['transform-typeof-symbol'], // 'transform-typeof-symbol' generates slower code
-                                },
-                            ],
-                        ],
-                        plugins: [['@babel/plugin-transform-spread', { loose: true }]],
-                        inputSourceMap: false,
-                        babelrc: false,
-                        configFile: false,
-                        minified: false,
-                        compact: false,
-                        cacheDirectory: true,
-                    },
-                },
-            },
             {
                 test: /\.m?js/,
                 resolve: {
