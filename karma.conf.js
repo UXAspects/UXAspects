a2ff8b8d6242804205d90308612459101c47d3fa
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2021-11-18 16:19:43 +0000

    Updating karma

--- karma.conf.js
+++ karma.conf.js
@@ -1,22 +1,36 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-const { join } = require('path');
-const { cwd } = require('process');
-const { ContextReplacementPlugin } = require('webpack');
-
 module.exports = function (config) {
     config.set({
         basePath: '',
-        frameworks: ['jasmine'],
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
         plugins: [
             require('karma-jasmine'),
             require('karma-chrome-launcher'),
-            require('karma-webpack'),
             require('karma-jasmine-html-reporter'),
+            require('karma-coverage'),
+            require('@angular-devkit/build-angular/plugins/karma')
         ],
         client: {
+            jasmine: {
+                // you can add configuration options for Jasmine here
+                // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+                // for example, you can disable the random execution with `random: false`
+                // or set a specific seed with `seed: 4321`
+            },
             clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        jasmineHtmlReporter: {
+            suppressAll: true // removes the duplicated traces
+        },
+        coverageReporter: {
+            dir: require('path').join(__dirname, './coverage/sample'),
+            subdir: '.',
+            reporters: [
+                { type: 'html' },
+                { type: 'text-summary' }
+            ]
         },
         reporters: ['progress', 'kjhtml'],
         port: 9876,
@@ -31,66 +45,6 @@ module.exports = function (config) {
             }
         },
         singleRun: false,
-        restartOnFileChange: true,
-        files: [
-            'src/karma-test-shim.ts'
-        ],
-        preprocessors: {
-            'src/karma-test-shim.ts': ['webpack']
-        },
-        webpack: {
-            mode: 'development',
-            resolve: {
-                extensions: ['.ts', '.js']
-            },
-            module: {
-                rules: [
-                    {
-                        test: /\.html$/,
-                        use: 'raw-loader'
-                    },
-                    {
-                        test: /\.css$/,
-                        use: ['style-loader', 'css-loader']
-                    },
-                    {
-                        test: /\.less$/,
-                        include: /styles/,
-                        use: ['style-loader', 'css-loader', 'less-loader']
-                    },
-                    {
-                        test: /\.less$/,
-                        exclude: /styles/,
-                        use: ['raw-loader', 'less-loader']
-                    },
-                    {
-                        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico|otf|mp4|mp3)$/,
-                        use: 'file-loader?name=assets/[name].[ext]'
-                    },
-                    {
-                        test: /\.ts$/,
-                        use: [
-                            {
-                                loader: 'awesome-typescript-loader',
-                                options: {
-                                    configFileName: join(cwd(), 'src', 'tsconfig-build.json')
-                                },
-                            },
-                            {
-                                loader: 'angular2-template-loader'
-                            }
-                        ]
-                    },
-                    // Ignore warnings about System.import in Angular
-                    {
-                        test: /[\/\\]@angular[\/\\].+\.js$/,
-                        parser: { system: true }
-                    }
-                ]
-            },
-            plugins: [
-                new ContextReplacementPlugin(/(.+)?angular(\\|\/)core(.+)?/, join(cwd(), 'src')),
-            ]
-        },
+        restartOnFileChange: true
     });
 };
--- src/components/table/table-column-resize/resizable-table-column.component.spec.ts
+++ src/components/table/table-column-resize/resizable-table-column.component.spec.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { options } from 'less';
 import { CheckboxModule } from '../../checkbox/checkbox.module';
 import { BaseResizableTableService } from '../table-column-resize/resizable-table-base.service';
 import { RESIZABLE_TABLE_SERVICE_TOKEN } from '../table-column-resize/resizable-table-service.token';
 import { TableModule } from '../table.module';
+import { ResizableTableService } from './table-column-resize-standard/resizable-table.service';
 
 interface TableDocument {
     selected: boolean;
@@ -17,28 +17,28 @@ interface TableDocument {
     template: `
         <table id="standard-table" class="table" uxResizableTable>
             <thead>
-                <tr>
-                    <th uxResizableTableColumn [width]="50" [disabled]="true"></th>
-                    <th uxResizableTableColumn [(width)]="titleWidth">
-                        Title <small class="column-size-label">{{ titleWidth | number }}px</small>
-                    </th>
-                    <th uxResizableTableColumn [(width)]="authorWidth" handleVisible="false">
-                        Author <small class="column-size-label">{{ authorWidth | number }}px</small>
-                    </th>
-                    <th uxResizableTableColumn (widthChange)="dateWidth = $event">
-                        Date <small class="column-size-label">{{ dateWidth | number }}px</small>
-                    </th>
-                </tr>
+            <tr>
+                <th uxResizableTableColumn [width]="50" [disabled]="true"></th>
+                <th uxResizableTableColumn [(width)]="titleWidth">
+                    Title <small class="column-size-label">{{ titleWidth | number }}px</small>
+                </th>
+                <th uxResizableTableColumn [(width)]="authorWidth" handleVisible="false">
+                    Author <small class="column-size-label">{{ authorWidth | number }}px</small>
+                </th>
+                <th uxResizableTableColumn (widthChange)="dateWidth = $event">
+                    Date <small class="column-size-label">{{ dateWidth | number }}px</small>
+                </th>
+            </tr>
             </thead>
             <tbody>
-                <tr *ngFor="let document of documents | slice:0:5">
-                    <td class="checkbox-column">
-                        <ux-checkbox tabindex="-1"></ux-checkbox>
-                    </td>
-                    <td>{{ document.title }}</td>
-                    <td>{{ document.author }}</td>
-                    <td>{{ document.date | date }}</td>
-                </tr>
+            <tr *ngFor="let document of documents | slice:0:5">
+                <td class="checkbox-column">
+                    <ux-checkbox tabindex="-1"></ux-checkbox>
+                </td>
+                <td>{{ document.title }}</td>
+                <td>{{ document.author }}</td>
+                <td>{{ document.date | date }}</td>
+            </tr>
             </tbody>
         </table>
     `
@@ -64,8 +64,8 @@ export class ResizableTableColumnComponent {
         for (let idx = 0; idx < 5; idx++) {
             this.documents.push({
                 selected: false,
-                title: `Document ${idx + 1}`,
-                author: `Author ${idx + 1}`,
+                title: `Document ${ idx + 1 }`,
+                author: `Author ${ idx + 1 }`,
                 date: new Date(2019, 8, 12)
             });
         }
@@ -83,7 +83,7 @@ describe('Resizable table column Component', () => {
             declarations: [ResizableTableColumnComponent],
             providers: [
                 BaseResizableTableService,
-                { provide: RESIZABLE_TABLE_SERVICE_TOKEN, useValue: options },
+                { provide: RESIZABLE_TABLE_SERVICE_TOKEN, useValue: ResizableTableService },
 
             ],
         }).compileComponents();
