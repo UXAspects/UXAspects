79cf9af9cc416c82168e5f294d6f31cbd6b93daa
Tobias Schreiner <Tobias.Schreiner@microfocus.com>
2021-11-24 18:52:19 +0100

    Wait before checking image

--- e2e/tests/components/dashboard-widgets/dashboard-widgets.e2e-spec.ts
+++ e2e/tests/components/dashboard-widgets/dashboard-widgets.e2e-spec.ts
@@ -107,22 +107,22 @@ describe('Dashboard Widgets', () => {
         expect(await label.getText()).toEqual('One');
     });
 
-    // TODO TS: re-enable test
-    // it('should allow to edit text', async () => {
-    //     await page.clickDetailsButton(widgetText);
-    //     expect(await imageCompare('dashboard-widgets-edit-open')).toEqual(0);
-    //     await page.writeText('edited');
-    //     expect(await imageCompare('dashboard-widgets-edit-edited')).toEqual(0);
-    //
-    //     await page.clickSidePanelButton('cancel');
-    //     expect(await imageCompare('dashboard-widgets-initial')).toEqual(0);
-    //
-    //     await page.clickDetailsButton(widgetText);
-    //     await page.writeText('edited again');
-    //
-    //     await page.clickSidePanelButton('save');
-    //     expect(await imageCompare('dashboard-widgets-edited')).toEqual(0);
-    // });
+    it('should allow to edit text', async () => {
+        await page.clickDetailsButton(widgetText);
+        await new Promise(resolve => setTimeout(resolve, 2000));
+        expect(await imageCompare('dashboard-widgets-edit-open')).toEqual(0);
+        await page.writeText('edited');
+        expect(await imageCompare('dashboard-widgets-edit-edited')).toEqual(0);
+
+        await page.clickSidePanelButton('cancel');
+        expect(await imageCompare('dashboard-widgets-initial')).toEqual(0);
+
+        await page.clickDetailsButton(widgetText);
+        await page.writeText('edited again');
+
+        await page.clickSidePanelButton('save');
+        expect(await imageCompare('dashboard-widgets-edited')).toEqual(0);
+    });
 
     it('should display read-only text', async () => {
         await page.clickDetailsButton(widgetTextReadOnly);
