3eeec30cece36f5a40d9e460ce37b2d676b96b8b
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2021-12-03 17:53:32 +0000

    Updating urls

--- docs/app/services/playground/strategies/themes/microfocus-strategy.ts
+++ docs/app/services/playground/strategies/themes/microfocus-strategy.ts
@@ -5,8 +5,8 @@ export class MicroFocusThemeStrategy implements ThemeStrategy {
     getStylesheets(assetsUrl: string): string[] {
         return [
             SystemJSHelper.getPackageUrl({ name: 'bootstrap', path: 'bootstrap@3.3.7/dist/css/bootstrap.min.css' }),
-            `${assetsUrl}/styles/ux-aspects.css`,
-            `${assetsUrl}/styles/quantum-ux-aspects.css`
+            `${ assetsUrl }/ux-aspects/styles/ux-aspects.css`,
+            `${ assetsUrl }/themes/ux-aspects/css/main.css`
         ];
     }
 }
