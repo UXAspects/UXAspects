7b5980fe21807404fd38a498a46296546aa0e8ed
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2021-12-03 11:21:12 +0000

    Updated docs

---
+++ docs/app/pages/components/components-sections/select/select/snippets/validation.ts
@@ -0,0 +1,38 @@
+import {
+    AbstractControl, FormControl,
+    FormGroup,
+    Validators
+} from '@angular/forms';
+
+export class AppComponent {
+
+    public formGroup: FormGroup;
+    public options: string[] | Function;
+
+    get select(): AbstractControl {
+        return this.formGroup.get('select');
+    }
+
+    get checkbox(): AbstractControl {
+        return this.formGroup.get('checkbox');
+    }
+
+    constructor() {
+
+        this.options = ['Option1', 'Option2'];
+
+        this.formGroup = new FormGroup({
+            checkbox: new FormControl(false, [Validators.required]),
+            select: new FormControl({ value: undefined, disabled: true }, [Validators.required]),
+        });
+
+        this.formGroup.get('checkbox').valueChanges.subscribe((value: boolean) => {
+            if (value) {
+                this.select.enable();
+            } else {
+                this.select.disable();
+            }
+            this.checkbox.updateValueAndValidity();
+        });
+    }
+}
