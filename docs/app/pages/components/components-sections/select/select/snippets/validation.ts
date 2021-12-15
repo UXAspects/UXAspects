d132ab170c50b156a29113091b9a2802892ed109
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2021-12-15 13:28:13 +0000

    updates to aspects

--- docs/app/pages/components/components-sections/select/select/snippets/validation.ts
+++ docs/app/pages/components/components-sections/select/select/snippets/validation.ts
@@ -1,13 +1,22 @@
+import { OnDestroy, OnInit } from '@angular/core';
 import {
 	AbstractControl, FormControl,
 	FormGroup,
 	Validators
 } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
 
-	public formGroup: FormGroup;
-	public options: string[] | Function;
+	formGroup: FormGroup = new FormGroup({
+		checkbox: new FormControl(false, [Validators.required]),
+		select: new FormControl({ value: undefined, disabled: true }, [Validators.required]),
+	});
+
+	options: string[] = ['Option1', 'Option2'];
+
+	private _onDestroy = new Subject<void>();
 
 	get select(): AbstractControl {
 		return this.formGroup.get('select');
@@ -17,21 +26,17 @@ export class AppComponent {
 		return this.formGroup.get('checkbox');
 	}
 
-	constructor() {
+	ngOnInit() {
 
-		this.options = ['Option1', 'Option2'];
-
-		this.formGroup = new FormGroup({
-			checkbox: new FormControl(false, [Validators.required]),
-			select: new FormControl({ value: undefined, disabled: true }, [Validators.required]),
-		});
-
-		this.formGroup.get('checkbox').valueChanges.subscribe((value: boolean) => {
-			if (value) {
-				this.select.enable();
-			} else {
-				this.select.disable();
-			}
-		});
+		this.checkbox.valueChanges
+			.pipe(takeUntil(this._onDestroy))
+			.subscribe((value: boolean) => {
+				value ? this.select.enable() : this.select.disable();
+			});
 	}
+
+	ngOnDestroy(): void {
+        this._onDestroy.next();
+        this._onDestroy.complete();
+    }
 }
