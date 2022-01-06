e0d1a560fa660ad4cd2f076cb7c42de7bdd4f71c
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2022-01-06 12:00:20 +0000

    documentation updates Changed exmaple to remove checkbox

--- docs/app/pages/components/components-sections/select/select/snippets/validation.ts
+++ docs/app/pages/components/components-sections/select/select/snippets/validation.ts
@@ -1,46 +1,23 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import {
 	AbstractControl,
 	FormControl,
 	FormGroup,
 	Validators
 } from '@angular/forms';
-import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 @Component({
     selector: 'app',
     templateUrl: './app.component.html'
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnDestroy {
 
 	formGroup: FormGroup = new FormGroup({
-		checkbox: new FormControl(false, [Validators.required]),
-		select: new FormControl({ value: undefined, disabled: true }, [Validators.required]),
+		select: new FormControl(undefined, [Validators.required]),
 	});
 
 	options: string[] = ['Option1', 'Option2'];
 
-	private _onDestroy$ = new Subject<void>();
-
 	get select(): AbstractControl {
 		return this.formGroup.get('select');
 	}
-
-	get checkbox(): AbstractControl {
-		return this.formGroup.get('checkbox');
-	}
-
-	ngOnInit(): void {
-
-		this.checkbox.valueChanges
-			.pipe(takeUntil(this._onDestroy$))
-			.subscribe((value: boolean) => {
-				value ? this.select.enable() : this.select.disable();
-			});
-	}
-
-	ngOnDestroy(): void {
-        this._onDestroy$.next();
-        this._onDestroy$.complete();
-    }
 }
