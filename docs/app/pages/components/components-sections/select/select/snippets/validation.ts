7c9d14542546ec2c312b13bcb1f389dd23a4dba3
Jonathan Todd <jonathan.todd@microfocus.com>
2022-01-10 09:24:25 +0000

    EL-4277 - Combobox: Issue with combox controls and custom validators (#1601)
    * fixing pr
    
    * updates to aspects
    
    * fix
    
    * adding decoratior
    
    * documentation updates
    Changed exmaple to remove checkbox
    
    * adding hyperlink styling

---
+++ docs/app/pages/components/components-sections/select/select/snippets/validation.ts
@@ -0,0 +1,23 @@
+import { Component, OnDestroy } from '@angular/core';
+import {
+	AbstractControl,
+	FormControl,
+	FormGroup,
+	Validators
+} from '@angular/forms';
+@Component({
+    selector: 'app',
+    templateUrl: './app.component.html'
+})
+export class AppComponent implements OnDestroy {
+
+	formGroup: FormGroup = new FormGroup({
+		select: new FormControl(undefined, [Validators.required]),
+	});
+
+	options: string[] = ['Option1', 'Option2'];
+
+	get select(): AbstractControl {
+		return this.formGroup.get('select');
+	}
+}
