f95f9dc4ad3bfdbf927b6846c872b832e8cacdcd
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2022-03-03 10:27:02 +0000

    Release 5.4.0

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -18,6 +18,11 @@ export class ChangeLogPageComponent implements OnInit {
     async ngOnInit(): Promise<void> {
         this.logs = [
             {
+                version: '5.4.0',
+                date: 'February 3rd 2022',
+                content: require('./logs/release-v5.4.0.md')
+            },
+            {
                 version: '5.3.0',
                 date: 'February 17th 2022',
                 content: require('./logs/release-v5.3.0.md')
