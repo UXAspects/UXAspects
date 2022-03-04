6a906be4576f7123f2e6ec40ce71df3ae6af94c8
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2022-03-04 16:57:38 +0000

    additional fix

--- src/plugins/chartjs/timeline/timeline-chart.module.ts
+++ src/plugins/chartjs/timeline/timeline-chart.module.ts
@@ -64,7 +64,7 @@ export class TimelineChartPlugin {
     }
 
     version3Check(): boolean {
-        return (window as any).Chart? false : true;
+        return (window as any).Chart?.pluginService ? false : true;
     }
 
     /**
