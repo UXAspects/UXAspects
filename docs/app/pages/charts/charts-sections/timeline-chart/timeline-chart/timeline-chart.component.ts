4cb57c7238ba069c4ed26d14f3720c26b056a7a7
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2022-03-08 17:55:02 +0000

    adding type fix

--- docs/app/pages/charts/charts-sections/timeline-chart/timeline-chart/timeline-chart.component.ts
+++ docs/app/pages/charts/charts-sections/timeline-chart/timeline-chart/timeline-chart.component.ts
@@ -148,7 +148,7 @@ export class ChartsTimelineChartComponent extends BaseDocumentationSection imple
                 },
                 selectionColor: this._colorService.getColor('alternate3').setAlpha(0.15).toRgba(),
                 onChange: (min: Date, max: Date) => {
-                    this.lineChartData[0].data = this._dataService.getDataset().filter((point: { x: Date}) => {
+                    this.lineChartData[0].data = this._dataService.getDataset().filter((point: any) => {
                         return point.x.getTime() >= min.getTime() && point.x.getTime() <= max.getTime();
                     });
                     this.baseChart.update();
