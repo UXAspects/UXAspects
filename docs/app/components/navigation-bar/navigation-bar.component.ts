1b04dc2004c5ae4429de472a2268279e8002bea1
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2022-03-14 17:30:43 +0000

    Adding get help link

--- docs/app/components/navigation-bar/navigation-bar.component.ts
+++ docs/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ILink } from '../../interfaces/ILink';
-import { AppConfiguration } from '../../services/app-configuration/app-configuration.service';
 import { SiteTheme } from '../../interfaces/SiteTheme';
+import { AppConfiguration } from '../../services/app-configuration/app-configuration.service';
 
 @Component({
     selector: 'uxd-navigation-bar',
@@ -13,9 +13,12 @@ export class NavigationBarComponent {
     navigation = this._appConfig.getConfigurationData('top-navigation');
     brand: ILink = this.navigation.brand;
     links: ILink[] = this.navigation.links;
+    help: ILink[] = this.navigation.help;
     social: ILink[] = this.navigation.social;
     themes: SiteTheme[] = this.navigation.themes;
     expanded: boolean = false;
 
-    constructor(private _appConfig: AppConfiguration) { }
+    constructor(private _appConfig: AppConfiguration) { 
+        console.log('module', this._appConfig.getConfigurationData('top-navigation').help);
+     }
 }
