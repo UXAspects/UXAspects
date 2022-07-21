f9d1977c541e4ee4ff747dfc43b2c966092c0ebe
Jonathan Todd <jonathan.todd@microfocus.com>
2022-07-21 11:18:17 +0100

    Release 6.1.0 (#1669)
    * release notes 6.1.0
    
    * fixing link in previous release notes

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -18,6 +18,11 @@ export class ChangeLogPageComponent implements OnInit {
     async ngOnInit(): Promise<void> {
         this.logs = [
             {
+                version: '6.1.0',
+                date: 'July 21st 2022',
+                content: require('./logs/release-v6.0.0.md')
+            },
+            {
                 version: '6.0.0',
                 date: 'July 7th 2022',
                 content: require('./logs/release-v6.0.0.md')
