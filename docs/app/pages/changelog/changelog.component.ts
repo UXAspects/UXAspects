b7124f9e106dcd9029b5de08b8c8017d59e197a3
Jonathan-Todd-MF <jonathan.todd@microfocus.com>
2023-03-30 09:59:46 +0100

    Release 7.3.0

--- docs/app/pages/changelog/changelog.component.ts
+++ docs/app/pages/changelog/changelog.component.ts
@@ -18,6 +18,11 @@ export class ChangeLogPageComponent implements OnInit {
     async ngOnInit(): Promise<void> {
         this.logs = [
             {
+                version: '7.3.0',
+                date: 'February 30th 2023',
+                content: require('./logs/release-v7.3.0.md')
+            },
+            {
                 version: '7.1.0',
                 date: 'February 2nd 2023',
                 content: require('./logs/release-v7.1.0.md')
