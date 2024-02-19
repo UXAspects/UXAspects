bd88ec98357e0c483f77b901a34d3908c5f15feb
Andrew Cochrane <andrew.cochrane24@gmail.com>
2024-02-19 13:00:13 +0000

    Initial WIP for fixing facet reordering

--- src/components/facets/facet-container.component.ts
+++ src/components/facets/facet-container.component.ts
@@ -1,5 +1,5 @@
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { Component, ContentChild, EventEmitter, inject, Input, OnDestroy, Output, TemplateRef } from '@angular/core';
+import { AfterViewInit, Component, ContentChild, EventEmitter, inject, Input, OnDestroy, Output, TemplateRef, ViewChild } from '@angular/core';
 import { Subject } from 'rxjs';
 import { filter } from 'rxjs/operators';
 import { ReorderEvent } from '../../directives/reorderable/index';
@@ -7,6 +7,7 @@ import { FacetClearButtonDirective } from './facet-clear-button/facet-clear-butt
 import { FacetDeselect, FacetEvent } from './facet-events';
 import { FacetService } from './facet.service';
 import { Facet } from './models/facet';
+import { CdkDragEnter, CdkDropList, DragRef, moveItemInArray } from '@angular/cdk/drag-drop';
 
 @Component({
     selector: 'ux-facet-container',
@@ -14,7 +15,7 @@ import { Facet } from './models/facet';
     providers: [FacetService],
     preserveWhitespaces: false
 })
-export class FacetContainerComponent implements OnDestroy {
+export class FacetContainerComponent implements OnDestroy, AfterViewInit {
     readonly facetService = inject(FacetService);
     private readonly _announcer = inject(LiveAnnouncer);
 
@@ -58,6 +59,14 @@ export class FacetContainerComponent implements OnDestroy {
     /** Allow a custom clear button */
     @ContentChild(FacetClearButtonDirective, { read: TemplateRef, static: false }) clearButton: TemplateRef<FacetClearButtonDirective>;
 
+    @ViewChild(CdkDropList) placeholder: CdkDropList;
+
+    private target: CdkDropList = null;
+    private targetIndex: number;
+    private source: CdkDropList = null;
+    private sourceIndex: number;
+    private dragRef: DragRef = null;
+
     private readonly _onDestroy = new Subject<void>();
 
     constructor() {
@@ -69,9 +78,83 @@ export class FacetContainerComponent implements OnDestroy {
             .subscribe(event => this._announcer.announce(`Option ${event.facet.title} deselected.`, 'assertive'));
     }
 
+    ngAfterViewInit() {
+        const placeholderElement = this.placeholder.element.nativeElement;
+        placeholderElement.style.display = 'none';
+        placeholderElement.parentNode.removeChild(placeholderElement);
+    }
+
     ngOnDestroy(): void {
         this._onDestroy.next();
         this._onDestroy.complete();
+    }
+
+    onDropListDropped() {
+        if (!this.target) {
+            return;
+        }
+        const placeholderElement: HTMLElement = this.placeholder.element.nativeElement;
+        const placeholderParentElement: HTMLElement = placeholderElement.parentElement;
+        placeholderElement.style.display = 'none';
+
+        placeholderParentElement.removeChild(placeholderElement);
+        placeholderParentElement.appendChild(placeholderElement);
+        placeholderParentElement.insertBefore(
+            this.source.element.nativeElement,
+            placeholderParentElement.children[this.sourceIndex]
+        );
+
+        if (this.placeholder._dropListRef.isDragging()) {
+            this.placeholder._dropListRef.exit(this.dragRef);
+        }
+
+        this.target = null;
+        this.source = null;
+        this.dragRef = null;
+
+        if (this.sourceIndex !== this.targetIndex) {
+            moveItemInArray(this.facets, this.sourceIndex, this.targetIndex);
+        }
+    }
+
+    onDropListEntered({ item, container }: CdkDragEnter) {
+        if (container == this.placeholder) {
+            return;
+        }
+        const placeholderElement: HTMLElement = this.placeholder.element.nativeElement;
+        const sourceElement: HTMLElement = item.dropContainer.element.nativeElement;
+        const dropElement: HTMLElement = container.element.nativeElement;
+        
+        const dragIndex: number = Array.prototype.indexOf.call(
+            dropElement.parentElement.children,
+            this.source ? placeholderElement : sourceElement
+        );
+        const dropIndex: number = Array.prototype.indexOf.call(
+            dropElement.parentElement.children,
+            dropElement
+        );
+
+        if (!this.source) {
+            this.sourceIndex = dragIndex;
+            this.source = item.dropContainer;
+            sourceElement.parentElement.removeChild(sourceElement);
+        }
+
+        this.targetIndex = dropIndex;
+        this.target = container;
+        this.dragRef = item._dragRef;
+        placeholderElement.style.display = '';
+
+        dropElement.parentElement.insertBefore(
+            placeholderElement,
+            dropIndex > dragIndex ? dropElement.nextSibling : dropElement
+        );
+
+        this.placeholder._dropListRef.enter(
+            item._dragRef,
+            item.element.nativeElement.offsetLeft,
+            item.element.nativeElement.offsetTop,
+        );
     }
 
     selectFacet(facet: Facet): void {
