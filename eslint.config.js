f7acc76038ccdf47723548057cc4897a78f6b287
Ashley Hunter <ahunter2@opentext.com>
2024-11-18 11:17:21 +0000

    update eslint

---
+++ eslint.config.js
@@ -0,0 +1,93 @@
+const nx = require('@nx/eslint-plugin');
+
+module.exports = [
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  ...nx.configs['flat/angular'],
+  ...nx.configs['flat/angular-template'],
+  {
+    files: ['**/*.ts'],
+    rules: {
+      '@angular-eslint/component-selector': 'off',
+      '@angular-eslint/directive-selector': 'off',
+      'eol-last': 'off',
+      'max-len': 'off',
+      radix: 'off',
+      '@typescript-eslint/member-delimiter-style': 'off',
+      'no-underscore-dangle': 'off',
+      '@typescript-eslint/member-ordering': 'off',
+      '@typescript-eslint/no-inferrable-types': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      'jsdoc/newline-after-description': 'off',
+      'guard-for-in': 'off',
+      'quote-props': 'off',
+      'prefer-arrow/prefer-arrow-functions': 'off',
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@angular-eslint/no-host-metadata-property': 'off',
+      '@angular-eslint/component-class-suffix': 'off',
+      '@angular-eslint/no-input-rename': 'off',
+      '@angular-eslint/no-output-rename': 'off',
+      '@angular-eslint/no-output-on-prefix': 'off',
+      'arrow-body-style': 'off',
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/prefer-for-of': 'off',
+      '@angular-eslint/template/no-negated-async': 'off',
+      '@angular-eslint/directive-class-suffix': 'off',
+      '@typescript-eslint/adjacent-overload-signatures': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'brace-style': 'off',
+      'one-var': 'off',
+      '@typescript-eslint/require-await': 'error',
+      '@typescript-eslint/semi': 'error',
+      'no-redeclare': 'error',
+      'no-trailing-spaces': 'off',
+      '@typescript-eslint/prefer-readonly': 'error',
+    },
+    languageOptions: {
+      parserOptions: {
+        project: ['tsconfig.json', 'e2e/tsconfig.json'],
+        createDefaultProgram: true,
+      },
+    },
+  },
+  {
+    files: ['**/*.html'],
+    rules: {
+      'no-mixed-spaces-and-tabs': 'off',
+      '@typescript-eslint/adjacent-overload-signatures': 'off',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+      },
+    },
+  },
+  {
+    files: ['**/*.json'],
+    // Override or add rules here
+    rules: {},
+    languageOptions: {
+      parser: require('jsonc-eslint-parser'),
+    },
+  },
+  {
+    files: ['**/*.spec.ts', '**/*.e2e-spec.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/adjacent-overload-signatures': 'off',
+    },
+  },
+  {
+    files: ['**/snippets/*.ts'],
+    rules: {
+      '@typescript-eslint/no-empty-function': 'off',
+    },
+  },
+];
---
+++ tools/workspace-plugin/eslint.config.js
@@ -0,0 +1,36 @@
+const { FlatCompat } = require('@eslint/eslintrc');
+const js = require('@eslint/js');
+const baseConfig = require('../../eslint.base.config.js');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+module.exports = [
+  ...baseConfig,
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    rules: {},
+  },
+  {
+    files: ['./package.json', './generators.json'],
+    rules: {
+      '@nx/nx-plugin-checks': 'error',
+    },
+    languageOptions: {
+      parser: require('jsonc-eslint-parser'),
+    },
+  },
+];
