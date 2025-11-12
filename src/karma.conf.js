9b33ff98565047798c0cfc110691758fd0fbcc4f
Ashley Hunter <ashley.hunter@hotmail.co.uk>
2025-11-12 12:35:22 +0000

    fixing build

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,6 +1,9 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
+const puppeteer = require('puppeteer');
+process.env.CHROME_BIN = puppeteer.executablePath();
+
 module.exports = function (config) {
   config.set({
     basePath: '',
