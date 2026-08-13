548c69f8e7cd7780bea4f5c3ca39865648927f2a
Andy Reid <andrew.reid@microfocus.com>
2026-08-13 15:06:36 +0100

    chore: update copyright year to 2026

--- src/components/table/column-picker/interfaces/column-picker-tree-node.interface.ts
+++ src/components/table/column-picker/interfaces/column-picker-tree-node.interface.ts
@@ -1,3 +1,19 @@
+///
+/// Copyright 2015-2026 Micro Focus or one of its affiliates.
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///      http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+
 import { ColumnPickerGroupItem } from '../interfaces/column-picker-group-item.interface';
 
 /** Represents a tree node item. Normalises data for both groups and columns into one format */
